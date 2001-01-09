bae6f9ac4c9e3c94e94e744e0a4a4d01ac0552d4
Stefan Hoops <shoops@vbi.vt.edu>
2001-01-09 20:52:55 +0000

    Included documentation.

--- include/CReadConfig.h
+++ include/CReadConfig.h
@@ -1,5 +1,7 @@
-// CReadConfig
-// 
+/**
+ *  CReadConfig class. A more elaborate class description.
+ */
+
 // New Class based on pmutils read functionality
 // (C) Stefan Hoops 2001
 
@@ -10,19 +12,73 @@
 class CReadConfig
 {
 public:
+    /**
+     *  Default consructor. 
+     *  This opens the configuration file using a the default filename 
+     *  "test.txt".
+     */
     CReadConfig();
+    /**
+     *  Specified consructor. 
+     *  This opens the configuration file using the filename specified 
+     *  as the argument.
+     *  @param name name of the confguration file. 
+     */
     CReadConfig(string name);
+    /**
+     *  Destructor. 
+     *  The destructor calls the method Free().
+     */
     ~CReadConfig();
+    /**
+     *  Frees all allocated memory. 
+     *  This method is called by the destructor.
+     */
     Free();
+    /**
+     *  Returns the failure status.
+     *  @return mFail
+     *  @see mFail  
+     */
     Fail();
+    /**
+     *  Retrieves the next variable from the input file.
+     *  @param name name of the variable to be retrieved.
+     *  @param type type of the variable to be retrieved.
+     *  @param *pout pointer to the location where the retrieved variable 
+     *               is stored.
+     *  @return mFail
+     *  @see mFail  
+     */
     GetVariable(string name, string type, void * pout);
 
 private:
+    /**
+     *  Initializes the input buffer for reading.
+     */
     InitInputBuffer();
+    /**
+     *  Name of the configuration file.
+     */
     string mFilename;               // Config File Name
+    /**
+     *  Begin of the input buffer
+     */
     char*  mpBufferBegin;           // Input Buffer
+    /**
+     *  Current position in the input buffer
+     */
     char*  mpBufferPos;             // Current Position
+    /**
+     *  Current line number in the configuration file
+     */
     long   mLineNumber;             // Current Line Number 
+    /**
+     *  Failure status:
+     *  0 = no error
+     *  !0 = error
+     */
     int    mFail;                   // Failure State
 };
 #endif // COPASI_CReadConfig
+
