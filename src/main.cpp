3beb2b30a85fa8f7dcfdfdbc60747e17045fa716
Stefan Hoops <shoops@vbi.vt.edu>
2001-01-16 15:33:54 +0000

    Implemented Flush method to commit changes to the output stream. Use references for Load() and Save() methods.

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(void)
     // TestWriteConfig();
     // TestException();
     // TestReadConfig();
-    // TestCompartment();
+    TestCompartment();
     // TestDatum();
 
     cout << "Leaving main program." << endl;
@@ -52,7 +52,9 @@ int  TestException()
         cout << Exception.Message.GetText() << endl;
         cout << "Leaving fatal error handling." << endl;
     }
-    
+
+    // newline
+
     catch (char *str)
     {
         cout << "Caugth other exception" <<endl;
@@ -65,7 +67,7 @@ int  TestException()
 int  TestReadConfig(void)
 {
     CReadConfig Default;
-    CReadConfig Specific((string) "test.txt");
+    CReadConfig Specific((string) "TestWriteConfig.txt");
     string outstring = "";
     Specific.GetVariable((string) "Compartment", 
                          (string) "string", 
@@ -117,24 +119,26 @@ int TestCompartment(void)
     cout << "creating a CCompartment object..." << endl;
     CCompartment c;
     cout << "Opening an output stream" << endl;
-    CWriteConfig of("test.txt");
-    c.Save(&of);
-	    
+    CWriteConfig of("TestCompartment.txt");
+    c.Save(of);
+    of.Flush();
+
     CCompartment *d = NULL;
     d = new CCompartment[1];
 
     CCompartment g((string) "test2", 1.1e-2);
     d[0] = g;
-    d[0].Save(&of);
-
+    d[0].Save(of);
+    of.Flush();
+    
     c=d[0];
 
     delete [] d;
 
-    CReadConfig Specific((string) "test.txt");
+    CReadConfig Specific((string) "TestCompartment.txt");
     CCompartment e,f;
-    e.Load(&Specific);
-    f.Load(&Specific);
+    e.Load(Specific);
+    f.Load(Specific);
     Specific.Free();
 
     return 0;
@@ -145,20 +149,24 @@ int TestDatum(void)
 {
     double doublevariable;
     cout << "creating a CDatum object..." << endl;
-    CDatum d((string)"[medicarpin]t", D_TCONC, (string)"medicarpin", (string)"", &doublevariable);
+    CDatum d((string)"[medicarpin]t", 
+             D_TCONC, 
+             (string)"medicarpin", 
+             (string)"", &doublevariable);
     cout << "Opening an output stream" << endl;
     CWriteConfig of("test2.txt");
-    d.Save(&of);
-
+    d.Save(of);
+    of.Flush();
+    
     CReadConfig Specific((string) "test2.txt");
     CDatum* e;
     e = new CDatum[2];
-    e[0].Load(&Specific);
+    e[0].Load(Specific);
     Specific.Free();
     e[1] = e[0];
     cout << "Opening another output stream" << endl;
     CWriteConfig of2("test3.txt");
-    e[1].Save(&of2);
+    e[1].Save(of2);
 
     delete [] e;
 
