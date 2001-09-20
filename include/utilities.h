9eaa2f078aa474a38fd51e32acfad21b59ad132b
Stefan Hoops <shoops@vbi.vt.edu>
2001-09-20 13:13:55 +0000

    *** empty log message ***

--- include/utilities.h
+++ include/utilities.h
@@ -15,38 +15,38 @@
 string StringPrint( char * format, ...);
 
 
-    void dgefa(C_FLOAT64 ** a, 
-                     C_INT32 n, 
-                     C_INT32 * ipvt, 
-                     C_INT32 * info);
+void dgefa(C_FLOAT64 ** a, 
+           C_INT32 n, 
+           C_INT32 * ipvt, 
+           C_INT32 * info);
 
-    C_INT32 idamax(C_INT32 n,
-                         C_FLOAT64 * dx,
-                         C_INT32 incx);
-			 
-    void dgesl(C_FLOAT64 ** a,
-                     C_INT32 n,
-                     C_INT32 * ipvt, 
-                     C_FLOAT64 * b, 
-                     C_INT32 job);
+C_INT32 idamax(C_INT32 n,
+               C_FLOAT64 * dx,
+               C_INT32 incx);
+                         
+void dgesl(C_FLOAT64 ** a,
+           C_INT32 n,
+           C_INT32 * ipvt, 
+           C_FLOAT64 * b, 
+           C_INT32 job);
 
-    void daxpy(C_INT32 n,
-                     C_FLOAT64 da, 
-                     C_FLOAT64 * dx,
-                     C_INT32 incx, 
-                     C_FLOAT64 * dy,
-                     C_INT32 incy);
+void daxpy(C_INT32 n,
+           C_FLOAT64 da, 
+           C_FLOAT64 * dx,
+           C_INT32 incx, 
+           C_FLOAT64 * dy,
+           C_INT32 incy);
 
-    void dscal(C_INT32 n,
-                     C_FLOAT64 da,
-                     C_FLOAT64 * dx,
-                     C_INT32 incx);
+void lsoda_dscal(C_INT32 n,
+                 C_FLOAT64 da,
+                 C_FLOAT64 * dx,
+                 C_INT32 incx);
 
-    C_FLOAT64 ddot(C_INT32 n, 
-                         C_FLOAT64 * dx,
-                         C_INT32 incx, 
-                         C_FLOAT64 * dy,
-                         C_INT32 incy);
+C_FLOAT64 ddot(C_INT32 n, 
+               C_FLOAT64 * dx,
+               C_INT32 incx, 
+               C_FLOAT64 * dy,
+               C_INT32 incy);
 
 
 #endif COPASI_utilities
