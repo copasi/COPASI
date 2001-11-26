a1707e6ff76f05b851349705cc08d8f1ab55dd07
Yongqun He <yohe@vt.edu>
2001-11-26 22:24:29 +0000

    add more #define

--- copasi/copasi.h
+++ copasi/copasi.h
@@ -33,7 +33,21 @@ using namespace std ;
 #define C_FLOAT64 double
 #define C_FLOAT32 float
 
+//YH: new defined parameters used by more than one classes
+#define SS_FOUND 1             //steady state found
+#define SS_NOT_FOUND 0         //steady state not found
+#define SS_SINGULAR_JACOBIAN 2
+#define SS_DAMPING_LIMIT 3
+#define SS_ITERATION_LIMIT 4
+#define MCA_OK 0
+#define MCA_SINGULAR 1
+
+
+
+
+
 // protected free
 #define pfree(p) {if (p) {free(p); p = NULL;}}
     
 #endif // COPASI_copasi
+
