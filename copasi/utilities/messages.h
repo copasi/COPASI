ec45c9b7b6512ad840b4349af0fd97cdcd5a77fb
Carel van Gend <carel.vangend@eml.villa-bosch.de>
2002-02-26 15:50:37 +0000

    Added some braces to get rid compiler warnings.

--- copasi/utilities/messages.h
+++ copasi/utilities/messages.h
@@ -1,23 +1,23 @@
 MESSAGES Messages[] =
   {
   // CReadConfig
-  MCReadConfig + 1, "CReadConfig (1): Variable '%s' not found in '%s(%d)'.",
-  MCReadConfig + 2, "CReadConfig (2): Cannot open file '%s'.",
-  MCReadConfig + 3, "CReadConfig (3): Cannot read file '%s'.",
-  MCReadConfig + 4, "CReadConfig (4): Cannot close file '%s'.",
-  MCReadConfig + 5, "CReadConfig (5): Invalid type '%s' for  Variable '%s'.",
+      {MCReadConfig + 1, "CReadConfig (1): Variable '%s' not found in '%s(%d)'.",},
+      {MCReadConfig + 2, "CReadConfig (2): Cannot open file '%s'.",},
+      {MCReadConfig + 3, "CReadConfig (3): Cannot read file '%s'.",},
+      {MCReadConfig + 4, "CReadConfig (4): Cannot close file '%s'.",},
+      {MCReadConfig + 5, "CReadConfig (5): Invalid type '%s' for  Variable '%s'.",},
 
   // CKinFunction
-  MCKinFunction + 1, "CKinFunction (1): Cannot find identifier '%s'"
-  " in function '%s'.",
-  MCKinFunction + 2, "CKinFunction (2): Missing operant in function '%s'.",
+      {MCKinFunction + 1, "CKinFunction (1): Cannot find identifier '%s'"
+       " in function '%s'.",},
+      {MCKinFunction + 2, "CKinFunction (2): Missing operant in function '%s'.",},
     
   // CRange
-  MCRange + 1, "CRange (1): Invalid range ('%d', '%d').",
+      {MCRange + 1, "CRange (1): Invalid range ('%d', '%d').",},
 
   // CCopasiVector
-  MCCopasiVector + 1, "CCopasiVector (1): Object '%s' not found.",
+      {MCCopasiVector + 1, "CCopasiVector (1): Object '%s' not found.",},
 
   // This must be the last element of the message list! Do not delete!
-  0, NULL
+  {0, NULL}
   };
