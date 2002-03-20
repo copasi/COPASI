51731c42edb8a3f6cb9e4e5209b7a6aa057124eb
Wei Sun <wsun@vt.edu>
2002-03-20 19:49:31 +0000

    Update for using new function class

--- copasi/output/CNodeO.h
+++ copasi/output/CNodeO.h
@@ -23,12 +23,41 @@ class CNodeO: public CNodeK
    *  A datum needed to specify the identifiers
    */
   CDatum mDatum;
+  /**
+   *  The left branch of the tree originating from tyhis node
+   */
+  CNodeO * mLeft;
+
+  /**
+   *  The right branch of the tree originating from tyhis node
+   */
+  CNodeO * mRight;
 
  public:
   /**
    * Default constructor
    */
   CNodeO();
+
+  /**
+   * Constructor for operator
+   * @param "const char" type
+   * @param "const char" subtype
+   */
+  CNodeO(char type, char subtype);
+
+  /**
+   * Constructor for identifier
+   * @param "const string" &name
+   */
+  CNodeO(const string & name);
+
+  /**
+   * Constructor for a constant
+   * @param "const C_FLOAT64" constant
+   */
+  CNodeO(C_FLOAT64 constant);
+
 
   /**
    * Destructor
@@ -48,6 +77,48 @@ class CNodeO: public CNodeK
    *  @return "const CDatum &" datum
    */
   const CDatum & getDatum() const;
+
+  virtual C_FLOAT64 value();
+
+  /**
+   * Retrieving mLeft the left branch of a node
+   * @return CNodeO
+   */
+  CNodeO & getLeft() const;
+
+  /**
+   * Retrieving mRight the left branch of a node
+   * @return CNodeO
+   */
+  CNodeO & getRight() const;
+  /**
+   * Setting mLeft the pointer to the left branch
+   * @param CNodeO &left
+   */
+  void setLeft(CNodeO & left);
+
+  /**
+   * Setting mLeft the pointer to the left branch
+   * @param CNodeO *pleft
+   */
+  void setLeft(CNodeO * pleft);
+
+  /**
+   * Setting mRight the pointer to the right branch
+   * @param CNodeO &right
+   */
+  void setRight(CNodeO & right);
+
+  /**
+   * Setting mRight the pointer to the right branch
+   * @param CNodeO *pright
+   */
+  void setRight(CNodeO * pright);
+
+  C_INT16 isLeftValid() const;
+
+  C_INT16 isRightValid() const; 
+
 };
 #endif
 
--- copasi/output/CUDFunction.cpp
+++ copasi/output/CUDFunction.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include "copasi.h"
 #include "utilities/CGlobals.h"
+#include "function/lexkk.h"
 #include "CNodeO.h"
 #include "CUDFunction.h"
 
@@ -51,6 +52,7 @@ CUDFunction::~CUDFunction() {cleanup(); DESTRUCTOR_TRACE;}
 void CUDFunction::cleanup()
 {
   mNodes.cleanup();
+
   CFunction::cleanup();
 }
 
@@ -95,7 +97,7 @@ void CUDFunction::load(CReadConfig & configbuffer,
   else
     CFunction::load(configbuffer, mode);
 
-  parse();
+  parse(); 
   
   if (configbuffer.getVersion() < "4")
     {
@@ -103,10 +105,13 @@ void CUDFunction::load(CReadConfig & configbuffer,
 
       mNodes.load(configbuffer,Size);
       createParameters();
-      mNodes.cleanup();
+      //mNodes.cleanup();		// create exception
     }
   
-  compile();
+  //compile();
+  //parse();
+  connectNodes();
+
   return;
 }
 
@@ -141,4 +146,283 @@ void CUDFunction::createParameters()
   imax = Data.size();
   for (i=0; i<imax; i++)
       getParameters().add(Data[i]);
+
 }
+
+/**
+ * Calculate the value of this user defined function
+ * return the pointer of this user defined function
+ */
+C_FLOAT64 CUDFunction::calcValue()
+{
+
+	mValue = mNodes[0]->getLeft().value();
+	return mValue;
+}
+
+/**
+ *	Get the pointer of user defined functions
+ */
+void * CUDFunction::getValueAddr()
+{
+	return &mValue;
+}
+
+/**
+ * Return the value of user defined function
+ */
+C_FLOAT64 CUDFunction::getValue() const
+{
+	return mValue;
+}
+
+
+C_INT32 CUDFunction::connectNodes()
+{
+  C_INT32 errfl = 0;     // !!! do we need this?
+  C_INT32 errnode = -1;  // !!! do we need this?
+  unsigned C_INT32 i;
+
+  // initialise the control variables
+  mNidx = 1;
+
+  // point all Left & Right to the root node
+  for (i=1; i<mNodes.size(); i++)
+    {
+	  CNodeO *npt = mNodes[i];
+      mNodes[i]->setLeft(mNodes[0]);
+      mNodes[i]->setRight(mNodes[0]);
+    }
+
+  // update pointers and references in the tree
+  mNodes[0]->setLeft(parseExpression(0));
+
+  // further checking for errors
+  if (mNodes[0]->isLeftValid() &&
+      mNodes[0]->getLeft().isOperator() &&
+      mNodes[0]->getLeft().getSubtype() == '(')
+    {
+      //  sprintf(errstr, "ERROR - missing operand");
+      //  errnode should index the node in error
+      //  but we don't know its index (pointer only)
+      CCopasiMessage(CCopasiMessage::ERROR, MCKinFunction + 2,
+		     getName().c_str());
+      errnode = -1;
+      errfl++;
+    }
+
+  for (i=1; i<mNodes.size() && !errfl; i++)
+    {
+      switch (mNodes[i]->getType())
+        {
+        case N_OPERATOR:
+	  if (!mNodes[i]->isLeftValid()      ||
+	      !mNodes[i]->isRightValid()     ||
+	      &mNodes[i]->getLeft()  == mNodes[0] ||
+	      &mNodes[i]->getRight() == mNodes[0])
+	    if (mNodes[i]->getSubtype() != '(' &&
+		mNodes[i]->getSubtype() != ')')
+	      {
+		if (!errfl)
+		  {
+		    // sprintf(errstr, "ERROR - incorrect number of operands");
+		    fatalError();
+		    errnode = i;
+		  }
+		errfl++;
+	      }
+	  if (!errfl)
+            {
+	      if (mNodes[i]->isLeftValid()    &&
+		  mNodes[i]->getLeft().isOperator() &&
+		  mNodes[i]->getLeft().getSubtype() == '(' )
+                {
+		  //           sprintf(errstr, "ERROR - missing operand");
+		  fatalError();
+		  errnode = -1;
+		  errfl++;
+                }
+	      if (mNodes[i]->isRightValid()    &&
+		  mNodes[i]->getRight().isOperator() &&
+		  mNodes[i]->getRight().getSubtype() == ')' )
+                {
+		  //           sprintf(errstr, "ERROR - missing operand");
+		  fatalError();
+		  errnode = -1;
+		  errfl++;
+                }
+            }
+	  break;
+        case N_IDENTIFIER:
+	  if (mNodes[i]->isLeftValid() ||
+	      mNodes[i]->isRightValid()  )
+            {
+	      if (!errfl)
+                {
+		  //           sprintf(errstr, "ERROR - unexpected identifier");
+		  fatalError();
+		  errnode = -1;
+                }
+	      ++errfl;
+            }
+	  break;
+        case N_NUMBER:
+	  if (mNodes[i]->isLeftValid() ||
+	      mNodes[i]->isRightValid()  )
+            {
+	      if (!errfl)
+                {
+		  //           sprintf(errstr, "ERROR - unexpected constant");
+		  fatalError();
+		  errnode = -1;
+                }
+	      ++errfl;
+            }
+	  break;
+        }
+    }
+  // return
+  return errfl;
+}
+
+CNodeO * CUDFunction::parseExpression(C_INT16 priority)
+{
+  C_INT32 errfl = 0;     // !!! do we need this?
+  C_INT32 errnode = -1;  // !!! do we need this?
+
+  CNodeO *lhs, *rhs;
+  C_INT32 op;
+
+  lhs = parsePrimary();
+  if (!lhs) return NULL;
+
+  while( mNidx < mNodes.size() &&
+	 mNodes[mNidx]->isOperator() &&
+	 priority < mNodes[mNidx]->leftPrecedence())
+    {
+      op = mNidx;
+      rhs = NULL;
+      ++mNidx;
+      rhs = parseExpression(mNodes[op]->rightPrecedence());
+      if (!rhs)
+        {
+	  if (!errfl)
+            {
+	      //    sprintf(errstr, "ERROR - unexpected operator");
+	      fatalError();
+	      errnode = op;
+            }
+	  ++errfl;
+        }
+      else
+        {
+	  mNodes[op]->setLeft(lhs);
+	  mNodes[op]->setRight(rhs);
+	  lhs = mNodes[op] ;
+        }
+    }
+  return lhs;
+}
+
+CNodeO * CUDFunction::parsePrimary()
+{
+  C_INT32 errfl = 0;     // !!! do we need this?
+  C_INT32 errnode = -1;  // !!! do we need this?
+
+  CNodeO *npt, *primary;
+  char t;
+  C_INT32 op;
+  npt = NULL;
+
+  //    if (Node[mNidx]==NULL)
+  if (mNidx >= mNodes.size())
+    {
+      //  if (!errfl) // execute only if no previous error
+      //   errnode = mNidx-1;
+      //  errfl++;
+      return NULL;
+    }
+
+  if (mNodes[mNidx]->isNumber() ||
+      mNodes[mNidx]->isIdentifier())
+    {
+      t = 'K';
+    }
+  else
+    {
+      t = mNodes[mNidx]->getSubtype();
+    }
+
+  switch (t)
+    {
+    case 'K':
+      mNodes[mNidx]->setLeft(NULL);
+      mNodes[mNidx]->setRight(NULL);
+      npt = mNodes[mNidx];
+      ++mNidx;
+      return npt;
+    case '(': ++mNidx;
+      npt = parseExpression(0);
+      if (mNidx < mNodes.size()      &&
+	  mNodes[mNidx]->isOperator() &&
+	  mNodes[mNidx]->getSubtype() == ')')
+        {
+	  ++mNidx;
+	  return npt;
+        }
+      else
+        {
+	  if (!errfl) // execute only if no previous error
+            {
+	      //             sprintf(errstr, "ERROR - right bracket missing");
+	      errnode = mNidx;
+            }
+	  errfl++;
+        }
+    case '+':
+    case '-':
+    case N_LOG:
+    case N_LOG10:
+    case N_EXP:
+    case N_SIN:
+    case N_COS:   op = mNidx; primary = NULL;
+      ++mNidx;
+      primary = parsePrimary();
+      if (primary==NULL)
+        {
+	  if (!errfl)
+            {
+	      //                  sprintf(errstr, "ERROR - missing operator");
+	      errnode = op;
+            }
+	  ++errfl;
+        }
+      else
+        {
+	  npt = mNodes[op];
+	  // unary operators are taken as functions
+	  mNodes[op]->setType(N_FUNCTION);
+	  mNodes[op]->setLeft(primary);
+	  mNodes[op]->setRight(NULL);
+	  return mNodes[op];
+        }
+    default:  return NULL;
+    }
+  if (mNidx < mNodes.size()      &&
+      mNodes[mNidx]->isOperator() &&
+      mNodes[mNidx]->getSubtype() == '(')
+    {
+      ++mNidx;
+      if (mNidx < mNodes.size()      &&
+	  mNodes[mNidx]->isOperator() &&
+	  mNodes[mNidx]->getSubtype() == ')')
+        {
+	  mNodes[mNidx]->setLeft(npt);
+	  mNodes[mNidx]->setRight(NULL);
+	  return mNodes[mNidx];
+        }
+      else parseExpression(0);
+    }
+}
+
+CCopasiVectorS < CNodeO > & CUDFunction::getNodes() {return mNodes;}
