3527f4f543ded19b35815c7cecdf310ccf13d7f8
Carel van Gend <carel.vangend@eml.villa-bosch.de>
2002-04-05 09:11:56 +0000

    Added files which will, when finished, be able to read a model of the form given in exampleinput, and produce a Copasi model.

---
+++ copasi/model/CSpec2Model.cpp
@@ -0,0 +1,281 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include "copasi.h"
+#include "model.h"
+#include "utilities/utilities.h"
+//#include "utilities/CCopasiMessage.h"
+#include "CSpecLine.h"
+
+#undef yyFlexLexer
+#define yyFlexLexer CScanInputFlexLexer
+
+#include "FlexLexer.h"
+
+CSpec2Model::CSpec2Model()
+{}
+
+CSpec2Model::CSpec2Model(std::string filename)
+{
+    mSpecFileName = filename;
+}
+
+CSpec2Model::~CSpec2Model()
+{}
+
+C_INT32 CSpec2Model::addFileContents()
+{
+    if (0 == mSpecFileName.length())
+    {
+        CCopasiMessage(CCopasiMessage::ERROR, "Input file not specified");
+    }
+    std::ifstream infile(mSpecFileName.c_str());
+    if (!infile)
+    {
+        CCopasiMessage(CCopasiMessage::ERROR, "Error opening spec file %s", mSpecFileName.c_str());
+    }
+    string bufstr;
+    while (getline(infile, bufstr, '\n'))
+    {
+        mSpecContents.push_back(bufstr);
+    }
+    return 0;
+}
+
+void CSpec2Model::printInput()
+{
+    cout << "\n\n\n";
+//     std::cout << "What we read: \n";
+//     std::vector<std::string>::iterator it = mSpecContents.begin();
+//     int cnt = mSpecContents.size();
+//     std::cout << "There were " << cnt << " lines." << std::endl;
+//     int linecount = 0;
+//     while (it <  mSpecContents.end())
+//     {
+//         std::cout << "Line " << linecount++ << ": ";
+//         std::cout << *it++ << std::endl;
+//     }
+    std::vector<CSpecLine>::iterator it = mSpecLines.begin();
+    cout << endl;
+    while (it < mSpecLines.end())
+    {
+        cout << "Type = " << CSpecLine::convertedType(it->getType());
+        cout << " Contents = " << it->getString() << endl;
+        it++;
+    }
+}
+
+CModel *CSpec2Model::createModel()
+{
+    cout << "Creating a model\n";
+    mModel = new CModel();
+    addFileContents();
+    // Read the lines in the file one at a time, determine the type, 
+    // and store each string in the list for the corresponding type.
+    vector<string>::iterator it = mSpecContents.begin();
+    while (it < mSpecContents.end())
+    {
+        determineType(*it);
+        it++;
+    }
+    processCompartments();
+    processEQNs();
+    processInits();
+    processConstants();
+    processRates();
+//     processFunctions();
+//     processMoieties();
+//     processDETerms();
+    return mModel;
+}
+
+void CSpec2Model::determineType(string str)
+{
+    istringstream *strstr = new istringstream(str);
+    CScanInputFlexLexer scanner(strstr);
+//    yyFlexLexer scanner(strstr);
+    int type = scanner.yylex();
+    if (type)
+    {
+        mSpecLines.push_back(CSpecLine(type, scanner.YYText()));
+    }
+}
+    
+void CSpec2Model::processCompartments()
+{
+    vector<CSpecLine>::iterator it = mSpecLines.begin();
+    for ( ; it < mSpecLines.end(); it++)
+    {
+        if (it->getType() == CSpecLine::CPT)
+        {
+            string comp_name = it->extractCpt();
+            C_FLOAT64 comp_vol = atof(it->extractRight().c_str());
+            CCompartment *compartment = new CCompartment(comp_name, comp_vol);
+            mModel->getCompartments().add(compartment);
+        }
+    }
+    for (unsigned C_INT32 i = 0; i < mModel->getCompartments().size(); i++)
+    {
+        CCompartment *comp = mModel->getCompartments()[i];
+        cout << "Added compartment with name " << comp->getName() << " and volume = " << comp->getVolume() << endl;
+    }
+}
+
+void CSpec2Model::processEQNs()
+{
+    // pick out the EQN or DE lines. Store the compartment name, metabolite 
+    // name and contents string in a CBaseEqn instance, for later processing.
+    vector<CSpecLine>::iterator it = mSpecLines.begin();
+    for ( ; it < mSpecLines.end(); it++)
+    {
+        if (it->getType() == CSpecLine::DE)
+        {
+            string comp_name = it->extractCpt();
+            string metab = it->extractLeft();
+            string contents = it->extractRight();
+            CBaseEqn tmp(comp_name, metab, contents);
+            mReactVector.push_back(tmp);
+        }
+        if (it->getType() == CSpecLine::EQN)
+        {
+            string comp_name = it->extractCpt();
+            string metab = it->extractLeft();
+            string contents = it->extractRight();
+            CBaseEqn tmp(comp_name, metab, contents);
+            mMoietyVector.push_back(tmp);
+        }
+    }
+    for (vector<CBaseEqn>::iterator it = mReactVector.begin(); it < mReactVector.end(); it++)
+    {
+        cout << "Added DE " << it->getMetabolite();
+        cout << " in compartment " << it->getCompartment();
+        cout << " with RHS = " << it->getContents() << endl;
+    }
+    for (vector<CBaseEqn>::iterator it = mMoietyVector.begin(); it < mMoietyVector.end(); it++)
+    {
+        cout << "Added EQN " << it->getMetabolite();
+        cout << " in compartment " << it->getCompartment();
+        cout << " with RHS = " << it->getContents() << endl;
+    }
+}
+
+void CSpec2Model::processInits()
+{
+    // Find each INIT line, and determine whether it corresponds to a reactant.
+    vector<CSpecLine>::iterator it = mSpecLines.begin();
+    for ( ; it < mSpecLines.end(); it++)
+    {
+        if (it->getType() == CSpecLine::INIT)
+        {
+            string comp_name = it->extractCpt();
+            string metab_name = it->extractLeft();
+            string contents = it->extractRight();
+            CBaseEqn tmp(comp_name, metab_name, "");
+            vector<CBaseEqn>::iterator vit = find(mReactVector.begin(), mReactVector.end(), tmp);
+            if (vit != mReactVector.end())
+            {
+                // this does correspond to a reactant.
+                CMetab metab(metab_name);
+                metab.setCompartment( mModel->getCompartments()[comp_name]);
+                metab.setStatus(METAB_VARIABLE);
+                C_FLOAT64 iconc = atof(contents.c_str());
+                metab.setInitialConcentration(iconc);
+                metab.setConcentration(iconc);    // XXX TODO: is this neccessary?
+//                mModel.getMetabolites().push_back(&metab); // XXX TODO: Are metabolites also added to a model, or just compartment
+                mModel->getCompartments()[comp_name]->addMetabolite(metab);
+            }
+            else
+            {
+//                CCopasiMessage(CCopasiMessage::ERROR, "Not a reactant initialization %s: %s = %s", comp_name, metab_name, contents);
+            }
+        }
+    }
+    unsigned C_INT32 i = 0, j = 0;
+    for (i = 0 ; i < mModel->getCompartments().size(); i++)
+    {
+        CCompartment comp = *mModel->getCompartments()[i];
+        cout << "In compartment " << comp.getName() << ":\n";
+        for (j = 0 ; j < comp.metabolites().size(); j++)
+        {
+            CMetab metab = *comp.metabolites()[j];
+            cout << "Metabolite " << metab.getName() << " with initial concentration " << *metab.getInitialConcentration() << endl;
+        }
+    }
+}
+
+void CSpec2Model::processConstants()
+{
+    cout << "Doing consts\n";
+    // Find each CNST line
+    vector<CSpecLine>::iterator it = mSpecLines.begin();
+    for ( ; it < mSpecLines.end(); it++)
+    {
+        if (it->getType() == CSpecLine::CNST)
+        {
+            string metab_name = it->extractLeft();
+            string contents = it->extractRight();
+            CMetab *metab = new CMetab(metab_name);
+            metab->setStatus(METAB_FIXED);
+            metab->setInitialConcentration(atof(contents.c_str()));
+            metab->setConcentration(atof(contents.c_str()));
+            mModel->getMetabolites().push_back(metab); // XXX TODO: is this the right place to store these?
+            cout << "For fixed metabolite " << 
+                mModel->getMetabolites()[mModel->getMetabolites().size() -1]->getName();
+            cout << " set init concentration to " << *mModel->getMetabolites()[mModel->getMetabolites().size() -1]->getInitialConcentration() << endl;
+        }
+    }
+}
+
+void CSpec2Model::processRates()
+{
+    // Find each RATE line
+    vector<CSpecLine>::iterator it = mSpecLines.begin();
+    for ( ; it < mSpecLines.end(); it++)
+    {
+        if (it->getType() == CSpecLine::RATE)
+        {
+            string rate_name = it->extractLeft();
+            string contents = it->extractRight();
+        }
+    }            
+}
+
+C_INT32 CSpec2Model::parseLine(string) {}
+
+#ifdef TEST
+
+#include "../utilities/CGlobals.h"
+
+CGlobals *Copasi = new CGlobals;
+
+std::ofstream DebugFile("junk");
+
+int main(int argc, char**argv)
+{
+    string filename = "exampleinput";
+    if (argc == 2)
+    {
+        cout << "Assigning " << argv[1] << " to input file name\n";
+        filename = argv[1];
+    }
+    else 
+    {
+        cout << "Using default input file exampleinput\n";
+    }
+    try
+    {
+        CSpec2Model specreader(filename);
+        CModel *model;
+        // create a model
+        model = specreader.createModel();
+        // Test that we read the input file correctly
+        specreader.printInput();
+    }
+    catch (CCopasiException Exception)
+    {
+        cout << Exception.getMessage().getText() << endl;
+    }
+}
+
+#endif //TEST
---
+++ copasi/model/CSpecLine.h
@@ -0,0 +1,128 @@
+#ifndef Copasi_SpecLine
+#define Copasi_SpecLine
+
+#include <string>
+#include "copasi.h"
+
+/**
+ * CSpecLine
+ * 
+ * This class represents a line of input from the spec file. It contains
+ * a type identifier and a string, representing the actual input.
+ */
+
+class CSpecLine
+{
+ public:
+    /**
+     * This enumerated type specifies the type of line contained in the spec.
+     */
+    enum SpecLineType
+        {
+            CMNT,	// Comments
+            DE,		// Differential equations
+            EQN,	// Moiety specification equations
+            INIT,	// Initializations
+            CNST,	// Constant assignments
+            VOL,	// Volume assignment
+            CPT,	// Compartment volume assignments
+            RATE,	// Rate constant assignments
+            FUN     	// Kinetic function specifications
+        };
+
+    /**
+     * The named constructor
+     */
+    CSpecLine(C_INT32 type, string contents);
+
+    /**
+     * Retrieve the type.
+     * @return The type
+     */
+    C_INT32 getType() {return mType;}
+
+    static string convertedType(int type);
+
+    /**
+     * Retrieve the content string
+     */
+    string &getString() {return mContents;}
+    
+    /**
+     * Extract and return the left hand side of the contents string. 
+     * If the left hand side is preceded by a compartment specifier, 
+     * this is stripped from the returned string.
+     * @return The (stripped) left hand side string
+    */
+    string extractLeft();
+
+    /**
+     * Extract and return the right hand side of the contents string. 
+     * @return The right hand side string
+    */
+    string extractRight();
+
+    /** 
+     * Extract and return the compartment name/number on the right hand side
+     * @return The compartment specifier
+     */
+    string extractCpt();
+                
+ private:
+    /**
+     * The type
+     */
+    C_INT32 mType;
+    /**
+     * The contents
+     */
+    string mContents;
+
+    // private operations
+    string stripBlanks(string instr);
+};
+
+/**
+ * The CBaseEqn class holds basic information about an equation, 
+ * and is used as an intermediate in processing the spec input.
+ * It stores the LHS metabolite, the compartment this is in, and the RHS string.
+ */
+class CBaseEqn
+{
+ public:
+    /**
+     * Named constructor
+     */
+    CBaseEqn(string comp, string metab, string contents) 
+        :mCompartment(comp), mMetabolite(metab), mContents(contents) {}
+    /**
+     * Destructor
+     */
+    ~CBaseEqn() {}
+    /**
+     * Retrieve the string containing the name of the compartment.
+     * @return The compartment name.
+     */
+    string getCompartment() const {return mCompartment;}
+    /**
+     * Retrieve the string containing the name of the metabolite
+     * @return The metabolite name
+     */
+    string getMetabolite() const {return mMetabolite;}
+    /**
+     * Retrieve the string containing the contents of the right hand side.
+     * @return The RHS contents.
+     */
+    string getContents() const{return mContents;}
+    /**
+     * Overloads the == operator. Equality is true if the strings
+     * mCompartment and mMetabolite match.
+     */
+    bool operator==(const CBaseEqn &rhs) const;
+ private:
+    string mCompartment;
+    string mMetabolite;
+    string mContents;
+};
+
+#endif // Copasi_SpecLine
