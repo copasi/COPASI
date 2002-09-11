821e440575e5d87df248a64c5fa1b2813a59c81a
Stefan Hoops <shoops@vbi.vt.edu>
2002-09-11 15:47:54 +0000

    Renamed files to lowercase so that make rules work

--- copasi/ABiochem/r250.h
+++ copasi/ABiochem/r250.h
@@ -1,5 +1,5 @@
 /*** Function prototypes for R250 random number generator, by W. L. Maier  ***/
-unsigned int r250( void );
+unsigned int r250(void);
 unsigned int r250n(unsigned n);
-double dr250( void );
+double dr250(void);
 void r250_init(int seed);
