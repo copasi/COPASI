a6f3c5023b3a2529a8f2c50722c9cc5af3254e40
Stefan Hoops <shoops@vbi.vt.edu>
2002-09-19 17:43:46 +0000

    Removed save() and load() methods. See BugID: 8

--- copasi/model/CMoiety.h
+++ copasi/model/CMoiety.h
@@ -15,141 +15,131 @@
 #include "CChemEqElement.h"
 
 class CMoiety
-{
-  // Attributes
- private:
-  /**
-   *
-   */
-  string mName;
+  {
+    // Attributes
+  private:
+    /**
+     *
+     */
+    string mName;
 
-  /**
-   *  Number of Particles of Moietiy.
-   */
-  C_FLOAT64 mNumber;
+    /**
+     *  Number of Particles of Moietiy.
+     */
+    C_FLOAT64 mNumber;
 
-  /**
-   *  Initial Number of Particles of Moietiy.
-   */
-  C_FLOAT64 mINumber;
+    /**
+     *  Initial Number of Particles of Moietiy.
+     */
+    C_FLOAT64 mINumber;
 
-  /**
-   *  Vector of linear dependent CChemEqElement
-   * @supplierCardinality 0..*
-   */
-  CCopasiVector < CChemEqElement > mEquation;
-    
-  // Operations
- public:
-  /**
-   *  Default constructor
-   */
-  CMoiety();
+    /**
+     *  Vector of linear dependent CChemEqElement
+     * @supplierCardinality 0..*
+     */
+    CCopasiVector < CChemEqElement > mEquation;
 
-  /**
-   *  Copy constructor
-   *  @param "const CMoiety &" src
-   */
-  CMoiety(const CMoiety & src);
+    // Operations
+  public:
+    /**
+     *  Default constructor
+     */
+    CMoiety();
 
-  /**
-   *
-   */
-  CMoiety(const string & name);
- 
-  /**
-   *  Destructor
-   */
- ~CMoiety();
+    /**
+     *  Copy constructor
+     *  @param "const CMoiety &" src
+     */
+    CMoiety(const CMoiety & src);
 
-  /**
-   *
-   */
-  C_INT32 load(CReadConfig & configBuffer);
+    /**
+     *
+     */
+    CMoiety(const string & name);
 
-  /**
-   *
-   */
-  C_INT32 save(CWriteConfig & configBuffer);
+    /**
+     *  Destructor
+     */
+    ~CMoiety();
 
-  /**
-   *
-   */
-  void add(C_FLOAT64 value, CMetab & metabolite);
+    /**
+     *
+     */
+    void add
+      (C_FLOAT64 value, CMetab & metabolite);
 
-  /**
-   *
-   */
-  void add(C_FLOAT64 value, CMetab * metabolite);
+    /**
+     *
+     */
+    void add
+      (C_FLOAT64 value, CMetab * metabolite);
 
-  /**
-   *
-   */
-  void cleanup();
+    /**
+     *
+     */
+    void cleanup();
 
-  /**
-   *
-   */
-  void cleanup(const string & name);
+    /**
+     *
+     */
+    void cleanup(const string & name);
 
-  /**
-   *
-   */
-  void cleanup(C_INT32 index);
+    /**
+     *
+     */
+    void cleanup(C_INT32 index);
 
-  /**
-   *
-   */
-  void change(C_INT32 index,
-	      C_FLOAT64 value);
+    /**
+     *
+     */
+    void change(C_INT32 index,
+                C_FLOAT64 value);
 
-  /**
-   *
-   */
-  void change(const string & name,
-	      C_FLOAT64 value);
+    /**
+     *
+     */
+    void change(const string & name,
+                C_FLOAT64 value);
 
-  /**
-   *
-   */
-  void setName(const string name);
+    /**
+     *
+     */
+    void setName(const string name);
 
-  /**
-   *
-   */
-  void setInitialValue();
+    /**
+     *
+     */
+    void setInitialValue();
 
-  /**
-   *
-   */
-  string getName() const;
+    /**
+     *
+     */
+    string getName() const;
 
-  /**
-   *
-   */
-  string getDescription() const;
+    /**
+     *
+     */
+    string getDescription() const;
 
-  /**
-   *
-   */
-  C_FLOAT64 dependentNumber();
+    /**
+     *
+     */
+    C_FLOAT64 dependentNumber();
 
-  /**
-   *
-   */
-  C_FLOAT64 dependentRate();
+    /**
+     *
+     */
+    C_FLOAT64 dependentRate();
 
-  /**
-   *
-   */
-  C_FLOAT64 getNumber() const;
+    /**
+     *
+     */
+    C_FLOAT64 getNumber() const;
 
-  /**
-   *	Returns the address of mNumber
-   */
-  void * getNumberAddr();
-
-
-};
+    /**
+     * Returns the address of mNumber
+     */
+    void * getNumberAddr();
+  };
 
 #endif // COPASI_CMoiety
