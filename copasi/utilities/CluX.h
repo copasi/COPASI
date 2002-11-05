b69fd16c88b6d632abe3ff15c93f95d9baa2de4d
Stefan Hoops <shoops@vbi.vt.edu>
2002-11-05 18:41:39 +0000

    Removed Fortran style access.

--- copasi/utilities/CluX.h
+++ copasi/utilities/CluX.h
@@ -50,107 +50,104 @@
 //
 
 template <class Matrix, class Subscript>
-int LUfactor(Matrix &A, vector< Subscript > & row, vector< Subscript > & col)
-{
-  /* :TODO: get rid of this for performance reasons */
-  CFortranAccess< Matrix > fA(A);
+      int LUfactor(Matrix &A, vector< Subscript > & row, vector< Subscript > & col)
+  {
+    Subscript M = A.numRows();
+    Subscript N = A.numCols();
 
-  Subscript M = A.numRows();
-  Subscript N = A.numCols();
+    if (M == 0 || N == 0)
+      return 0;
+    if (row.size() != M)
+      row.resize(M);
+    if (col.size() != N)
+      col.resize(N);
 
-  if (M == 0 || N == 0)
-    return 0;
-  if (row.size() != M)
-    row.resize(M);
-  if (col.size() != N)
-    col.resize(N);
+    Subscript i = 0, j = 0, k = 0;
+    Subscript jp = 0;
+    Subscript jl = N - 1;
 
-  Subscript i = 1, j = 1, k = 1;
-  Subscript jp = 0;
-  Subscript jl = N;
+    for (i = 0; i < M; i++)
+      row[i] = i;
+    for (i = 0; i < N; i++)
+      col[i] = i;
 
-  for (i = 1; i <= M; i++)
-    row[i - 1] = i;
-  for (i = 1; i <= N; i++)
-    col[i - 1] = i;
-
-  typename Matrix::elementType t;
+    typename Matrix::elementType t;
 
   Subscript minMN = (M < N ? M : N);        // min(M,N);
 
-  for (j = 1; j <= minMN; j++)
-    {
-      // find pivot in column j and  test for singularity.
-      while (TRUE)
-        {
-          jp = j;
-          t = fabs(fA(j, j));
-          for (i = j + 1; i <= M; i++)
-            if (fabs(fA(i, j)) > t)
-              {
-                jp = i;
-                t = fabs(fA(i, j));
-              }
-
-          row[j - 1] = jp;
-
-          // jp now has the index of maximum element
-          // of column j, below the diagonal
-
-          if (fA(jp, j) == 0) // now we have to swap colums to find a pivot
-            {
-              if (jl <= j)
-                return 1; // we are done
-              for (k = 1; k <= M; k++)
-                {
-                  t = fA(k, jl);
-                  fA(k, jl) = fA(k, j);
-                  fA(k, j) = t;
-                }
-              col[jl - 11] = j;
-              jl--;
-              continue;
-            }
-          else
-            break;
-        }
-
-      if (jp != j)            // swap rows j and jp
-        for (k = 1; k <= N; k++)
+    for (j = 0; j < minMN; j++)
+      {
+        // find pivot in column j and  test for singularity.
+        while (TRUE)
           {
-            t = fA(j, k);
-            fA(j, k) = fA(jp, k);
-            fA(jp, k) = t;
+            jp = j;
+            t = fabs(A(j, j));
+            for (i = j + 1; i < M; i++)
+              if (fabs(A(i, j)) > t)
+                {
+                  jp = i;
+                  t = fabs(A(i, j));
+                }
+
+            row[j] = jp;
+
+            // jp now has the index of maximum element
+            // of column j, below the diagonal
+
+            if (A(jp, j) == 0) // now we have to swap colums to find a pivot
+              {
+                if (jl <= j)
+                  return 1; // we are done
+                for (k = 0; k < M; k++)
+                  {
+                    t = A(k, jl);
+                    A(k, jl) = A(k, j);
+                    A(k, j) = t;
+                  }
+                col[jl] = j;
+                jl--;
+                continue;
+              }
+            else
+              break;
           }
 
-      if (j < M)                // compute elements j+1:M of jth column
-        {
-          // note fA(j,j), was fA(jp,p) previously which was
-          // guarranteed not to be zero (Label #1)
-          //
-          typename Matrix::elementType recp = 1.0 / fA(j, j);
+        if (jp != j)      // swap rows j and jp
+          for (k = 0; k < N; k++)
+            {
+              t = A(j, k);
+              A(j, k) = A(jp, k);
+              A(jp, k) = t;
+            }
 
-          for (k = j + 1; k <= M; k++)
-            fA(k, j) *= recp;
-        }
+        if (j < M - 1)    // compute elements j+1 <= k < M of jth column
+          {
+            // note A(j,j), was A(jp,p) previously which was
+            // guarranteed not to be zero (Label #1)
+            //
+            typename Matrix::elementType recp = 1.0 / A(j, j);
 
-      if (j < minMN)
-        {
-          // rank-1 update to trailing submatrix:   E = E - x*y;
-          //
-          // E is the region fA(j+1:M, j+1:N)
-          // x is the column vector fA(j+1:M,j)
-          // y is row vector fA(j,j+1:N)
+            for (k = j + 1; k < M; k++)
+              A(k, j) *= recp;
+          }
 
-          Subscript ii, jj;
+        if (j < minMN - 1)
+          {
+            // rank-1 update to trailing submatrix:   E = E - x*y;
+            //
+            // E is the region A(j+1:M, j+1:N)
+            // x is the column vector A(j+1:M,j)
+            // y is row vector A(j,j+1:N)
 
-          for (ii = j + 1; ii <= M; ii++)
-            for (jj = j + 1; jj <= N; jj++)
-              fA(ii, jj) -= fA(ii, j) * fA(j, jj);
-        }
-    }
+            Subscript ii, jj;
 
-  return 0;
-}
+            for (ii = j + 1; ii < M; ii++)
+              for (jj = j + 1; jj < N; jj++)
+                A(ii, jj) -= A(ii, j) * A(j, jj);
+          }
+      }
+
+    return 0;
+  }
 
 #endif // COPASI_CluX
