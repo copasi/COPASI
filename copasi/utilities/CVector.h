14dc98b8c8c5bbc1d539426f071ac19c01bb6fd9
Stefan Hoops <shoops@vbi.vt.edu>
2002-11-05 17:54:45 +0000

    Fixed comments and some variable declarations.

--- copasi/utilities/CVector.h
+++ copasi/utilities/CVector.h
@@ -12,8 +12,7 @@ using std::endl;
  * C-style and fortran style access to the elements. It also supplies 
  * an ostream operator.
  */
-template <class CType>
-class CVector
+template <class CType> class CVector
   {
   public:
     typedef CType elementType;
@@ -35,163 +34,152 @@ class CVector
     /**
      * Default constructor
      * @param unsigned C_INT32 rows (default = 0)
-     * @param unsigned C_INT32 cols (default = 0)
      */
     CVector(unsigned C_INT32 rows = 0) :
         mRows(rows),
         mVector(NULL)
-    {
-      if (mRows)
-        mVector = new CType[mRows];
-    }
+    {if (mRows) mVector = new CType[mRows];}
 
-    /**
-     * Copy constructor
-     * @param const CVector <CType> & src
-     */
-    CVector(const CVector <CType> & src):
-        mRows(src.mRows),
-        mVector(NULL)
-    {
-      if (mRows)
-        {
-          mVector = new CType[mRows];
-          memcpy(mVector, src.mVector, mRows * sizeof(CType));
-        }
-    }
+                          /**
+                           * Copy constructor
+                           * @param const CVector <CType> & src
+                           */
+                          CVector(const CVector <CType> & src):
+                              mRows(src.mRows),
+                              mVector(NULL)
+                          {
+                            if (mRows)
+                              {
+                                mVector = new CType[mRows];
+                                          memcpy(mVector, src.mVector, mRows * sizeof(CType));
+                                        }
+                                    }
 
-    /**
-     * Destructor.
-     */
-    ~CVector()
-    {
-      if (mVector)
-        delete [] mVector;
-    }
+                                    /**
+                                     * Destructor.
+                                     */
+                                    ~CVector()
+                                    {if (mVector) delete [] mVector;}
 
-    /**
-     * The number of elements stored in the vector.
-     * @return unsigned C_INT32 size
-     */
-  unsigned C_INT32 size() const {return mRows;}
+                                    /**
+                                     * The number of elements stored in the vector.
+                                     * @return unsigned C_INT32 size
+                                     */
+                                    unsigned C_INT32 size() const {return mRows;}
 
-    /**
-     * The number of rows of the vector.
-     * @return unsigned C_INT32 rows
-     */
-    unsigned C_INT32 numRows() const {return mRows;}
+                                    /**
+                                     * The number of rows of the vector.
+                                     * @return unsigned C_INT32 rows
+                                     */
+                                    unsigned C_INT32 numRows() const {return mRows;}
 
-    /**
-     * Resize the vector. The previous content is lost
-     * @param unsigned C_INT32 rows
-     */
-    void resize(unsigned C_INT32 rows)
-    {
-      if (mVector)
-        {
-          delete [] mVector;
-          mVector = NULL;
-        }
+                                    /**
+                                     * Resize the vector. The previous content is lost
+                                     * @param unsigned C_INT32 rows
+                                     */
+                                    void resize(unsigned C_INT32 rows)
+                                    {
+                                      if (mVector)
+                                        {
+                                          delete [] mVector;
+                                          mVector = NULL;
+                                                  }
 
-      mRows = rows;
+                                                mRows = rows;
 
-      if (mRows)
-        mVector = new CType[mRows];
-    }
+                                                        if (mRows) mVector = new CType[mRows];
+                                                                             }
 
-    /**
-     * Assignement operator
-     * @param const CVector <CType> & rhs
-     * @return CVector <CType> & lhs
-     */
-    CVector <CType> & operator = (const CVector <CType> & rhs)
-    {
-      if (mRows != rhs.mRows)
-        resize(rhs.mRows);
+                                                                           /**
+                                                                            * Assignement operator
+                                                                            * @param const CVector <CType> & rhs
+                                                                            * @return CVector <CType> & lhs
+                                                                            */
+                                                                           CVector <CType> & operator = (const CVector <CType> & rhs)
+                                                                           {
+                                                                             if (mRows != rhs.mRows) resize(rhs.mRows);
 
-      memcpy(mVector, rhs.mVector, mRows * sizeof(CType));
+                                                                               memcpy(mVector, rhs.mVector, mRows * sizeof(CType));
 
-      return *this;
-    }
+                                                                               return * this;
+                                                                             }
 
-    /**
-     * Retrieve an element of the vector
-     * @param unsigned C_INT32 row
-     * @return CType & row
-     */
-    inline CType & operator[](unsigned C_INT32 row)
-  {return *(mVector + row);}
+                                                                           /**
+                                                                            * Retrieve an element of the vector
+                                                                            * @param const unsigned C_INT32 & row
+                                                                            * @return CType & element
+                                                                            */
+                                                                           inline CType & operator[](const unsigned C_INT32 & row)
+                                                                         {return * (mVector + row);}
 
-    /**
-     * Retrieve an element of the vector
-     * @param unsigned C_INT32 row
-     * @return const CType & row
-     */
-    inline const CType & operator[](unsigned C_INT32 row) const
-      {return *(mVector + row);}
+                                                                           /**
+                                                                            * Retrieve an element of the vector
+                                                                            * @param const unsigned C_INT32 & row
+                                                                            * @return const CType & element
+                                                                            */
+                                                                           inline const CType & operator[](const unsigned C_INT32 & row) const
+                                                                           {return * (mVector + row);}
 
-    /**
-     * Retrieve a vector element using Fortan style indexing.
-     * @param const unsigned C_INT32 & row
-     * @param const unsigned C_INT32 & col
-     * @return const CType & element
-     */
-    inline CType & operator()(const unsigned C_INT32 & row)
-    {return *(mVector + (row - 1));}
+                                                                           /**
+                                                                            * Retrieve a vector element using Fortan style indexing.
+                                                                            * @param const unsigned C_INT32 & row
+                                                                            * @return const CType & element
+                                                                            */
+                                                                           inline CType & operator()(const unsigned C_INT32 & row)
+                                                                           {return * (mVector + (row - 1));}
 
-    /**
-     * Retrieve a vector element using Fortan style indexing.
-     * @param const unsigned C_INT32 & row
-     * @param const unsigned C_INT32 & col
-     * @return const CType & element
-     */
-    inline const CType & operator()(const unsigned C_INT32 & row) const
-      {return *(mVector + (row - 1));}
+                                                                           /**
+                                                                            * Retrieve a vector element using Fortan style indexing.
+                                                                            * @param const unsigned C_INT32 & row
+                                                                            * @return const CType & element
+                                                                            */
+                                                                           inline const CType & operator()(const unsigned C_INT32 & row) const
+                                                                           {return * (mVector + (row - 1));}
 
-    /**
-     * Retrieve the array of the vector elements. This is suitable
-     * for interfacing with clapack routines.
-     * @return CType * array
-     */
-    CType * array() {return mVector;}
+                                                                           /**
+                                                                            * Retrieve the array of the vector elements. This is suitable
+                                                                            * for interfacing with clapack routines.
+                                                                            * @return CType * array
+                                                                            */
+                                                                           CType * array() {return mVector;}
 
-    /**
-     * Retrieve the array of the vector elements. This is suitable
-     * for interfacing with clapack routines.
-     * @return const CType * array
-     */
-    const CType * array() const {return mVector;}
+                                                                           /**
+                                                                            * Retrieve the array of the vector elements. This is suitable
+                                                                            * for interfacing with clapack routines.
+                                                                            * @return const CType * array
+                                                                            */
+                                                                           const CType * array() const {return mVector;}
 
-    /**
-     * Output stream operator
-     * @param ostream & os
-     * @param const CVector< CType > & A
-     * @retrun ostream & os
-     */
-    friend ostream &operator<<(ostream &os, const CVector< CType > & A)
-    {
-      os << "Vector(" << A.mRows << ")" << endl;
+                                                                           /**
+                                                                            * Output stream operator
+                                                                            * @param ostream & os
+                                                                            * @param const CVector< CType > & A
+                                                                            * @retrun ostream & os
+                                                                            */
+                                                                           friend ostream &operator<<(ostream &os, const CVector< CType > & A)
+                                                                           {
+                                                                             os << "Vector(" << A.mRows << ")" << endl;
 
-      unsigned C_INT32 i, j;
-      CType * tmp = A.mVector;
+                                                                             unsigned C_INT32 i, j;
+                                                                             CType * tmp = A.mVector;
 
-      for (i = 0; i < A.mRows; i++)
-        cout << "  " << * (tmp++);
-      cout << endl;
-      return os;
-    }
-  };
+                                                                                           for (i = 0; i < A.mRows; i++)
+                                                                                             cout << "  " << * (tmp++);
+                                                                                             cout << endl;
+                                                                                             return os;
+                                                                                           }
+                                                                                       };
 
-/**
- * This creates an instantiation of 
- * the ostream operator << for CVector <C_FLOAT64>
- */
-class CVectorDbl : public CVector <C_FLOAT64> {};
+                                                                                     /**
+                                                                                      * This creates an instantiation of 
+                                                                                      * the ostream operator << for CVector <C_FLOAT64>
+                                                                                      */
+                                                                                     class CVectorDbl : public CVector <C_FLOAT64> {};
 
-/**
- * This creates an instantiation of 
- * the ostream operator << for CVector <C_INT32>
- */
-class CVectorInt : public CVector <C_INT32> {};
+                                                                                     /**
+                                                                                      * This creates an instantiation of 
+                                                                                      * the ostream operator << for CVector <C_INT32>
+                                                                                      */
+                                                                                     class CVectorInt : public CVector <C_INT32> {};
 
 #endif // COPASI_CVector
