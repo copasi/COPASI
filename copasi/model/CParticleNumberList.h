67c18611a47743e614706300392bd34cab496d6d
Stefan Hoops <shoops@vbi.vt.edu>
2002-11-06 02:03:29 +0000

    Renamed some functions to reflect the return type CVector in CParticleNumberList. Added documentation.

--- copasi/model/CParticleNumberList.h
+++ copasi/model/CParticleNumberList.h
@@ -9,25 +9,103 @@ class CParticleNumberList
     // Associations
 
     // Attributes
+    /**
+     * The size of the particle number list
+     */
     unsigned C_INT32 mSize;
+
+    /**
+     * The particle numbers stored as C_FLOAT64
+     */
     CVector< C_FLOAT64 > mDbl;
+
+    /**
+     * The particle numbers stored as C_INT32
+     */
     CVector< C_INT32 > mInt;
 
     // Operations
   public:
-    CParticleNumberList(unsigned C_INT32 size = 0);
+    /**
+     * Default constructor
+     * @param const unsigned C_INT32 & size (default 0)
+     */
+    CParticleNumberList(const unsigned C_INT32 & size = 0);
+
+    /**
+     * Copy constructor
+     * @param const CParticleNumberList & src
+     */
     CParticleNumberList(const CParticleNumberList & src);
+
+    /**
+     * Destructor
+     */
     ~CParticleNumberList();
+
+    /**
+     * Resize the particle number list
+     * @param const unsigned C_INT32 & size
+     */
     void resize(const unsigned C_INT32 & size);
-    void set(const unsigned C_INT32 & index, const C_FLOAT64 & value);
-    const C_FLOAT64 & getDbl(const unsigned C_INT32 & index) const;
-    void set(const unsigned C_INT32 & index, const C_INT32 & value);
-    const C_INT32 & getInt(const unsigned C_INT32 & index) const;
-    void setArray(const CVector< C_FLOAT64 > & values);
-    const CVector< C_FLOAT64 > & getDblArray() const;
-    void setArray(const CVector< C_INT32 > & values);
-    const CVector< C_INT32 > & getIntArray() const;
+
+    /**
+     * Retreive the size of the particle number list
+     * @return const unsigned C_INT32 & size
+     */
     const unsigned C_INT32 & size() const;
+
+    /**
+     * Set the indexed particle number to value
+     * @param const unsigned C_INT32 & index
+     * @param const C_FLOAT64 & value
+     */
+    void set(const unsigned C_INT32 & index, const C_FLOAT64 & value);
+
+    /**
+     * Retreive the indexed particle number as C_FLOAT64
+     * @param const unsigned C_INT32 & index
+     * @return const C_FLOAT64 & value
+     */
+    const C_FLOAT64 & getDbl(const unsigned C_INT32 & index) const;
+
+    /**
+     * Set the indexed particle number to value
+     * @param const unsigned C_INT32 & index
+     * @param const C_FLOAT64 & value
+     */
+    void set(const unsigned C_INT32 & index, const C_INT32 & value);
+
+    /**
+     * Retreive the indexed particle number as C_INT32
+     * @param const unsigned C_INT32 & index
+     * @return const C_INT32 & value
+     */
+    const C_INT32 & getInt(const unsigned C_INT32 & index) const;
+
+    /**
+     * Set all particle numbers at once
+     * @param const CVector< C_FLOAT64 > & vector
+     */
+    void setVector(const CVector< C_FLOAT64 > & vector);
+
+    /**
+     * Retreive all particle numbers as C_FLOAT64
+     * @return const CVector< C_FLOAT64 > & vector
+     */
+    const CVector< C_FLOAT64 > & getVectorDbl() const;
+
+    /**
+     * Set all particle numbers at once
+     * @param const CVector< C_INT32 > & vector
+     */
+    void setVector(const CVector< C_INT32 > & vector);
+
+    /**
+     * Retreive all particle numbers as C_INT32
+     * @return const CVector< C_INT32 > & vector
+     */
+    const CVector< C_INT32 > & getVectorInt() const;
   };
 
 #endif // COPASI_CParticleNumberList
