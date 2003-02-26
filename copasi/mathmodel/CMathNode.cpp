e33e889c6596ddedf48ffd3ca66e845f0f930232
Stefan Hoops <shoops@vbi.vt.edu>
2003-02-26 23:47:00 +0000

    Added class CMathNode and it derivations.

---
+++ copasi/mathmodel/CMathNode.cpp
@@ -0,0 +1,175 @@
+/**
+ * CMathNode class.
+ * The class CMathNode is describes a node of the equation tree.
+ *
+ * Created for Copasi by Stefan Hoops 2003
+ */
+
+#include <sstream>
+
+#include "copasi.h"
+#include "CMathNode.h"
+#include "CMathSymbol.h"
+
+CMathNode::CMathNode():
+    CCopasiNode()
+{}
+
+CMathNode::CMathNode(const CMathNode &src):
+    CCopasiNode(src)
+{}
+
+CMathNode::~CMathNode() {}
+
+std::string CMathNode::getText() const
+  {return ((CMathNode *) const_cast< CMathNode * >(this)->getChild())->getText();}
+
+const std::string CMathNodeOperation::Operations("+-*/%^");
+
+CMathNodeOperation::CMathNodeOperation():
+    CMathNode(),
+    mOperation()
+{}
+
+CMathNodeOperation::CMathNodeOperation(const std::string & operation):
+    CMathNode(),
+    mOperation(operation)
+{assert (Operations.find(operation) != std::string::npos);}
+
+CMathNodeOperation::CMathNodeOperation(const CMathNodeOperation & src):
+    CMathNode(src),
+    mOperation(src.mOperation)
+{}
+
+CMathNodeOperation::~CMathNodeOperation() {}
+
+std::string CMathNodeOperation::getText() const
+  {
+    stringstream text;
+    CMathNode * pChild =
+      (CMathNode *) const_cast< CMathNodeOperation * >(this)->getChild();
+
+    if (pChild)
+      {
+        text << pChild->getText() << mOperation;
+
+        if (pChild->getSibbling())
+          text << ((CMathNode *) pChild->getSibbling())->getText();
+        else
+          text << "???";
+      }
+    else
+      text << "???" << mOperation << "???";
+
+    return text.str();
+  }
+
+CMathNodeDerivative::CMathNodeDerivative(const std::string & operation):
+    CMathNodeOperation()
+{mOperation = operation;}
+
+CMathNodeDerivative::CMathNodeDerivative(const CMathNodeDerivative &src):
+    CMathNodeOperation(src)
+{}
+
+CMathNodeDerivative::~CMathNodeDerivative() {}
+
+std::string CMathNodeDerivative::getText() const
+  {
+    stringstream text;
+    CMathNode * pChild =
+      (CMathNode *) const_cast< CMathNodeDerivative * >(this)->getChild();
+
+    text << mOperation << " ";
+
+    if (pChild)
+      {
+        text << pChild->getText() << "(";
+
+        if (pChild->getSibbling())
+          text << ((CMathNode *) pChild->getSibbling())->getText();
+        else
+          text << "???";
+
+        text << ")";
+      }
+    else
+      text << "???(???)";
+
+    return text.str();
+  }
+
+CMathNodeNumber::CMathNodeNumber(const C_FLOAT64 & number):
+    CMathNode(),
+    mNumber(number)
+{}
+
+CMathNodeNumber::CMathNodeNumber(const CMathNodeNumber &src):
+    CMathNode(src),
+    mNumber(src.mNumber)
+{}
+
+CMathNodeNumber::~CMathNodeNumber() {}
+
+std::string CMathNodeNumber::getText() const
+  {
+    stringstream text;
+    text << mNumber;
+
+    return text.str();
+  }
+
+CMathNodeSymbol::CMathNodeSymbol(const CMathSymbol * pSymbol):
+    CMathNode(),
+    mpSymbol(pSymbol)
+{}
+
+CMathNodeSymbol::CMathNodeSymbol(const CMathNodeSymbol & src):
+    CMathNode(src),
+    mpSymbol(src.mpSymbol)
+{}
+
+CMathNodeSymbol::~CMathNodeSymbol() {}
+
+std::string CMathNodeSymbol::getText() const
+  {
+    if (mpSymbol)
+      return mpSymbol->getName();
+    else
+      return "???";
+  }
+
+CMathNodeFunction::CMathNodeFunction(const CMathSymbol * pSymbol):
+    CMathNodeSymbol(pSymbol)
+{}
+
+CMathNodeFunction::CMathNodeFunction(const CMathNodeFunction & src):
+    CMathNodeSymbol(src)
+{}
+
+CMathNodeFunction::~CMathNodeFunction() {}
+
+std::string CMathNodeFunction::getText() const
+  {
+    stringstream text;
+
+    if (!mpSymbol)
+      text << "???()";
+    else
+      {
+        text << mpSymbol->getName() << "(";
+
+        CMathNode * pChild =
+          (CMathNode *) const_cast< CMathNodeFunction * >(this)->getChild();
+
+        while (pChild)
+          {
+            text << pChild->getText();
+            pChild = (CMathNode *) pChild->getSibbling();
+          }
+
+        text << ")";
+      }
+
+    return text.str();
+  }
---
+++ copasi/mathmodel/CMathNode.h
@@ -0,0 +1,113 @@
+/**
+ * CMathNode class.
+ * The class CMathNode is describes a node of the equation tree.
+ *
+ * Created for Copasi by Stefan Hoops 2003
+ */
+
+#ifndef COPASI_CMathNode
+#define COPASI_CMathNode
+
+#include <string>
+#include "utilities/CCopasiNode.h"
+
+class CMathSymbol;
+
+/** @dia:pos -23.1584,-36.8607 */
+class CMathNode: public CCopasiNode
+  {
+    // Operations:
+  public:
+    CMathNode();
+
+    CMathNode(const CMathNode &src);
+
+    virtual ~CMathNode();
+
+    virtual std::string getText() const;
+  };
+
+class CMathNodeOperation: public CMathNode
+  {
+    // Attributes
+  protected:
+    static const std::string Operations;
+
+    std::string mOperation;
+
+    // Operations
+  protected:
+    CMathNodeOperation();
+
+  public:
+    CMathNodeOperation(const std::string & operation);
+
+    CMathNodeOperation(const CMathNodeOperation &src);
+
+    virtual ~CMathNodeOperation();
+
+    virtual std::string getText() const;
+  };
+
+class CMathNodeDerivative: public CMathNodeOperation
+  {
+    // Operations
+  public:
+    CMathNodeDerivative(const std::string & operation = "d/d");
+
+    CMathNodeDerivative(const CMathNodeDerivative &src);
+
+    virtual ~CMathNodeDerivative();
+
+    virtual std::string getText() const;
+  };
+
+class CMathNodeNumber: public CMathNode
+  {
+    // Attributes
+  private:
+    C_FLOAT64 mNumber;
+
+    // Operations
+
+  public:
+    CMathNodeNumber(const C_FLOAT64 & number = 0.0);
+
+    CMathNodeNumber(const CMathNodeNumber &src);
+
+    virtual ~CMathNodeNumber();
+
+    virtual std::string getText() const;
+  };
+
+class CMathNodeSymbol: public CMathNode
+  {
+    // Attributes
+  protected:
+    const CMathSymbol * mpSymbol;
+
+    // Operations
+  public:
+    CMathNodeSymbol(const CMathSymbol * pSymbol = NULL);
+
+    CMathNodeSymbol(const CMathNodeSymbol & src);
+
+    virtual ~CMathNodeSymbol();
+
+    virtual std::string getText() const;
+  };
+
+class CMathNodeFunction: public CMathNodeSymbol
+  {
+    // Operations
+  public:
+    CMathNodeFunction(const CMathSymbol * pSymbol = NULL);
+
+    CMathNodeFunction(const CMathNodeFunction & src);
+
+    virtual ~CMathNodeFunction();
+
+    virtual std::string getText() const;
+  };
+
+#endif // COPASI_CMathNode
