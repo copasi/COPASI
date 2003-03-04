b99f39f0a0749b64e4fe07b9a51ee99103e04b4c
Stefan Hoops <shoops@vbi.vt.edu>
2003-03-04 13:49:21 +0000

    Removed CCopasiNode.cpp since the class CCopasiNode is now an abstract template class.

--- copasi/utilities/CCopasiNode.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * CCopasiNode class.
- * The class CCopasiNode is describes a node of the CCopasiTree.
- *
- * Created for Copasi by Stefan Hoops 2003
- */
-
-#include "copasi.h"
-#include "CCopasiNode.h"
