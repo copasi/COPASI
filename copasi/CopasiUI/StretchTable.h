2e4c47637ea080372b18072f5499470a5f2a77cc
Liang Xu <lixu1@vt.edu>
2003-03-10 06:32:31 +0000

    Create stretch table

---
+++ copasi/CopasiUI/StretchTable.h
@@ -0,0 +1,24 @@
+#ifndef STRETCH_TABLE_H
+#define STRETCH_TABLE_H
+
+#include <qtable.h>
+
+class StretchTable: public QTable
+  {
+  public:
+    StretchTable (QWidget * parent = 0, const char * name = 0);
+    void columnWidthChanged (int col);
+    void setNumCols (int r);
+
+    int numColumn;
+    int* minColWidth;
+    void DisableColWidthUpdate();
+    void EnableColWidthUpdate();
+  private:
+    bool systemUpdate;
+
+  private:
+    bool* binitialized;
+  };
+
+#endif
---
+++ copasi/UI/StretchTable.cpp
@@ -0,0 +1,59 @@
+#include <qmessagebox.h>
+#include "StretchTable.h"
+
+StretchTable::StretchTable (QWidget * parent, const char * name)
+    : QTable (parent, name)
+{
+  numColumn = 0;
+  minColWidth = NULL;
+  binitialized = NULL;
+  systemUpdate = false;
+}
+
+void StretchTable::DisableColWidthUpdate()
+{
+  systemUpdate = true;
+}
+
+void StretchTable::EnableColWidthUpdate()
+{
+  systemUpdate = false;
+}
+
+void StretchTable::columnWidthChanged (int col)
+{
+  QTable::columnWidthChanged(col);
+  if (systemUpdate)
+    return;
+  if (numColumn == 0)
+    return;
+  if (binitialized[col])
+    binitialized[col] = false;
+  else
+    minColWidth[col] = columnWidth(col);
+}
+
+void StretchTable::setNumCols (int r)
+{
+  QTable::setNumCols(r);
+  if (numColumn > 0)
+    {
+      delete[] minColWidth;
+      delete[] binitialized;
+    }
+  numColumn = r;
+  if (numColumn == 0)
+    {
+      minColWidth = NULL;
+      binitialized = NULL;
+      return;
+    }
+  minColWidth = new int[numColumn];
+  binitialized = new bool[numColumn];
+  int i;
+  for (i = 0; i < numColumn; i++)
+    {
+      binitialized[i] = true;
+      minColWidth[i] = 0;
+    }
+}
