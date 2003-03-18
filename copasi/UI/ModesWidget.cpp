d02a89f18ef3b0e030b73c938b67509773c01db2
Liang Xu <lixu1@vt.edu>
2003-03-18 20:48:33 +0000

    Updated the Direct link to widget1

--- copasi/UI/ModesWidget.cpp
+++ copasi/UI/ModesWidget.cpp
@@ -63,12 +63,12 @@ ModesWidget::ModesWidget(QWidget *parent, const char * name, WFlags f)
   table->setFocusPolicy(QWidget::WheelFocus);
 
   // signals and slots connections
-  //connect(table, SIGNAL(doubleClicked(int, int, int, const QPoint &)), this, SLOT(slotTableCurrentChanged(int, int, int, const QPoint &)));
-  //connect(this, SIGNAL(name(QString &)), (ListViews*)parent, SLOT(slotCompartmentTableChanged(QString &)));
+  connect(table, SIGNAL(doubleClicked(int, int, int, const QPoint &)), this, SLOT(slotTableCurrentChanged(int, int, int, const QPoint &)));
+  connect(this, SIGNAL(name(QString &)), (ListViews*)parent, SLOT(slotModesTableChanged(QString &)));
 
   connect(table, SIGNAL(selectionChanged ()), this, SLOT(slotTableSelectionChanged ()));
   connect(btnCalculate, SIGNAL(clicked ()), this, SLOT(slotBtnCalculateClicked()));
-  //connect(table, SIGNAL(valueChanged(int , int)), this, SLOT(tableValueChanged(int, int)));
+  connect(table, SIGNAL(valueChanged(int , int)), this, SLOT(tableValueChanged(int, int)));
 }
 
 void ModesWidget::loadModes(CModel *model)
