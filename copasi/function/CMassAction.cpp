b16f247d31cc6e86fa4f9221e9094f5b921e12cf
Stefan Hoops <shoops@vbi.vt.edu>
2003-03-19 19:39:05 +0000

    Added missing usage information.

--- copasi/function/CMassAction.cpp
+++ copasi/function/CMassAction.cpp
@@ -43,6 +43,7 @@ CMassAction::CMassAction(const TriLogic & reversible,
   getParameters().add("substrate",
                       CFunctionParameter::VFLOAT64,
                       "SUBSTRATE");
+  addUsage("SUBSTRATES", 0, CRange::Infinity);
 
   if (isReversible() == TriTrue)
     {
@@ -55,6 +56,7 @@ CMassAction::CMassAction(const TriLogic & reversible,
       getParameters().add("product",
                           CFunctionParameter::VFLOAT64,
                           "PRODUCT");
+      addUsage("PRODUCTS", 0, CRange::Infinity);
     }
   else
     {
