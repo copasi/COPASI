c0566773d6774534861ae701351a0873e473b122
Liang Xu <lixu1@vt.edu>
2003-04-08 14:37:08 +0000

    Updated version 3, recorded down minimum size,

--- copasi/CopasiUI/copasiWidget.cpp
+++ copasi/CopasiUI/copasiWidget.cpp
@@ -30,38 +30,33 @@ void CopasiWidget::resize (int w, int h)
       int tmpH = realMinHeight;
       int t = sizeHint().width();
       t = sizeHint().height();
-      if (minimumSizeHint().width() > realMinWidth)
-        realMinWidth = minimumSizeHint().width();
+      if (minimumSizeHint().width() + 200 > realMinWidth)
+        realMinWidth = minimumSizeHint().width() + 200;
       if (minimumSizeHint().height() > realMinHeight)
         realMinHeight = minimumSizeHint().height();
       if ((tmpW != realMinWidth) || (tmpH != realMinHeight))
         {
           QWidget* qParent = topLevelWidget();
+          qParent->setMinimumSize(realMinWidth, realMinHeight);
           qParent->resize(realMinWidth, realMinHeight);
         }
-      //;
-      //  if (realMinHeight>Height)
-      //   realMinHeight
+      return;
     }
-  //need to calculate a minimum size for all widget
-  /*
-    if (w < maxMinWidth)
-      w = maxMinWidth;
-    else
-      maxMinWidth = w;
-    if (h < maxMinHeight)
-      h = maxMinHeight;
-    else
-      maxMinHeight = h;
-  */  QWidget::resize(minimumSizeHint().width(),  minimumSizeHint().height());
+  //after initialization
+  //realMinWidth and realMinHeight are set,
+  //   QWidget* qParent=topLevelWidget();
+  //    QWidget::resize(topLevelWidget()->width(), topLevelWidget()->height());
+  QWidget::resize(w, h);
 }
 
 void CopasiWidget::resizeEvent (QResizeEvent * event)
 {
-  //  int i = 0;
-  //  int w = event->size().width();
-  //  int h = event->size().height();
-  resize(0, 0);
-  //  if (w!=1) //if w==1 then means hide,
+  int w = event->size().width();
+  int h = event->size().height();
+  if (w < realMinWidth)
+    w = realMinWidth;
+  if (h < realMinHeight)
+    h = realMinHeight;
+  resize(w, h);
   // QWidget::resizeEvent (event);
 }
