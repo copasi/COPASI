0b1573550fd3c17e153001c25fabff1ac1a6b243
Stefan Hoops <shoops@vbi.vt.edu>
2003-04-09 21:07:06 +0000

    Added documentation.

--- copasi/report/CKeyFactory.h
+++ copasi/report/CKeyFactory.h
@@ -19,14 +19,33 @@ class CKeyFactory
   {
     // Attributes
   protected:
+    /**
+     * The map of currently existing keys and the related objects.
+     */
     static std::map< std::string, CCopasiObject * > mKeyMap;
 
     // Operations
   public:
-    static std::string add(const std::string & prefix, CCopasiObject *);
+    /**
+     * Add an object with a key generated from the given prefix to the key map.
+     * @param const std::string & prefix
+     * @param CCopasiObject * pObject
+     * @return std::string key
+     */
+    static std::string add(const std::string & prefix, CCopasiObject * pObject);
 
+    /**
+     * Remove the key an the related object from the key map.
+     * @param const std::string & key
+     * @return bool success
+     */
     static bool remove(const std::string & key);
 
+    /**
+     * Retreive the object referred bey key from the key map.
+     * @param const std::string & key
+     * @return CCopasiObject * pObject
+     */
     static CCopasiObject * get(const std::string & key);
   };
 
