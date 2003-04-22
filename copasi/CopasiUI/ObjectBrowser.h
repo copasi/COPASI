90cd21ee7fb6022bd1c198c058bc9c51605edbf9
Liang Xu <lixu1@vt.edu>
2003-04-22 18:47:53 +0000

    add icon,

--- copasi/CopasiUI/ObjectBrowser.h
+++ copasi/CopasiUI/ObjectBrowser.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  ** Form interface generated from reading ui file '.\ObjectBrowser.ui'
  **
- ** Created: Mon Apr 21 14:10:30 2003
+ ** Created: Tue Apr 22 13:27:37 2003
  **      by:  The User Interface Compiler (uic)
  **
  ** WARNING! All changes made in this file will be lost!
@@ -14,9 +14,9 @@
 class QVBoxLayout;
 class QHBoxLayout;
 class QGridLayout;
-class QListView;
-class QListViewItem;
 class QPushButton;
+class QListView;
+class ObjectBrowserItem;
 
 class ObjectBrowser : public QWidget
   {
@@ -26,13 +26,18 @@ class ObjectBrowser : public QWidget
     ObjectBrowser(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
     ~ObjectBrowser();
 
-    QPushButton* backButton;
-    QPushButton* nextButton;
     QPushButton* cancelButton;
-    QListView* ObjectList;
+    QPushButton* nextButton;
+    QPushButton* backButton;
+    QListView* ObjectListView;
+
+  public slots:
+    virtual void cancelClicked();
+    virtual void listviewChecked(ObjectBrowserItem*);
+    virtual void backClicked();
+    virtual void nextClicked();
 
   protected:
     QGridLayout* ObjectBrowserLayout;
   };
-
 #endif // OBJECTBROWSER_H
