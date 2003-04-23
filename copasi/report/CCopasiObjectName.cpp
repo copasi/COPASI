3007cb5b0daa74ff6dcea74166a012fcc6a6f16b
Stefan Hoops <shoops@vbi.vt.edu>
2003-04-23 16:32:07 +0000

    Fixed findEx bug not working correctly when the firest character is a match.

--- copasi/report/CCopasiObjectName.cpp
+++ copasi/report/CCopasiObjectName.cpp
@@ -28,16 +28,16 @@ CCopasiObjectName::CCopasiObjectName(const CCopasiObjectName & src):
 CCopasiObjectName::~CCopasiObjectName() {}
 
 CCopasiObjectName CCopasiObjectName::getPrimary() const
-{return substr(0, findEx(","));}
+  {return substr(0, findEx(","));}
 
 CCopasiObjectName CCopasiObjectName::getRemainder() const
-{
-  std::string::size_type pos = findEx(",");
+  {
+    std::string::size_type pos = findEx(",");
 
-  if (pos == std::string::npos) return CCopasiObjectName();
+    if (pos == std::string::npos) return CCopasiObjectName();
 
-  return substr(pos + 1);
-}
+    return substr(pos + 1);
+  }
 
 std::string CCopasiObjectName::getObjectType() const
   {
@@ -46,7 +46,7 @@ std::string CCopasiObjectName::getObjectType() const
     return CCopasiObjectName::unescape(Primary.substr(0, Primary.findEx("=")));
   }
 
-  std::string CCopasiObjectName::getObjectName() const
+std::string CCopasiObjectName::getObjectName() const
   {
     CCopasiObjectName Primary = getPrimary();
     std::string::size_type pos = findEx("=");
@@ -57,55 +57,55 @@ std::string CCopasiObjectName::getObjectType() const
     return CCopasiObjectName::unescape(tmp.substr(0, tmp.findEx("[")));
   }
 
-  unsigned C_INT32
-  CCopasiObjectName::getIndex(const unsigned C_INT32 & pos) const
-    {
-      std::string Index = getName(pos);
-      std::stringstream tmp(Index);
+unsigned C_INT32
+CCopasiObjectName::getIndex(const unsigned C_INT32 & pos) const
+  {
+    std::string Index = getName(pos);
+    std::stringstream tmp(Index);
 
-      unsigned C_INT32 index = C_INVALID_INDEX;
+    unsigned C_INT32 index = C_INVALID_INDEX;
 
-      tmp >> index;
-      if (tmp.fail()) return C_INVALID_INDEX;
+    tmp >> index;
+    if (tmp.fail()) return C_INVALID_INDEX;
 
-      tmp << index;
-      if (Index != tmp.str()) return C_INVALID_INDEX;
+    tmp << index;
+    if (Index != tmp.str()) return C_INVALID_INDEX;
 
-        return index;
-      }
+    return index;
+  }
 
-      std::string CCopasiObjectName::getName(const unsigned C_INT32 & pos) const
-        {
-          CCopasiObjectName Primary = getPrimary();
+std::string CCopasiObjectName::getName(const unsigned C_INT32 & pos) const
+  {
+    CCopasiObjectName Primary = getPrimary();
 
-          std::string::size_type open = findEx("[");
-          for (unsigned C_INT32 i = 0; i < pos && open != std::string::npos; i++)
-          open = findEx("[", open + 1);
+    std::string::size_type open = findEx("[");
+    for (unsigned C_INT32 i = 0; i < pos && open != std::string::npos; i++)
+      open = findEx("[", open + 1);
 
-          std::string::size_type close = findEx("]", open + 1);
+    std::string::size_type close = findEx("]", open + 1);
 
-          if (open == std::string::npos || close == std::string::npos) return "";
+    if (open == std::string::npos || close == std::string::npos) return "";
 
-            return CCopasiObjectName::unescape(Primary.substr(open + 1,
-                                               close - open - 1));
-          }
+    return CCopasiObjectName::unescape(Primary.substr(open + 1,
+                                       close - open - 1));
+  }
 
-          std::string CCopasiObjectName::escape(const std::string & name)
-            {
+std::string CCopasiObjectName::escape(const std::string & name)
+{
 #define toBeEscaped "\\[]=,"
-              std::string Escaped(name);
-              std::string::size_type pos = Escaped.find_first_of(toBeEscaped);
+  std::string Escaped(name);
+  std::string::size_type pos = Escaped.find_first_of(toBeEscaped);
 
-              while (pos != std::string::npos)
-                {
-                  Escaped.insert(pos, "\\");
-                  pos += 2;
-                  pos = Escaped.find_first_of(toBeEscaped, pos);
-                }
+  while (pos != std::string::npos)
+    {
+      Escaped.insert(pos, "\\");
+      pos += 2;
+      pos = Escaped.find_first_of(toBeEscaped, pos);
+    }
 
-              return Escaped;
+  return Escaped;
 #undef toBeEscaped
-            }
+}
 
 std::string CCopasiObjectName::unescape(const std::string & name)
 {
@@ -125,18 +125,19 @@ std::string CCopasiObjectName::unescape(const std::string & name)
 std::string::size_type
 CCopasiObjectName::findEx(const std::string & toFind,
                           const std::string::size_type & pos) const
-{
-  std::string::size_type where = find_first_of(toFind, pos);
-
-  std::string::size_type tmp;
-
-  while (where != std::string::npos)
   {
-    tmp = find_last_not_of("\\", where - 1);
-      if ((where - tmp) % 2) return where;
+    std::string::size_type where = find_first_of(toFind, pos);
 
-      where = find_first_of(toFind, where + 1);
-    }
+    std::string::size_type tmp;
 
-  return where;
-}
+    while (where && where != std::string::npos)
+      {
+        tmp = find_last_not_of("\\", where - 1);
+        if ((where - tmp) % 2)
+          return where;
+
+        where = find_first_of(toFind, where + 1);
+      }
+
+    return where;
+  }
