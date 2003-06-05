7e4c5df565c111a596a0f912ff78c883356ef645
Mudita Singhal <msinghal@vt.edu>
2003-06-05 17:51:26 +0000

    REport definition files

---
+++ copasi/report/CReportDefinition.cpp
@@ -0,0 +1,20 @@
+/**
+ *  CReportDefinition class.
+ *  This class describes the Report Definition
+ *
+ *  Created for Copasi by Mudita Singhal
+ */
+
+#include <string>
+#include "copasi.h"
+#include "model/CModel.h"
+#include "utilities/CGlobals.h"
+#include "utilities/CReadConfig.h"
+#include "utilities/CWriteConfig.h"
+#include "CReportDefinition.h"
+#include "utilities/CMethodParameter.h"
+
+CReportDefinition::CReportDefinition()
+{}
+
+CReportDefinition::~CReportDefinition(){}
---
+++ copasi/report/CReportDefinition.h
@@ -0,0 +1,30 @@
+/**
+ *  CReportDefinition class.
+ *  This class is the header file for the ReportDefinition.
+ *
+ *  Created for Copasi by Mudita Singhal
+ */
+
+#ifndef COPASI_CReportDefinition
+#define COPASI_CReportDefinition
+
+#include <string>
+
+class CReportDefinition
+  {
+    // Operations
+  protected:
+    /**
+     *  Default constructor.
+     */
+    CReportDefinition();
+
+  public:
+
+    /**
+    *  Destructor.
+    */
+    ~CReportDefinition();
+  };
+
+#endif
