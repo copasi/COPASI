0d157844d3933516976df1878e21bc9359cc618a
Wenjun Hu <huwenjun@eml.org>
2003-10-14 15:04:48 +0000

    customised scroll view for the plotting device

---
+++ copasi/plot/crvspecscrlview.h
@@ -0,0 +1,9 @@
+#include <qscrollview.h>
+
+// a customised scrollview...
+
+class CurveSpecScrollView : public QScrollView
+  {
+  public:
+    CurveSpecScrollView(QWidget * parent = 0, const char * name = 0, WFlags f = 0);
+  };
