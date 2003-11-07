77ddd3163ebd0ea425c5846349422ca2968e631b
Stefan Hoops <shoops@vbi.vt.edu>
2003-11-07 17:07:26 +0000

    Added documentation.

--- copasi/CopasiUI/qtUtilities.h
+++ copasi/CopasiUI/qtUtilities.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/qtUtilities.h,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2003/11/07 16:59:55 $
+   $Date: 2003/11/07 17:07:26 $
    End CVS Header */
 
 #ifndef COPASI_qtUtilities
@@ -13,10 +13,25 @@
 
 class CCopasiParameterGroup;
 
+/**
+ * Retrieve a parameter from a parameter group and convert it to a QString.
+ * @param const CCopasiParameterGroup * group
+ * @param const unsigned C_INT32 & index
+ * @param CCopasiParameter::Type * type (default: NULL)
+ * @return QString value
+ */
 QString getParameterValue(const CCopasiParameterGroup * group,
                           const unsigned C_INT32 & index,
                           CCopasiParameter::Type * type = NULL);
-
+/**
+ * Set a parameter of a parameter group from a value given as a QString.
+ * Success = true is returned when the set operation was succesful, i.e.,
+ * value == getParameterValue.
+ * @param CCopasiParameterGroup * group
+ * @param const unsigned C_INT32 & index
+ * @param const QString & value
+ * @return bool success.
+ */
 bool setParameterValue(CCopasiParameterGroup * group,
                        const unsigned C_INT32 & index,
                        const QString & value);
