02282599f2a0aef32684e0e3e2540e270fee21e4
Stefan Hoops <shoops@vbi.vt.edu>
2004-02-18 20:42:04 +0000

    Added header containing the current version of COPASI.

---
+++ copasi/copasiversion.h
@@ -0,0 +1,16 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: shoops $ 
+   $Date: 2004/02/18 20:42:04 $
+   End CVS Header */
+
+#ifndef COPASI_VERSION
+#define COPASI_VERSION
+
+#define COPASI_VERSION_MAJOR 4
+#define COPASI_VERSION_MINOR 0
+#define COPASI_VERSION_BUILD 1
+
+#endif COPASI_VERSION
