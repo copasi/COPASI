187194b2f0094de9aa1e4249ddc8ce396ceb5c72
Stefan Hoops <shoops@vbi.vt.edu>
2004-02-19 03:28:58 +0000

    Fixed warning message regarding extra characters in #endif statement.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.2 $
+   $Revision: 1.3 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/02/19 02:34:17 $
+   $Date: 2004/02/19 03:28:58 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -13,4 +13,4 @@
 #define COPASI_VERSION_MINOR 0
 #define COPASI_VERSION_BUILD 2
 
-#endif COPASI_VERSION
+#endif // COPASI_VERSION
