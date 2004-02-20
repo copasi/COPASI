160887b31b1794d15e14ce09bf11a1fa80b6f74c
Stefan Hoops <shoops@vbi.vt.edu>
2004-02-20 18:15:46 +0000

    Build number increased to 3.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.3 $
+   $Revision: 1.4 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/02/19 03:28:58 $
+   $Date: 2004/02/20 18:15:46 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -11,6 +11,6 @@
 
 #define COPASI_VERSION_MAJOR 4
 #define COPASI_VERSION_MINOR 0
-#define COPASI_VERSION_BUILD 2
+#define COPASI_VERSION_BUILD 3
 
 #endif // COPASI_VERSION
