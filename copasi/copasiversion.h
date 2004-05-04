fa3f1c94652dfc94e99ad11f889b425bad3e5040
Stefan Hoops <shoops@vbi.vt.edu>
2004-05-04 20:52:11 +0000

    New 4.0 Build 5 release.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.5 $
+   $Revision: 1.6 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/03/01 22:14:52 $
+   $Date: 2004/05/04 20:52:11 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -11,6 +11,6 @@
 
 #define COPASI_VERSION_MAJOR 4
 #define COPASI_VERSION_MINOR 0
-#define COPASI_VERSION_BUILD 4
+#define COPASI_VERSION_BUILD 5
 
 #endif // COPASI_VERSION
