f2d1c535395dd5c550dbda9a85841bbe7b2a077e
Stefan Hoops <shoops@vbi.vt.edu>
2004-05-04 20:10:09 +0000

    Fixed compilation errors for release.

--- copasi/utilities/COutputHandler.cpp
+++ copasi/utilities/COutputHandler.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/COutputHandler.cpp,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
-   $Author: ssahle $ 
-   $Date: 2004/04/06 15:01:18 $
+   $Author: shoops $ 
+   $Date: 2004/05/04 20:10:09 $
    End CVS Header */
 
 #include "copasi.h"
@@ -11,18 +11,24 @@
 
 bool COutputHandler::init()
 {
+#ifdef COPASI_DEBUG
   std::cout << "OutputHandler: init" << std::endl;
+#endif // COPASI_DEBUG
   return true;
 }
 
 bool COutputHandler::doOutput()
 {
+#ifdef COPASI_DEBUG
   std::cout << "OutputHandler: do" << std::endl;
+#endif // COPASI_DEBUG
   return true;
 }
 
 bool COutputHandler::finish()
 {
+#ifdef COPASI_DEBUG
   std::cout << "OutputHandler: finish" << std::endl;
+#endif // COPASI_DEBUG
   return true;
 }
