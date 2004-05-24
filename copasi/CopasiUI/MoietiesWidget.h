7d69b9dd1b1def0e52a8fdeb13589420a519efc7
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-05-24 08:17:31 +0000

    These seem to be empty

--- copasi/CopasiUI/MoietiesWidget.h
+++
@@ -1,7 +0,0 @@
-/* Begin CVS Header
-   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/MoietiesWidget.h,v $
-   $Revision: 1.3 $
-   $Name:  $
-   $Author: shoops $ 
-   $Date: 2003/10/16 16:12:54 $
-   End CVS Header */
