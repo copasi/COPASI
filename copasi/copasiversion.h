42c756a0d37a743e5ee99354f71431511114a01c
Stefan Hoops <shoops@vbi.vt.edu>
2004-05-31 04:01:53 +0000

    Increasse Build Level to 7.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.7 $
+   $Revision: 1.8 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/05/07 21:00:09 $
+   $Date: 2004/05/31 04:01:53 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -11,6 +11,6 @@
 
 #define COPASI_VERSION_MAJOR 4
 #define COPASI_VERSION_MINOR 0
-#define COPASI_VERSION_BUILD 6
+#define COPASI_VERSION_BUILD 7
 
 #endif // COPASI_VERSION
