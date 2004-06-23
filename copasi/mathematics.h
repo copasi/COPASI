6c5cea52467f7495fdfc127fd52f7c54b7040559
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-06-23 09:26:56 +0000

    this is just a wrapper for <math.h>, since on windows C_PI and C_E are not declared in math.h

---
+++ copasi/mathematics.h
@@ -0,0 +1,15 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/Attic/mathematics.h,v $
+   $Revision: 1.2 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2004/06/23 09:29:01 $
+   End CVS Header */
+
+#include <math.h>
+#ifndef M_PI
+#define M_PI           3.14159265358979323846  /* pi */
+#endif
+#ifndef M_E
+#define M_E            2.7182818284590452354   /* e */
+#endif
