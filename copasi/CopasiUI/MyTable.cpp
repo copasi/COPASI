83de675dc230f503fbacfda281ae16757e275520
Ralph Gauges <gauges@hs-albsig.de>
2004-08-13 14:58:58 +0000

    Resize events are handled fairly well if the size goes up, unfortunatelly if the widget is made smaller, oldWidth and width are suddenly the same and the columns are scaled by 1 which effectively does nothing.

--- copasi/CopasiUI/MyTable.cpp
+++ copasi/CopasiUI/MyTable.cpp
@@ -1,11 +1,13 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/MyTable.cpp,v $
-   $Revision: 1.17 $
+   $Revision: 1.18 $
    $Name:  $
    $Author: gauges $ 
-   $Date: 2004/08/12 21:02:22 $
+   $Date: 2004/08/13 14:58:58 $
    End CVS Header */
 
+#include <iostream>
+#include <math.h>
 #include "copasi.h"
 #include <qmessagebox.h>
 #include "MyTable.h" 
@@ -40,67 +42,151 @@ void MyTable::keyPressEvent (QKeyEvent * e)
 
 void MyTable::showEvent(QShowEvent* e)
 {
-  /*
-      if (this->firstTime)
-      {
-        this->firstTime = false;
-        unsigned int widthOfColumns=0;
-        unsigned int counter, numCols;
-        numCols = this->numCols();
-        for (counter = 0; counter < numCols; counter++)
-          {
-            this->adjustColumn(counter);
-            widthOfColumns+=this->columnWidth(counter);
-          }
-        if(widthOfColumns<this->width()){
-            double factor=this->width()/widthOfColumns; 
-            for (counter = 0; counter < numCols; counter++)
+  int vertHeaderWidth = 0;
+  if (this->verticalHeader() && this->verticalHeader()->count() > 0)
+    {
+      vertHeaderWidth = this->verticalHeader()->sectionRect(0).width();
+    }
+  int width = this->width() - vertHeaderWidth;
+
+  if (this->firstTime)
+    {
+      this->firstTime = false;
+      int widthOfColumns = 0;
+      unsigned int counter, numCols;
+      numCols = this->numCols();
+      for (counter = 0; counter < numCols; counter++)
+        {
+          this->adjustColumn(counter);
+          this->exactColumnWidth[counter] = (double)this->columnWidth(counter);
+          widthOfColumns += this->columnWidth(counter);
+        }
+      if (widthOfColumns < width)
+        {
+          double factor = ((double)width) / ((double)widthOfColumns);
+          for (counter = 0; counter < numCols; counter++)
             {
-                this->setColumnWidth(counter,(int)((double)this->columnWidth(counter)*factor));
+              this->exactColumnWidth[counter] = (double)this->columnWidth(counter) * factor;
+              this->setColumnWidth(counter, (int)lround(this->exactColumnWidth[counter]));
             }
         }
-      }
-    QTable::showEvent(e);
-    */
+    }
+  QTable::showEvent(e);
 }
 
 void MyTable::resizeEvent(QResizeEvent* e)
 {
-  /*
-      double oldWidth=(double)(e->oldSize().width());
-      double width=(double)(e->size().width());
-      if(width>oldWidth){
-          double factor=width/oldWidth;
-          int counter;
-          for(counter = 0; counter < this->numCols(); counter++){
-              this->setColumnWidth(counter,(int)(this->columnWidth(counter)*factor));
-          }
-      }
-      else{
-          double factor=width/oldWidth;
-          int sizes[this->numCols()];
-          int optSizes[this->numCols()];
-          int counter;
-          int tooSmall=-1;
-          for(counter = 0; counter < this->numCols(); counter++){
-              sizes[counter]=this->columnWidth(counter);
-              int newSize=(int)((double)sizes[counter]*factor);
-              this->adjustColumn(counter);
-              optSizes[counter]=this->columnWidth(counter);
-              if(newSize<optSizes[counter]){
-                  tooSmall=counter;
-                  break;
-              }
-              else{
-                  this->setColumnWidth(counter,newSize);
-              }
-          }
+  double vertHeaderWidth = 0.0;
+  if (this->verticalHeader() && this->verticalHeader()->count() > 0)
+    {
+      vertHeaderWidth = (double)(this->verticalHeader()->sectionRect(0).width());
+    }
+  double oldWidth = (double)(e->oldSize().width() - vertHeaderWidth);
+  double width = (double)(e->size().width()) - vertHeaderWidth;
+  int numColumns = this->numCols();
+  int optSizes[numColumns];
+  int counter;
+  if (this->contentsWidth() > width && oldWidth < width)
+    {
+      QTable::resizeEvent(e);
+    }
+  else
+    {
+      double factor = width / oldWidth;
+      //std::cout << "oldWidth: " << oldWidth << std::endl;
+      //std::cout << "width: " << width << std::endl;
+      //std::cout << "factor: " << factor << std::endl;
+      if (width < oldWidth)
+        {
+          //int tooSmall=-1;
+          for (counter = 0; counter < numColumns; counter++)
+            {
+              optSizes[counter] = this->getOptimalColumnWidth(counter);
+              if ((int)(this->exactColumnWidth[counter]*factor) < optSizes[counter])
+                {
+                  //tooSmall=counter;
+                  //break;
+                  return;
+                }
+            }
+          /*
           if(tooSmall!=-1){
-              for(counter=0; counter<tooSmall+1; counter++){
-                  this->setColumnWidth(counter,sizes[counter]);
-              }
+           return;
           }
-          
-      }
-  */
+          */
+        }
+      this->scaleColumns(factor);
+    }
+}
+
+int MyTable::getOptimalColumnWidth(int index)
+{
+  int counter;
+  int largest = this->horizontalHeader()->sectionSize(index);
+  for (counter = 0; counter < this->numRows(); counter++)
+    {
+      QTableItem* it = this->item(counter, index);
+      if (it)
+        {
+          int width = it->sizeHint().width();
+          if (width > largest)
+            {
+              largest = width;
+            }
+        }
+    }
+  return largest;
+}
+
+void MyTable::scaleColumns(double factor)
+{
+  int counter;
+  for (counter = 0; counter < this->numCols(); counter++)
+    {
+      this->exactColumnWidth[counter] = this->exactColumnWidth[counter] * factor;
+      this->setColumnWidth(counter, (int)lround(this->exactColumnWidth[counter]));
+    }
+}
+
+void MyTable::setNumCols(int count)
+{
+  this->exactColumnWidth.resize(count);
+  QTable::setNumCols(count);
+}
+
+void MyTable::insertColumns(int index, int count)
+{
+  this->exactColumnWidth.resize(this->exactColumnWidth.size() + count);
+  int counter;
+  for (counter = index; counter < index + count; counter++)
+    {
+      this->exactColumnWidth[counter + count] = this->exactColumnWidth[counter];
+    }
+  QTable::insertColumns(index, count);
+}
+
+void MyTable::removeColumn(int col)
+{
+  unsigned int counter;
+  for (counter = col + 1; counter < this->exactColumnWidth.size(); counter++)
+    {
+      this->exactColumnWidth[counter - 1] = this->exactColumnWidth[counter];
+    }
+  this->exactColumnWidth.resize(this->exactColumnWidth.size() - 1);
+  QTable::removeColumn(col);
+}
+
+void MyTable::removeColumns(const QMemArray<int> & cols)
+{
+  unsigned int counter1;
+  for (counter1 = 0; counter1 < cols.size(); counter1++)
+    {
+      unsigned int counter2;
+      for (counter2 = cols[counter1] + 1; counter2 < this->exactColumnWidth.size() - counter1; counter2++)
+        {
+          this->exactColumnWidth[counter2 - 1] = this->exactColumnWidth[counter2];
+        }
+    }
+  this->exactColumnWidth.resize(this->exactColumnWidth.size() - cols.size());
+  QTable::removeColumns(cols);
 }
--- copasi/CopasiUI/MyTable.h
+++ copasi/CopasiUI/MyTable.h
@@ -1,14 +1,15 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/MyTable.h,v $
-   $Revision: 1.15 $
+   $Revision: 1.16 $
    $Name:  $
    $Author: gauges $ 
-   $Date: 2004/08/12 21:02:22 $
+   $Date: 2004/08/13 14:58:58 $
    End CVS Header */
 
 #ifndef MY_TABLE_H
 #define MY_TABLE_H
 
+#include <vector>
 #include "qtable.h"
 #include <qevent.h> 
 //#include "StretchTable.h"
@@ -24,12 +25,19 @@ class MyTable: public QTable
     MyTable (QWidget * parent = 0, const char * name = 0);
     //void mousePressEvent(QMouseEvent *mpe);
     void keyPressEvent (QKeyEvent * e);
+    virtual void insertColumns(int index, int count = 1);
+    virtual void removeColumn(int col);
+    virtual void removeColumns(const QMemArray<int> & cols);
+    virtual void MyTable::setNumCols(int count);
 
   protected:
     virtual void showEvent(QShowEvent* e);
     virtual void MyTable::resizeEvent(QResizeEvent* e);
+    virtual int getOptimalColumnWidth(int index);
+    virtual void scaleColumns(double factor);
 
     bool firstTime;
+    std::vector<double> exactColumnWidth;
 
   signals:
     void delKeyPressed();
