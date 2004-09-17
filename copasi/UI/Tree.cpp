c67dea6326ccd9957cc644d34adcfef054d2c435
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-09-17 14:05:26 +0000

    cleaning up listviews, dataModelUI, Tree

---
+++ copasi/UI/Tree.cpp
@@ -0,0 +1,43 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/Tree.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2004/09/17 14:05:26 $
+   End CVS Header */
+
+#include <assert.h>
+
+#include "Tree.h"
+
+void IndexedTree::add(int parentId, int newId, const QString & name, const std::string & key)
+{
+  IndexedNode & parent = findNodeFromId(parentId);
+  parent.addChild(newId, name, key);
+}
+
+IndexedNode & IndexedTree::findNodeFromId(int id)
+{
+  IndexedNode* tmp = findNodeFromId(root, id);
+  assert(tmp != NULL);
+
+  return *tmp;
+}
+
+IndexedNode * IndexedTree::findNodeFromId(IndexedNode & node, int id)
+{
+  if (node.getId() == id)
+    return &node;
+
+  std::vector< IndexedNode > & children = node.children();
+  std::vector< IndexedNode >::iterator it, itEnd = children.end();
+
+  IndexedNode * tmp;
+  for (it = children.begin(); it != itEnd; ++it)
+    {
+      tmp = findNodeFromId(*it, id);
+      if (tmp) return tmp;
+    }
+
+  return NULL;
+}
