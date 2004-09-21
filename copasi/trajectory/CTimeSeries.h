915c5170bfb0997c4dcfeb5142ba35bff7a42d8c
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-09-21 12:24:07 +0000

    forgot to commit that with the .cpp file

---
+++ copasi/trajectory/CTimeSeries.h
@@ -0,0 +1,15 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/trajectory/CTimeSeries.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2004/09/21 12:24:07 $
+   End CVS Header */
+
+#ifndef TIMESERIES_H
+#define TIMESERIES_H
+
+class CTimeSeries
+  {};
+
+#endif
