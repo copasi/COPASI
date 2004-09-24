b53bc0ea2ec6395715543574b5da53c894caadbb
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-09-24 13:57:30 +0000

    ...

--- copasi/trajectory/CTimeSeries.h
+++ copasi/trajectory/CTimeSeries.h
@@ -1,15 +1,19 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/trajectory/CTimeSeries.h,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: ssahle $ 
-   $Date: 2004/09/21 12:24:07 $
+   $Date: 2004/09/24 13:57:30 $
    End CVS Header */
 
 #ifndef TIMESERIES_H
 #define TIMESERIES_H
 
-class CTimeSeries
-  {};
+#include <vector>
+#include "model/CState.h"
+
+class CTimeSeries : public std::vector<CState>
+  {
+  };
 
 #endif
