16d0596974b21914cbdd5714a27d0d9beccdfc65
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-09-30 09:22:15 +0000

    new widget for displaying time series

---
+++ copasi/UI/CTimeSeriesTable.cpp
@@ -0,0 +1,40 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CTimeSeriesTable.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2004/09/30 09:22:14 $
+   End CVS Header */
+
+#include "CTimeSeriesTable.h"
+
+void CTimeSeriesTable::setTimeSeries(const CTimeSeries & ts)
+{
+  mTS = &ts;
+  setNumRows(mTS->getNumSteps());
+  setNumCols(mTS->getNumVariables());
+
+  QHeader *tableHeader = horizontalHeader();
+  C_INT32 i, imax = mTS->getNumVariables();
+  for (i = 0; i < imax; ++i)
+    tableHeader->setLabel(i, mTS->getTitle(i).c_str());
+
+  //TODO save old current cell and set afterwards
+}
+
+QString CTimeSeriesTable::textForCell(int row, int col)
+{
+  if (row > mTS->getNumSteps()) return "";
+  if (col > mTS->getNumVariables()) return "";
+
+  if (mFlagConc)
+    return QString::number(mTS->getConcentrationData(row, col));
+  else
+    return QString::number(mTS->getData(row, col));
+}
+
+void CTimeSeriesTable::showConcentrations(bool conc)
+{
+  mFlagConc = conc;
+  hide(); show(); //TODO: ugly!
+}
---
+++ copasi/UI/CTimeSeriesTable.h
@@ -0,0 +1,60 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CTimeSeriesTable.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2004/09/30 09:22:15 $
+   End CVS Header */
+
+#ifndef CTSTABLE
+#define CTSTABLE
+
+#include <qtable.h> 
+//#include <iostream>
+
+#include "trajectory/CTimeSeries.h"
+
+class CTimeSeriesTable : public QTable
+  {
+  public:
+
+    CTimeSeriesTable(QWidget* p, const char * name = 0)
+        : QTable(3, 3, p, name),
+        mFlagConc(true)
+    {
+      //setCaption("TimeSeries Table");
+      //setLeftMargin(fontMetrics().width("W999999W"));
+      setLeftMargin(0);
+      mItem = new QTableItem(this, QTableItem::Never);
+    }
+
+    void setTimeSeries(const CTimeSeries & ts);
+    void showConcentrations(bool conc = true);
+
+  protected:
+
+    QString textForCell(int row, int col);
+
+    virtual void paintCell (QPainter * p, int row, int col, const QRect & cr, bool selected)
+    {
+      mItem->setText(textForCell(row, col));
+      QTable::paintCell(p, row, col, cr, selected);
+    }
+
+    //basically do nothing in the following overloaded methods
+    void resizeData(int) {}
+    QTableItem *item(int , int) const {return mItem;}
+    void setItem(int , int , QTableItem *) {}
+    void clearCell(int , int) {}
+    void takeItem(QTableItem *) {}
+    void insertWidget(int , int , QWidget *) {}
+    QWidget *cellWidget(int , int) const {return NULL;}
+    void clearCellWidget(int , int) {}
+
+  private:
+    QTableItem * mItem;
+    const CTimeSeries * mTS;
+    bool mFlagConc;
+  };
+
+#endif
