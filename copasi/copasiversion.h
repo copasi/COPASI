fe9acd3a2bac8f188ac149d1c64fb7ee85b1da6d
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2004-10-09 10:56:51 +0000

    new build

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.9 $
+   $Revision: 1.10 $
    $Name:  $
-   $Author: shoops $ 
-   $Date: 2004/09/30 20:29:39 $
+   $Author: ssahle $ 
+   $Date: 2004/10/09 10:56:51 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -11,6 +11,6 @@
 
 #define COPASI_VERSION_MAJOR 4
 #define COPASI_VERSION_MINOR 0
-#define COPASI_VERSION_BUILD 8
+#define COPASI_VERSION_BUILD 9
 
 #endif // COPASI_VERSION
