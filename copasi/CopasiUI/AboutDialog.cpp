458663bdcc4918289708f2b2062f7bf9c8d7b146
Juergen Pahle <juergen@pahle.de>
2004-10-19 08:30:40 +0000

    added Expat XML parser

--- copasi/CopasiUI/AboutDialog.cpp
+++ copasi/CopasiUI/AboutDialog.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/AboutDialog.cpp,v $
-   $Revision: 1.2 $
+   $Revision: 1.3 $
    $Name:  $
-   $Author: gauges $ 
-   $Date: 2004/10/19 07:14:49 $
+   $Author: jpahle $ 
+   $Date: 2004/10/19 08:30:40 $
    End CVS Header */
 
 #include "AboutDialog.h"
@@ -16,7 +16,7 @@
 #include "copasi.h"
 #include "icons/copasi_beta_background.xpm"
 
-const char* AboutDialog::text = "COPASI is a simulator for biochemical networks. It is a joint project by the Mendes and Kummer groups at VBI and EML Research, respectively.\n\nThis version of COPASI is a test version. Most likely a new one has already been released and you should download it from www.copasi.org.\nPlease report errors to sahle@eml.org .\n\n\n\nCOPASI is provided \"AS IS\" without warranty of any kind, either express or implied, including but not limited to the implied warranties of merchantability and fitness for a particular purpose. In no event shall the Authors be liable for any damages whatsoever including direct, indirect, incidental, consequential, loss of business profits or special damages, even if the Authors have been advised of the possibility of such damages.\n\nThe following software and algorithms are being used by Copasi:\n\n- Qt3 GUI framework\n- QWT 4.2rc2 plotting framework\n- libsbml 2.2.0\n- CLAPACK\n- LSODA from ODEPACK";
+const char* AboutDialog::text = "COPASI is a simulator for biochemical networks. It is a joint project by the Mendes and Kummer groups at VBI and EML Research, respectively.\n\nThis version of COPASI is a test version. Most likely a new one has already been released and you should download it from www.copasi.org.\nPlease report errors to sahle@eml.org .\n\n\n\nCOPASI is provided \"AS IS\" without warranty of any kind, either express or implied, including but not limited to the implied warranties of merchantability and fitness for a particular purpose. In no event shall the Authors be liable for any damages whatsoever including direct, indirect, incidental, consequential, loss of business profits or special damages, even if the Authors have been advised of the possibility of such damages.\n\nThe following software and algorithms are being used by Copasi:\n\n- Qt3 GUI framework\n- QWT 4.2rc2 plotting framework\n- Expat XML parser\n- libsbml 2.2.0\n- CLAPACK\n- LSODA from ODEPACK";
 
 AboutDialog::AboutDialog(QWidget* parent): QDialog(parent), closeButton(NULL), textEdit(NULL), backgroundPixmap(NULL), mainLayout(NULL)
 {
