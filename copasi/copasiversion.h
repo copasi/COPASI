fe8c244b6707d500bb96d7893d397051396efe5b
Ralph Gauges <gauges@hs-albsig.de>
2004-10-22 14:56:20 +0000

    Forgot to change the version number prior to tagging build 10.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-   $Revision: 1.10 $
+   $Revision: 1.11 $
    $Name:  $
-   $Author: ssahle $ 
-   $Date: 2004/10/09 10:56:51 $
+   $Author: gauges $ 
+   $Date: 2004/10/22 14:56:20 $
    End CVS Header */
 
 #ifndef COPASI_VERSION
@@ -11,6 +11,6 @@
 
 #define COPASI_VERSION_MAJOR 4
 #define COPASI_VERSION_MINOR 0
-#define COPASI_VERSION_BUILD 9
+#define COPASI_VERSION_BUILD 10
 
 #endif // COPASI_VERSION
