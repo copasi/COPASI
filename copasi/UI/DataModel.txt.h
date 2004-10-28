35d47297995f39006598225b111eded9cd0ec43f
Ralph Gauges <gauges@hs-albsig.de>
2004-10-28 07:34:27 +0000

    Uncommented the MCA widgets.

--- copasi/UI/DataModel.txt.h
+++ copasi/UI/DataModel.txt.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/DataModel.txt.h,v $
-   $Revision: 1.6 $
+   $Revision: 1.7 $
    $Name:  $
    $Author: gauges $ 
-   $Date: 2004/10/27 07:33:06 $
+   $Date: 2004/10/28 07:34:27 $
    End CVS Header */
 
 static char DataModeltxt[] =
@@ -28,8 +28,8 @@ static char DataModeltxt[] =
   "22:222:Mass Conservation\n"
   "2:23:Time Course\n"
   "23:231:Result\n"
-  //  "2:24:Metabolic Control Analysis\n"
-  //  "24:241:Result\n"
+  "2:24:Metabolic Control Analysis\n"
+  "24:241:Result\n"
   "0:4:Output\n"
   "4:42:Plots\n"
   "4:43:Reports\n"
