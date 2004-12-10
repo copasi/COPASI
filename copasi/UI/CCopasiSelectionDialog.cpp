ad631abc0253830e91e6bd54c54815f82d21517c
Ralph Gauges <gauges@hs-albsig.de>
2004-12-10 12:36:34 +0000

    Replacement for SimpleSelectionDialog. This class uses the new CCopasiSelectionWidget which makes a lot of things easier, more consistent and hopefully reusable.

---
+++ copasi/UI/CCopasiSelectionDialog.cpp
@@ -0,0 +1,85 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CCopasiSelectionDialog.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: gauges $ 
+   $Date: 2004/12/10 12:36:34 $
+   End CVS Header */
+
+#include "CCopasiSelectionDialog.h"
+#include "qpushbutton.h"
+#include "qcheckbox.h"
+#include "qhbox.h"
+#include "qvbox.h"
+#include "qlayout.h"
+#include "CCopasiSelectionWidget.h"
+
+CCopasiSelectionDialog::CCopasiSelectionDialog(QWidget * parent , const char * name , bool modal): QDialog(parent, name, modal), mpOKButton(NULL), mpCancelButton(NULL), mpModeCheckBox(NULL), mpButtonBox(NULL), mpMainWidget(NULL), mpMainLayout(NULL), mpTmpVector(new std::vector<CCopasiObject*>()), mpOutputVector(NULL), mExpertMode(false)
+{
+  this->setWFlags(this->getWFlags() | Qt::WDestructiveClose);
+  this->mpMainLayout = new QVBoxLayout(this);
+  this->mpSelectionWidget = new CCopasiSelectionWidget(this);
+  this->mpButtonBox = new QHBox(this);
+  ((QBoxLayout*)this->mpButtonBox->layout())->addStretch();
+  this->mpOKButton = new QPushButton(this->mpButtonBox, "OK");
+  this->mpOKButton->setText("OK");
+  this->mpOKButton->setDefault(true);
+  this->mpOKButton->setAutoDefault(true);
+  this->mpCancelButton = new QPushButton(this->mpButtonBox, "Cancel");
+  this->mpCancelButton->setText("Cancel");
+  this->mpModeCheckBox = new QCheckBox("expert mode", this->mpButtonBox, "expertMode");
+  this->mpModeCheckBox->setChecked(false);
+  //((QBoxLayout*)this->mpButtonBox->layout())->addStretch();
+
+  connect(this->mpOKButton, SIGNAL(clicked()), this, SLOT(okButton_clicked()));
+  connect(this->mpCancelButton, SIGNAL(clicked()), this, SLOT(cancelButton_clicked()));
+  connect(this->mpModeCheckBox, SIGNAL(toggled(bool)), this, SLOT(modeButton_toggld(bool)));
+
+  this->mpSelectionWidget->setOutputVector(NULL);
+
+  this->setTabOrder(this->mpOKButton, this->mpCancelButton);
+  this->setTabOrder(this->mpCancelButton, this->mpModeCheckBox);
+  this->setTabOrder(this->mpModeCheckBox, this->mpMainWidget);
+}
+
+CCopasiSelectionDialog::~CCopasiSelectionDialog()
+{
+  delete this->mpOKButton;
+  delete this->mpCancelButton;
+  delete this->mpModeCheckBox;
+  delete this->mpButtonBox;
+  delete this->mpMainLayout;
+  delete this->mpSelectionWidget;
+}
+
+void CCopasiSelectionDialog::setModel(CModel* model)
+{
+  this->mpSelectionWidget->populateTree(model);
+}
+
+void CCopasiSelectionDialog::setOutputVector(std::vector<CCopasiObject*>* outputVector)
+{
+  this->mpSelectionWidget->setOutputVector(outputVector);
+}
+
+void CCopasiSelectionDialog::okButton_clicked()
+{
+  // fill outputVector
+  this->mpSelectionWidget->commit();
+  done(QDialog::Accepted);
+}
+
+void CCopasiSelectionDialog::cancelButton_clicked()
+{
+  done(QDialog::Rejected);
+}
+
+void CCopasiSelectionDialog::modeButton_toggled(bool checked)
+{
+  this->mpSelectionWidget->setExpertMode(checked);
+}
+
+void CCopasiSelectionDialog::setSingleSelection(bool singleSelectionMode)
+{
+  this->mpSelectionWidget->setSingleSelection(singleSelectionMode);
+}
---
+++ copasi/UI/CCopasiSelectionDialog.h
@@ -0,0 +1,53 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CCopasiSelectionDialog.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: gauges $ 
+   $Date: 2004/12/10 12:36:34 $
+   End CVS Header */
+
+#ifndef CCopasiSelectionDialog_H__
+#define CCopasiSelectionDialog_H__
+
+#include "qdialog.h"
+#include <vector>
+
+class QPushButton;
+class QCheckBox;
+class CCopasiSelectionWidget;
+class QHBox;
+class QVBoxLayout;
+class QHBoxLayout;
+class CCopasiObject;
+class CModel;
+
+class CCopasiSelectionDialog: public QDialog
+  {
+    Q_OBJECT
+
+  protected:
+    QPushButton* mpOKButton;
+    QPushButton* mpCancelButton;
+    QCheckBox* mpModeCheckBox;
+    QHBox* mpButtonBox;
+    QWidget* mpMainWidget;
+    CCopasiSelectionWidget* mpSelectionWidget;
+    QVBoxLayout* mpMainLayout;
+    std::vector<CCopasiObject*>* mpTmpVector;
+    std::vector<CCopasiObject*>* mpOutputVector;
+    bool mExpertMode;
+
+  protected slots:
+    void okButton_clicked();
+    void cancelButton_clicked();
+    void modeButton_toggled(bool checked);
+
+  public:
+    CCopasiSelectionDialog(QWidget * parent = 0, const char * name = 0, bool modal = FALSE);
+    virtual ~CCopasiSelectionDialog();
+    void setOutputVector(std::vector<CCopasiObject*>* outputVector);
+    void setModel(CModel* model);
+    void setSingleSelection(bool singleSelectionMode);
+  };
+
+#endif /* SimpleSelectionDialog_H__ */
