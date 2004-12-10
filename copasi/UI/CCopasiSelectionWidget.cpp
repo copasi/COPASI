a797e728b691919868be97c044c7c13dbb578257
Ralph Gauges <gauges@hs-albsig.de>
2004-12-10 11:21:53 +0000

    A widget based on QWidgetStack that can switch between a simple selection tree and a object browser. It also lets you set wether it works as a single or a multi selection dialog.

---
+++ copasi/UI/CCopasiSelectionWidget.cpp
@@ -0,0 +1,94 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CCopasiSelectionWidget.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: gauges $ 
+   $Date: 2004/12/10 11:21:53 $
+   End CVS Header */
+
+#include "CCopasiSelectionWidget.h"
+#include "ObjectBrowserWidget.h"
+#include "CCopasiSimpleSelectionTree.h"
+
+#include "copasi.h"
+
+CCopasiSelectionWidget::CCopasiSelectionWidget(QWidget* parent , const char* name , WFlags fl): QWidgetStack(parent, name, fl), mpSimpleTree(NULL), mpObjectBrowser(NULL), mpOutputVector(NULL)
+{
+  this->mpSimpleTree = new CCopasiSimpleSelectionTree(this);
+  this->addWidget(this->mpSimpleTree);
+  this->setSingleSelection(false);
+  this->setExpertMode(false);
+}
+
+CCopasiSelectionWidget::~CCopasiSelectionWidget()
+{
+  pdelete(mpObjectBrowser);
+  delete mpSimpleTree;
+}
+
+void CCopasiSelectionWidget::populateTree(CModel * model)
+{
+  this->mpSimpleTree->populateTree(model);
+}
+
+void CCopasiSelectionWidget::setOutputVector(std::vector<CCopasiObject *> * outputVector)
+{
+  this->mpOutputVector = outputVector;
+}
+
+void CCopasiSelectionWidget::setSingleSelection(bool singleSelection)
+{
+  if (this->mSingleSelect == singleSelection) return;
+  this->mSingleSelect = singleSelection;
+  this->mpSimpleTree->clearSelection();
+  this->mpSimpleTree->setSelectionMode(QListView::Single);
+  if (this->mpObjectBrowser)
+    {
+      /* this needs to be implemented first !
+         
+      this->mpObjectBrowser->setSingleSelection(true);
+
+      */
+    }
+}
+
+bool CCopasiSelectionWidget::isSingleSelection() const
+  {
+    return this->mSingleSelect;
+  }
+
+void CCopasiSelectionWidget::setExpertMode(bool expertMode)
+{
+  if (this->mExpertMode == expertMode) return;
+  this->mExpertMode = expertMode;
+  if (this->mExpertMode)
+    {
+      if (!this->mpObjectBrowser)
+        {
+          // create the object browser only if needed
+          // this saves some time if the model is large
+          this->mpObjectBrowser = new ObjectBrowserWidget(this);
+          this->addWidget(this->mpObjectBrowser);
+        }
+      this->mpSimpleTree->commitClicked();
+      this->mpSimpleTree->setOutputVector(NULL);
+      this->mpObjectBrowser->setOutputVector(this->mpOutputVector);
+      this->raiseWidget(this->mpObjectBrowser);
+    }
+  else
+    {
+      // activate the SimpleTree and get the selection from the ObjectBrowser if there is one
+      if (this->mpObjectBrowser)
+        {
+          this->mpObjectBrowser->commitClicked();
+          this->mpObjectBrowser->setOutputVector(NULL);
+        }
+      this->mpSimpleTree->setOutputVector(this->mpOutputVector);
+      this->raiseWidget(this->mpSimpleTree);
+    }
+}
+
+bool CCopasiSelectionWidget::expertMode() const
+  {
+    return this->mExpertMode;
+  }
---
+++ copasi/UI/CCopasiSelectionWidget.h
@@ -0,0 +1,44 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CCopasiSelectionWidget.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: gauges $ 
+   $Date: 2004/12/10 11:21:53 $
+   End CVS Header */
+
+#ifndef CCopasiSelectionWidget_H__
+#define CCopasiSelectionWidget_H__
+
+#include "qwidgetstack.h"
+#include <vector>
+
+class ObjectBrowserWidget;
+class CCopasiSimpleSelectionTree;
+class CModel;
+class QWidget;
+class CCopasiObject;
+
+class CCopasiSelectionWidget: public QWidgetStack
+  {
+    Q_OBJECT
+
+  public:
+    CCopasiSelectionWidget(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
+    ~CCopasiSelectionWidget();
+
+    virtual void populateTree(CModel * model);
+    virtual void setOutputVector(std::vector<CCopasiObject *> * outputVector);
+    virtual void setSingleSelection(bool singleSelection);
+    virtual bool isSingleSelection() const;
+    virtual void setExpertMode(bool expertMode);
+    virtual bool expertMode() const;
+
+  protected:
+    CCopasiSimpleSelectionTree* mpSimpleTree;
+    ObjectBrowserWidget* mpObjectBrowser;
+    std::vector<CCopasiObject*>* mpOutputVector;
+    bool mSingleSelect;
+    bool mExpertMode;
+  };
+
+#endif
