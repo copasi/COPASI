ebd5af426309e65cbe9efdf24e754887973d2b41
Stefan Hoops <shoops@vbi.vt.edu>
2005-01-24 18:39:27 +0000

    Added class CCopasiTimeVariable.

--- copasi/utilities/CopasiTime.cpp
+++ copasi/utilities/CopasiTime.cpp
@@ -1,12 +1,15 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CopasiTime.cpp,v $
-   $Revision: 1.3 $
+   $Revision: 1.4 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/09/15 20:49:35 $
+   $Date: 2005/01/24 18:39:26 $
    End CVS Header */
 
+#include <sstream>
+
 #include "CopasiTime.h"
+#include "utility.h"
 
 CopasiTimePoint::CopasiTimePoint():
     mTime(getCurrentTime_msec())
@@ -21,8 +24,7 @@ C_INT32 CopasiTimePoint::get() const
 C_INT32 CopasiTimePoint::getTimeDiff() const
   {
     C_INT32 diff = getCurrentTime_msec() - mTime;
-    if (diff >= 0) return diff;
-    else return diff + (1 << 30);
+    return diff & 0x7fffffff;
   }
 
 //the following is linux (posix? unix?) specific and might not work under windows
@@ -38,13 +40,13 @@ C_INT32 CopasiTimePoint::getTimeDiff() const
 #include <sys/time.h>
 
 //static
-C_INT32 CopasiTimePoint::getCurrentTime_msec()
+C_INT64 CopasiTimePoint::getCurrentTime_msec()
 {
   timeval ttt;
   gettimeofday(&ttt, 0);
   long long int time;
   time = ttt.tv_sec * 1000 + ttt.tv_usec / 1000;
-  return time % (1 << 30);
+  return time & 0x7fffffffffffffff;
 }
 #else
 
@@ -52,11 +54,174 @@ C_INT32 CopasiTimePoint::getCurrentTime_msec()
 #include <winbase.h>
 
 //static
-C_INT32 CopasiTimePoint::getCurrentTime_msec()
+C_INT64 CopasiTimePoint::getCurrentTime_msec()
 {
   LARGE_INTEGER SystemTime;
   GetSystemTimeAsFileTime((FILETIME *) &SystemTime);
 
-  return (SystemTime.QuadPart / 10000) & 0x3fffffff;
+  return (SystemTime.QuadPart / 10000) & MAXLONGLONG;
 }
 #endif
+
+CCopasiTimeVariable::CCopasiTimeVariable():
+    mTime(0)
+{}
+
+CCopasiTimeVariable::CCopasiTimeVariable(const CCopasiTimeVariable & src):
+    mTime(src.mTime)
+{}
+
+CCopasiTimeVariable::CCopasiTimeVariable(const C_INT64 & value):
+    mTime(value)
+{}
+
+CCopasiTimeVariable::~CCopasiTimeVariable() {}
+
+CCopasiTimeVariable CCopasiTimeVariable::operator + (const CCopasiTimeVariable & value)
+{return mTime + value.mTime;}
+
+CCopasiTimeVariable CCopasiTimeVariable::operator - (const CCopasiTimeVariable & value)
+{return mTime - value.mTime;}
+
+CCopasiTimeVariable & CCopasiTimeVariable::operator = (const CCopasiTimeVariable & rhs)
+{
+  mTime = rhs.mTime;
+  return *this;
+}
+
+CCopasiTimeVariable & CCopasiTimeVariable::operator = (const C_INT64 & value)
+{
+  mTime = value;
+  return *this;
+}
+
+std::string CCopasiTimeVariable::isoFormat() const
+  {
+    std::stringstream Iso;
+    bool first = true;
+
+    if (mTime < 0)
+      {
+        CCopasiTimeVariable Tmp(-mTime);
+        Iso << "-";
+        Iso << Tmp.isoFormat();
+
+        return Iso.str();
+      }
+
+    if (mTime >= 86400000000)
+      {
+        Iso << LL2String(getDays()) << ":";
+        first = false;
+      }
+
+    if (mTime >= 3600000000)
+      Iso << LL2String(getHours(true), first ? 0 : 2) << ":";
+    if (mTime >= 60000000)
+      Iso << LL2String(getMinutes(true), first ? 0 : 2) << ":";
+    if (mTime >= 1000000)
+      Iso << LL2String(getSeconds(true), first ? 0 : 2) << ".";
+    else
+      Iso << "0.";
+
+    Iso << LL2String(getMilliSeconds(true), 3) << LL2String(getMicroSeconds(true), 3);
+
+    return Iso.str();
+  }
+
+C_INT64 CCopasiTimeVariable::getMicroSeconds(const bool & bounded) const
+  {
+    if (bounded) return mTime % 1000;
+    else return mTime;
+  }
+
+C_INT64 CCopasiTimeVariable::getMilliSeconds(const bool & bounded) const
+  {
+    C_INT64 MilliSeconds =
+      (mTime - (mTime % 1000)) / 1000;
+
+    if (bounded) return MilliSeconds % 1000;
+    else return MilliSeconds;
+  }
+
+C_INT64 CCopasiTimeVariable::getSeconds(const bool & bounded) const
+  {
+    C_INT64 Seconds =
+      (mTime - (mTime % 1000000)) / 1000000;
+
+    if (bounded) return Seconds % 60;
+    else return Seconds;
+  }
+
+C_INT64 CCopasiTimeVariable::getMinutes(const bool & bounded) const
+  {
+    C_INT64 Minutes =
+      (mTime - (mTime % 60000000)) / 60000000;
+
+    if (bounded) return Minutes % 60;
+    else return Minutes;
+  }
+
+C_INT64 CCopasiTimeVariable::getHours(const bool & bounded) const
+  {
+    C_INT64 Hours =
+      (mTime - (mTime % 3600000000)) / 3600000000;
+
+    if (bounded) return Hours % 24;
+    else return Hours;
+  }
+
+C_INT64 CCopasiTimeVariable::getDays() const
+  {
+    C_INT64 Days =
+      (mTime - (mTime % 86400000000)) / 86400000000;
+
+    return Days;
+  }
+
+#ifndef WIN32
+
+#include <sys/time.h>
+
+//static
+CCopasiTimeVariable CCopasiTimeVariable::getCurrentWallTime()
+{
+  timeval ttt;
+  gettimeofday(&ttt, 0);
+  C_INT64 time;
+  time = ((C_INT64) ttt.tv_sec) * 1000000 + (C_INT64) ttt.tv_usec;
+  return time;
+}
+#else
+
+#include <windows.h>
+#include <winbase.h>
+
+//static
+CCopasiTimeVariable CCopasiTimeVariable::getCurrentWallTime()
+{
+  LARGE_INTEGER SystemTime;
+  GetSystemTimeAsFileTime((FILETIME *) &SystemTime);
+
+  return SystemTime.QuadPart / 10;
+}
+#endif
+
+std::string CCopasiTimeVariable::LL2String(const C_INT64 & value,
+    const C_INT32 & digits)
+{
+  std::string format;
+
+  if (digits > 0)
+    format = "%0" + StringPrint("d", digits);
+  else
+    format = "%";
+
+#ifdef WIN32
+  format += "I64d";
+#else
+  format += "lld";
+#endif
+
+  return StringPrint(format.c_str(), value);
+}
--- copasi/utilities/CopasiTime.h
+++ copasi/utilities/CopasiTime.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CopasiTime.h,v $
-   $Revision: 1.2 $
+   $Revision: 1.3 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2004/09/15 20:47:52 $
+   $Date: 2005/01/24 18:39:27 $
    End CVS Header */
 
 #ifndef COPASI_time
@@ -23,9 +23,52 @@ class CopasiTimePoint
     C_INT32 getTimeDiff() const;
 
   private:
-    C_INT32 mTime;
+    C_INT64 mTime;
 
-    static C_INT32 getCurrentTime_msec();
+    static C_INT64 getCurrentTime_msec();
+  };
+
+class CCopasiTimeVariable
+  {
+  public:
+    CCopasiTimeVariable();
+
+    CCopasiTimeVariable(const CCopasiTimeVariable & src);
+
+    CCopasiTimeVariable(const C_INT64 & value);
+
+    ~CCopasiTimeVariable();
+
+    CCopasiTimeVariable operator + (const CCopasiTimeVariable & value);
+
+    CCopasiTimeVariable operator - (const CCopasiTimeVariable & value);
+
+    CCopasiTimeVariable & operator = (const CCopasiTimeVariable & rhs);
+
+    CCopasiTimeVariable & operator = (const C_INT64 & value);
+
+    std::string isoFormat() const;
+
+    C_INT64 getMicroSeconds(const bool & bounded = false) const;
+
+    C_INT64 getMilliSeconds(const bool & bounded = false) const;
+
+    C_INT64 getSeconds(const bool & bounded = false) const;
+
+    C_INT64 getMinutes(const bool & bounded = false) const;
+
+    C_INT64 getHours(const bool & bounded = false) const;
+
+    C_INT64 getDays() const;
+
+    static CCopasiTimeVariable getCurrentWallTime();
+
+  private:
+    static std::string LL2String(const C_INT64 & value,
+                                 const C_INT32 & digits = 0);
+
+  private:
+    C_INT64 mTime;
   };
 
 #endif
