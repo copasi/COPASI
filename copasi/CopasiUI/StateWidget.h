e5eb5774cc0d57948a783bb01ccf3a69ba5f9a7e
Sameer Tupe <stupe@vbi.vt.edu>
2005-01-25 21:51:29 +0000

    Added Functionality "SetAsInitialState" in SteadyState analysis results screen.

--- copasi/CopasiUI/StateWidget.h
+++ copasi/CopasiUI/StateWidget.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/StateWidget.h,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
-   $Author: ssahle $ 
-   $Date: 2004/09/30 09:13:51 $
+   $Author: stupe $ 
+   $Date: 2005/01/25 21:51:29 $
    End CVS Header */
 
 #ifndef STATEWIDGET_H
@@ -14,21 +14,27 @@
 class QVBoxLayout;
 class QHBoxLayout;
 class QGridLayout;
-//class QPushButton;
-
 class StateSubwidget;
+class QPushButton;
+class QHBoxLayout;
+class QVBoxLayout;
 
 class StateWidget : public CopasiWidget
   {
     Q_OBJECT
 
   public:
+    QPushButton* setInitialState;
+
     StateWidget(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
     ~StateWidget();
 
     virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
     virtual bool leave();
     virtual bool enter(const std::string & key = "");
+
+  public slots:
+    virtual void runSetInitialState();
 
   protected slots:
     //virtual void slotBtnCancelClicked();
@@ -40,7 +46,8 @@ class StateWidget : public CopasiWidget
     std::string objKey;
 
     QGridLayout* mWidgetLayout;
-
+    QHBoxLayout* stateLayout;
+    QVBoxLayout* parentLayout;
     StateSubwidget* mCentralWidget;
   };
 
