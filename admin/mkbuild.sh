3b5a855f88fd1866151d809be21cf1d5665626e8
Stefan Hoops <shoops@vbi.vt.edu>
2005-01-31 17:46:50 +0000

    Added Darwin to supported package platforms.

--- admin/mkbuild.sh
+++ admin/mkbuild.sh
@@ -10,7 +10,81 @@ if [ x"$#" = x1 ]; then
     ;;
 
   xDarwin)
-    tar -czf CopasiUI-$build-$1.tar.gz CopasiUI.app
+    if otool -L ./copasi/CopasiUI/CopasiUI.app/Contents/MacOS/CopasiUI \
+      | grep libqt 2>&1 > /dev/null ;then
+        echo ""
+        echo "CopasiUI is dynamicall linked against Qt."
+        echo "Please link the binary statically before making an image."
+        echo ""
+        exit 1
+    fi
+    
+    SETFILE=/Developer/Tools/SetFile
+
+# Create temporary package image drive
+    hdiutil create Copasi-tmp.dmg -megabytes 200 -volname Copasi-$build-$1 -fs HFS+ \
+      -layout NONE
+
+# Mount temporary package image drive
+    drive=`hdid Copasi-tmp.dmg | gawk -- '{print $1}'`
+    TMPDIR=`df -l | gawk -- '$1 ~ "'$drive'" {print $6}'`
+    echo $drive '->' $TMPDIR
+
+    echo "Copying application bundle."
+    mkdir ${TMPDIR}/copasi
+    cp -r copasi/CopasiUI/CopasiUI.app ${TMPDIR}/copasi/
+
+    strip ${TMPDIR}/copasi/CopasiUI.app/Contents/MacOS/CopasiUI
+
+# copy the icon into the Resources directory
+    echo "Creating Resources directory."
+    mkdir -p ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources
+    echo "Copy the icon file to the bundle."
+    cp copasi.icns ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/
+
+# set the icon in the Info.plist file in the Contents directory
+# the icon file name is located between <string></string> tags in the line after
+# <key>CFBundleIconFile</key>
+# Maybe this would better be done with a dom parser.
+echo "Set the icon in the Info.plist file."
+    LINE=`grep -n "CFBundleIconFile" ${TMPDIR}/copasi/CopasiUI.app/Contents/Info.plist \
+      | cut -c1`
+    LINE=$((LINE+1))
+    sed -e "${LINE}s%<string>.*</string>%<string>copasi.icns</string>%" \
+      ${TMPDIR}/copasi/CopasiUI.app/Contents/Info.plist > ${TMPDIR}/tmp.plist
+    mv ${TMPDIR}/tmp.plist ${TMPDIR}/copasi/CopasiUI.app/Contents/Info.plist
+
+# copy the examples into the Resources directory
+    echo "Make example directory."
+    mkdir -p ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/examples
+    echo "Copy examples to example directory."
+    for EXAMPLE in $EXAMPLES;do
+        cp ${EXAMPLE} ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/examples/
+    done
+
+# copy the files for the wizard into the Resources directory
+    echo "Making directory for wizard."
+    mkdir -p ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/doc/html
+    echo "Copying files for wizard."
+    cp ./copasi/wizard/help_html/*.html \
+      ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/doc/html/
+    cp -r ./copasi/wizard/help_html/figures \
+      ${TMPDIR}/copasi/CopasiUI.app/Contents/Resources/doc/html/
+# add the readme to the image
+    echo "Copying readme file."
+    cp README_MAC.rtf ${TMPDIR}/copasi/COPASI-README.rtf
+
+# set the bundles flag with SetFile
+    echo "Setting the bundle flag."
+    ${SETFILE} -a b ${TMPDIR}/copasi/CopasiUI.app
+
+    chown -R 99:99 ${TMPDIR}/copasi
+
+# Unmount temporary image
+    hdiutil eject ${drive}
+
+    hdiutil convert -format UDCO Copasi-tmp.dmg -o Copasi-$build-$1.dmg
+    rm -rf Copasi-tmp.dmg
     ;;
 
   xLinux|xSunOS)
