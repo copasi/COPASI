97383cbf0749481bff66d8cce4696479c4161dc4
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2005-02-14 13:40:00 +0000

    rearranging the layout

--- copasi/CopasiUI/ModelWidget.cpp
+++ copasi/CopasiUI/ModelWidget.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/ModelWidget.cpp,v $
-   $Revision: 1.38 $
+   $Revision: 1.39 $
    $Name:  $
-   $Author: shoops $ 
-   $Date: 2005/02/09 01:07:38 $
+   $Author: ssahle $ 
+   $Date: 2005/02/14 13:40:00 $
    End CVS Header */
 
 /*******************************************************************
@@ -44,34 +44,56 @@ ModelWidget::ModelWidget(QWidget* parent, const char* name, WFlags fl)
   ModelWidgetLayout = new QGridLayout(this, 1, 1, 11, 5, "ModelWidgetLayout");
 
   TextLabel1 = new QLabel(this, "TextLabel1");
-  TextLabel1->setText(trUtf8("Name"));
+  TextLabel1->setText(trUtf8("Model name"));
+  TextLabel1->setAlignment(int(QLabel::AlignVCenter
+                               | QLabel::AlignRight));
   ModelWidgetLayout->addWidget(TextLabel1, 0, 0);
   LineEdit = new QLineEdit(this, "LineEdit");
-  ModelWidgetLayout->addWidget(LineEdit, 0, 1);
+  ModelWidgetLayout->addMultiCellWidget(LineEdit, 0, 0, 1, 2);
+
+  QSpacerItem* spacer2 = new QSpacerItem(0, 6, QSizePolicy::Minimum, QSizePolicy::Fixed);
+  ModelWidgetLayout->addItem(spacer2, 1, 1);
 
   TextLabel2 = new QLabel(this, "TextLabel2");
-  TextLabel2->setText(trUtf8("Time Unit"));
-  ModelWidgetLayout->addWidget(TextLabel2, 1, 0);
+  TextLabel2->setText(trUtf8("Time unit"));
+  TextLabel2->setAlignment(int(QLabel::AlignVCenter
+                               | QLabel::AlignRight));
+  ModelWidgetLayout->addWidget(TextLabel2, 2, 0);
   ComboBox1 = new QComboBox(FALSE, this, "ComboBox1");
-  ModelWidgetLayout->addWidget(ComboBox1, 1, 1);
+  //ComboBox1->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  ModelWidgetLayout->addWidget(ComboBox1, 2, 1);
 
   TextLabel3 = new QLabel(this, "TextLabel3");
-  TextLabel3->setText(trUtf8("Volume Unit"));
-  ModelWidgetLayout->addWidget(TextLabel3, 2, 0);
+  TextLabel3->setText(trUtf8("Volume unit"));
+  TextLabel3->setAlignment(int(QLabel::AlignVCenter
+                               | QLabel::AlignRight));
+  ModelWidgetLayout->addWidget(TextLabel3, 3, 0);
   ComboBox2 = new QComboBox(FALSE, this, "ComboBox2");
-  ModelWidgetLayout->addWidget(ComboBox2, 2, 1);
+  //ComboBox2->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  ModelWidgetLayout->addWidget(ComboBox2, 3, 1);
 
   TextLabel4 = new QLabel(this, "TextLabel4");
-  TextLabel4->setText(trUtf8("Quantity Unit"));
-  ModelWidgetLayout->addWidget(TextLabel4, 3, 0);
+  TextLabel4->setText(trUtf8("Quantity unit"));
+  TextLabel4->setAlignment(int(QLabel::AlignVCenter
+                               | QLabel::AlignRight));
+  ModelWidgetLayout->addWidget(TextLabel4, 4, 0);
   ComboBox3 = new QComboBox(FALSE, this, "ComboBox3");
-  ModelWidgetLayout->addWidget(ComboBox3, 3, 1);
+  //ComboBox3->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  ModelWidgetLayout->addWidget(ComboBox3, 4, 1);
+
+  QFrame * Line1 = new QFrame(this, "Line1");
+  Line1->setFrameShape(QFrame::HLine);
+  ModelWidgetLayout->addMultiCellWidget(Line1, 5, 5, 0, 2);
 
   TextLabel5 = new QLabel(this, "TextLabel5");
   TextLabel5->setText(trUtf8("Description"));
-  ModelWidgetLayout->addWidget(TextLabel5, 4, 0);
+  TextLabel5->setAlignment(int(QLabel::AlignTop
+                               | QLabel::AlignRight));
+  ModelWidgetLayout->addWidget(TextLabel5, 6, 0);
   textBrowser = new QTextBrowser (this, "Text Browser");
-  ModelWidgetLayout->addWidget(textBrowser, 4, 1);
+  ModelWidgetLayout->addMultiCellWidget(textBrowser, 6, 6, 1, 2);
+
+  //********* buttons ***********
 
   Layout5 = new QHBoxLayout(0, 0, 6, "Layout5");
 
@@ -85,7 +107,7 @@ ModelWidget::ModelWidget(QWidget* parent, const char* name, WFlags fl)
 
   // preliminary
 
-  ModelWidgetLayout->addMultiCellLayout(Layout5, 8, 8, 0, 1);
+  ModelWidgetLayout->addMultiCellLayout(Layout5, 8, 8, 0, 2);
 
   // signals and slots connections
   connect(commitChanges, SIGNAL(clicked()), this, SLOT(slotBtnOKClicked()));
