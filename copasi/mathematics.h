49d5dd031badb97330c1ef236d962c12b36585f7
Stefan Hoops <shoops@vbi.vt.edu>
2005-05-10 12:48:22 +0000

    Fixed definition of atanh for WIN32

--- copasi/mathematics.h
+++ copasi/mathematics.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/Attic/mathematics.h,v $
-   $Revision: 1.3 $
+   $Revision: 1.4 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2005/05/10 12:10:40 $
+   $Date: 2005/05/10 12:48:22 $
    End CVS Header */
 
 #include <math.h>
@@ -17,5 +17,5 @@
 #ifdef WIN32 // These are not part of ANSI C and Visual C++ 6.0 misses them.
 #define asinh(__x) log(__x + sqrt(__x * __x + 1.0))
 #define acosh(__x) log(__x + sqrt(__x * __x - 1.0))
-#define atanh(__x) log((1.0 + __x)/(1.0 - __x))
+#define atanh(__x) (.5 * log((1.0 + __x)/(1.0 - __x)))
 #endif // WIN32
