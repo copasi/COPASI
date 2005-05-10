84dc088f72f7419791bb2c68640ad986739324e8
Stefan Hoops <shoops@vbi.vt.edu>
2005-05-10 12:10:40 +0000

    Added defines for asinh, acosh, and atanh which are missing under Visual C++ 6.0.

--- copasi/mathematics.h
+++ copasi/mathematics.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/Attic/mathematics.h,v $
-   $Revision: 1.2 $
+   $Revision: 1.3 $
    $Name:  $
-   $Author: ssahle $ 
-   $Date: 2004/06/23 09:29:01 $
+   $Author: shoops $ 
+   $Date: 2005/05/10 12:10:40 $
    End CVS Header */
 
 #include <math.h>
@@ -13,3 +13,9 @@
 #ifndef M_E
 #define M_E            2.7182818284590452354   /* e */
 #endif
+
+#ifdef WIN32 // These are not part of ANSI C and Visual C++ 6.0 misses them.
+#define asinh(__x) log(__x + sqrt(__x * __x + 1.0))
+#define acosh(__x) log(__x + sqrt(__x * __x - 1.0))
+#define atanh(__x) log((1.0 + __x)/(1.0 - __x))
+#endif // WIN32
