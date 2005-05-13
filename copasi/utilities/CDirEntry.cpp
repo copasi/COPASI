7058b2b3eda7dd413bd849c1f77869e30983da5e
Stefan Hoops <shoops@vbi.vt.edu>
2005-05-13 13:15:52 +0000

    Works now under WIN32.

--- copasi/utilities/CDirEntry.cpp
+++ copasi/utilities/CDirEntry.cpp
@@ -1,17 +1,17 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CDirEntry.cpp,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2005/05/12 19:04:36 $
+   $Date: 2005/05/13 13:15:52 $
    End CVS Header */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <io.h>
-#include <direct.h>
 
 #ifdef WIN32
+# include <direct.h>
 # define stat _stat
 # define S_IFREG _S_IFREG
 # define S_IFDIR _S_IFDIR
@@ -19,7 +19,9 @@
 # define mkdir _mkdir
 # define rmdir _rmdir
 #else
-#endif
+# include <dirent.h>
+# include <unistd.h>
+#endif // WIN32
 
 #include "copasi.h"
 
@@ -30,7 +32,11 @@ bool CDirEntry::isFile(const std::string & path)
   struct stat st;
   if (stat(path.c_str(), & st) == -1) return false;
 
-  return ((st.st_mode | S_IFREG) != 0);
+#ifdef WIN32
+  return ((st.st_mode & S_IFREG) == S_IFREG);
+#else
+  return S_ISREG(st.st_mode);
+#endif
 }
 
 bool CDirEntry::isDir(const std::string & path)
@@ -38,7 +44,11 @@ bool CDirEntry::isDir(const std::string & path)
   struct stat st;
   if (stat(path.c_str(), & st) == -1) return false;
 
-  return ((st.st_mode | S_IFDIR) != 0);
+#ifdef WIN32
+  return ((st.st_mode & S_IFDIR) == S_IFDIR);
+#else
+  return S_ISDIR(st.st_mode);
+#endif
 }
 
 bool CDirEntry::exist(const std::string & path)
@@ -46,7 +56,12 @@ bool CDirEntry::exist(const std::string & path)
   struct stat st;
   if (stat(path.c_str(), & st) == -1) return false;
 
-  return ((st.st_mode | S_IFREG) || (st.st_mode | S_IFDIR));
+#ifdef WIN32
+  return ((st.st_mode & S_IFREG) == S_IFREG ||
+          (st.st_mode & S_IFDIR) == S_IFDIR);
+#else
+  return (S_ISREG(st.st_mode) || S_ISDIR(st.st_mode));
+#endif
 }
 
 bool CDirEntry::isReadable(const std::string & path)
@@ -91,86 +106,55 @@ bool CDirEntry::remove(const std::string & path)
 bool CDirEntry::removeFiles(const std::string & pattern,
                             const std::string & path)
 {
+  bool success = true;
+
   // :TODO:
-#ifdef XXXX
 #ifdef WIN32
 
   // Append to the "path" mask for all files in directory
-  string pattern = path_base + string("*");
-
-  bool skip_recursive_entry;
+  std::string FilePattern = path + "\\" + pattern;
 
   // Open directory stream and try read info about first entry
-  struct _finddata_t entry;
-  long desc = _findfirst(pattern.c_str(), &entry);
-  if (desc != -1)
-    {
-      skip_recursive_entry = (mode == eIgnoreRecursive) &&
-                             ((::strcmp(entry.name, ".") == 0) ||
-                              (::strcmp(entry.name, "..") == 0));
-      // check all masks
-      if (!skip_recursive_entry)
-        {
-          ITERATE(vector<string>, it, masks)
-          {
-            const string& mask = *it;
-            if (mask.empty() ||
-                 MatchesMask(entry.name, mask.c_str(), use_case))
-              {
-                s_AddDirEntry(contents, path_base + entry.name);
-                break;
-              }
-          } // ITERATE
-        }
+  struct _finddata_t Entry;
+  C_INT32 hList = _findfirst(FilePattern.c_str(), &Entry);
 
-      while (_findnext(desc, &entry) != -1)
-        {
-          if ((mode == eIgnoreRecursive) &&
-               ((::strcmp(entry.name, ".") == 0) ||
-                (::strcmp(entry.name, "..") == 0)))
-            {
-              continue;
-            }
-          ITERATE(vector<string>, it, masks)
-          {
-            const string& mask = *it;
-            if (mask.empty() ||
-                 MatchesMask(entry.name, mask.c_str(), use_case))
-              {
-                s_AddDirEntry(contents, path_base + entry.name);
-                break;
-              }
-          } // ITERATE
-        }
-      _findclose(desc);
-    }
+  if (hList == -1) return success;
+
+  if (Entry.attrib | _A_NORMAL)
+  {if (::remove((path + "\\" + Entry.name).c_str()) != 0) success = false;}
+  else
+  {if (rmdir((path + "\\" + Entry.name).c_str()) != 0) success = false;}
+
+  while (_findnext(hList, &Entry) == 0)
+    if (Entry.attrib | _A_NORMAL)
+    {if (::remove((path + "\\" + Entry.name).c_str()) != 0) success = false;}
+    else
+    {if (rmdir((path + "\\" + Entry.name).c_str()) != 0) success = false;}
+
+  _findclose(hList);
 
 #else
-  DIR* dir = opendir(GetPath().c_str());
-  if (dir)
+
+  DIR * pDir = opendir(path.c_str());
+  if (!pDir) return false;
+
+  struct dirent * pEntry;
+
+  while (pEntry = readdir(dir))
     {
-      while (struct dirent* entry = readdir(dir))
-        {
-          if ((mode == eIgnoreRecursive) &&
-               ((::strcmp(entry->d_name, ".") == 0) ||
-                (::strcmp(entry->d_name, "..") == 0)))
-            {
-              continue;
-            }
-          ITERATE(vector<string>, it, masks)
-          {
-            const string& mask = *it;
-            if (mask.empty() ||
-                 MatchesMask(entry->d_name, mask.c_str(), use_case))
-              {
-                s_AddDirEntry(contents, path_base + entry->d_name);
-                break;
-              }
-          } // ITERATE
-        } // while
-      closedir(dir);
+      // Match pattern.
+
+      if (pEntry->d_type == DT_DIR)
+      {if (rmdir((path + "/" + pEntry.d_name).c_str()) != 0) success = false;}
+      else
+      {if (::remove((path + "/" + pEntry.d_name).c_str()) != 0) success = false;}
+
+      pEntry = readdir(dir);
     }
+
+  closedir(dir);
+
 #endif // WIN32
-#endif // XXXX
-  return false;
+
+  return success;
 }
