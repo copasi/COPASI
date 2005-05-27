120d4ef6ac151c20fb8a872c06d9ae61d8c0ba0a
Stefan Hoops <shoops@vbi.vt.edu>
2005-05-27 20:59:51 +0000

    Fixed defines for WIN32.

--- copasi/mathematics.h
+++ copasi/mathematics.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/Attic/mathematics.h,v $
-   $Revision: 1.4 $
+   $Revision: 1.5 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2005/05/10 12:48:22 $
+   $Date: 2005/05/27 20:59:51 $
    End CVS Header */
 
 #include <math.h>
@@ -15,7 +15,7 @@
 #endif
 
 #ifdef WIN32 // These are not part of ANSI C and Visual C++ 6.0 misses them.
-#define asinh(__x) log(__x + sqrt(__x * __x + 1.0))
-#define acosh(__x) log(__x + sqrt(__x * __x - 1.0))
-#define atanh(__x) (.5 * log((1.0 + __x)/(1.0 - __x)))
+#define asinh(__x) log((__x) + sqrt((__x) * (__x) + 1.0))
+#define acosh(__x) log((__x) + sqrt((__x) * (__x) - 1.0))
+#define atanh(__x) (.5 * log((1.0 + (__x))/(1.0 - (__x))))
 #endif // WIN32
