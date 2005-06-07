51e3fb5e973284be4f5908be627201de57e02bbf
Stefan Hoops <shoops@vbi.vt.edu>
2005-06-07 19:15:38 +0000

    Implmented proper destruction of the node list generated during parsing.

--- copasi/function/CEvaluationTree.cpp
+++ copasi/function/CEvaluationTree.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/function/CEvaluationTree.cpp,v $
-   $Revision: 1.4 $
+   $Revision: 1.5 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2005/06/07 15:18:54 $
+   $Date: 2005/06/07 19:15:38 $
    End CVS Header */
 
 #include "copasi.h"
@@ -19,11 +19,14 @@
 CEvaluationTree::CEvaluationTree():
     mDescription(),
     mErrorPosition(std::string::npos),
+    mpNodeList(NULL),
     mpRoot(NULL)
 {}
 
 CEvaluationTree::~CEvaluationTree()
-{}
+{
+  if (mpNodeList != NULL) CEvaluationParser::freeNodeList(mpNodeList);
+}
 
 bool CEvaluationTree::setDescription(const std::string & description)
 {
@@ -49,16 +52,17 @@ C_FLOAT64 * CEvaluationTree::getObjectValue(const CCopasiObjectName & /* CN */) 
 
 bool CEvaluationTree::parse()
 {
+  if (mpNodeList != NULL) CEvaluationParser::freeNodeList(mpNodeList);
+  mpNodeList = NULL;
+  mpRoot = NULL;
+
   std::istringstream buffer(mDescription);
+  CEvaluationLexer Parser(&buffer);
 
-  CEvaluationLexer Scanner(&buffer);
+  Parser.yyparse();
 
-  Scanner.yyparse();
-
-  pdelete(mpRoot);
-
-  mpRoot = Scanner.getRootNode();
-  mpNodeList = Scanner.getNodeList();
+  mpRoot = Parser.getRootNode();
+  mpNodeList = Parser.getNodeList();
 
   /*
   mErrorPosition = Scanner.yylex();
