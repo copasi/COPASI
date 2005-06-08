875c5ae95fa273c1b2b3d93740b86e84dd6ce042
Stefan Hoops <shoops@vbi.vt.edu>
2005-06-08 16:06:00 +0000

    This file is missing in the cvs it is autogenerated by the resource compiler.

---
+++ copasi/CopasiUI/resource.h
@@ -0,0 +1,24 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/resource.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: shoops $ 
+   $Date: 2005/06/08 16:06:00 $
+   End CVS Header */
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by CopasiUI.rc
+//
+#define IDI_ICON1                       101
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
