f49c2024b0d7256628b1caf11fcc1cb5e485bf37
Christine Lee <chlee2@vt.edu>
2005-06-24 19:09:48 +0000

    Evolutionary Computation GA Algorithm with Stochastic Ranking Selection. In GASR files, selection has been changed with the forcing of boundaries removed or commented out.  Phi function is also added as well as addition of phi Value exchange in the Swap function.

---
+++ copasi/optimization/COptMethodGASR.cpp
@@ -0,0 +1,686 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/optimization/COptMethodGASR.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: chlee $ 
+   $Date: 2005/06/24 19:09:47 $
+   End CVS Header */
+
+// ga.cpp : Genetic algorithm optimisation.
+//
+#include "COptMethodGASR.h"
+#include <float.h>
+
+#include "copasi.h"
+#include "mathematics.h"
+#include "COptMethod.h"
+#include "COptProblem.h"
+#include "COptItem.h"
+
+#include "randomGenerator/CRandom.h"
+#include "utilities/CProcessReport.h"
+#include "report/CCopasiObjectReference.h"
+
+COptMethodGASR::COptMethodGASR(const CCopasiContainer * pParent):
+    COptMethod(CCopasiMethod::GeneticAlgorithm, pParent),
+    mGenerations(0),
+    mPopulationSize(0),
+    mpRandom(NULL),
+    mVariableSize(0),
+    mIndividual(0),
+    mCrossOverFalse(0),
+    mCrossOver(0),
+    mValue(0),
+    mShuffle(0),
+    mWins(0),
+    mMutationVarians(0.1),
+    mBestValue(DBL_MAX),
+    mBestIndex(C_INVALID_INDEX),
+    mGeneration(0)
+
+{
+  addParameter("Number of Generations", CCopasiParameter::UINT, (unsigned C_INT32) 200);
+  addParameter("Population Size", CCopasiParameter::UINT, (unsigned C_INT32) 20);
+  addParameter("Random Number Generator", CCopasiParameter::UINT, (unsigned C_INT32) CRandom::mt19937);
+  addParameter("Seed", CCopasiParameter::UINT, (unsigned C_INT32) 0);
+  addParameter("Pf", CCopasiParameter::DOUBLE, (C_FLOAT64) 0.475);  //*****ADDED for SR
+}
+
+COptMethodGASR::COptMethodGASR(const COptMethodGASR & src,
+                               const CCopasiContainer * pParent):
+    COptMethod(src, pParent),
+    mGenerations(0),
+    mPopulationSize(0),
+    mpRandom(NULL),
+    mVariableSize(0),
+    mIndividual(0),
+    mCrossOverFalse(0),
+    mCrossOver(0),
+    mValue(0),
+    mShuffle(0),
+    mWins(0),
+    mMutationVarians(0.1),
+    mBestValue(DBL_MAX),
+    mBestIndex(C_INVALID_INDEX),
+    mGeneration(0)
+{}
+
+COptMethodGASR::~COptMethodGASR()
+{cleanup();}
+
+bool COptMethodGASR::setCallBack(CProcessReport * pCallBack)
+{
+  CCopasiMethod::setCallBack(pCallBack);
+
+  pCallBack->addItem("Current Generation",
+                     CCopasiParameter::UINT,
+                     getObject(CCopasiObjectName("Reference=Current Generation")),
+                     & mGenerations);
+
+  C_FLOAT64 EndValue = DBL_MAX;
+  pCallBack->addItem("Objective Value",
+                     CCopasiParameter::DOUBLE,
+                     getObject(CCopasiObjectName("Reference=Objective Value")),
+                     & EndValue);
+
+  return true;
+}
+
+// evaluate the fitness of one individual
+C_FLOAT64 COptMethodGASR::evaluate(const CVector< C_FLOAT64 > & individual)
+{
+  unsigned C_INT32 j;
+
+  std::vector< UpdateMethod *>::const_iterator itMethod = mpSetCalculateVariable->begin();
+
+  // set the paramter values
+  for (j = 0; j < mVariableSize; j++, ++itMethod)
+    (**itMethod)(individual[j]);
+
+  // check whether the parametric constraints are fulfilled
+  if (!mpOptProblem->checkParametricConstraints()) return DBL_MAX;
+
+  // evaluate the fitness
+  try
+    {
+      if (!mpOptProblem->calculate()) return DBL_MAX;
+    }
+
+  catch (...)
+    {
+      return DBL_MAX;
+    }
+
+  // check wheter the functional constraints are fulfilled
+  if (!mpOptProblem->checkFunctionalConstraints()) return DBL_MAX;
+
+  return mpOptProblem->getCalculateValue();
+}
+
+bool COptMethodGASR::swap(unsigned C_INT32 from, unsigned C_INT32 to)
+{
+  CVector< C_FLOAT64 > * pTmp = mIndividual[to];
+  mIndividual[to] = mIndividual[from];
+  mIndividual[from] = pTmp;
+
+  //**** Added for swapping individual Phi values also for Stochastic Ranking
+  CVector < C_FLOAT64 > * jTmp = indvPhi[to];
+  indvPhi[to] = indvPhi[from];
+  indvPhi[from] = jTmp;
+
+  C_FLOAT64 dTmp = mValue[to];
+  mValue[to] = mValue[from];
+  mValue[from] = dTmp;
+
+  C_INT32 iTmp = mWins[to];
+  mWins[to] = mWins[from];
+  mWins[from] = iTmp;
+
+  return true;
+}
+
+//mutate one individual
+bool COptMethodGASR::mutate(CVector< C_FLOAT64 > & individual)
+{
+  unsigned C_INT32 j;
+
+  // mutate the parameters
+  for (j = 0; j < mVariableSize; j++)
+    {
+      COptItem & OptItem = *(*mpOptItem)[j];
+      C_FLOAT64 & mut = individual[j];
+
+      // calculate the mutatated parameter
+      mut *= mpRandom->getRandomNormal(1, mMutationVarians);
+
+      /****** For SR, cannot force within bounds
+      // force it to be within the bounds
+      switch (OptItem.checkConstraint(mut))
+        {
+        case - 1:
+          mut = *OptItem.getLowerBoundValue();
+          if (!OptItem.checkLowerBound(mut)) // Inequality
+            {
+              if (mut == 0.0)
+                mut = DBL_MIN;
+              else
+                mut += mut * DBL_EPSILON;
+            }
+          break;
+
+        case 1:
+          mut = *OptItem.getUpperBoundValue();
+          if (!OptItem.checkUpperBound(mut)) // Inequality
+            {
+              if (mut == 0.0)
+                mut = -DBL_MIN;
+              else
+                mut -= mut * DBL_EPSILON;
+            }
+          break;
+        }*/
+    }
+
+  return true;
+}
+
+bool COptMethodGASR::crossover(const CVector< C_FLOAT64 > & parent1,
+                               const CVector< C_FLOAT64 > & parent2,
+                               CVector< C_FLOAT64 > & child1,
+                               CVector< C_FLOAT64 > & child2)
+{
+  unsigned C_INT32 i, crp;
+  unsigned C_INT32 nCross = 0;
+
+  mCrossOver = mCrossOverFalse;
+
+  if (mVariableSize > 1)
+    nCross = mpRandom->getRandomU(mVariableSize / 2);
+
+  if (nCross == 0)
+    {
+      // if less than 0 just copy parent to child
+      child1 = parent1;
+      child2 = parent2;
+
+      return true;
+    }
+
+  // choose cross over points;
+  // We do not mind if a crossover point gets drawn twice
+  for (i = 0; i < nCross; i++)
+    {
+      crp = mpRandom->getRandomU(mVariableSize);
+      mCrossOver[crp] = true;
+    }
+
+  const CVector< C_FLOAT64 > * pParent1 = & parent1;
+  const CVector< C_FLOAT64 > * pParent2 = & parent2;
+  const CVector< C_FLOAT64 > * pTmp;
+
+  for (i = 0; i < mVariableSize; i++)
+    {
+      if (mCrossOver[i])
+        {
+          pTmp = pParent1;
+          pParent1 = pParent2;
+          pParent2 = pTmp;
+        }
+
+      child1[i] = (*pParent1)[i];
+      child2[i] = (*pParent2)[i];
+    }
+
+  return true;
+}
+
+bool COptMethodGASR::shuffle()
+{
+  unsigned C_INT32 i, from, to, tmp;
+
+  //  Why sort first? We can just keep shuffling.
+  //  for(i=0; i<mPopulationSize; i++) mShuffle[i] = i;
+
+  for (i = 0; i < mPopulationSize / 2; i++)
+    {
+      from = mpRandom->getRandomU(mPopulationSize - 1);
+      to = mpRandom->getRandomU(mPopulationSize - 1);
+
+      tmp = mShuffle[to];
+      mShuffle[to] = mShuffle[from];
+      mShuffle[from] = tmp;
+    }
+
+  return true;
+}
+
+bool COptMethodGASR::replicate()
+{
+  unsigned C_INT32 i;
+
+  C_INT32 temp_size = mIndividual.size();
+
+  // generate a random order for the parents
+  shuffle();
+
+  // reproduce in consecutive pairs
+  for (i = 0; i < mPopulationSize / 2; i++)
+    crossover(*mIndividual[mShuffle[i * 2]],
+              *mIndividual[mShuffle[i * 2 + 1]],
+              *mIndividual[mPopulationSize + i * 2],
+              *mIndividual[mPopulationSize + i * 2 + 1]);
+
+  // check if there is one left over and just copy it
+  if (mPopulationSize % 2 > 0)
+    *mIndividual[2 * mPopulationSize - 1] = *mIndividual[mPopulationSize - 1];
+
+  // mutate the offspring
+  for (i = mPopulationSize; i < 2 * mPopulationSize; i++)
+    {
+      mutate(*mIndividual[i]);
+      mValue[i] = evaluate(*mIndividual[i]);
+    }
+
+  return true;
+}
+
+// select mPopulationSize individuals
+bool COptMethodGASR::select()
+{
+  unsigned C_INT32 i, j, u;
+  //unsigned C_INT32 nopp, opp,MaxIndex;
+  unsigned C_INT32 TotalPopulation = 2 * mPopulationSize;
+  bool wasSwapped;
+  unsigned C_INT32 sweepNum = TotalPopulation;  // This is default based on paper
+  //C_FLOAT64 MaxValue;
+
+  /*// tournament competition
+  mWins = 0; // Set all wins to 0.
+
+  // compete with ~ 20% of the TotalPopulation
+  nopp = std::min<unsigned C_INT32>(1, mPopulationSize / 5);
+
+  // parents and offspring are all in competition
+  for (i = 0; i < TotalPopulation; i++)
+    for (j = 0; j < nopp; j++)
+      {
+        // get random opponent
+        opp = mpRandom->getRandomU(TotalPopulation);
+
+        if (mValue[i] < mValue[opp])
+          mWins[i]++;
+        else
+          mWins[opp]++;
+      }
+
+  // selection of top mPopulationSize winners
+  for (i = 0; i < mPopulationSize; i++)
+    {
+      MaxIndex = i;
+      MaxValue = mWins[i];
+
+      for (j = i + 1; j < TotalPopulation; j++)
+        if (MaxValue < mWins[j])
+          {
+            MaxIndex = j;
+            MaxValue = mWins[j];
+          }
+
+      swap(i, MaxIndex); // The best individual in [i, TotalPopulation] is swapped to the top (i) position.
+    }*/
+
+  //*** Selection Method for Stochastic Ranking
+  //go through the array of individuals to retrieve phi values
+  for (i = 0; i < TotalPopulation; i++)
+    {
+      *indvPhi[i] = Phi(i);
+    }
+
+  // stochastic ranking "bubble sort"
+  for (i = 0; i < sweepNum; i++) // Here sweepNum is optimal number of sweeps from paper
+    {
+      wasSwapped = false;
+      for (j = 0; j < TotalPopulation - 1; j++)  // lambda is number of individuals
+        {
+          //currentI = 0; // <- this may not be needed if swap is used instead
+          //u = dr250();  // retrieve random number between 0 and 1 uniformly distributed
+          u = mpRandom->getRandomOO();
+          //if ((indvPhi[j]== indvPhi[j+1] && indvPhi[j] == 0) || (u < 0.475))
+          // if individual value is equal to next value and within boundary or randomly generated number is less than Pf
+          if ((indvPhi[j] == indvPhi[j + 1] && indvPhi[j] == 0) || (u < Pf))  // (Pf =0.475 default)
+            {
+              // compare obj fcn using mValue alternative code
+              if (mValue[j] > mValue[j + 1])
+                {
+                  swap(j, j + 1);  //The best individual is swapped to the top position.
+                  wasSwapped = true;
+                }
+            }
+          else //indvPhi values are not equal and not within boundary
+            {
+              if (indvPhi[j] > indvPhi[j + 1])
+                {
+                  swap(j, j + 1);  //The best individual is swapped to the top position.
+                  wasSwapped = true;
+                }
+            }
+          //}
+        }
+      // if no swap then break
+      if (wasSwapped == false)
+        break;
+    }
+  // This is because from original Switch/case statement...  break;
+
+  return true;
+}
+
+// evaluate the distance ofparameters to boundaries
+C_FLOAT64 COptMethodGASR::Phi(C_INT32 indivNum)
+{
+  C_FLOAT64 phiVal = 0.0;
+  for (int i = 0; i < mVariableSize; i++)  /******* here double check to see if indv array parameter is same as number of parameters ****
+                Clarify if should go through additional for loop for number of parameters */
+    {
+      C_FLOAT64 phiCalc;
+      COptItem & OptItem = *(*mpOptItem)[i];
+      //C_FLOAT64 & mut = individual[j];
+      C_FLOAT64 & indvValue = (*mIndividual[indivNum])[i];
+
+      // Go through parameter and constraints (all taken care of in single call)
+      switch (OptItem.checkConstraint(indvValue))
+        {
+        case 1: phiCalc = (*mIndividual[indivNum])[i] - *OptItem.getUpperBoundValue();
+          phiVal += phiCalc * phiCalc;
+          break;
+
+        case - 1: phiCalc = (*mIndividual[indivNum])[i] - *OptItem.getLowerBoundValue();
+          phiVal += phiCalc * phiCalc;
+          break;
+        }
+    }
+  return phiVal;
+}
+
+// check the best individual at this generation
+unsigned C_INT32 COptMethodGASR::fittest()
+{
+  unsigned C_INT32 i, BestIndex = 0;
+  C_FLOAT64 BestValue = mValue[0];
+
+  for (i = 1; i < mPopulationSize; i++)
+    if (mValue[i] < BestValue)
+      {
+        BestIndex = i;
+        BestValue = mValue[i];
+      }
+
+  return BestIndex;
+}
+
+// initialise the population
+bool COptMethodGASR::creation(unsigned C_INT32 first,
+                              unsigned C_INT32 last)
+{
+  unsigned C_INT32 Last = std::min(last, mPopulationSize);
+
+  unsigned C_INT32 i;
+  unsigned C_INT32 j;
+
+  C_FLOAT64 mn;
+  C_FLOAT64 mx;
+  C_FLOAT64 la;
+
+  bool linear;
+
+  for (i = first; i < Last; i++)
+    {
+      for (j = 0; j < mVariableSize; j++)
+        {
+          // calculate lower and upper bounds
+          COptItem & OptItem = *(*mpOptItem)[j];
+          mn = *OptItem.getLowerBoundValue();
+          mx = *OptItem.getUpperBoundValue();
+
+          C_FLOAT64 & mut = (*mIndividual[i])[j];
+
+          try
+            {
+              // determine if linear or log scale
+              linear = false; la = 1.0;
+
+              if (mn == 0.0) mn = DBL_EPSILON;
+
+              if ((mn < 0.0) || (mx <= 0.0))
+                linear = true;
+              else
+                {
+                  la = log10(mx) - log10(mn);
+                  if (la < 1.8) linear = true;
+                }
+
+              // set it to a random value within the interval
+              if (linear)
+                mut = mn + mpRandom->getRandomCC() * (mx - mn);
+              else
+                mut = mn * pow(10, la * mpRandom->getRandomCC());
+            }
+
+          catch (...)
+            {
+              mut = (mx + mn) * 0.5;
+            }
+
+          /****** Do not force within bounds for SR ****
+          // force it to be within the bounds
+          switch (OptItem.checkConstraint(mut))
+            {
+            case - 1:
+              mut = *OptItem.getLowerBoundValue();
+              if (!OptItem.checkLowerBound(mut)) // Inequality
+                {
+                  if (mut == 0.0)
+                    mut = DBL_MIN;
+                  else
+                    mut += mut * DBL_EPSILON;
+                }
+              break;
+
+            case 1:
+              mut = *OptItem.getUpperBoundValue();
+              if (!OptItem.checkUpperBound(mut)) // Inequality
+                {
+                  if (mut == 0.0)
+                    mut = - DBL_MIN;
+                  else
+                    mut -= mut * DBL_EPSILON;
+                }
+              break;
+            }*/
+        }
+      try
+        {
+          // calculate its fitness
+          mValue[i] = evaluate(*mIndividual[i]);
+        }
+      catch (...)
+        {
+          mValue[i] = DBL_MAX;
+        }
+    }
+
+  return true;
+}
+
+void COptMethodGASR::initObjects()
+{
+  addObjectReference("Current Generation", mGeneration, CCopasiObject::ValueInt);
+  addObjectReference("Objective Value", mBestValue, CCopasiObject::ValueDbl);
+}
+
+bool COptMethodGASR::initialize()
+{
+  unsigned C_INT32 i;
+
+  if (!COptMethod::initialize()) return false;
+
+  mGenerations = * (unsigned C_INT32 *) getValue("Number of Generations");
+  mPopulationSize = * (unsigned C_INT32 *) getValue("Population Size");
+  mpRandom =
+    CRandom::createGenerator(* (CRandom::Type *) getValue("Random Number Generator"),
+                             * (unsigned C_INT32 *) getValue("Seed"));
+
+  mVariableSize = mpOptItem->size();
+
+  //***** Below added initialization of indvPhi for SR ****
+  mIndividual.resize(2*mPopulationSize);
+  indvPhi.resize(2*mPopulationSize);
+  for (i = 0; i < 2*mPopulationSize; i++)
+    {
+      mIndividual[i] = new CVector< C_FLOAT64 >(mVariableSize);
+      indvPhi[i] = new CVector< C_FLOAT64 >(mVariableSize);
+    }
+
+  mCrossOverFalse.resize(mVariableSize);
+  mCrossOverFalse = false;
+  mCrossOver.resize(mVariableSize);
+
+  mValue.resize(2*mPopulationSize);
+
+  mShuffle.resize(mPopulationSize);
+  for (i = 0; i < mPopulationSize; i++)
+    mShuffle[i] = i;
+
+  mWins.resize(2*mPopulationSize);
+  // initialise the variance for mutations
+  mMutationVarians = 0.1;
+
+  C_FLOAT64 p = *(C_FLOAT64*) getValue("Pf");
+  if ((p > 0.4) && (p < 0.5))
+    Pf = p;
+  else Pf = 0.475;
+
+  return true;
+}
+
+bool COptMethodGASR::cleanup()
+{
+  unsigned C_INT32 i;
+
+  pdelete(mpRandom);
+  for (i = 0; i < 2*mPopulationSize; i++)
+    {
+      pdelete(mIndividual[i]);
+      pdelete(indvPhi[i]);
+    }
+
+  return true;
+}
+
+bool COptMethodGASR::optimise()
+{
+  bool success = true;
+
+  if (!initialize()) return false;
+
+  // Counters to determine whether the optimization process has stalled
+  // They count the number of generations without advances.
+  unsigned C_INT32 Stalled, Stalled10, Stalled30, Stalled50;
+  Stalled = Stalled10 = Stalled30 = Stalled50 = 0;
+
+  unsigned C_INT32 i;
+
+  // initialise the population
+  // first individual is the initial guess
+  for (i = 0; i < mVariableSize; i++)
+    (*mIndividual[0])[i] = *(*mpOptItem)[i]->getObjectValue();
+
+  try
+    {
+      // calculate the fitness
+      mValue[0] = evaluate(*mIndividual[0]);
+    }
+
+  catch (...)
+    {
+      mValue[0] = DBL_MAX;
+    }
+
+  // the others are random
+  creation(1, mPopulationSize);
+  // initialise the update register
+  // get the index of the fittest
+  mBestIndex = fittest();
+  // and store that value
+  mBestValue = mValue[mBestIndex];
+
+  mpOptProblem->setSolutionValue(mBestValue);
+  mpOptProblem->setSolutionVariables(*mIndividual[mBestIndex]);
+
+  // ITERATE FOR gener GENERATIONS
+  for (mGeneration = 0; mGeneration < mGenerations; mGeneration++, Stalled++, Stalled10++, Stalled30++, Stalled50++)
+    {
+      // perturb the population if we have stalled for a while
+      if (Stalled > 50 && Stalled50 > 50)
+        {
+          if (!creation(mPopulationSize / 2, mPopulationSize))
+            {
+              success = false;
+              break;
+            }
+          Stalled10 = Stalled30 = Stalled50 = 0;
+        }
+      else if (Stalled > 30 && Stalled30 > 30)
+        {
+          if (!creation(mPopulationSize * 0.7, mPopulationSize))
+            {
+              success = false;
+              break;
+            }
+          Stalled10 = Stalled30 = 0;
+        }
+      else if (Stalled > 10 && Stalled10 > 10)
+        {
+          if (!creation(mPopulationSize * 0.9, mPopulationSize))
+            {
+              success = false;
+              break;
+            }
+          Stalled10 = 0;
+        }
+      // replicate the individuals
+      else if (!replicate())
+        {
+          success = false;
+          break;
+        }
+
+      // select the most fit
+      if (!select())
+        {
+          success = false;
+          break;
+        }
+
+      // get the index of the fittest
+      mBestIndex = fittest();
+      if (mValue[mBestIndex] < mBestValue)
+        {
+          Stalled = Stalled10 = Stalled30 = Stalled50 = 0;
+          mBestValue = mValue[mBestIndex];
+
+          mpOptProblem->setSolutionValue(mBestValue);
+          mpOptProblem->setSolutionVariables(*mIndividual[mBestIndex]);
+        }
+
+      if (mpCallBack && !mpCallBack->progress())
+        {
+          success = false;
+          break;
+        }
+    }
+
+  cleanup();
+
+  return success;
+}
---
+++ copasi/optimization/COptMethodGASR.h
@@ -0,0 +1,219 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/optimization/COptMethodGASR.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: chlee $ 
+   $Date: 2005/06/24 19:09:48 $
+   End CVS Header */
+
+/**
+ * COptMethodGASR class
+ */
+
+#ifndef COPASI_COptMethodGASR
+#define COPASI_COptMethodGASR
+
+#include "COptMethod.h"
+#include "utilities/CVector.h"
+
+class CRandom;
+
+class COptMethodGASR : public COptMethod
+  {
+    friend COptMethod * COptMethod::createMethod(CCopasiMethod::SubType subType);
+
+    // Operations
+  public:
+    /**
+     * Copy Constructor
+     * @param const COptMethodGASR & src
+     * @param const CCopasiContainer * pParent (default: NULL)
+     */
+    COptMethodGASR(const COptMethodGASR & src,
+                   const CCopasiContainer * pParent = NULL);
+
+    /**
+     * Destructor
+     */
+    virtual ~COptMethodGASR();
+
+    /**
+     * Set the call back of the problem
+     * @param CProcessReport * pCallBack
+     * @result bool succes
+     */
+    virtual bool setCallBack(CProcessReport * pCallBack);
+
+    /**
+     * Execute the optimization algorithm calling simulation routine 
+     * when needed. It is noted that this procedure can give feedback 
+     * of its progress by the callback function set with SetCallback.
+     * @ return success;
+     */
+    virtual bool optimise();
+
+  private:
+    /**
+     * Default Constructor
+     * @param const CCopasiContainer * pParent (default: NULL)
+     */
+    COptMethodGASR(const CCopasiContainer * pParent = NULL);
+
+    virtual void initObjects();
+
+    /**
+     * Initialize arrays and pointer.
+     * @return bool success
+     */
+    virtual bool initialize();
+
+    /**
+     * Cleanup arrays and pointers.
+     * @return bool success
+     */
+    virtual bool cleanup();
+
+    /**
+     * Evaluate the fitness of one individual
+     * @param const CVector< C_FLOAT64 > & individual
+     * @return C_FLOAT64 fitness
+     */
+    C_FLOAT64 evaluate(const CVector< C_FLOAT64 > & individual);
+
+    /**
+     * Swap individuals from and to
+     * @param unsigned C_INT32 from
+     * @param unsigned C_INT32 to
+     * @return bool success
+     */
+    bool swap(unsigned C_INT32 from, unsigned C_INT32 to);
+
+    /**
+     * fm one individual
+     * @param CVector< C_FLOAT64 > & individual
+     * @return bool success
+     */
+    bool mutate(CVector< C_FLOAT64 > & individual);
+
+    /**
+     * @param const CVector< C_FLOAT64 > & parent1
+     * @param const CVector< C_FLOAT64 > & parent2
+     * @param CVector< C_FLOAT64 > & child1
+     * @param CVector< C_FLOAT64 > & child2
+     */
+    bool crossover(const CVector< C_FLOAT64 > & parent1,
+                   const CVector< C_FLOAT64 > & parent2,
+                   CVector< C_FLOAT64 > & child1,
+                   CVector< C_FLOAT64 > & child2);
+
+    /**
+     * Shuffle the parents for breading
+     * @return bool success
+     */
+    bool shuffle();
+
+    /**
+     * Replicate the individuals with crossover
+     * @return bool success
+     */
+    bool replicate();
+
+    /**
+     * Select surviving population
+     * @return bool success
+     */
+    bool select();
+
+    /**
+     * Find the best individual at this generation
+     * @return unsigned C_INT32 fittest
+     */
+    unsigned C_INT32 fittest();
+
+    /**
+     * Initialise the population
+     * @param unsigned C_INT32 first
+     * @param unsigned C_INT32 last (default: population size)
+     * @return bool success
+     */
+    bool creation(unsigned C_INT32 first, unsigned C_INT32 last = ULONG_MAX);
+
+    /**
+     * For Stochastic Ranking, evaluate the distance ofparameters to boundaries
+     * @return C_FLOAT64 phiVal
+     */
+    C_FLOAT64 Phi(C_INT32 indvNum);
+
+    // Attributes
+  private:
+    /**
+     * number of generations
+     */
+    unsigned C_INT32 mGenerations;
+
+    /**
+     * size of the population
+     */
+    unsigned C_INT32 mPopulationSize;
+
+    /**
+     * a pointer to the randomnumber generator.
+     */
+    CRandom * mpRandom;
+
+    /**
+     * Probability value used for SR ****
+     */
+    C_FLOAT64 Pf;
+
+    /**
+     * number of parameters
+     */
+    unsigned C_INT32 mVariableSize;
+
+    /**
+     * for array of individuals w/ candidate values for the parameters
+     */
+    std::vector< CVector < C_FLOAT64 > * > mIndividual;
+
+    /**
+        * For Stochastic Ranking.  Used for array of Phi values for the individuals
+        */
+    std::vector< CVector < C_FLOAT64 > * > indvPhi;
+
+    /**
+     * Vector used to initialize the crossover point to false
+     */
+    CVector< bool > mCrossOverFalse;
+
+    /**
+     * Vector of crossover points.
+     */
+    CVector< bool > mCrossOver;
+
+    /**
+     * array of values of objective function f/ individuals
+     */
+    CVector< C_FLOAT64 > mValue;
+
+    /**
+     * indexes for shuffling the population
+     */
+    CVector< unsigned C_INT32 > mShuffle;
+
+    /**
+     * number of wins of each individual in the tournament
+     */
+    CVector< unsigned C_INT32 > mWins;
+
+    /**
+     * variance for mutations
+     */
+    C_FLOAT64 mMutationVarians;
+
+    C_FLOAT64 mBestValue;
+    unsigned C_INT32 mBestIndex;
+    unsigned C_INT32 mGeneration;
+  };
+
+#endif  // COPASI_COptMethodGASR
