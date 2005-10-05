9fb1151f3e10f2656b3a0970f17a3e3cc7897029
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2005-10-05 15:57:04 +0000

    Just some work in progress

--- copasi/utilities/CAnnotatedMatrix.cpp
+++ copasi/utilities/CAnnotatedMatrix.cpp
@@ -1,13 +1,69 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CAnnotatedMatrix.cpp,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: ssahle $ 
-   $Date: 2004/11/26 15:45:59 $
+   $Date: 2005/10/05 15:57:04 $
    End CVS Header */
 
 #include "CAnnotatedMatrix.h"
 #include "report/CKeyFactory.h"
+
+CCopasiArray::CCopasiArray()
+    : mDim(0) {}
+
+CCopasiArray::CCopasiArray(const index_type & sizes)
+{
+  resize(sizes);
+}
+
+void CCopasiArray::resize(const index_type & sizes)
+{
+  mDim = sizes.size();
+  mSizes = sizes;
+  mFactors.resize(mDim);
+
+  unsigned int tmpDataSize = 1;
+  index_type::const_reverse_iterator it, itEnd = sizes.rend();
+  index_type::reverse_iterator itFaktor;
+  for (it = sizes.rbegin(), itFaktor = mFactors.rbegin(); it != itEnd; ++it, ++itFaktor)
+    {
+      *itFaktor = tmpDataSize;
+      tmpDataSize *= *it;
+    }
+  mData.resize(tmpDataSize);
+}
+
+CCopasiArray::data_type & CCopasiArray::operator[] (const index_type & index)
+{
+#ifdef COPASI_DEBUG
+  assert(index.size() == mDim);
+#endif
+
+  unsigned int tmpindex = 0;
+  index_type::const_iterator itIndex, it, itEnd = mFactors.end();
+  for (itIndex = index.begin(), it = mFactors.begin(); it != itEnd; ++it, ++itIndex)
+    tmpindex += *itIndex * *it;
+
+  return mData[tmpindex];
+}
+
+const CCopasiArray::data_type & CCopasiArray::operator[] (const index_type & index) const
+  {
+#ifdef COPASI_DEBUG
+    assert(index.size() == mDim);
+#endif
+
+    unsigned int tmpindex = 0;
+    index_type::const_iterator itIndex, it, itEnd = mFactors.end();
+    for (itIndex = index.begin(), it = mFactors.begin(); it != itEnd; ++it, ++itIndex)
+      tmpindex += *itIndex * *it;
+
+    return mData[tmpindex];
+  }
+
+//
+//*******************************************************
 
 CAnnotatedMatrix::CAnnotatedMatrix(const std::string & name,
                                    const CCopasiContainer * pParent):
--- copasi/utilities/CAnnotatedMatrix.h
+++ copasi/utilities/CAnnotatedMatrix.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CAnnotatedMatrix.h,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: ssahle $ 
-   $Date: 2004/11/26 16:54:26 $
+   $Date: 2005/10/05 15:57:04 $
    End CVS Header */
 
 #ifndef CANNOTATEDMATRIX_H
@@ -14,12 +14,46 @@
 
 //preliminary
 #include <string>
-#define C_KEYTYPE std::string
 
+#include "copasi.h"
+
+/**
+ * this class contains an n-dimensional array
+ */
+class CCopasiArray
+  {
+  public:
+    typedef std::vector<unsigned int> index_type;
+    typedef C_FLOAT64 data_type;
+
+    CCopasiArray();
+    CCopasiArray(const index_type & sizes);
+
+    void resize(const index_type & sizes);
+
+    data_type & operator[] (const index_type & index);
+    const data_type & operator[] (const index_type & index) const;
+
+  private:
+    std::vector<data_type> mData;
+    std::vector<unsigned int> mSizes;
+    unsigned int mDim;
+    std::vector<unsigned int> mFactors;
+
+  public:
+    const index_type & size() const
+      {return mSizes;}
+
+    unsigned int dimensionality() const
+      {return mDim;}
+  };
+
+#define C_KEYTYPE std::string
 #include "CCopasiVector.h"
 
 /**
  */
+
 class CAnnotatedMatrix : public CMatrix<C_FLOAT64>, public CCopasiContainer
   {
   private:
