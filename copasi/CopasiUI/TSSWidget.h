2cd7272bbcd3e677aa6bf573f8583e120b308c95
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2005-10-05 13:41:09 +0000

    merged changes from Branch-16. For now, this class can be used as a template for new task widgets.

--- copasi/CopasiUI/TSSWidget.h
+++ copasi/CopasiUI/TSSWidget.h
@@ -1,83 +1,75 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/TSSWidget.h,v $
-   $Revision: 1.1 $
+   $Revision: 1.2 $
    $Name:  $
    $Author: ssahle $ 
-   $Date: 2005/06/22 14:23:07 $
+   $Date: 2005/10/05 13:41:09 $
    End CVS Header */
 
 #ifndef TSSWIDGET_H
 #define TSSWIDGET_H
 
-#include "copasi.h"
-#include <qvariant.h>
-#include <qwidget.h>
-#include "copasiWidget.h"
+#include "copasi.h" 
+//#include <qvariant.h>
+//#include <qwidget.h>
+#include "TaskWidget.h"
 
-class QVBoxLayout;
-class QHBoxLayout;
-class QGridLayout;
-class QCheckBox;
-class QFrame;
-class QLabel;
-class QLineEdit;
-class QPushButton;
-class QRadioButton;
-class QTable;
-class CTSSTask;
-class CTSSProblem;
-class CTSSMethod;
-class CModel;
+//class QVBoxLayout;
+//class QHBoxLayout;
+//class QGridLayout;
+//class QCheckBox;
+//class QFrame;
+//class QLabel;
+//class QLineEdit;
+//class QPushButton;
+//class QRadioButton;
+//class QTable;
 
-class TSSWidget : public CopasiWidget
+class TSSWidget : public TaskWidget
   {
     Q_OBJECT
 
   public:
-    virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
-    virtual bool leave();
-    virtual bool enter(const std::string & key = "");
+    //virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
+    //virtual bool leave();
+    //virtual bool enter(const std::string & key = "");
 
     TSSWidget(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
     ~TSSWidget();
 
-    void loadTask();
+    virtual bool runTask();
 
-    QFrame* line7;
-    QFrame* line6;
-    QFrame* line8;
-    QFrame* line8_2;
-    QLabel* taskNameLabel;
+  protected:
+    virtual bool loadTask();
+    virtual bool saveTask();
+
     //QCheckBox* taskJacobian;
     //QCheckBox* taskStability;
     //QLabel* taskDescriptionLabel;
-    QLabel* parameterValueLabel;
-    QCheckBox* bExecutable;
+    //QLabel* parameterValueLabel;
+    //QCheckBox* bExecutable;
     // QCheckBox* setInitialState;
     //QLineEdit* taskName;
-    QTable* parameterTable;
-    QPushButton* bRunButton;
-    QPushButton* cancelChange;
+    //QTable* parameterTable;
+    //QPushButton* bRunButton;
+    //QPushButton* cancelChange;
     //QPushButton* ExportFileButton;
-    QPushButton* reportDefinitionButton;
-
-  signals:
-    //virtual void runFinished(CModel*);
+    //QPushButton* reportDefinitionButton;
 
   public slots:
-    virtual void CancelButtonClicked();
-    virtual void CommitButtonClicked();
+    //virtual void CancelButtonClicked();
+    //virtual void CommitButtonClicked();
     //virtual void ExportToFileButtonClicked();
-    virtual void parameterValueChanged();
+    //virtual void parameterValueChanged();
     //virtual void RunButtonChecked();
-    virtual void runTask();
-    virtual void ReportDefinitionClicked();
+    //virtual void runTask();
+    //virtual void ReportDefinitionClicked();
 
   protected:
-    QGridLayout* TSSWidgetLayout;
-    QHBoxLayout* Layout2;
-    std::string objKey;
-    QWidget* pParent;
+    //QGridLayout* TSSWidgetLayout;
+    //QHBoxLayout* Layout2;
+    //std::string objKey;
+    //QWidget* pParent;
   };
 
 #endif // STEADYSTATEWIDGET_H
