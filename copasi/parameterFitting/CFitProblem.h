6128b033f47f996e41fe1d786b01ad3d30fd219b
Stefan Hoops <shoops@vbi.vt.edu>
2005-10-05 16:21:16 +0000

    Removed unused methods.

--- copasi/parameterFitting/CFitProblem.h
+++ copasi/parameterFitting/CFitProblem.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/parameterFitting/CFitProblem.h,v $
-   $Revision: 1.4 $
+   $Revision: 1.5 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2005/10/03 15:35:08 $
+   $Date: 2005/10/05 16:21:16 $
    End CVS Header */
 
 #ifndef COPASI_CFitProblem
@@ -73,25 +73,6 @@ class CFitProblem : public COptProblem
      * @result bool continue
      */
     virtual bool calculate();
-
-    /**
-     * Do all neccessary restore procedures so that the
-     * model is in the same state as before
-     * @result bool succes
-     */
-    virtual bool restore();
-
-    /**
-     * calculate function for optimization
-     * @result bool fullfilled
-     */
-    virtual bool checkParametricConstraints();
-
-    /**
-     * calculate function for optimization
-     * @result bool fullfilled
-     */
-    virtual bool checkFunctionalConstraints();
 
     /**
      * This is the output method for any object. The default implementation
