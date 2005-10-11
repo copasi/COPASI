10f0208445e5457ebadab70e4e6359a8187c3ca9
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2005-10-11 16:33:26 +0000

    add widget for sensitivity analysis (work in progress, not activated)

---
+++ copasi/CopasiUI/SensitivitiesWidget.cpp
@@ -0,0 +1,135 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/SensitivitiesWidget.cpp,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2005/10/11 16:33:26 $
+   End CVS Header */
+
+#include <qfiledialog.h>
+
+#include <qvariant.h>
+#include <qcheckbox.h>
+#include <qframe.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qradiobutton.h>
+#include <qtable.h>
+#include <qlayout.h>
+#include <qtooltip.h>
+#include <qwhatsthis.h>
+#include <qmessagebox.h>
+
+#include "SensitivitiesWidget.h"
+#include "DataModelGUI.h"
+#include "qtUtilities.h"
+#include "listviews.h"
+#include "CProgressBar.h"
+#include "copasiui3window.h"
+#include "CQTaskBtnWidget.h"
+#include "CQTaskHeaderWidget.h"
+
+#include "CopasiDataModel/CCopasiDataModel.h"
+#include "sensitivities/CSensTask.h"
+#include "sensitivities/CSensProblem.h"
+#include "model/CModel.h"
+#include "utilities/CCopasiException.h"
+#include "report/CKeyFactory.h"
+
+/**
+ *  Constructs a SensitivitiesWidget which is a child of 'parent', with the 
+ *  name 'name' and widget flags set to 'f'.
+ */
+SensitivitiesWidget::SensitivitiesWidget(QWidget* parent, const char* name, WFlags fl)
+    : TaskWidget(parent, name, fl)
+{
+  if (!name)
+    setName("SensitivitiesWidget");
+  setCaption(trUtf8("SensitivitiesWidget"));
+
+  //if a mpMethodLayout is created here, it will be used by addMethodXXX() below.
+  mpMethodLayout = new QGridLayout(this, 1, 1, 11, 6, "mpMethodLayout");
+
+  //SensitivitiesWidgetLayout->insertWidget(0, mpHeaderWidget);
+
+  mpHeaderWidget->setTaskName("Sensitivities");
+  addHeaderToGrid();
+  addHLineToGrid(mpMethodLayout, 1, 2);
+
+  //addMethodSelectionBox(CSensTask::ValidMethods);
+  addMethodParameterTable();
+
+  //QSpacerItem* spacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+  //mpMethodLayout->addMultiCell(spacer, 10, 10, 0, 1);
+
+  mpMethodLayout->addMultiCellWidget(mpBtnWidget, 11, 11, 0, 2);
+}
+
+/*
+ *  Destroys the object and frees any allocated resources
+ */
+SensitivitiesWidget::~SensitivitiesWidget()
+{}
+
+bool SensitivitiesWidget::saveTask()
+{
+  //saveCommon();
+  //saveMethod();
+
+  CSensTask* sensTask =
+    dynamic_cast<CSensTask *>(GlobalKeys.get(mObjectKey));
+  assert(sensTask);
+
+  CSensProblem* problem =
+    dynamic_cast<CSensProblem *>(sensTask->getProblem());
+  assert(problem);
+
+  CSensMethod* method =
+    dynamic_cast<CSensMethod *>(sensTask->getMethod());
+  assert(method);
+
+  //...
+
+  // :TODO Bug 322: This should only be called when actual changes have been saved.
+  if (mpChanged) CCopasiDataModel::Global->changed();
+  return true;
+}
+
+CCopasiMethod * SensitivitiesWidget::createMethod(const CCopasiMethod::SubType & type)
+{return CSensMethod::createSensMethod(type);}
+
+bool SensitivitiesWidget::runTask()
+{
+  //if (!commonBeforeRunTask()) return false;
+
+  //commonAfterRunTask();
+
+  //pListView->switchToOtherWidget(211, ""); //change to the results window
+
+  return true;
+}
+
+bool SensitivitiesWidget::loadTask()
+{
+  //loadCommon();
+  //loadMethod();
+
+  CSensTask* sensTask =
+    dynamic_cast<CSensTask *>(GlobalKeys.get(mObjectKey));
+  assert(sensTask);
+
+  CSensProblem* problem =
+    dynamic_cast<CSensProblem *>(sensTask->getProblem());
+  assert(problem);
+
+  CSensMethod* method =
+    dynamic_cast<CSensMethod *>(sensTask->getMethod());
+  assert(method);
+
+  //...
+
+  mpChanged = false;
+
+  return true;
+}
---
+++ copasi/UI/SensitivitiesWidget.h
@@ -0,0 +1,76 @@
+/* Begin CVS Header
+   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/SensitivitiesWidget.h,v $
+   $Revision: 1.1 $
+   $Name:  $
+   $Author: ssahle $ 
+   $Date: 2005/10/11 16:33:26 $
+   End CVS Header */
+
+#ifndef SENSWIDGET_H
+#define SENSWIDGET_H
+
+#include "copasi.h" 
+//#include <qvariant.h>
+//#include <qwidget.h>
+#include "TaskWidget.h"
+
+//class QVBoxLayout;
+//class QHBoxLayout;
+//class QGridLayout;
+//class QCheckBox;
+//class QFrame;
+//class QLabel;
+//class QLineEdit;
+//class QPushButton;
+//class QRadioButton;
+//class QTable;
+
+class SensitivitiesWidget : public TaskWidget
+  {
+    Q_OBJECT
+
+  public:
+    //virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
+    //virtual bool leave();
+    //virtual bool enter(const std::string & key = "");
+
+    explicit SensitivitiesWidget(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
+    ~SensitivitiesWidget();
+
+    virtual bool runTask();
+
+  protected:
+    virtual bool loadTask();
+    virtual bool saveTask();
+    virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
+
+    //QCheckBox* taskJacobian;
+    //QCheckBox* taskStability;
+    //QLabel* taskDescriptionLabel;
+    //QLabel* parameterValueLabel;
+    //QCheckBox* bExecutable;
+    // QCheckBox* setInitialState;
+    //QLineEdit* taskName;
+    //QTable* parameterTable;
+    //QPushButton* bRunButton;
+    //QPushButton* cancelChange;
+    //QPushButton* ExportFileButton;
+    //QPushButton* reportDefinitionButton;
+
+  public slots:
+    //virtual void CancelButtonClicked();
+    //virtual void CommitButtonClicked();
+    //virtual void ExportToFileButtonClicked();
+    //virtual void parameterValueChanged();
+    //virtual void RunButtonChecked();
+    //virtual void runTask();
+    //virtual void ReportDefinitionClicked();
+
+  protected:
+    //QVBoxLayout* SensitivitiesWidgetLayout;
+    //QGridLayout* mpGridLayout;
+    //QTable* mpTblParameter;
+    //QLabel* mpLblParameter;
+  };
+
+#endif // STEADYSTATEWIDGET_H
