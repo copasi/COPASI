694628c61032701f131e5ccc30f30116c11c0e0c
Stefan Hoops <shoops@vbi.vt.edu>
2006-01-11 16:45:38 +0000

    Use blas routine dgemm for matrix multiplications.

--- copasi/model/CState.cpp
+++ copasi/model/CState.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/model/CState.cpp,v $
-   $Revision: 1.53.2.2 $
+   $Revision: 1.53.2.3 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2006/01/10 14:12:07 $
+   $Date: 2006/01/11 16:45:38 $
    End CVS Header */
 
 // CSate.cpp
@@ -12,10 +12,13 @@
 //
 
 #include "copasi.h"
+
 #include "CState.h"
 #include "CModel.h"
 #include "utilities/CVector.h"
 #include "utilities/utility.h"
+
+#include "blaswrap.h"
 
 /*************************/
 /* Code for class CState */
@@ -188,26 +191,145 @@ void CState::calculateJacobian(CMatrix< C_FLOAT64 > & jacobian,
 #ifdef COPASI_DEBUG
     this->check("calculate Jacobian");
 #endif
-
+    static CMatrix< C_FLOAT64 > E;
     const CMatrix< C_FLOAT64 > & Stoi = mpModel->getStoi();
-    unsigned C_INT32 mNo = Stoi.numRows();
-    unsigned C_INT32 rNo = Stoi.numCols();
 
-    CMatrix< C_FLOAT64 > E(rNo, mNo);
+    char T = 'N';
+    C_INT M = Stoi.numRows(); /* LDA, LDC, N */
+    C_INT K = Stoi.numCols(); /* LDB */
+
+    C_FLOAT64 Alpha = 1.0;
+    C_FLOAT64 Beta = 0.0;
+
+    E.resize(K, M);
+
     calculateElasticityMatrix(E, factor, resolution);
 
-    unsigned C_INT32 i, j, k;
-    C_FLOAT64 * sum;
-
-    for (i = 0; i < mNo; i++)
-      for (j = 0; j < mNo; j++)
-        {
-          sum = &jacobian(i, j);
-          *sum = 0.0;
-
-          for (k = 0; k < rNo; k++)
-            *sum += Stoi(i, k) * E(k, j);
-        }
+    /*     SUBROUTINE DGEMM (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB,
+     *                        BETA, C, LDC)
+     *     .. Scalar Arguments ..
+     *     CHARACTER*1        TRANSA, TRANSB
+     *      INTEGER            M, N, K, LDA, LDB, LDC
+     *      DOUBLE PRECISION   ALPHA, BETA
+     *     .. Array Arguments ..
+     *      DOUBLE PRECISION   A(LDA, *), B(LDB, *), C(LDC, *)
+     *     ..
+     *
+     *  Purpose
+     *  =======
+     *
+     *  DGEMM  performs one of the matrix-matrix operations
+     *
+     *     C := alpha*op(A)*op(B) + beta*C,
+     *
+     *  where  op(X) is one of
+     *
+     *     op(X) = X   or   op(X) = X',
+     *
+     *  alpha and beta are scalars, and A, B and C are matrices, with op(A)
+     *  an m by k matrix,  op(B)  a  k by n matrix and  C an m by n matrix.
+     *
+     *  Parameters
+     *  ==========
+     *
+     *  TRANSA - CHARACTER*1.
+     *           On entry, TRANSA specifies the form of op(A) to be used in
+     *           the matrix multiplication as follows:
+     *
+     *              TRANSA = 'N' or 'n',  op(A) = A.
+     *
+     *              TRANSA = 'T' or 't',  op(A) = A'.
+     *
+     *              TRANSA = 'C' or 'c',  op(A) = A'.
+     *
+     *           Unchanged on exit.
+     *
+     *  TRANSB - CHARACTER*1.
+     *           On entry, TRANSB specifies the form of op(B) to be used in
+     *           the matrix multiplication as follows:
+     *
+     *              TRANSB = 'N' or 'n',  op(B) = B.
+     *
+     *              TRANSB = 'T' or 't',  op(B) = B'.
+     *
+     *              TRANSB = 'C' or 'c',  op(B) = B'.
+     *
+     *           Unchanged on exit.
+     *
+     *  M      - INTEGER.
+     *           On entry,  M  specifies  the number  of rows  of the  matrix
+     *           op(A)  and of the  matrix  C.  M  must  be at least  zero.
+     *           Unchanged on exit.
+     *
+     *  N      - INTEGER.
+     *           On entry,  N  specifies the number  of columns of the matrix
+     *           op(B) and the number of columns of the matrix C. N must be
+     *           at least zero.
+     *           Unchanged on exit.
+     *
+     *  K      - INTEGER.
+     *           On entry,  K  specifies  the number of columns of the matrix
+     *           op(A) and the number of rows of the matrix op(B). K must
+     *           be at least  zero.
+     *           Unchanged on exit.
+     *
+     *  ALPHA  - DOUBLE PRECISION.
+     *           On entry, ALPHA specifies the scalar alpha.
+     *           Unchanged on exit.
+     *
+     *  A      - DOUBLE PRECISION array of DIMENSION (LDA, ka), where ka is
+     *           k  when  TRANSA = 'N' or 'n',  and is  m  otherwise.
+     *           Before entry with  TRANSA = 'N' or 'n',  the leading  m by k
+     *           part of the array  A  must contain the matrix  A,  otherwise
+     *           the leading  k by m  part of the array  A  must contain  the
+     *           matrix A.
+     *           Unchanged on exit.
+     *
+     *  LDA    - INTEGER.
+     *           On entry, LDA specifies the first dimension of A as declared
+     *           in the calling (sub) program. When  TRANSA = 'N' or 'n' then
+     *           LDA must be at least  max(1, m), otherwise  LDA must be at
+     *           least  max(1, k).
+     *           Unchanged on exit.
+     *
+     *  B      - DOUBLE PRECISION array of DIMENSION (LDB, kb), where kb is
+     *           n  when  TRANSB = 'N' or 'n',  and is  k  otherwise.
+     *           Before entry with  TRANSB = 'N' or 'n',  the leading  k by n
+     *           part of the array  B  must contain the matrix  B,  otherwise
+     *           the leading  n by k  part of the array  B  must contain  the
+     *           matrix B.
+     *           Unchanged on exit.
+     *
+     *  LDB    - INTEGER.
+     *           On entry, LDB specifies the first dimension of B as declared
+     *           in the calling (sub) program. When  TRANSB = 'N' or 'n' then
+     *           LDB must be at least  max(1, k), otherwise  LDB must be at
+     *           least  max(1, n).
+     *           Unchanged on exit.
+     *
+     *  BETA   - DOUBLE PRECISION.
+     *           On entry,  BETA  specifies the scalar  beta.  When  BETA  is
+     *           supplied as zero then C need not be set on input.
+     *           Unchanged on exit.
+     *
+     *  C      - DOUBLE PRECISION array of DIMENSION (LDC, n).
+     *           Before entry, the leading  m by n  part of the array  C must
+     *           contain the matrix  C,  except when  beta  is zero, in which
+     *           case C need not be set on entry.
+     *           On exit, the array  C  is overwritten by the  m by n  matrix
+     *           (alpha*op(A)*op(B) + beta*C).
+     *
+     *  LDC    - INTEGER.
+     *           On entry, LDC specifies the first dimension of C as declared
+     *           in  the  calling  (sub)  program.   LDC  must  be  at  least
+     *           max(1, m).
+     *           Unchanged on exit.
+     *
+     *
+     *  Level 3 Blas routine.
+     */
+    dgemm_(&T, &T, &M, &M, &K, &Alpha, E.array(), &M,
+           const_cast<C_FLOAT64 *>(Stoi.array()), &K, &Beta, jacobian.array(), &M);
   }
 
 void CState::calculateElasticityMatrix(CMatrix< C_FLOAT64 > & elasticityMatrix,
@@ -396,41 +518,161 @@ void CStateX::calculateJacobian(CMatrix< C_FLOAT64 > & jacobian,
 #ifdef COPASI_DEBUG
     this->check("calculate Jacobian");
 #endif
+    static CMatrix< C_FLOAT64 > E;
+    static CMatrix< C_FLOAT64 > tmp;
 
-    const CModel::CLinkMatrixView & L = mpModel->getL();
-    unsigned C_INT32 mNo = L.numRows();
-    unsigned C_INT32 iNo = L.numCols();
-
+    const CMatrix< C_FLOAT64 > & L = mpModel->getL0();
     const CMatrix< C_FLOAT64 > & redStoi = mpModel->getRedStoi();
-    unsigned C_INT32 rNo = redStoi.numCols();
 
-    CMatrix< C_FLOAT64 > E(rNo, mNo);
+    char T = 'N';
+    C_INT M = mpModel->getNumIndependentMetabs(); /* LDA, LDC */
+    C_INT N = redStoi.numCols();
+    C_INT K = mpModel->getNumDependentMetabs();
+    C_INT LD = mpModel->getNumVariableMetabs();
+
+    C_FLOAT64 Alpha = 1.0;
+    C_FLOAT64 Beta = 1.0;
+
+    E.resize(N, LD);
+    tmp.resize(N, LD);
+
     calculateElasticityMatrix(E, factor, resolution);
 
-    CMatrix< C_FLOAT64 > tmp(rNo, iNo);
+    memcpy(tmp.array(), E.array(), N * LD * sizeof(C_FLOAT64));
 
-    unsigned C_INT32 i, j, k;
-    C_FLOAT64 * sum;
+    /*     SUBROUTINE DGEMM (TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB,
+     *                        BETA, C, LDC)
+     *     .. Scalar Arguments ..
+     *     CHARACTER*1        TRANSA, TRANSB
+     *      INTEGER            M, N, K, LDA, LDB, LDC
+     *      DOUBLE PRECISION   ALPHA, BETA
+     *     .. Array Arguments ..
+     *      DOUBLE PRECISION   A(LDA, *), B(LDB, *), C(LDC, *)
+     *     ..
+     *
+     *  Purpose
+     *  =======
+     *
+     *  DGEMM  performs one of the matrix-matrix operations
+     *
+     *     C := alpha*op(A)*op(B) + beta*C,
+     *
+     *  where  op(X) is one of
+     *
+     *     op(X) = X   or   op(X) = X',
+     *
+     *  alpha and beta are scalars, and A, B and C are matrices, with op(A)
+     *  an m by k matrix,  op(B)  a  k by n matrix and  C an m by n matrix.
+     *
+     *  Parameters
+     *  ==========
+     *
+     *  TRANSA - CHARACTER*1.
+     *           On entry, TRANSA specifies the form of op(A) to be used in
+     *           the matrix multiplication as follows:
+     *
+     *              TRANSA = 'N' or 'n',  op(A) = A.
+     *
+     *              TRANSA = 'T' or 't',  op(A) = A'.
+     *
+     *              TRANSA = 'C' or 'c',  op(A) = A'.
+     *
+     *           Unchanged on exit.
+     *
+     *  TRANSB - CHARACTER*1.
+     *           On entry, TRANSB specifies the form of op(B) to be used in
+     *           the matrix multiplication as follows:
+     *
+     *              TRANSB = 'N' or 'n',  op(B) = B.
+     *
+     *              TRANSB = 'T' or 't',  op(B) = B'.
+     *
+     *              TRANSB = 'C' or 'c',  op(B) = B'.
+     *
+     *           Unchanged on exit.
+     *
+     *  M      - INTEGER.
+     *           On entry,  M  specifies  the number  of rows  of the  matrix
+     *           op(A)  and of the  matrix  C.  M  must  be at least  zero.
+     *           Unchanged on exit.
+     *
+     *  N      - INTEGER.
+     *           On entry,  N  specifies the number  of columns of the matrix
+     *           op(B) and the number of columns of the matrix C. N must be
+     *           at least zero.
+     *           Unchanged on exit.
+     *
+     *  K      - INTEGER.
+     *           On entry,  K  specifies  the number of columns of the matrix
+     *           op(A) and the number of rows of the matrix op(B). K must
+     *           be at least  zero.
+     *           Unchanged on exit.
+     *
+     *  ALPHA  - DOUBLE PRECISION.
+     *           On entry, ALPHA specifies the scalar alpha.
+     *           Unchanged on exit.
+     *
+     *  A      - DOUBLE PRECISION array of DIMENSION (LDA, ka), where ka is
+     *           k  when  TRANSA = 'N' or 'n',  and is  m  otherwise.
+     *           Before entry with  TRANSA = 'N' or 'n',  the leading  m by k
+     *           part of the array  A  must contain the matrix  A,  otherwise
+     *           the leading  k by m  part of the array  A  must contain  the
+     *           matrix A.
+     *           Unchanged on exit.
+     *
+     *  LDA    - INTEGER.
+     *           On entry, LDA specifies the first dimension of A as declared
+     *           in the calling (sub) program. When  TRANSA = 'N' or 'n' then
+     *           LDA must be at least  max(1, m), otherwise  LDA must be at
+     *           least  max(1, k).
+     *           Unchanged on exit.
+     *
+     *  B      - DOUBLE PRECISION array of DIMENSION (LDB, kb), where kb is
+     *           n  when  TRANSB = 'N' or 'n',  and is  k  otherwise.
+     *           Before entry with  TRANSB = 'N' or 'n',  the leading  k by n
+     *           part of the array  B  must contain the matrix  B,  otherwise
+     *           the leading  n by k  part of the array  B  must contain  the
+     *           matrix B.
+     *           Unchanged on exit.
+     *
+     *  LDB    - INTEGER.
+     *           On entry, LDB specifies the first dimension of B as declared
+     *           in the calling (sub) program. When  TRANSB = 'N' or 'n' then
+     *           LDB must be at least  max(1, k), otherwise  LDB must be at
+     *           least  max(1, n).
+     *           Unchanged on exit.
+     *
+     *  BETA   - DOUBLE PRECISION.
+     *           On entry,  BETA  specifies the scalar  beta.  When  BETA  is
+     *           supplied as zero then C need not be set on input.
+     *           Unchanged on exit.
+     *
+     *  C      - DOUBLE PRECISION array of DIMENSION (LDC, n).
+     *           Before entry, the leading  m by n  part of the array  C must
+     *           contain the matrix  C,  except when  beta  is zero, in which
+     *           case C need not be set on entry.
+     *           On exit, the array  C  is overwritten by the  m by n  matrix
+     *           (alpha*op(A)*op(B) + beta*C).
+     *
+     *  LDC    - INTEGER.
+     *           On entry, LDC specifies the first dimension of C as declared
+     *           in  the  calling  (sub)  program.   LDC  must  be  at  least
+     *           max(1, m).
+     *           Unchanged on exit.
+     *
+     *
+     *  Level 3 Blas routine.
+     */
 
-    for (i = 0; i < rNo; i++)
-      for (j = 0; j < iNo; j++)
-        {
-          sum = &tmp(i, j);
-          *sum = E(i, j);
+    // tmp = (E1, E2) (I, L0')' = E1 + E2 * L0
+    dgemm_(&T, &T, &M, &N, &K, &Alpha, const_cast<C_FLOAT64 *>(L.array()), &M,
+           E.array() + M, &LD, &Beta, tmp.array(), &LD);
 
-          for (k = iNo; k < mNo; k++)
-            *sum += E(i, k) * L(k, j);
-        }
+    Beta = 0.0;
 
-    for (i = 0; i < iNo; i++)
-      for (j = 0; j < iNo; j++)
-        {
-          sum = &jacobian(i, j);
-          *sum = 0.0;
-
-          for (k = 0; k < rNo; k++)
-            *sum += redStoi(i, k) * tmp(k, j);
-        }
+    // j = R * tmp
+    dgemm_(&T, &T, &M, &M, &N, &Alpha, tmp.array(), &LD,
+           const_cast<C_FLOAT64 *>(redStoi.array()), &N, &Beta, jacobian.array(), &M);
   }
 
 void CStateX::updateDependentNumbers()
