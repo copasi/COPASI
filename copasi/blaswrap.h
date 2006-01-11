45976a32e07957f90648de591f4da4e3f19107b0
Stefan Hoops <shoops@vbi.vt.edu>
2006-01-11 16:19:39 +0000

    Fixed for Darwin.

--- copasi/blaswrap.h
+++ copasi/blaswrap.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/blaswrap.h,v $
-   $Revision: 1.1.2.3 $
+   $Revision: 1.1.2.4 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2006/01/11 14:45:49 $
+   $Date: 2006/01/11 16:19:39 $
    End CVS Header */
 
 #ifndef __BLAS_H
@@ -21,15 +21,26 @@ extern "C"
   {
 #ifdef USE_MKL
 # include "mkl_lapack.h"
-#else
-# if (defined USE_CLAPACK || defined USE_LAPACK)
-#  include "f2c.h"
-#  include "blas.h"
-#  ifdef USE_CLAPACK
-#   define dgemm_ f2c_dgemm
-#  endif // USE_CLAPACK
-# endif // USE_CLAPACK || USE_LAPACK
 #endif // USE_MKL
+
+#if (defined USE_CLAPACK || defined USE_LAPACK)
+# include "f2c.h"
+# include "blas.h"
+# ifdef USE_CLAPACK
+#  define dgemm_ f2c_dgemm
+# endif // USE_CLAPACK
+#endif // USE_CLAPACK || USE_LAPACK
+
+#ifdef Darwin
+# define vector
+# include "cblas.h"
+# define dgemm_(TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC) \
+    cblas_dgemm(CblasColMajor, \
+                (* TRANSA == 'N' ? CblasNoTrans: CblasTrans), \
+                (* TRANSB == 'N' ? CblasNoTrans: CblasTrans), \
+                * M, * N, * K, * ALPHA, A, * LDA, B, * LDB, * BETA, C, * LDC)
+# define vector vector
+#endif // Darwin
   }
 
 #ifdef min
