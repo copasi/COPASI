96efd87715a3e5b27b8faf54f2e5b70f1fb533c6
Stefan Hoops <shoops@vbi.vt.edu>
2006-01-24 16:27:52 +0000

    Fixed code indentation.

--- copasi/copasilicense.h
+++ copasi/copasilicense.h
@@ -1,20 +1,20 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/Attic/copasilicense.h,v $
-   $Revision: 1.1.2.1 $
+   $Revision: 1.1.2.2 $
    $Name:  $
    $Author: shoops $ 
-   $Date: 2006/01/24 16:10:42 $
+   $Date: 2006/01/24 16:27:52 $
    End CVS Header */
 
 #ifndef COPASI_license
- #define COPASI_license
+#define COPASI_license
 
 #ifdef COPASI_LICENSE_US
- # include "LicenseUS.txt.h"
- #endif // COPASI_LICENSE_US
+# include "LicenseUS.txt.h"
+#endif // COPASI_LICENSE_US
 
 #ifdef COPASI_LICENSE_DE
- # include "LicenseDE.txt.h"
- #endif // COPASI_LICENSE_DE
+# include "LicenseDE.txt.h"
+#endif // COPASI_LICENSE_DE
 
 #endif // COPASI_license
