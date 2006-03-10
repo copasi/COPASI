99fb7bf6927f61c06ac2082539ae78ac35ac9f70
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2006-03-10 10:04:54 +0000

    disable the not yet complete expand feature

--- copasi/CopasiUI/DifferentialEquations.cpp
+++ copasi/CopasiUI/DifferentialEquations.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/Attic/DifferentialEquations.cpp,v $
-   $Revision: 1.25 $
+   $Revision: 1.26 $
    $Name:  $
    $Author: ssahle $ 
-   $Date: 2006/03/10 09:57:41 $
+   $Date: 2006/03/10 10:04:54 $
    End CVS Header */
 
 /*******************************************************************
@@ -311,7 +311,7 @@ void DifferentialEquations::loadDifferentialEquations(CModel * model)
           mml << SPC(l + 2) << "<mtd columnalign='left'>" << std::endl;
           writeRHS(mml, model->getMetabolites()[i],
                    dynamic_cast<CReaction*>(GlobalKeys.get(*it)) ,
-                   true, false, l + 3); //TODO make configurable
+                   false, false, l + 3); //TODO make configurable
           mml << SPC(l + 2) << "</mtd>" << std::endl;
 
           mml << SPC(l + 1) << "</mtr>" << std::endl;
