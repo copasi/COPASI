dd09e86b118f0aeb24a0088cdc6b4b846439a427
Stefan Hoops <shoops@vbi.vt.edu>
2006-03-16 18:08:58 +0000

    Fixed Bug 451. The check for mDim == 0 was broken since mDim had become an array to move the "this" pointer as its second value.

--- copasi/trajectory/CLsodaMethod.cpp
+++ copasi/trajectory/CLsodaMethod.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/trajectory/CLsodaMethod.cpp,v $
-   $Revision: 1.38 $
+   $Revision: 1.39 $
    $Name:  $
-   $Author: ssahle $ 
-   $Date: 2006/03/16 10:07:05 $
+   $Author: shoops $ 
+   $Date: 2006/03/16 18:08:58 $
    End CVS Header */
 
 #include "copasi.h"
@@ -50,7 +50,7 @@ CLsodaMethod::~CLsodaMethod()
 
 void CLsodaMethod::step(const double & deltaT)
 {
-  if (!mDim) //just do nothing if there are no variables
+  if (!mDim[0]) //just do nothing if there are no variables
     {
       mTime = mTime + deltaT;
       mpState->setTime(mTime);
@@ -64,23 +64,23 @@ void CLsodaMethod::step(const double & deltaT)
   C_INT DSize = mDWork.size();
   C_INT ISize = mIWork.size();
 
-  mLSODA(&EvalF,             //  1. evaluate F
-         mDim,               //  2. number of variables
+  mLSODA(&EvalF ,          //  1. evaluate F
+         mDim ,            //  2. number of variables
          mY ,             //  3. the array of current concentrations
-         &mTime,             //  4. the current time
-         &EndTime,           //  5. the final time
-         &one,               //  6. scalar error control
-         &mRtol,             //  7. relative tolerance array
-         &mAtol,             //  8. absolute tolerance array
-         &mState,            //  9. output by overshoot & interpolatation
-         &mLsodaStatus,      // 10. the state control variable
-         &one,               // 11. futher options (one)
-         mDWork.array(),     // 12. the double work array
-         &DSize,             // 13. the double work array size
-         mIWork.array(),     // 14. the int work array
-         &ISize,             // 15. the int work array size
-         NULL,               // 16. evaluate J (not given)
-         &mJType);       // 17. the type of jacobian calculate (2)
+         &mTime ,          //  4. the current time
+         &EndTime ,        //  5. the final time
+         &one ,            //  6. scalar error control
+         &mRtol ,          //  7. relative tolerance array
+         &mAtol ,          //  8. absolute tolerance array
+         &mState ,         //  9. output by overshoot & interpolatation
+         &mLsodaStatus ,   // 10. the state control variable
+         &one ,            // 11. futher options (one)
+         mDWork.array() ,  // 12. the double work array
+         &DSize ,          // 13. the double work array size
+         mIWork.array() ,  // 14. the int work array
+         &ISize ,          // 15. the int work array size
+         NULL ,            // 16. evaluate J (not given)
+         &mJType);        // 17. the type of jacobian calculate (2)
 
   if (mLsodaStatus == -1) mLsodaStatus = 2;
 
