d190f46b8e7542143f9e84bbba6806d6da1bf0f7
Ralph Gauges <gauges@hs-albsig.de>
2006-04-21 15:40:51 +0000

    Fixed a few bugs. The run.sh script can now be called with a file that contains a list of model names so we don't have to run all. Somehow the wrapper does not run correctly yet.

--- stochastic-testsuite/run.sh
+++ stochastic-testsuite/run.sh
@@ -1,13 +1,20 @@
 
-NUM_REPEATS=10000
+NUM_REPEATS=10
 
 HEAD=/usr/bin/head
 CUT=/usr/bin/cut
 
+#HEAD=/sw/bin/head
+#CUT=/sw/bin/cut
+
 WRAPPER=./stochastic-testsuite
 
+#WRAPPER=`pwd`/stochastic-testsuite.app/Contents/MacOS/stochastic-testsuite
+
 # go over all models in tests/model-list
-MODELS=`cat tests/model-list`
+#MODELS=`cat tests/model-list`
+
+MODELS=`cat $1` 
 
 function getSpecies()
 {
@@ -29,6 +36,6 @@ for MODEL in $MODELS;do
   # calculate mean and standard deviation
   ./calculate_statistics.py $OUTFILE $MEAN_OUTFILE $SD_OUTFILE $STEPNUMBER $NUM_REPEATS
   # compare results
-  ./compare_mean.py ${MODEL}-mean.RESULT$ ${MODEL}-mean.csv ${MODEL}-sd.csv $NUM_REPEATS ${MODEL}-mean-compare.RESULT 
-  ./compare_sd.py ${MODEL}-sd.RESULT$ {MODEL}-sd.csv $NUM_REPEATS ${MODEL}-sd-compare.RESULT
+  ./compare_mean.py ${MODEL}-mean.RESULT ${MODEL}-mean.csv ${MODEL}-sd.csv $NUM_REPEATS ${MODEL}-mean-compare.RESULT 
+  ./compare_sd.py ${MODEL}-sd.RESULT {MODEL}-sd.csv $NUM_REPEATS ${MODEL}-sd-compare.RESULT
 done
