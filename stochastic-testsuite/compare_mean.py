4ed729000c2bfb77a8db3edc0798684fd7c6e86c
Ralph Gauges <gauges@hs-albsig.de>
2006-04-21 15:27:53 +0000

    Fixed some bugs, now the scripts run. I still have to check if they produce the correct result.

--- stochastic-testsuite/compare_mean.py
+++ stochastic-testsuite/compare_mean.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import math
 import string
@@ -5,7 +7,7 @@ import string
 MEAN_FILE=sys.argv[1]
 REFERENCE_MEAN_FILE=sys.argv[2]
 REFERENCE_SD_FILE=sys.argv[3]
-REPEATS=sys.argv[4]
+REPEATS=float(sys.argv[4])
 OUTFILE=sys.argv[5]
 
 MEAN=file(MEAN_FILE,"r").readlines()
@@ -19,18 +21,18 @@ if(len(REFERENCE_MEAN)!=len(REFERENCE_SD) or len(MEAN) != len(REFERENCE_MEAN)):
 NUMCOLUMNS=len(string.split(MEAN[1],","))
 OUT=file(OUTFILE,"w")
 OUT.write(MEAN[0])
-for X in range(1,len(MEAN)):
-    REF_MEAN_COLS=string.split(REFERENCE_MEAN,",")[1:]
-    REF_SD_COLS=string.split(REFERENCE_SD,",")[1:]
-    MEAN_COLS=string.split(MEAN,",")[1:]
+for X in range(2,len(MEAN)):
+    REF_MEAN_COLS=string.split(REFERENCE_MEAN[X],",")[1:]
+    REF_SD_COLS=string.split(REFERENCE_SD[X],",")[1:]
+    MEAN_COLS=string.split(MEAN[X],",")[1:]
     if(len(REF_MEAN_COLS)!=len(REF_SD_COLS) or len(MEAN_COLS) != len(REF_MEAN_COLS)):
         print "Error. Number of columns differs between files at line",Y
         OUT.close()
         sys.exit(1)
     RESULT=str(X-1)+","
-    for Y in range(1,NUMCOLUMNS):
+    for Y in range(0,NUMCOLUMNS-1):
        v=(float(MEAN_COLS[Y])-float(REF_MEAN_COLS[Y]))/float(REF_SD_COLS[Y])*math.sqrt(REPEATS) 
-       if(math.abs(v)>=3.0):
+       if(math.fabs(v)>=3.0):
           print "Warning. Value to hight at %s line %d."%(MEAN_FILE,X) 
        RESULT=string.join([RESULT,str(v)],",")
     RESULT=RESULT+"\n"
--- stochastic-testsuite/compare_sd.py
+++ stochastic-testsuite/compare_sd.py
@@ -1,13 +1,15 @@
+#!/usr/bin/python
+
 import sys
 import math
 import string
 
 SD_FILE=sys.argv[1]
 REFERENCE_SD_FILE=sys.argv[2]
-REPEATS=sys.argv[3]
+REPEATS=float(sys.argv[3])
 OUTFILE=sys.argv[4]
 
-SD=file(SD,"r").readlines()
+SD=file(SD_FILE,"r").readlines()
 REFERENCE_SD=file(REFERENCE_SD_FILE,"r").readlines()
 
 if(len(SD) != len(REFERENCE_SD)):
@@ -17,17 +19,17 @@ if(len(SD) != len(REFERENCE_SD)):
 NUMCOLUMNS=len(string.split(SD[1],","))
 OUT=file(OUTFILE,"w")
 OUT.write(SD[0])
-for X in range(1,len(SD)):
-    REF_SD_COLS=string.split(REFERENCE_SD,",")[1:]
-    SD_COLS=string.split(SD,",")[1:]
+for X in range(2,len(SD)):
+    REF_SD_COLS=string.split(REFERENCE_SD[X],",")[1:]
+    SD_COLS=string.split(SD[X],",")[1:]
     if(len(SD_COLS) != len(REF_SD_COLS)):
         print "Error. Number of columns differs between files at line %d"%(X)
         OUT.close()
         sys.exit(1)
     RESULT=str(X-1)+","
-    for Y in range(1,NUMCOLUMNS):
-       v=(math.power(float(SD_COLS[Y]),2)/math.power(float(REF_SD_COLS[Y]),2)-1.0)*math.sqrt(REPEATS/2.0)
-       if(math.abs(v)>=5.0):
+    for Y in range(0,NUMCOLUMNS-1):
+       v=(math.pow(float(SD_COLS[Y]),2)/math.pow(float(REF_SD_COLS[Y]),2)-1.0)*math.sqrt(REPEATS/2.0)
+       if(math.fabs(v)>=5.0):
           print "Warning. Value to hight at %s line %d."%(MEAN_FILE,X) 
        RESULT=string.join([RESULT,str(v)],",")
     RESULT=RESULT+"\n"
