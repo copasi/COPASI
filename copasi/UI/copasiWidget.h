1bf6fd8da1a8406dcdda6d62a80fa55ff65193dd
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2006-05-20 01:44:37 +0000

    remove obsolete class

--- copasi/UI/copasiWidget.h
+++ copasi/UI/copasiWidget.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/copasiWidget.h,v $
-   $Revision: 1.17 $
+   $Revision: 1.17.2.1 $
    $Name:  $
-   $Author: shoops $
-   $Date: 2006/04/27 01:27:46 $
+   $Author: ssahle $
+   $Date: 2006/05/20 01:44:37 $
    End CVS Header */
 
 // Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
@@ -31,19 +31,9 @@ class CopasiWidget : public QWidget
   {
   public:
     CopasiWidget(QWidget * parent = 0, const char * name = 0, WFlags f = 0);
-    //virtual void resizeEvent (QResizeEvent *);
     virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
     virtual bool leave();
     virtual bool enter(const std::string & key = "");
-
-  public slots:
-    //virtual void resize (int w, int h);
-
-  private:
-    //static int realMinWidth;
-    //static int realMinHeight;
-    //bool bInitialized;
-    //bool bSetMinSize;
 
   protected:
     bool protectedNotify(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key = "");
@@ -51,13 +41,6 @@ class CopasiWidget : public QWidget
 
     static DataModelGUI* dataModel;
     ListViews* pListView;
-  };
-
-class CopasiParametersWidget : public CopasiWidget
-  {
-  public:
-    CopasiParametersWidget(QWidget * parent = 0, const char * name = 0, WFlags f = 0);
-    virtual void viewMousePressEvent(QMouseEvent * e) = 0;
   };
 
 #endif // !defined(COPASI_WIDGET_H)
