3f055ac9ee4ec32142d3437cc38ae1c87aa475f4
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2006-06-02 18:50:35 +0000

    add default report for mca

--- copasi/CopasiDataModel/CCopasiDataModel.cpp
+++ copasi/CopasiDataModel/CCopasiDataModel.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiDataModel/CCopasiDataModel.cpp,v $
-   $Revision: 1.64.2.5 $
+   $Revision: 1.64.2.6 $
    $Name:  $
-   $Author: shoops $
-   $Date: 2006/06/01 23:24:27 $
+   $Author: ssahle $
+   $Date: 2006/06/02 18:50:35 $
    End CVS Header */
 
 // Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
@@ -729,7 +729,19 @@ CReportDefinition * CCopasiDataModel::addReport(const CCopasiTask::Type & taskTy
 
       //**************************************************************************
     case CCopasiTask::mca:
-      // :TODO: implement default report for MCA
+      pReport = new CReportDefinition(CCopasiTask::TypeName[taskType]);
+      pReport->setTaskType(taskType);
+      pReport->setComment("Automatically generated report.");
+      pReport->setIsTable(false);
+      pReport->setTitle(false);
+      pReport->setSeparator(CCopasiReportSeparator("\t"));
+
+      // Header
+      pReport->getHeaderAddr()->push_back(CCopasiObjectName("CN=Root,Vector=TaskList[Metabolic Control Analysis],Object=Description"));
+
+      // Footer
+      pReport->getFooterAddr()->push_back(CCopasiObjectName("String=\n"));
+      pReport->getFooterAddr()->push_back(CCopasiObjectName("CN=Root,Vector=TaskList[Metabolic Control Analysis],Object=Result"));
       break;
 
     default:
