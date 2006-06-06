b702be4eaede7a1fbb52f3c67f33f1f326237785
Stefan Hoops <shoops@vbi.vt.edu>
2006-06-06 16:15:27 +0000

    Changed copyright statement by adding a reference that the C++ code is based on an f2c version of the Fortran ODEPACK library.

--- copasi/odepack++/common.cpp
+++ copasi/odepack++/common.cpp
@@ -1,14 +1,17 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/odepack++/common.cpp,v $
-   $Revision: 1.2 $
+   $Revision: 1.2.2.1 $
    $Name:  $
    $Author: shoops $
-   $Date: 2006/04/27 01:29:35 $
+   $Date: 2006/06/06 16:15:26 $
    End CVS Header */
 
-// Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright © 2006 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
+//
+// This C++ code is based on an f2c version of the Fortran
+// library ODEPACK available at: http://www.netlib.org/odepack/
 
 #include "copasi.h"
 
