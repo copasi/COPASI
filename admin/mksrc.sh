a3282670f06051ca0f1533a3557c844a2a3d96a9
Stefan Hoops <shoops@vbi.vt.edu>
2007-01-09 15:38:19 +0000

    Enhanced to remove private code.

--- admin/mksrc.sh
+++ admin/mksrc.sh
@@ -51,11 +51,14 @@ AM_INIT_AUTOMAKE(copasi,${major}.${minor}.${build})
 dnl make /usr/local the default for the installation
 AC_PREFIX_DEFAULT(/usr/local)
 
+QMAKE_ARG="\$QMAKE_ARG COPASI_SRC_PACKAGE=true"
+
+dnl determine whether we build release or debug binaries
 AC_ARG_ENABLE([debug],
   AS_HELP_STRING([--enable-debug],
                  [enable debug build (default: enabled)]),
   [ENABLE_DEBUG=\$enable_debug],
-  [ENABLE_DEBUG="no"])
+  [ENABLE_DEBUG="yes"])
 
 AC_ARG_ENABLE([release],
   AS_HELP_STRING([--enable-release],
@@ -63,21 +66,67 @@ AC_ARG_ENABLE([release],
   [ENABLE_RELEASE=\$enable_release],
   [ENABLE_RELEASE="no"])
   
+if test x"\${ENABLE_RELEASE}" != x"yes" -a x"\${ENABLE_DEBUG}" != x"no"; then
+  QMAKE_ARG="\$QMAKE_ARG CONFIG+=debug CONFIG-=release"
+else
+  QMAKE_ARG="\$QMAKE_ARG CONFIG+=release CONFIG-=debug"
+fi
+
+
+dnl determine whether we link against static libraries
 AC_ARG_ENABLE([static],
   AS_HELP_STRING([--enable-static],
                  [enable static linkage (default: disabled)]),
   [ENABLE_STATIC=\$enable_static],
   [ENABLE_STATIC="no"])
   
+if test x"\${ENABLE_STATIC}" != x"yes"; then
+  QMAKE_ARG="\$QMAKE_ARG STATIC_LINKAGE=no"
+else
+  QMAKE_ARG="\$QMAKE_ARG STATIC_LINKAGE=yes"
+fi
+
+dnl enable or disable build of GUI
+AC_ARG_ENABLE([gui],
+  AS_HELP_STRING([--enable-gui],
+                 [enable build of GUI (default: enabled)]),
+  [ENABLE_GUI=\$enable_gui],
+  [ENABLE_GUI="yes"])
+QMAKE_ARG="\$QMAKE_ARG BUILD_GUI=\$ENABLE_GUI"
+
+
+dnl qwt library
+AC_ARG_WITH([qwt],
+  AS_HELP_STRING([--with-qwt=PATH],
+                 [Path to QWT Library ]),
+  [QWT_PATH=\$withval])
+
+if test x"\${QWT_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG QWT_PATH=\$QWT_PATH"
+fi
+
+
+dnl expat library   
 AC_ARG_WITH([expat],
   AC_HELP_STRING([--with-expat=PATH],
                  [Path to Expat XML Library ]),
   [EXPAT_PATH=\$withval])
 
+if test x"\${EXPAT_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG EXPAT_PATH=\$EXPAT_PATH"
+fi
+
+
+dnl sbml library
 AC_ARG_WITH([sbml],
   AS_HELP_STRING([--with-sbml=PATH],
                  [Path to SBML Library ]),
   [SBML_PATH=\$withval])
+
+if test x"\${SBML_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG SBML_PATH=\$SBML_PATH"
+fi
+
 
 dnl LAPACK library alternatives
 AC_ARG_WITH([clapack],
@@ -85,83 +134,48 @@ AC_ARG_WITH([clapack],
                  [Path to CLAPACK Library ]),
   [CLAPACK_PATH=\$withval])
 
+if test x"\${CLAPACK_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG CLAPACK_PATH=\$CLAPACK_PATH"
+fi
+
+
 AC_ARG_WITH([lapack],
   AS_HELP_STRING([--with-lapack=PATH],
                  [Path to LAPACK Library ]),
   [LAPACK_PATH=\$withval])
+
+if test x"\${LAPACK_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG LAPACK_PATH=\$LAPACK_PATH"
+fi
+
 
 AC_ARG_WITH([mkl],
   AS_HELP_STRING([--with-mkl=PATH],
                  [Path to MKL Library ]),
   [MKL_PATH=\$withval])
 
+if test x"\${MKL_PATH}" != x""; then
+  QMAKE_ARG="\$QMAKE_ARG MKL_PATH=\$MKL_PATH"
+fi
 
-AC_ARG_WITH([qwt],
-  AS_HELP_STRING([--with-qwt=PATH],
-                 [Path to QWT Library ]),
-  [QWT_PATH=\$withval])
 
+
+dnl Determine whether any lex and yacc sources shall be builf
 AC_ARG_WITH([build-parser],
   AS_HELP_STRING([--with-build-parser],
                  [Specify whether to build lex and yacc sources.]),
   [BUILD_PARSER=\$withval])
 
-QMAKE_ARG="COPASI_SRC_PACKAGE=true"
+if test x"\$BUILD_PARSER" = x"yes"; then
+  QMAKE_ARG="\$QMAKE_ARG BUILD_PARSER=yes"
+fi
+
 
 if test "x\$prefix" = "xNONE"; then
   prefix=\$ac_default_prefix
   ac_configure_args="\$ac_configure_args --prefix \$prefix"
 fi
 
-if test x"\${COPASI_LICENSE}" = x"DE" -o x"\${COPASI_LICENSE}" = x"US"; then
-  QMAKE_ARG="USE_LICENSE=\$COPASI_LICENSE"
-fi
-
-if test x"\$BUILD_PARSER" = x"yes"; then
-  QMAKE_ARG="\$QMAKE_ARG BUILD_PARSER=yes"
-fi
-
-if test x"\${ENABLE_RELEASE}" != x"yes" -a x"\${ENABLE_DEBUG}" != x"no"; then
-  QMAKE_ARG="\$QMAKE_ARG CONFIG+=debug CONFIG-=release"
-else
-  QMAKE_ARG="\$QMAKE_ARG CONFIG+=release CONFIG-=debug"
-fi
-
-if test x"\${ENABLE_STATIC}" != x"yes"; then
-  QMAKE_ARG="\$QMAKE_ARG STATIC_LINKAGE=no"
-else
-  QMAKE_ARG="\$QMAKE_ARG STATIC_LINKAGE=yes"
-fi
-
-dnl if test x"\${enable_thread}" != x"no"; then
-dnl   QMAKE_ARG="\$QMAKE_ARG CONFIG+=thread"
-dnl else
-dnl   QMAKE_ARG="\$QMAKE_ARG CONFIG-=thread"
-dnl fi
-
-if test x"\${MKL_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG MKL_PATH=\$MKL_PATH"
-fi
-
-if test x"\${CLAPACK_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG CLAPACK_PATH=\$CLAPACK_PATH"
-fi
-
-if test x"\${LAPACK_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG LAPACK_PATH=\$LAPACK_PATH"
-fi
-
-if test x"\${EXPAT_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG EXPAT_PATH=\$EXPAT_PATH"
-fi
-
-if test x"\${QWT_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG QWT_PATH=\$QWT_PATH"
-fi
-
-if test x"\${SBML_PATH}" != x""; then
-  QMAKE_ARG="\$QMAKE_ARG SBML_PATH=\$SBML_PATH"
-fi
 
 
 (
@@ -183,7 +197,6 @@ dnl to prevent calling aclocal, automake and configure again
  mv \$\$.tmp Makefile && \\
  echo depend:  >> Makefile && \\
  echo "	touch Makefile" >> Makefile && \\
- rm -f CopasiSE/CopasiSE.o CopasiUI/main.o && \ 
  cd ..
 )
 
@@ -255,6 +268,19 @@ rm -rf autom4te.cache
 
 mv copasi/License${license}.txt.h copasi/copasilicense.h
 rm copasi/License*.txt.h
+
+#remove any reference to commercial license code
+CleanFiles=`find . -type f -exec grep -Hq '#ifdef COPASI_LICENSE_COM' {} \; -exec echo {} \; | sort -u`
+for file in $CleanFiles; do
+  gawk -- ' BEGIN {keep = 1} $0 ~ "#ifdef COPASI_LICENSE_COM" {keep = 0} {if (keep == 1) {print $0}} $0 ~ "#endif // COPASI_LICENSE_COM" {keep = 1}' $file > $$.tmp && mv $$.tmp $file;
+done;
+
+#remove any reference to CROSSVALIDATION
+CleanFiles=`find . -type f -exec grep -Hq '#ifdef COPASI_CROSSVALIDATION' {} \; -exec echo {} \; | sort -u`
+for file in $CleanFiles; do
+  gawk -- ' BEGIN {keep = 1} $0 ~ "#ifdef COPASI_CROSSVALIDATION" {keep = 0} {if (keep == 1) {print $0}} $0 ~ "#endif // COPASI_CROSSVALIDATION" {keep = 1}' $file > $$.tmp && mv $$.tmp $file;
+done;
+
 cd ..
 
 tar -czf Copasi-${build}-SRC.tar.gz copasi-${build}-src 
