f2b0227030b774f8bbfa408e8d97672a00a9662a
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2007-02-08 14:06:11 +0000

    add summarized result to result widget

--- copasi/UI/CQSensResultWidget.h
+++ copasi/UI/CQSensResultWidget.h
@@ -1,12 +1,12 @@
-/* Begin CVS Header
-   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQSensResultWidget.h,v $
-   $Revision: 1.8 $
-   $Name:  $
-   $Author: ssahle $
-   $Date: 2007/01/10 17:08:16 $
-   End CVS Header */
+// Begin CVS Header
+//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQSensResultWidget.h,v $
+//   $Revision: 1.8.2.1 $
+//   $Name:  $
+//   $Author: ssahle $
+//   $Date: 2007/02/08 14:06:11 $
+// End CVS Header
 
-// Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -54,9 +54,11 @@ class CQSensResultWidget : public CopasiWidget
 
     CQArrayAnnotationsWidget* mArrayWidget;
     CQArrayAnnotationsWidget* mArrayWidgetScaled;
+    CQArrayAnnotationsWidget* mArrayWidgetCollapsed;
 
     const CArrayAnnotation * mpResult;
     const CArrayAnnotation * mpScaledResult;
+    const CArrayAnnotation * mpCollapsedResult;
   };
 
 #endif
