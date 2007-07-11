673606866652b883f7fc5a4d8d4f6055efeed4c6
Stefan Hoops <shoops@vbi.vt.edu>
2007-07-11 14:09:47 +0000

    Changed to final version for Build 22.

--- copasi/copasiversion.h
+++ copasi/copasiversion.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/copasiversion.h,v $
-//   $Revision: 1.21.4.1.2.1 $
+//   $Revision: 1.21.4.1.2.2 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2007/07/10 18:30:38 $
+//   $Date: 2007/07/11 14:09:47 $
 // End CVS Header
 
 // Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
@@ -14,7 +14,7 @@
 #define COPASI_VERSION
 
 #define COPASI_VERSION_MAJOR 4
-#define COPASI_VERSION_MINOR 1
+#define COPASI_VERSION_MINOR 2
 #define COPASI_VERSION_BUILD 22
 #define COPASI_VERSION_COMMENT "stable"
 
