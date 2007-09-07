cd445939a208060098398c226c111ce59a977b09
Ursula Rost <Ursula.Rost@eml-r.villa-bosch.de>
2007-09-07 16:11:55 +0000

    original value is now also stored in data set

--- copasi/layoutUI/CDataEntity.h
+++ copasi/layoutUI/CDataEntity.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/CDataEntity.h,v $
-//   $Revision: 1.6 $
+//   $Revision: 1.7 $
 //   $Name:  $
 //   $Author: urost $
-//   $Date: 2007/07/22 19:18:18 $
+//   $Date: 2007/09/07 16:11:55 $
 // End CVS Header
 
 // Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
@@ -23,6 +23,7 @@ class CDataEntity
   private:
     C_FLOAT64 mTime;
     std::map<std::string, C_FLOAT64> mSpeciesValueMap;
+    std::map<std::string, C_FLOAT64> mOrigValueMap;
 
   public:
     CDataEntity();
@@ -30,7 +31,10 @@ class CDataEntity
     CDataEntity(const CDataEntity & src);
 
     void putValueForSpecies(std::string nodeKey, C_FLOAT64 value);
+    void putOrigValueForSpecies(std::string nodeKey, C_FLOAT64 value);
+
     C_FLOAT64 getValueForSpecies(std::string nodeKey);
+    C_FLOAT64 getOrigValueForSpecies(std::string nodeKey);
     int getNumberOfElements(){return mSpeciesValueMap.size();}
     int getTime(){return mTime;}
   };
