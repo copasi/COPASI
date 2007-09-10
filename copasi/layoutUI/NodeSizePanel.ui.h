6766a9e1ff2f496c02a923a94d7849e953422185
Ursula Rost <Ursula.Rost@eml-r.villa-bosch.de>
2007-09-10 07:10:51 +0000

    *** empty log message ***

---
+++ copasi/layoutUI/NodeSizePanel.ui.h
@@ -0,0 +1,34 @@
+// Begin CVS Header
+//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/Attic/NodeSizePanel.ui.h,v $
+//   $Revision: 1.1 $
+//   $Name:  $
+//   $Author: urost $
+//   $Date: 2007/09/10 07:10:51 $
+// End CVS Header
+
+// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc. and EML Research, gGmbH.
+// All rights reserved.
+
+/****************************************************************************
+ ** ui.h extension file, included from the uic-generated form implementation.
+ **
+ ** If you want to add, delete, or rename functions or slots, use
+ ** Qt Designer to update this file, preserving your code.
+ **
+ ** You should not define a constructor or destructor in this file.
+ ** Instead, write your code in functions called init() and destroy().
+ ** These will automatically be called by the form's constructor and
+ ** destructor.
+ *****************************************************************************/
+#include "copasi.h"
+
+void NodeSizePanel::setMinAndMaxValues()
+{
+  std::cout << "set min and max value" << std::endl;
+}
+
+void NodeSizePanel::cancel()
+{
+  std::cout << "cancel" << std::endl;
+}
