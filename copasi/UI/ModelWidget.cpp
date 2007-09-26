f274524dc02d2222de842e2c41c493e2cf2e9c23
Stefan Hoops <shoops@vbi.vt.edu>
2007-09-26 22:19:15 +0000

    notify specified RENAME instead of CHANGE

--- copasi/UI/ModelWidget.cpp
+++ copasi/UI/ModelWidget.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/ModelWidget.cpp,v $
-//   $Revision: 1.50 $
+//   $Revision: 1.50.4.1 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2007/05/15 12:36:53 $
+//   $Date: 2007/09/26 22:19:15 $
 // End CVS Header
 
 // Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
@@ -332,7 +332,7 @@ bool ModelWidget::saveToModel()
 
   if (changed)
     {
-      protectedNotify(ListViews::MODEL, ListViews::RENAME, objKey);
+      protectedNotify(ListViews::MODEL, ListViews::CHANGE, objKey);
       CCopasiDataModel::Global->changed(true);
     }
 
