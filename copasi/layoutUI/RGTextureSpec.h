baf500aaa2d3da9a566399a1ea8dab95581db179
Ursula Rost <Ursula.Rost@eml-r.villa-bosch.de>
2007-10-08 19:16:41 +0000

    *** empty log message ***

--- copasi/layoutUI/RGTextureSpec.h
+++ copasi/layoutUI/RGTextureSpec.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/RGTextureSpec.h,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
 //   $Author: urost $
-//   $Date: 2007/10/04 17:22:45 $
+//   $Date: 2007/10/08 19:16:41 $
 // End CVS Header
 
 // Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
@@ -20,10 +20,10 @@
 class RGTextureSpec
   {
   public:
-    C_INT32 textWidth;
-    C_INT32 textHeight;
-    C_INT32 textureWidth;
-    C_INT32 textureHeight;
+    C_FLOAT64 textWidth;
+    C_FLOAT64 textHeight;
+    C_FLOAT64 textureWidth;
+    C_FLOAT64 textureHeight;
 
     GLubyte* textureData;
 
