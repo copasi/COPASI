1b15246988a6fa35558158aa800a7ee470e44aec
Ralph Gauges <gauges@hs-albsig.de>
2007-10-11 13:50:55 +0000

    Reactivated the rest of the test. The test for reversible hill fails right now. All others pass.

--- copasi/compareExpressions/unittests/test.cpp
+++ copasi/compareExpressions/unittests/test.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/compareExpressions/unittests/test.cpp,v $
-//   $Revision: 1.5 $
+//   $Revision: 1.6 $
 //   $Name:  $
 //   $Author: gauges $
-//   $Date: 2007/08/23 09:03:48 $
+//   $Date: 2007/10/11 13:50:55 $
 // End CVS Header
 
 // Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
@@ -26,8 +26,8 @@ int main(int /*argc*/, char ** /*argv*/)
   CppUnit::TextUi::TestRunner runner;
   runner.addTest(test_cnormallogical::suite());
   runner.addTest(test_normalform::suite());
-  //runner.addTest(test_node_conversion::suite());
-  //runner.addTest(test_expression_comparison::suite());
+  runner.addTest(test_node_conversion::suite());
+  runner.addTest(test_expression_comparison::suite());
   runner.run();
   return 0;
 }
