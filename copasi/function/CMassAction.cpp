55d59bac9e70807a452e72798a38d32937aba5b0
Stefan Hoops <shoops@vbi.vt.edu>
2008-01-11 14:30:56 +0000

    Manually changed copyright statement to include The University of Manchester and the University of Heidelberg.

--- copasi/function/CMassAction.cpp
+++ copasi/function/CMassAction.cpp
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/function/CMassAction.cpp,v $
-//   $Revision: 1.38.4.1 $
+//   $Revision: 1.38.4.2 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2008/01/10 14:42:30 $
+//   $Date: 2008/01/11 14:30:56 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
