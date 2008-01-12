66143edc936b60d9727c4c59271ce3de5284f814
Ralph Gauges <gauges@hs-albsig.de>
2008-01-12 15:26:05 +0000

    Due to changes in the way initial values are set on model entities, I had to update the test to build an initial refresh sequence.

--- copasi/bindings/java/unittests/Test_CreateSimpleModel.java
+++ copasi/bindings/java/unittests/Test_CreateSimpleModel.java
@@ -1,14 +1,18 @@
 // Begin CVS Header 
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/bindings/java/unittests/Test_CreateSimpleModel.java,v $ 
-//   $Revision: 1.8 $ 
+//   $Revision: 1.9 $ 
 //   $Name:  $ 
 //   $Author: gauges $ 
-//   $Date: 2008/01/12 13:17:41 $ 
+//   $Date: 2008/01/12 15:26:05 $ 
 // End CVS Header 
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual 
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
 // and The University of Manchester. 
+// All rights reserved. 
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
 // All rights reserved. 
 
 // Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
@@ -63,6 +67,12 @@ public class Test_CreateSimpleModel extends TestCase
     mapping.add(react.getChemEq().getSubstrate(0).getMetabolite().getKey());
     react.setParameterMappingVector(react.getFunction().getVariables().getParameter(1).getObjectName(),mapping);
     model.compileIfNecessary();
+    ObjectStdVector changedObjects=new ObjectStdVector();
+    changedObjects.add(comp.getObject(new CCopasiObjectName("Reference=InitialVolume")));
+    changedObjects.add(A.getObject(new CCopasiObjectName("Reference=InitialConcentration")));
+    changedObjects.add(B.getObject(new CCopasiObjectName("Reference=InitialConcentration")));
+    changedObjects.add(react.getParameters().getParameter(0).getObject(new CCopasiObjectName("Reference=Value")));
+    model.updateInitialValues(changedObjects);
     return model;
    }
 
@@ -80,6 +90,10 @@ public class Test_CreateSimpleModel extends TestCase
       mapping.add(react.getChemEq().getSubstrate(0).getMetabolite().getKey());
       react.setParameterMappingVector(react.getFunction().getVariables().getParameter(1).getObjectName(),mapping);;
       model.compileIfNecessary();
+      ObjectStdVector changedObjects=new ObjectStdVector();
+      changedObjects.add(metab.getObject(new CCopasiObjectName("Reference=InitialConcentration")));
+      changedObjects.add(react.getParameters().getParameter(0).getObject(new CCopasiObjectName("Reference=Value")));
+      model.updateInitialValues(changedObjects);
   }
 
    
