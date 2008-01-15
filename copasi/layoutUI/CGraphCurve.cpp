36f98964cdb14fdff1b2298a2bbb7f45365a6ea0
Ursula Rost <Ursula.Rost@eml-r.villa-bosch.de>
2008-01-15 09:51:40 +0000

    corrected broken line

--- copasi/layoutUI/CGraphCurve.cpp
+++ copasi/layoutUI/CGraphCurve.cpp
@@ -1,14 +1,18 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/CGraphCurve.cpp,v $
-//   $Revision: 1.6 $
+//   $Revision: 1.7 $
 //   $Name:  $
 //   $Author: urost $
-//   $Date: 2008/01/14 11:59:59 $
+//   $Date: 2008/01/15 09:51:40 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
 // and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
 // Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
