ccf7c83b438d262d9747f1820c55fe3544a35dab
Ursula Rost <Ursula.Rost@eml-r.villa-bosch.de>
2008-04-14 10:24:03 +0000

    moved DEFAULT_NODE_SIZE to this class

--- copasi/layoutUI/CVisParameters.cpp
+++ copasi/layoutUI/CVisParameters.cpp
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/CVisParameters.cpp,v $
-//   $Revision: 1.10 $
+//   $Revision: 1.11 $
 //   $Name:  $
 //   $Author: urost $
-//   $Date: 2007/10/29 10:19:14 $
+//   $Date: 2008/04/14 10:24:03 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -20,6 +25,8 @@ const C_INT16 CVisParameters::INDIVIDUAL_SCALING = 1;
 const C_INT16 CVisParameters::COLOR_MODE = 0;
 const C_INT16 CVisParameters::SIZE_DIAMETER_MODE = 1;
 const C_INT16 CVisParameters::SIZE_AREA_MODE = 2;
+
+C_INT16 CVisParameters::DEFAULT_NODE_SIZE = 20;
 
 CVisParameters::CVisParameters()
 {
--- copasi/layoutUI/CVisParameters.h
+++ copasi/layoutUI/CVisParameters.h
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layoutUI/CVisParameters.h,v $
-//   $Revision: 1.8 $
+//   $Revision: 1.9 $
 //   $Name:  $
 //   $Author: urost $
-//   $Date: 2007/10/29 10:09:12 $
+//   $Date: 2008/04/14 10:24:03 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -26,6 +31,8 @@ class CVisParameters
     static const C_INT16 COLOR_MODE;
     static const C_INT16 SIZE_DIAMETER_MODE;
     static const C_INT16 SIZE_AREA_MODE;
+
+    static C_INT16 DEFAULT_NODE_SIZE;
 
     C_FLOAT64 minNodeSize;
     C_FLOAT64 maxNodeSize;
