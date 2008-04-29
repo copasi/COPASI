cab06bdbcc9c76035f3c742c6ddc0cbc86a1bb0d
Ralph Gauges <gauges@hs-albsig.de>
2008-04-29 07:16:42 +0000

    Fix to the java gui example pointed out by Akiya.

--- copasi/bindings/java/gui/org/COPASI/gui/TaskRunEventListener.java
+++ copasi/bindings/java/gui/org/COPASI/gui/TaskRunEventListener.java
@@ -1,12 +1,17 @@
 // Begin CVS Header 
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/bindings/java/gui/org/COPASI/gui/TaskRunEventListener.java,v $ 
-//   $Revision: 1.1 $ 
+//   $Revision: 1.1.10.1 $ 
 //   $Name:  $ 
 //   $Author: gauges $ 
-//   $Date: 2007/06/20 14:01:24 $ 
+//   $Date: 2008/04/29 07:16:42 $ 
 // End CVS Header 
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
 // Properties, Inc. and EML Research, gGmbH. 
 // All rights reserved. 
 
@@ -15,7 +20,7 @@ package org.COPASI.gui;
 import java.util.EventListener;
 import org.COPASI.gui.TaskRunEvent;
 
-interface TaskRunEventListener extends EventListener 
+public interface TaskRunEventListener extends EventListener 
 {
     public void taskRun(TaskRunEvent event);        
 };
