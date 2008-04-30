14f6b0e8fec418931f83d770d1f6a912cb740610
Ralph Gauges <gauges@hs-albsig.de>
2008-04-30 07:55:05 +0000

    Some of the unittests made assumptions about the order of the rules in the result. Out of some reason every time I recompile the sources, this order seems to change. So now the tests don't make those assumptions any more, but I am still puzzled why the code should behave like that.

--- copasi/sbml/unittests/test000012.cpp
+++ copasi/sbml/unittests/test000012.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/sbml/unittests/test000012.cpp,v $
-//   $Revision: 1.1.2.9 $
+//   $Revision: 1.1.2.9.2.1 $
 //   $Name:  $
 //   $Author: gauges $
-//   $Date: 2008/02/26 17:02:21 $
+//   $Date: 2008/04/30 07:55:05 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -67,12 +67,16 @@ void test000012::test_references_to_species()
   std::string idSpeciesA = pSpecies->getId();
   CPPUNIT_ASSERT(pSpecies->getHasOnlySubstanceUnits() == true);
   CPPUNIT_ASSERT(pModel->getNumRules() == 2);
-  // there are two rules, the first rule is for the compartment
-  AssignmentRule* pRule = dynamic_cast<AssignmentRule*>(pModel->getRule(1));
+  // there are two rules, one rule is for the compartment
+  AssignmentRule* pRule = dynamic_cast<AssignmentRule*>(pModel->getRule(0));
   CPPUNIT_ASSERT(pRule != NULL);
   CPPUNIT_ASSERT(pModel->getNumParameters() == 1);
   Parameter* pParameter = pModel->getParameter(0);
   CPPUNIT_ASSERT(pParameter != NULL);
+  if (pRule->getVariable() != pParameter->getId())
+    {
+      pRule = dynamic_cast<AssignmentRule*>(pModel->getRule(1));
+    }
   CPPUNIT_ASSERT(pRule->getVariable() == pParameter->getId());
   const ASTNode* pMath = pRule->getMath();
   CPPUNIT_ASSERT(pMath != NULL);
