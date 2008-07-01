3c6fb4ef318931c2932d9d40d416e64c74679088
Ralph Gauges <gauges@hs-albsig.de>
2008-07-01 11:09:50 +0000

    Remove extra qualifiers to compile with SunCC.

--- copasi/UI/CQTSSATimeScaleWidget.cpp
+++ copasi/UI/CQTSSATimeScaleWidget.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQTSSATimeScaleWidget.cpp,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
-//   $Author: akoenig $
-//   $Date: 2008/02/24 17:33:04 $
+//   $Author: gauges $
+//   $Date: 2008/07/01 11:09:50 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -125,19 +125,19 @@ void PaintWidget::paintEvent(QPaintEvent *)
 
   paint.setWindow(0, 0, 1000, 1000);
 
-  paint.setFont(QFont::QFont("Courier", 15));
+  paint.setFont(QFont("Courier", 15));
 
-  paint.setPen(QPen::QPen(QColor(180, 0, 0), 5));
+  paint.setPen(QPen(QColor(180, 0, 0), 5));
   paint.drawLine(space, 100, space, 110);
-  paint.setPen(QPen::QPen(QColor(0, 0, 0), 1));
+  paint.setPen(QPen(QColor(0, 0, 0), 1));
   paint.drawText (space + 5, 110, " - negativ time scale values");
 
-  paint.setPen(QPen::QPen(QColor(0, 180, 0), 5));
+  paint.setPen(QPen(QColor(0, 180, 0), 5));
   paint.drawLine(space, 130, space, 140);
-  paint.setPen(QPen::QPen(QColor(0, 0, 0), 1));
+  paint.setPen(QPen(QColor(0, 0, 0), 1));
   paint.drawText (space + 5, 140, " - positiv time scale values");
 
-  paint.setPen(QPen::QPen(QColor(100, 100, 100), 1));
+  paint.setPen(QPen(QColor(100, 100, 100), 1));
   scaleBegin = space;
   scaleEnd = (maxScaleValue - minScaleValue) * (1000 - 2 * space) / scaleValueRange + space;
   paint.drawLine((int)scaleBegin, 700, (int)scaleEnd, 700);
@@ -149,7 +149,7 @@ void PaintWidget::paintEvent(QPaintEvent *)
       paint.drawText ((position - 4), 750, QString::number(minScaleValue + i));
     }
 
-  paint.setPen(QPen::QPen(QColor(0, 180, 0), 1));
+  paint.setPen(QPen(QColor(0, 180, 0), 1));
 
   for (i = 0; i < mVector.size(); i++)
     {
@@ -157,15 +157,15 @@ void PaintWidget::paintEvent(QPaintEvent *)
       if ((uint)mSelection == (uint)(mVector.size() - 1 - i))
         {
           if (mVector[i] < 0)
-            paint.setPen(QPen::QPen(QColor(180, 20, 20), 4));
+            paint.setPen(QPen(QColor(180, 20, 20), 4));
           else
-            paint.setPen(QPen::QPen(QColor(20, 180, 20), 4));
+            paint.setPen(QPen(QColor(20, 180, 20), 4));
           paint.drawLine(position, 697, position, 600);
-          paint.setPen(QPen::QPen(QColor(200, 200, 200), 1));
-          paint.setFont(QFont::QFont("Courier", 20));
+          paint.setPen(QPen(QColor(200, 200, 200), 1));
+          paint.setFont(QFont("Courier", 20));
           paint.drawLine(500, 400, position + 1, 599);
           paint.drawLine(500, 400, 930, 400);
-          paint.setPen(QPen::QPen(QColor(0, 0, 0), 1));
+          paint.setPen(QPen(QColor(0, 0, 0), 1));
           if (mVector[i] < 0)
             paint.drawText (510, 380, " log10 (|" + QString::number(mVector[i]) + "|) = " + QString::number(log10(fabs(mVector[i]))));
           else
@@ -175,9 +175,9 @@ void PaintWidget::paintEvent(QPaintEvent *)
       else
         {
           if (mVector[i] < 0)
-            paint.setPen(QPen::QPen(QColor(180, 20, 20), 2));
+            paint.setPen(QPen(QColor(180, 20, 20), 2));
           else
-            paint.setPen(QPen::QPen(QColor(20, 180, 20), 2));
+            paint.setPen(QPen(QColor(20, 180, 20), 2));
           paint.drawLine(position, 699, position, 600);
         }
     }
