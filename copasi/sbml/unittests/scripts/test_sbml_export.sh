0267bb1c1325db43f4414bb80a0fc7174b99210b
Ralph Gauges <gauges@hs-albsig.de>
2008-07-17 20:18:41 +0000

    Two new scripts that automate part of the import/export checks.

---
+++ copasi/sbml/unittests/scripts/test_sbml_export.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+VALGRIND_NUMCALLERS=30
+GREP=/bin/grep
+VALGRIND=/usr/bin/valgrind
+UNAME=/bin/uname
+TPUT=/usr/bin/tput
+
+SYSTEM=`${UNAME} -s`
+
+if [ -z $TMP_DIR ];then
+  TMP_DIR=/tmp/
+fi
+
+if [ -z $USE_VALGRIND ];then
+  USE_VALGRIND=no
+fi
+
+if [ -z $DO_LEAKCHECK ];then
+  DO_LEAKCHECK=no
+fi
+
+if [ -z $COPASISE ];then
+  if [ "$SYSTEM" == "Darwin" ];then
+    COPASISE=../../CopasiSE/CopasiSE.app/Contents/MacOS/CopasiSE
+  else
+    COPASISE=../../CopasiSE/CopasiSE
+  fi
+fi
+
+if [ -z $COPASISE_OPTIONS ];then
+  COPASISE_OPTIONS="--nologo --verbose"
+fi
+
+VALGRIND_OPTIONS="--num-callers=${VALGRIND_NUMCALLERS} ${VALGRIND_OPTIONS}"
+
+source ./utilities.sh
+
+function test_export_single_file
+{
+    # we pass all the commands as parameters so we don't have to check
+    # for their existence in each call. Thoses checks have to be done by the
+    # caller
+    if [ $# != 2 ];then
+        echo "Error. test_import_single_file expects exactly two arguments.";
+        return 1;
+    fi
+    CPS_FILE=$1
+    OUTPUT_DIR=$2
+    NAME=${SBML_FILE##*/};
+    NAME=${NAME%%.cps};
+    SBML_FILE=${NAME}.xml
+    OUTPUT_FILE=${NAME}.export.output
+    VALGRIND_LOG=${NAME}.export.log
+    COMMAND="${COPASISE} ${COPASISE_OPTIONS} --exportSBML ${OUTPUT_DIR}/${SBML_FILE} ${CPS_FILE}"
+    if [ "$USE_VALGRIND" == "yes" ];then
+       COMMAND="${VALGRIND} ${VALGRIND_OPTIONS} --log-file=${OUTPUT_DIR}/${VALGRIND_LOG} ${COMMAND}"
+    fi
+    RESULT="PASSED"
+    $COMMAND &> ${OUTPUT_DIR}/${OUTPUT_FILE} || return 1;
+    # check if the CPS file has been generated and has a size different from 0
+    if [ ! -s ${OUTPUT_DIR}/${SBML_FILE} ];then
+        return 1;
+    fi
+    # check if the output file has a size different from 0
+    if [ -s ${OUTPUT_DIR}/$OUTPUT_FILE ];then
+        if [ "$USE_VALGRIND" == "yes" ];then
+          # additional valgrind checks
+          check_valgrind_errors ${OUTPUT_DIR}/${VALGRIND_LOG}
+          return $?
+        else
+            return 2;
+        fi
+    fi
+    # todo check if valgrind found an error
+    if [ "$USE_VALGRIND" == "yes" ];then
+      check_valgrind_errors ${OUTPUT_DIR}/${VALGRIND_LOG}
+      return $?
+    fi
+    return 0
+}
+
+function test_export_files
+{
+    # create a direcory in TMP_DIR that holds all result files
+    OUTPUT_DIR=$TMP_DIR
+    FILENAME=$1
+    while [ -n "$FILENAME" ];do
+      # check if the file exists
+      if [ ! -r $FILENAME ];then
+          echo "$FILENAME does not exist, is not a regular file or is not readable."
+      else
+        echo "Importing $FILENAME ...";
+        NAME=${FILENAME##*/};
+        NAME=${NAME%%.cps};
+        test_export_single_file ${FILENAME} ${OUTPUT_DIR}
+        case $? in
+        0 )
+            echo -n "Export of $FILENAME ";
+            echo -n -e '\E[32;47mOK';
+            ${TPUT} sgr0;
+            echo ".";
+            ;;
+        1 )
+            echo -n "Export of $FILENAME ";
+            echo -n -e '\E[31;47mFAILED';
+            ${TPUT} sgr0;
+            echo ".";
+            ;;
+        2 )
+            echo -n "Export of $FILENAME ";
+            echo -n -e '\E[33;47mSUCCEDED';
+            ${TPUT} sgr0;
+            echo -e " but there was additional output from COPASI.\nCheck ${OUTPUT_DIR}/${NAME}.export.output for details.";
+            ;;
+        3 ) 
+            echo -n "Export of $FILENAME ";
+            echo -n -e '\E[33;47mSUCCEDED';
+            ${TPUT} sgr0;
+            echo -e " but valgrind reported errors.\nCheck ${OUTPUT_DIR}/${NAME}.export.log for details.";
+            ;;
+        4 ) 
+            echo -n "Export of $FILENAME ";
+            echo -e -n '\E[33;47mSUCCEDED';
+            ${TPUT} sgr0;
+            echo -e " but valgrind reported memory leaks.\nCheck ${OUTPUT_DIR}/${NAME}.export.log for details.";
+            ;;
+        5 ) 
+            echo -n "Export of $FILENAME";
+            echo -n -e '\E[33;47mSUCCEDED';
+            ${TPUT} sgr0;
+            echo -e " but valgrind reported errors and memory leaks.\nCheck ${OUTPUT_DIR}/${NAME}.export.log.";
+            ;;
+        * )
+            echo "An unknown error code was reported from test_export_file.";
+            ;;
+        esac
+      fi
+      shift
+      FILENAME=$1
+    done
+}
+
+# enable leak checking in valgrind if requested
+if [ "${DO_LEAKCHECK}" == "yes" ];then
+    VALGRIND_OPTIONS="${VALGRIND_OPTIONS} --leak-check=full"
+fi
+
+# check if valgrind is there if needed
+if [ ${USE_VALGRIND} == "yes" ]; then
+   # todo check if the valgrind executable exists
+   check_executable $VALGRIND || exit 1; 
+fi
+
+# check if tput is there
+check_executable $TPUT || exit 1;
+
+# check if grep is there
+check_executable $GREP || exit 1;
+
+# check if CopasiSE is there
+check_executable $COPASISE || exit 1;
+
+# check if TMP_DIR is present, a directry and writable
+if [ -d $TMP_DIR ];then
+    touch ${TMP_DIR}/test.test
+    if [ $? != 0 ];then
+        echo "Error. Can not write to ${TMP_DIR}."
+        exit 1
+    else
+      rm ${TMP_DIR}/test.test
+    fi
+else
+    echo "Error. ${TMP_DIR} is not a directory."
+    exit 1;
+fi
+
+test_export_files $*
+
---
+++ copasi/sbml/unittests/scripts/utilities.sh
@@ -0,0 +1,52 @@
+function check_executable
+{
+    if [ $# != 1 ];then
+      echo "Error. check_executable takes exactly one argument.";
+      return 1;
+    else
+        if [ -f $1 ];then
+          if [ -x $1 ];then
+            return 0;
+          else
+            echo "Error. $1 is not executable.";
+            return 1;
+          fi
+        else
+            echo "Error. The executable $1 is not a file or does not exist.";
+            return 1;
+        fi
+    fi
+}
+
+function check_valgrind_errors
+{
+    if [ $# != 1 ];then
+        echo "Error. check_valgrind_errors takes exactly one argument.";
+        return 100;
+    fi
+    LOGFILE=$1
+    if [ ! -s $LOGFILE ];then
+        echo "Error. Could not examine valgrind log at $LOGFILE".
+        return 101;
+    fi
+    RESULT=`$GREP "ERROR SUMMARY: 0" $LOGFILE`
+    if [ -z "$RESULT" ];then
+        if [ "$DO_LEAKCHECK" == "yes" ];then
+          RESULT=`$GREP "definitely lost: 0" $LOGFILE`
+          if [ -z "$RESULT" ];then
+            return 5;
+          fi
+        fi
+        return 3;
+    else
+        if [ "$DO_LEAKCHECK" == "yes" ];then
+          RESULT=`$GREP "definitely lost: 0" $LOGFILE`
+          if [ -z "$RESULT" ];then
+            return 4;
+          fi
+        fi
+    fi
+    return 0;
+}
+
+
