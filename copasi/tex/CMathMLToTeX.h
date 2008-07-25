6ad440194c1705b9a4b67a2cbc2c9adbccfa3bb6
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2008-07-25 08:09:00 +0000

    Delete unnecessary lines of code.

--- copasi/tex/CMathMLToTeX.h
+++ copasi/tex/CMathMLToTeX.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/tex/CMathMLToTeX.h,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
 //   $Author: pwilly $
-//   $Date: 2008/07/24 10:56:50 $
+//   $Date: 2008/07/25 08:09:00 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -44,30 +44,6 @@ class CMathMLToTeX
 
     // table
     static void mtableNode(QString &text);
-
-    // mtr
-    static void mtrNode(QString &text, QString &allignment);
-
-    // mtd
-    static void mtdNode(QString &text);
-
-    // frac
-    static void mfracNode(QString &text);
-
-    // row
-    static void mrowNode(QString &text);
-
-    // fence
-    static void mfencedNode(QString &text);
-
-    // identifier, eg. variable, constant, function.
-    static void miNode(QString &text);
-
-    // operator (eg. '+'), fence (eg. '{'), or separator (eg. ',')
-    static void moNode(QString &text);
-
-    // sub
-    static void msubNode(QString &text);
   };
 
 #endif
