7bd0810f7a2e31c92e47379b1866f63591fa9d8e
Stefan Hoops <shoops@vbi.vt.edu>
2008-08-03 17:25:53 +0000

    Fixed memory leak caused by allocations in COptions.

--- copasi/CopasiUI/main.cpp
+++ copasi/CopasiUI/main.cpp
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/CopasiUI/main.cpp,v $
-//   $Revision: 1.33 $
+//   $Revision: 1.33.14.1 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2007/05/15 12:36:10 $
+//   $Date: 2008/08/03 17:25:53 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -73,6 +78,7 @@ int main(int argc, char **argv)
   try // To surpress any access violations during destruction
     {
       pdelete(CCopasiDataModel::Global);
+      COptions::cleanup();
       pdelete(CCopasiContainer::Root);
     }
   catch (...)
