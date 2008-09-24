fea57239ca8c61f3e1fad88042f4683b97065bf6
Ralph Gauges <gauges@hs-albsig.de>
2008-09-24 13:40:21 +0000

    Fix formatting issues in the C++ code.

--- copasi/UI/copasiui3window.cpp
+++ copasi/UI/copasiui3window.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/copasiui3window.cpp,v $
-//   $Revision: 1.234 $
+//   $Revision: 1.235 $
 //   $Name:  $
 //   $Author: gauges $
-//   $Date: 2008/09/24 13:02:03 $
+//   $Date: 2008/09/24 13:40:21 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
