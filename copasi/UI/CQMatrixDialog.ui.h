3a5bd6e288ca258a164a36de49fe7b20a9cdd5ae
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2008-10-16 08:35:21 +0000

    Use function size() of CArrayAnnotation object to get the correct size of array. Support showing an 1-D and a 2-D as well as a 3-D array.

--- copasi/UI/CQMatrixDialog.ui.h
+++ copasi/UI/CQMatrixDialog.ui.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CQMatrixDialog.ui.h,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.1.4.1 $
 //   $Name:  $
 //   $Author: pwilly $
-//   $Date: 2008/08/18 09:07:10 $
+//   $Date: 2008/10/16 08:35:21 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -30,30 +30,40 @@ void CQMatrixDialog::setArray(const CArrayAnnotation *tmp)
   int i;
 
   mpLabelRow->setText("Rows : " + FROM_UTF8(tmp->getDimensionDescription(0)));
-  int nRows = tmp->getAnnotationsCN(0).size();
+  int nRows = tmp->size()[0];
 
   mpCBRow->insertItem("ALL");
   for (i = 0; i < nRows; i++)
     mpCBRow->insertItem(FROM_UTF8(tmp->getAnnotationsString(0, true)[i]));
 
-  mpLabelColumn->setText("Columns : " + FROM_UTF8(tmp->getDimensionDescription(1)));
-  int nCols = tmp->getAnnotationsCN(1).size();
+  mpLabelColumn->hide();
+  mpCBColumn->hide();
 
-  mpCBColumn->insertItem("ALL");
-  for (i = 0; i < nCols; i++)
-    mpCBColumn->insertItem(FROM_UTF8(tmp->getAnnotationsString(1, true)[i]));
+  mpLabelDim3->hide();
+  mpCBDim3->hide();
 
-  if (tmp->dimensionality() == 2)
+  if (tmp->dimensionality() > 1)
     {
-      mpLabelDim3->hide();
-      mpCBDim3->hide();
-    }
-  else if (tmp->dimensionality() == 3)
-    {
-      mpLabelDim3->setText("Dimension : " + FROM_UTF8(tmp->getDimensionDescription(2)));
-      int nDims = tmp->getAnnotationsCN(2).size();
+      mpLabelColumn->show();
+      mpCBColumn->show();
 
-      for (i = 0; i < nDims; i++)
-        mpCBDim3->insertItem(FROM_UTF8(tmp->getAnnotationsString(2, true)[i]));
+      mpLabelColumn->setText("Columns : " + FROM_UTF8(tmp->getDimensionDescription(1)));
+      int nCols = tmp->size()[1];
+
+      mpCBColumn->insertItem("ALL");
+      for (i = 0; i < nCols; i++)
+        mpCBColumn->insertItem(FROM_UTF8(tmp->getAnnotationsString(1, true)[i]));
+
+      if (tmp->dimensionality() == 3)
+        {
+          mpLabelDim3->show();
+          mpCBDim3->show();
+
+          mpLabelDim3->setText("Dimension : " + FROM_UTF8(tmp->getDimensionDescription(2)));
+          int nDims = tmp->size()[2];
+
+          for (i = 0; i < nDims; i++)
+            mpCBDim3->insertItem(FROM_UTF8(tmp->getAnnotationsString(2, true)[i]));
+        }
     }
 }
