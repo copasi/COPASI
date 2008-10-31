7bb0f8297ced1f17b5d97ab0c7a525d1cf9abbff
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2008-10-31 00:36:09 +0000

    fix for #1033. now  in case of a model with assignments the priority queue is updated for all reactions with a changed propensity after each event; not considering explicit dependencies.

--- copasi/trajectory/CStochNextReactionMethod.cpp
+++ copasi/trajectory/CStochNextReactionMethod.cpp
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/trajectory/CStochNextReactionMethod.cpp,v $
-//   $Revision: 1.10 $
+//   $Revision: 1.10.16.1 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2007/07/24 18:40:24 $
+//   $Author: ssahle $
+//   $Date: 2008/10/31 00:36:09 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -64,31 +69,60 @@ void CStochNextReactionMethod::setupPriorityQueue(C_FLOAT64 start_time)
 
 void CStochNextReactionMethod::updatePriorityQueue(C_INT32 reaction_index, C_FLOAT64 time)
 {
-  const std::set<unsigned C_INT32> & dep_nodes = mDG.getDependents(reaction_index);
-
+  //first the new time for the currently fired reaction
   C_FLOAT64 new_time = time + generateReactionTime(reaction_index);
-
   mPQ.updateNode(reaction_index, new_time);
 
-  std::set<unsigned C_INT32>::const_iterator di;
+  //now the updates for the other reactions (whose propensities may have changed)
 
-  for (di = dep_nodes.begin(); di != dep_nodes.end(); di++)
+  //if the model contains assignment we use a less efficient loop over all reactions since
+  // we do not know the exact dependencies
+  if (mHasAssignments)
     {
-      if (*di != (unsigned C_INT32) reaction_index)
+      unsigned C_INT32 i;
+      for (i = 0; i < mNumReactions; ++i)
         {
-          C_INT32 index = *di;
-          C_FLOAT64 new_time;
-
-          if (mAmuOld[index] > 0)
+          if (i != (unsigned C_INT32)reaction_index)
             {
-              new_time = time + (mAmuOld[index] / mAmu[index]) * (mPQ.getKey(index) - time);
-            }
-          else
-            {
-              new_time = time + generateReactionTime(index);
-            }
+              if (mAmu[i] == mAmuOld[i])
+                continue;
 
-          mPQ.updateNode(index, new_time);
+              C_FLOAT64 new_time;
+              if (mAmuOld[i] > 0)
+                {
+                  new_time = time + (mAmuOld[i] / mAmu[i]) * (mPQ.getKey(i) - time);
+                }
+              else
+                {
+                  new_time = time + generateReactionTime(i);
+                }
+
+              mPQ.updateNode(i, new_time);
+            }
+        }
+    }
+  else
+    {
+      const std::set<unsigned C_INT32> & dep_nodes = mDG.getDependents(reaction_index);
+      std::set<unsigned C_INT32>::const_iterator di;
+      for (di = dep_nodes.begin(); di != dep_nodes.end(); ++di)
+        {
+          if (*di != (unsigned C_INT32) reaction_index)
+            {
+              C_INT32 index = *di;
+              C_FLOAT64 new_time;
+
+              if (mAmuOld[index] > 0)
+                {
+                  new_time = time + (mAmuOld[index] / mAmu[index]) * (mPQ.getKey(index) - time);
+                }
+              else
+                {
+                  new_time = time + generateReactionTime(index);
+                }
+
+              mPQ.updateNode(index, new_time);
+            }
         }
     }
 }
