32e0476466c9a1dabcba9dad2174cd972ebc2f49
Ralph Gauges <gauges@hs-albsig.de>
2008-11-06 08:05:05 +0000

    Included limits.h for ULONG_MAX.

--- copasi/optimization/COptMethod.cpp
+++ copasi/optimization/COptMethod.cpp
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/optimization/COptMethod.cpp,v $
-//   $Revision: 1.36 $
+//   $Revision: 1.36.14.1 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2007/12/11 21:46:07 $
+//   $Author: gauges $
+//   $Date: 2008/11/06 08:00:55 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -18,6 +23,8 @@
  *
  *  Created for Copasi by Stefan Hoops 2002
  */
+
+#include <limits.h>
 
 #include "copasi.h"
 
