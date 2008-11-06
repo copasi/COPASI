5e0a37ed03d03e7862a249efda7e1caf87e4ae2a
Ralph Gauges <gauges@hs-albsig.de>
2008-11-06 07:41:24 +0000

    Included <stdlib.h> for strtod.

--- copasi/function/CEvaluationNodeNumber.cpp
+++ copasi/function/CEvaluationNodeNumber.cpp
@@ -1,14 +1,20 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/function/CEvaluationNodeNumber.cpp,v $
-//   $Revision: 1.26 $
+//   $Revision: 1.26.16.1 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2007/09/21 18:27:06 $
+//   $Author: gauges $
+//   $Date: 2008/11/06 07:41:24 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
+#include <stdlib.h>
 
 #include "copasi.h"
 #include "CEvaluationNode.h"
