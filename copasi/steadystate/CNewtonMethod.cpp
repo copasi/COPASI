0369737dbfdaa81a39f4c1ddd0e35eda29de7ec1
Stefan Hoops <shoops@vbi.vt.edu>
2008-11-12 14:18:33 +0000

    Avvoid division by zero.

--- copasi/steadystate/CNewtonMethod.cpp
+++ copasi/steadystate/CNewtonMethod.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/steadystate/CNewtonMethod.cpp,v $
-//   $Revision: 1.88.2.4 $
+//   $Revision: 1.88.2.5 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2008/11/11 14:46:29 $
+//   $Date: 2008/11/12 14:18:33 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -692,7 +692,7 @@ C_FLOAT64 CNewtonMethod::targetFunction(const CVector< C_FLOAT64 > & particleflu
 
     for (; pIt != pEnd; ++pIt, ++pAtol, ++ppEntity)
       {
-        tmp = fabs(*pIt) / std::min(*pAtol, fabs((*ppEntity)->getValue()));
+        tmp = fabs(*pIt) / std::min(*pAtol, std::max(100.0 * DBL_MIN, fabs((*ppEntity)->getValue())));
 
         if (tmp > store)
           store = tmp;
