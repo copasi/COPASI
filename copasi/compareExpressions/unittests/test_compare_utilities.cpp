daa5f230348804e6d95d3b15f8493e8515d6c8f1
Ralph Gauges <gauges@hs-albsig.de>
2009-02-20 10:41:39 +0000

    Changes to run with static methods in CCopasiRootContainer.

--- copasi/compareExpressions/unittests/test_compare_utilities.cpp
+++ copasi/compareExpressions/unittests/test_compare_utilities.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/compareExpressions/unittests/test_compare_utilities.cpp,v $
-//   $Revision: 1.3 $
+//   $Revision: 1.4 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2009/02/19 19:50:16 $
+//   $Author: gauges $
+//   $Date: 2009/02/20 10:41:39 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -36,8 +36,7 @@ void test_compare_utilities::setUp()
 
 void test_compare_utilities::tearDown()
 {
-  delete CCopasiRootContainer::Root;
-  CCopasiRootContainer::Root = NULL;
+  CCopasiRootContainer::destroy();
 }
 
 void test_compare_utilities::test_copasi_function_expansion()
