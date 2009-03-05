5d6b36c09a94d1ed9a9cc6a4f756aa4a29f1af35
Aejaaz Kamal <aekamal@vbi.vt.edu>
2009-03-05 22:48:12 +0000

    Initial DM for CQReactionWidget.

---
+++ copasi/UI/CQReactionDM.h
@@ -0,0 +1,40 @@
+// Begin CVS Header
+//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQReactionDM.h,v $
+//   $Revision: 1.1 $
+//   $Name:  $
+//   $Author: aekamal $
+//   $Date: 2009/03/05 22:48:12 $
+// End CVS Header
+
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+#ifndef CQReactionDM_H
+#define CQReactionDM_H
+
+#include "model/CModel.h"
+#include "CQBaseDataModel.h"
+
+class CQReactionDM : public CQBaseDataModel
+  {
+    Q_OBJECT
+
+  public:
+    CQReactionDM(QObject *parent = 0);
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const;
+    bool setData(const QModelIndex &index, const QVariant &value,
+                 int role = Qt::EditRole);
+
+  protected:
+    bool insertRows(int position, int rows, const QModelIndex &index = QModelIndex());
+    bool removeRows(int position, int rows, const QModelIndex &index = QModelIndex());
+  };
+
+#endif //CQReactionDM_H
