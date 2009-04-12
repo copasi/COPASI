03c3a6561697a703441341e2580a5dcb8b01fff6
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2009-04-12 20:02:36 +0000

    These .cpp and .h files are newly implemented because of the existence of .ui file.

--- copasi/UI/SteadyStateWidget.cpp
+++ copasi/UI/SteadyStateWidget.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/SteadyStateWidget.cpp,v $
-//   $Revision: 1.118 $
+//   $Revision: 1.119 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2009/02/19 19:54:03 $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 20:02:36 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -19,7 +19,7 @@
 
 #include <qvariant.h>
 #include <qcheckbox.h>
-#include <q3frame.h>
+//#include <q3frame.h>
 #include <qlabel.h>
 #include <qlineedit.h>
 #include <qpushbutton.h>
@@ -29,8 +29,11 @@
 #include <q3whatsthis.h>
 #include <qmessagebox.h>
 //Added by qt3to4:
+/*
 #include <Q3GridLayout>
 #include <Q3VBoxLayout>
+ */
+#include <QHBoxLayout>
 
 #include "DataModelGUI.h"
 
@@ -59,65 +62,86 @@
 SteadyStateWidget::SteadyStateWidget(QWidget* parent, const char* name, Qt::WFlags fl)
     : TaskWidget(parent, name, fl)
 {
-  if (!name)
-    setName("SteadyStateWidget");
-  setCaption(trUtf8("SteadyStateWidget"));
+  setupUi(this);
 
-  SteadyStateWidgetLayout = new Q3VBoxLayout(this, 11, 6, "SteadyStateWidgetLayout");
+  init();
+  retranslateUi(this);
 
-  //if a mpMethodLayout is created here, it will be used by addMethodXXX() below.
-  mpLayoutProblem = new Q3GridLayout(0, 1, 1, 11, 6, "mpLayoutProblem");
+  /*
+    if (!name)
+      setName("SteadyStateWidget");
+    setCaption(trUtf8("SteadyStateWidget"));
 
-  //*************************
+  //  SteadyStateWidgetLayout = new Q3VBoxLayout(this, 11, 6, "SteadyStateWidgetLayout");
+  //  SteadyStateWidgetLayout = new QVBoxLayout(this, 11, 6, "SteadyStateWidgetLayout");
+    SteadyStateWidgetLayout = new QVBoxLayout(this);
 
-  taskJacobian = new QCheckBox(this, "taskJacobian");
-  taskJacobian->setText(trUtf8("calculate Jacobian"));
-  mpLayoutProblem->addWidget(taskJacobian, 0, 1);
+    //if a mpMethodLayout is created here, it will be used by addMethodXXX() below.
+  //  mpLayoutProblem = new Q3GridLayout(0, 1, 1, 11, 6, "mpLayoutProblem");
 
-  taskStability = new QCheckBox(this, "taskStability");
-  taskStability->setText(trUtf8("perform Stability Analysis"));
-  mpLayoutProblem->addWidget(taskStability, 0, 2);
-  taskStability->setEnabled(false);
+  //  mpLayoutProblem = new QGridLayout(0, 1, 1, 11, 6, "mpLayoutProblem");
 
-  SteadyStateWidgetLayout->addLayout(mpLayoutProblem);
+    QHBoxLayout* hLayout = new QHBoxLayout();
 
-  line1 = new Q3Frame(this, "line1");
-  line1->setFrameShape(Q3Frame::HLine);
-  line1->setFrameShadow(Q3Frame::Sunken);
-  line1->setFrameShape(Q3Frame::HLine);
-  SteadyStateWidgetLayout->addWidget(line1);
+    //*************************
 
-  // addHLineToGrid(mpMethodLayout, 4, 2);
+    taskJacobian = new QCheckBox(this, "taskJacobian");
+    taskJacobian->setText(trUtf8("calculate Jacobian"));
+  //  mpLayoutProblem->addWidget(taskJacobian, 0, 1);
+    hLayout->addWidget(taskJacobian);
 
-  //************* parameter table ****************
-  // addMethodParameterTable(10, 5);
-  //addMethodParameterTableToLayout(5, 2);
+    taskStability = new QCheckBox(this, "taskStability");
+    taskStability->setText(trUtf8("perform Stability Analysis"));
+    taskStability->setEnabled(false);
+  //  mpLayoutProblem->addWidget(taskStability, 0, 2);
+    hLayout->addWidget(taskStability);
 
+  //  SteadyStateWidgetLayout->addLayout(mpLayoutProblem);
+    SteadyStateWidgetLayout->addLayout(hLayout);
+  //  mpLayoutProblem->addLayout(hLayout, 1, 0);
+
+    line1 = new QFrame(this, "line1");
+    line1->setFrameShape(QFrame::HLine);
+    line1->setFrameShadow(QFrame::Sunken);
+    line1->setFrameShape(QFrame::HLine);
+    SteadyStateWidgetLayout->addWidget(line1);
+  //  mpLayoutProblem->addWidget(line1);
+
+    // addHLineToGrid(mpMethodLayout, 4, 2);
+
+    //************* parameter table ****************
+    // addMethodParameterTable(10, 5);
+    //addMethodParameterTableToLayout(5, 2);
+  */
   /*QSpacerItem* spacer_3 = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
   TaskWidgetLayout->addMultiCell(spacer_3, 7, 7, 0, 2);*/
 
   //*******************************************
+//  SteadyStateWidgetLayout->addLayout(mpLayoutProblem);
 
-  // mpMethodLayout->addMultiCellWidget(mpBtnWidget, 11, 11, 0, 2);
+// mpMethodLayout->addMultiCellWidget(mpBtnWidget, 11, 11, 0, 2);
+  /*
+    // signals and slots connections
+    //connect(parameterTable, SIGNAL(valueChanged(int, int)), this, SLOT(parameterValueChanged()));
+    connect(taskJacobian, SIGNAL(toggled(bool)), this, SLOT(taskJacobianToggled()));
 
-  // signals and slots connections
-  //connect(parameterTable, SIGNAL(valueChanged(int, int)), this, SLOT(parameterValueChanged()));
-  connect(taskJacobian, SIGNAL(toggled(bool)), this, SLOT(taskJacobianToggled()));
+    // tab order
+    //setTabOrder(taskName, bExecutable);
+    //setTabOrder(bExecutable, setInitialState);
+    //setTabOrder(setInitialState, taskJacobian);
+    setTabOrder(taskJacobian, taskStability);
+    //setTabOrder(taskStability, parameterTable);
+    //setTabOrder(parameterTable, bRunButton);
 
-  // tab order
-  //setTabOrder(taskName, bExecutable);
-  //setTabOrder(bExecutable, setInitialState);
-  //setTabOrder(setInitialState, taskJacobian);
-  setTabOrder(taskJacobian, taskStability);
-  //setTabOrder(taskStability, parameterTable);
-  //setTabOrder(parameterTable, bRunButton);
+    mpHeaderWidget->setTaskName("Steady State");
 
-  mpHeaderWidget->setTaskName("Steady State");
+    SteadyStateWidgetLayout->insertWidget(0, mpHeaderWidget);
 
-  SteadyStateWidgetLayout->insertWidget(0, mpHeaderWidget);
-  SteadyStateWidgetLayout->addWidget(mpBtnWidget);
+    std::cout << "from SteadyStateWidget.cpp" << std::endl;
+    addMethodParameterTable(0, 0);
 
-  addMethodParameterTable(0, 0);
+    SteadyStateWidgetLayout->addWidget(mpBtnWidget);
+  */
 }
 
 /*
@@ -125,6 +149,22 @@ SteadyStateWidget::SteadyStateWidget(QWidget* parent, const char* name, Qt::WFla
  */
 SteadyStateWidget::~SteadyStateWidget()
 {}
+
+void SteadyStateWidget::languageChange()
+{
+  retranslateUi(this);
+}
+
+void SteadyStateWidget::init()
+{
+  mpHeaderWidget->setTaskName("Steady State");
+
+  SteadyStateWidgetLayout->insertWidget(0, mpHeaderWidget); // header
+  SteadyStateWidgetLayout->insertSpacing(1, 14);      // space between header and body
+  SteadyStateWidgetLayout->addWidget(mpBtnWidget);      // 'footer'
+
+  addMethodParameterTable(0, 0);
+}
 
 CCopasiMethod * SteadyStateWidget::createMethod(const CCopasiMethod::SubType & /*type*/)
 {return NULL; /*CTSSMethod::createTSSMethod(type);*/}
@@ -134,11 +174,13 @@ bool SteadyStateWidget::runTask()
   if (!commonBeforeRunTask()) return false;
 
   bool success = true;
+
   if (!commonRunTask()) success = false;
 
   if (!commonAfterRunTask()) success = false;
 
   StateWidget *pResult = dynamic_cast< StateWidget * >(mpListView->findWidgetFromId(211));
+
   if (pResult) pResult->loadFromBackend();
 
   if (success && isShown()) mpListView->switchToOtherWidget(211, ""); //change to the results window
@@ -153,22 +195,26 @@ bool SteadyStateWidget::loadTask()
 
   CSteadyStateTask* mSteadyStateTask =
     dynamic_cast<CSteadyStateTask *>(CCopasiRootContainer::getKeyFactory()->get(mObjectKey));
+
   if (mSteadyStateTask == NULL)
     return false;
 
   CSteadyStateProblem* steadystateproblem =
     dynamic_cast<CSteadyStateProblem *>(mSteadyStateTask->getProblem());
+
   if (steadystateproblem == NULL)
     return false;
 
   CSteadyStateMethod* steadystatemethod =
     dynamic_cast<CSteadyStateMethod *>(mSteadyStateTask->getMethod());
+
   if (steadystatemethod == NULL)
     return false;
 
   bool bJacobian = steadystateproblem->isJacobianRequested();
   bool bStatistics = steadystateproblem->isStabilityAnalysisRequested();
   taskJacobian->setChecked(bJacobian);
+
   if (bJacobian)
     {
       taskStability->setEnabled(true);
@@ -185,16 +231,19 @@ bool SteadyStateWidget::saveTask()
 
   CSteadyStateTask* mSteadyStateTask =
     dynamic_cast<CSteadyStateTask *>(CCopasiRootContainer::getKeyFactory()->get(mObjectKey));
+
   if (mSteadyStateTask == NULL)
     return false;
 
   CSteadyStateProblem* steadystateproblem =
     dynamic_cast<CSteadyStateProblem *>(mSteadyStateTask->getProblem());
+
   if (steadystateproblem == NULL)
     return false;
 
   CSteadyStateMethod* steadystatemethod =
     dynamic_cast<CSteadyStateMethod *>(mSteadyStateTask->getMethod());
+
   if (steadystatemethod == NULL)
     return false;
 
--- copasi/UI/SteadyStateWidget.h
+++ copasi/UI/SteadyStateWidget.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/SteadyStateWidget.h,v $
-//   $Revision: 1.38 $
+//   $Revision: 1.39 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2008/12/18 19:58:29 $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 20:02:36 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -19,16 +19,24 @@
 #define STEADYSTATEWIDGET_H
 
 #include "copasi.h"
+
+#include "ui_SteadyStateWidget.h"
+
 //#include <qvariant.h>
 //#include <qwidget.h>
 #include "UI/TaskWidget.h"
 //Added by qt3to4:
+/*
 #include <Q3GridLayout>
 #include <Q3Frame>
 #include <Q3VBoxLayout>
+ */
+#include <QGridLayout>
+#include <QFrame>
+#include <QVBoxLayout>
 
 class QCheckBox;
-class Q3Frame;
+//class Q3Frame;
 class Q3GridLayout;
 
 //class QLabel;
@@ -41,34 +49,42 @@ class Q3GridLayout;
 //class CSteadyStateMethod;
 //class CModel;
 
-class SteadyStateWidget : public TaskWidget
-  {
-    Q_OBJECT
+class SteadyStateWidget : public TaskWidget, public Ui::SteadyStateWidget
+{
+  Q_OBJECT
 
-  public:
-    SteadyStateWidget(QWidget* parent = 0, const char* name = 0, Qt::WFlags fl = 0);
-    ~SteadyStateWidget();
+public:
+  SteadyStateWidget(QWidget* parent = 0, const char* name = 0, Qt::WFlags fl = 0);
+  ~SteadyStateWidget();
 
-    virtual bool runTask();
+  virtual bool runTask();
 
-  protected slots:
-    //virtual void parameterValueChanged();
-    virtual void taskJacobianToggled();
+protected slots:
+  //virtual void parameterValueChanged();
+  virtual void taskJacobianToggled();
 
-  protected:
-    virtual bool loadTask();
-    virtual bool saveTask();
+protected:
+  virtual bool loadTask();
+  virtual bool saveTask();
 
-    virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
+  virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
+  /*
+      //QFrame* line_ss;
+  //    Q3VBoxLayout* SteadyStateWidgetLayout;
+      QVBoxLayout* SteadyStateWidgetLayout;
+      QCheckBox* taskJacobian;
+      QCheckBox* taskStability;
 
-    //QFrame* line_ss;
-    Q3VBoxLayout* SteadyStateWidgetLayout;
-    QCheckBox* taskJacobian;
-    QCheckBox* taskStability;
-    Q3GridLayout * mpLayoutProblem;
-    Q3Frame* line1;
-    //QLabel* parameterValueLabel;
-    //QTable* parameterTable;
-  };
+      QGridLayout * mpLayoutProblem;
+      QFrame* line1;
+      //QLabel* parameterValueLabel;
+      //QTable* parameterTable;
+  */
+private:
+  void init();
+
+protected slots:
+  virtual void languageChange();
+};
 
 #endif // STEADYSTATEWIDGET_H
