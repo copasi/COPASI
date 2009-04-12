8a46474c81277c6daaa727cdc76194dcb7c61c4f
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2009-04-12 19:49:56 +0000

    Table has been converted from Q3Table to QTableWidget. The ui file has been converted to and modified by Qt4's Designer. The .cpp and .h are newly implemented according to the Qt4 structure.

--- copasi/UI/CQLyapWidget.cpp
+++ copasi/UI/CQLyapWidget.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQLyapWidget.cpp,v $
-//   $Revision: 1.7 $
+//   $Revision: 1.8 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2008/12/18 19:56:21 $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 19:49:56 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -13,8 +13,19 @@
 
 #include "CQLyapWidget.h"
 
-#include <qvariant.h>
-#include "CQLyapWidget.ui.h"
+#include <qmessagebox.h>
+
+#include "CQTaskBtnWidget.h"
+#include "CQTaskHeaderWidget.h"
+#include "CProgressBar.h"
+
+#include "lyap/CLyapTask.h"
+#include "lyap/CLyapProblem.h"
+#include "model/CModel.h"
+#include "report/CKeyFactory.h"
+#include "utilities/CCopasiException.h"
+#include "report/CCopasiRootContainer.h"
+
 /*
  *  Constructs a CQLyapWidget which is a child of 'parent', with the
  *  name 'name'.'
@@ -42,4 +53,122 @@ CQLyapWidget::~CQLyapWidget()
 void CQLyapWidget::languageChange()
 {
   retranslateUi(this);
+}
+
+bool CQLyapWidget::runTask()
+{
+  CLyapTask * pTask =
+    dynamic_cast< CLyapTask * >(CCopasiRootContainer::getKeyFactory()->get(mObjectKey));
+
+  if (!pTask) return false;
+
+  if (!commonBeforeRunTask()) return false;
+
+  bool success = commonRunTask();
+
+  commonAfterRunTask();
+
+  if (success)
+    mpListView->switchToOtherWidget(261, ""); //change to the results window
+
+  return success;
+}
+
+CCopasiMethod * CQLyapWidget::createMethod(const CCopasiMethod::SubType & type)
+{return CLyapMethod::createMethod(type);}
+
+bool CQLyapWidget::loadTask()
+{
+  CLyapTask * pTask = dynamic_cast< CLyapTask * >(mpTask);
+
+  if (!pTask) return false;
+
+  loadCommon();
+  loadMethod();
+
+  CLyapProblem* pProblem =
+    dynamic_cast< CLyapProblem * >(mpTask->getProblem());
+
+  if (!pProblem) return false;
+
+  mpEditExponent->setText(QString::number(std::max<unsigned C_INT32>(1, pProblem->getExponentNumber())));
+
+  assert(CCopasiRootContainer::getDatamodelList()->size() > 0);
+  bool enabled =
+    ((*CCopasiRootContainer::getDatamodelList())[0]->getModel()->getInitialTime() != pProblem->getTransientTime());
+
+  mpCheckDelay->setChecked(enabled);
+  mpEditDelay->setEnabled(enabled);
+  mpEditDelay->setText(QString::number(pProblem->getTransientTime()));
+
+  mpCheckDivergence->setChecked(pProblem->divergenceRequested());
+
+  mChanged = false;
+
+  return true;
+}
+
+bool CQLyapWidget::saveTask()
+{
+  CLyapTask * pTask = dynamic_cast< CLyapTask * >(mpTask);
+
+  if (!pTask) return false;
+
+  saveCommon();
+  saveMethod();
+
+  CLyapProblem* pProblem =
+    dynamic_cast< CLyapProblem * >(mpTask->getProblem());
+
+  if (!pProblem) return false;
+
+  if (QString::number(pProblem->getExponentNumber()) != mpEditExponent->text())
+    {
+      pProblem->setExponentNumber(std::max(1, mpEditExponent->text().toInt()));
+      mChanged = true;
+    }
+
+  assert(CCopasiRootContainer::getDatamodelList()->size() > 0);
+  bool enabled =
+    ((*CCopasiRootContainer::getDatamodelList())[0]->getModel()->getInitialTime() != pProblem->getTransientTime());
+
+  if (mpCheckDelay->isChecked() != enabled ||
+      (mpCheckDelay->isChecked() &&
+       mpEditDelay->text() != QString::number(pProblem->getTransientTime())))
+    {
+      if (mpCheckDelay->isChecked())
+        pProblem->setTransientTime(mpEditDelay->text().toDouble());
+      else
+        pProblem->setTransientTime((*CCopasiRootContainer::getDatamodelList())[0]->getModel()->getInitialTime());
+
+      mChanged = true;
+    }
+
+  if (mpCheckDivergence->isChecked() != pProblem->divergenceRequested())
+    {
+      pProblem->setDivergenceRequested(mpCheckDivergence->isChecked());
+      mChanged = true;
+    }
+
+  if (mChanged)(*CCopasiRootContainer::getDatamodelList())[0]->changed();
+
+  mChanged = false;
+  return true;
+}
+
+void CQLyapWidget::init()
+{
+  mpHeaderWidget->setTaskName("Lyapunov Exponents");
+
+  vboxLayout->insertWidget(0, mpHeaderWidget);  // header
+  vboxLayout->insertSpacing(1, 14);       // space between header and body
+  vboxLayout->addWidget(mpBtnWidget);     // 'footer'
+
+  //  addMethodSelectionBox(CLyapTask::ValidMethods);
+  addMethodParameterTable(0, 2);
+}
+
+void CQLyapWidget::slotDelayChecked()
+{
+  mpEditDelay->setEnabled(mpCheckDelay->isChecked());
 }
--- copasi/UI/CQLyapWidget.h
+++ copasi/UI/CQLyapWidget.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQLyapWidget.h,v $
-//   $Revision: 1.8 $
+//   $Revision: 1.9 $
 //   $Name:  $
-//   $Author: gauges $
-//   $Date: 2009/02/18 20:46:37 $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 19:49:56 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -16,6 +16,8 @@
 
 #include <qvariant.h>
 
+#include "ui_CQLyapWidget.h"
+/*
 #include <Qt3Support/Q3MimeSourceFactory>
 #include <QtCore/QVariant>
 #include <QtGui/QAction>
@@ -34,121 +36,31 @@
 #include <Q3VBoxLayout>
 #include <QLabel>
 #include <Q3Frame>
+ */
 #include "TaskWidget.h"
 
-class Ui_CQLyapWidget
-  {
-  public:
-    Q3VBoxLayout *vboxLayout;
-    Q3GridLayout *gridLayout;
-    Q3Frame *mpLine;
-    Q3HBoxLayout *hboxLayout;
-    QCheckBox *mpCheckDelay;
-    QLineEdit *mpEditDelay;
-    QLineEdit *mpEditExponent;
-    QCheckBox *mpCheckDivergence;
-    QLabel *mpLblExponents;
-
-    void setupUi(TaskWidget *CQLyapWidget)
-    {
-      if (CQLyapWidget->objectName().isEmpty())
-        CQLyapWidget->setObjectName(QString::fromUtf8("CQLyapWidget"));
-      CQLyapWidget->resize(292, 106);
-      vboxLayout = new Q3VBoxLayout(CQLyapWidget);
-      vboxLayout->setSpacing(6);
-      vboxLayout->setMargin(11);
-      vboxLayout->setObjectName(QString::fromUtf8("vboxLayout"));
-      gridLayout = new Q3GridLayout(1, 1);
-      gridLayout->setSpacing(6);
-      gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-      mpLine = new Q3Frame(CQLyapWidget);
-      mpLine->setObjectName(QString::fromUtf8("mpLine"));
-      mpLine->setFrameShape(Q3Frame::HLine);
-      mpLine->setFrameShadow(Q3Frame::Sunken);
-
-      gridLayout->addWidget(mpLine, 3, 0, 1, 2);
-
-      hboxLayout = new Q3HBoxLayout();
-      hboxLayout->setSpacing(0);
-      hboxLayout->setObjectName(QString::fromUtf8("hboxLayout"));
-      mpCheckDelay = new QCheckBox(CQLyapWidget);
-      mpCheckDelay->setObjectName(QString::fromUtf8("mpCheckDelay"));
-
-      hboxLayout->addWidget(mpCheckDelay);
-
-      mpEditDelay = new QLineEdit(CQLyapWidget);
-      mpEditDelay->setObjectName(QString::fromUtf8("mpEditDelay"));
-
-      hboxLayout->addWidget(mpEditDelay);
-
-      gridLayout->addLayout(hboxLayout, 1, 1, 1, 1);
-
-      mpEditExponent = new QLineEdit(CQLyapWidget);
-      mpEditExponent->setObjectName(QString::fromUtf8("mpEditExponent"));
-
-      gridLayout->addWidget(mpEditExponent, 0, 1, 1, 1);
-
-      mpCheckDivergence = new QCheckBox(CQLyapWidget);
-      mpCheckDivergence->setObjectName(QString::fromUtf8("mpCheckDivergence"));
-
-      gridLayout->addWidget(mpCheckDivergence, 2, 1, 1, 1);
-
-      mpLblExponents = new QLabel(CQLyapWidget);
-      mpLblExponents->setObjectName(QString::fromUtf8("mpLblExponents"));
-      mpLblExponents->setAlignment(Qt::AlignVCenter | Qt::AlignRight);
-      mpLblExponents->setWordWrap(false);
-
-      gridLayout->addWidget(mpLblExponents, 0, 0, 1, 1);
-
-      vboxLayout->addLayout(gridLayout);
-
-      QWidget::setTabOrder(mpEditExponent, mpCheckDelay);
-      QWidget::setTabOrder(mpCheckDelay, mpEditDelay);
-      QWidget::setTabOrder(mpEditDelay, mpCheckDivergence);
-
-      retranslateUi(CQLyapWidget);
-      QObject::connect(mpCheckDelay, SIGNAL(clicked()), CQLyapWidget, SLOT(slotDelayChecked()));
-
-      QMetaObject::connectSlotsByName(CQLyapWidget);
-    } // setupUi
-
-    void retranslateUi(TaskWidget *CQLyapWidget)
-    {
-      CQLyapWidget->setCaption(QApplication::translate("CQLyapWidget", "CQLyapWidget", 0, QApplication::UnicodeUTF8));
-      mpCheckDelay->setText(QApplication::translate("CQLyapWidget", "start averaging after t =", 0, QApplication::UnicodeUTF8));
-      mpCheckDivergence->setText(QApplication::translate("CQLyapWidget", "calculate Divergence", 0, QApplication::UnicodeUTF8));
-      mpLblExponents->setText(QApplication::translate("CQLyapWidget", "Number of Exponents", 0, QApplication::UnicodeUTF8));
-      Q_UNUSED(CQLyapWidget);
-    } // retranslateUi
-  };
-
-namespace Ui
-  {
-  class CQLyapWidget: public Ui_CQLyapWidget {};
-} // namespace Ui
-
 class CQLyapWidget : public TaskWidget, public Ui::CQLyapWidget
-  {
-    Q_OBJECT
+{
+  Q_OBJECT
 
-  public:
-    CQLyapWidget(QWidget* parent = 0, const char* name = 0);
-    ~CQLyapWidget();
+public:
+  CQLyapWidget(QWidget* parent = 0, const char* name = 0);
+  ~CQLyapWidget();
 
-    virtual bool runTask();
+  virtual bool runTask();
 
-  protected:
-    virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
-    virtual bool loadTask();
-    virtual bool saveTask();
+protected:
+  virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
+  virtual bool loadTask();
+  virtual bool saveTask();
 
-  protected slots:
-    virtual void languageChange();
+protected slots:
+  virtual void languageChange();
 
-    void slotDelayChecked();
+  void slotDelayChecked();
 
-  private:
-    void init();
-  };
+private:
+  void init();
+};
 
 #endif // CQLYAPWIDGET_H
