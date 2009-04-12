431913b303192e9648409a78ed7486eace9c4743
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2009-04-12 20:18:40 +0000

    Taken out from SensitivitiesWidget file such that the latter contains only the widget itself. This separation has a benefit according to the Qt4 structure, ie. the SensWidgetComboBox can act as a custom widget.

---
+++ copasi/UI/SensWidgetComboBox.cpp
@@ -0,0 +1,110 @@
+// Begin CVS Header
+//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/SensWidgetComboBox.cpp,v $
+//   $Revision: 1.1 $
+//   $Name:  $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 20:18:40 $
+// End CVS Header
+
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc. and EML Research, gGmbH.
+// All rights reserved.
+
+//#include <q3filedialog.h>
+
+#include <qvariant.h>
+#include <qcheckbox.h>
+//#include <qcombobox.h>
+//#include <q3frame.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qradiobutton.h>
+//#include <q3table.h>
+#include <qlayout.h>
+#include <qtooltip.h>
+#include <q3whatsthis.h>
+#include <qmessagebox.h>
+#include <qtoolbutton.h>
+#include <qimage.h>
+//Added by qt3to4:
+//#include <Q3GridLayout>
+
+#include <QFrame>
+#include <QFileDialog>
+#include <QTableWidget>
+
+#include <algorithm>
+
+#include "SensWidgetComboBox.h"
+#include "qtUtilities.h"
+
+/*
+#include "SensitivitiesWidget.h"
+#include "DataModelGUI.h"
+#include "qtUtilities.h"
+#include "CQTaskBtnWidget.h"
+#include "CQTaskHeaderWidget.h"
+#include "CCopasiSelectionDialog.h"
+
+#include "CopasiDataModel/CCopasiDataModel.h"
+#include "report/CCopasiRootContainer.h"
+#include "sensitivities/CSensTask.h"
+#include "sensitivities/CSensProblem.h"
+#include "sensitivities/CSensMethod.h"
+#include "model/CModel.h"
+#include "utilities/CCopasiException.h"
+#include "report/CKeyFactory.h"
+#include "CQSensResultWidget.h"
+ */
+
+SensWidgetComboBox::SensWidgetComboBox(QWidget * parent, const char * name)
+    : QComboBox(false, parent, name)
+{}
+
+void SensWidgetComboBox::fillFromList(const std::vector<CObjectLists::ListType> & list)
+{
+  //store old selection
+  CObjectLists::ListType oldItem = getCurrentObjectList();
+
+  mIndexTable = list;
+
+  //fill combobox
+  clear();
+  std::vector<CObjectLists::ListType>::const_iterator it, itEnd = mIndexTable.end();
+
+  for (it = mIndexTable.begin(); it != itEnd; ++it)
+    insertItem(FROM_UTF8(CObjectLists::ListTypeName[*it]));
+
+  //restore old selection, if possible
+  if (!setCurrentObjectList(oldItem))
+    setCurrentItem(0);
+}
+
+CObjectLists::ListType SensWidgetComboBox::getCurrentObjectList() const
+{
+  unsigned int index = currentItem();
+
+  if (index < mIndexTable.size())
+    return mIndexTable[currentItem()];
+  else
+    return CObjectLists::EMPTY_LIST;
+}
+
+bool SensWidgetComboBox::setCurrentObjectList(CObjectLists::ListType lt)
+{
+  std::vector<CObjectLists::ListType>::const_iterator it;
+  it = std::find(mIndexTable.begin(),
+                 mIndexTable.end(),
+                 lt);
+
+  if (it == mIndexTable.end()) return false;
+
+  setCurrentItem(it - mIndexTable.begin());
+  return true;
+}
---
+++ copasi/UI/SensWidgetComboBox.h
@@ -0,0 +1,68 @@
+// Begin CVS Header
+//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/SensWidgetComboBox.h,v $
+//   $Revision: 1.1 $
+//   $Name:  $
+//   $Author: pwilly $
+//   $Date: 2009/04/12 20:18:40 $
+// End CVS Header
+
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc. and EML Research, gGmbH.
+// All rights reserved.
+
+#ifndef SENSWIDGETCOMBOBOX_H
+#define SENSWIDGETCOMBOBOX_H
+
+#include "copasi.h"
+//#include <qvariant.h>
+//#include <qwidget.h>
+
+//#include "UI/TaskWidget.h"
+#include "sensitivities/CSensProblem.h"
+
+#include <qcombobox.h>
+//Added by qt3to4:
+//#include <Q3VBoxLayout>
+#include <QLabel>
+
+#include <QVBoxLayout>
+
+class QToolButton;
+
+/**
+  * This class handles a combo box to choose from lists of object lists
+  */
+class SensWidgetComboBox : public QComboBox
+{
+  Q_OBJECT
+
+public:
+
+  SensWidgetComboBox(QWidget * parent = 0, const char * name = 0);
+
+  /**
+   * this fills the combobox with a new list of entries. If the
+   * old current value is still allowed in the new list, it is set.
+   * Otherwise the box shows the first item.
+   */
+  void fillFromList(const std::vector<CObjectLists::ListType> & list);
+
+  CObjectLists::ListType getCurrentObjectList() const;
+
+  /**
+   * set current selection (if possible). It returns true
+   * if the operation was succesful.
+   */
+  bool setCurrentObjectList(CObjectLists::ListType lt);
+
+protected:
+
+  std::vector<CObjectLists::ListType> mIndexTable;
+};
+
+#endif // SENSWIDGETCOMBOBOX_H
