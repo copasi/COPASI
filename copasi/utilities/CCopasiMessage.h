e52ed86648c987404c3b5d9ac15c31a6bf9d9e3c
Stefan Hoops <shoops@vbi.vt.edu>
2009-05-19 16:08:58 +0000

    Added message for simultaneous events.

--- copasi/utilities/CCopasiMessage.h
+++ copasi/utilities/CCopasiMessage.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CCopasiMessage.h,v $
-//   $Revision: 1.58 $
+//   $Revision: 1.59 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2009/01/07 19:38:35 $
+//   $Date: 2009/05/19 16:08:58 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -63,205 +63,206 @@ Message;
 #define MCTSSAProblem            MCopasiBase + 3100
 #define MCEigen                  MCopasiBase + 3200
 #define MCMiriam                 MCopasiBase + 3300
+#define MCMathModel              MCopasiBase + 3400
 
 /**
- *  This throws an exception with information where the error occured.
+ *  This throws an exception with information where the error occurred.
  */
 #define fatalError() {CCopasiMessage(CCopasiMessage::EXCEPTION, \
-                                     "%s (%d) compiled: %s %s", \
-                                     __FILE__, __LINE__, __DATE__, __TIME__);}
+                                       "%s (%d) compiled: %s %s", \
+                                       __FILE__, __LINE__, __DATE__, __TIME__);}
 
 class CCopasiMessage
+{
+public:
+  enum Type
   {
-  public:
-    enum Type
-    {
-      RAW = 0,
-      TRACE = 1,
-      COMMANDLINE = 2,
-      WARNING = 3,
-      // In the case that ERROR is defined we have to remove its definition
-      // temporarely.
+    RAW = 0,
+    TRACE = 1,
+    COMMANDLINE = 2,
+    WARNING = 3,
+    // In the case that ERROR is defined we have to remove its definition
+    // temporarily.
 #ifdef ERROR
 # undef ERROR
 #endif
-      ERROR = 4,
-      EXCEPTION = 5,
-      RAW_FILTERED = 128,
-      TRACE_FILTERED = 129,
-      COMMANDLINE_FILTERED = 130,
-      WARNING_FILTERED = 131,
-      ERROR_FILTERED = 132,
-      EXCEPTION_FILTERED = 133
-    };
+    ERROR = 4,
+    EXCEPTION = 5,
+    RAW_FILTERED = 128,
+    TRACE_FILTERED = 129,
+    COMMANDLINE_FILTERED = 130,
+    WARNING_FILTERED = 131,
+    ERROR_FILTERED = 132,
+    EXCEPTION_FILTERED = 133
+  };
 
-    // Attributes
+  // Attributes
 
-  private:
-    /**
-     * Message text.
-     */
-    std::string mText;
+private:
+  /**
+   * Message text.
+   */
+  std::string mText;
 
-    /**
-     * Message type.
-     */
-    CCopasiMessage::Type mType;
+  /**
+   * Message type.
+   */
+  CCopasiMessage::Type mType;
 
-    /**
-     * Message Number
-     */
-    unsigned C_INT32 mNumber;
+  /**
+   * Message Number
+   */
+  unsigned C_INT32 mNumber;
 
 #ifndef WIN32
-    /**
-     * The stack of messages. Each message created with one of
-     * the specific constructors is automically added to the stack.
-     */
-    static std::deque< CCopasiMessage > mMessageDeque;
+  /**
+   * The stack of messages. Each message created with one of
+   * the specific constructors is automatically added to the stack.
+   */
+  static std::deque< CCopasiMessage > mMessageDeque;
 #endif // not WIN32
 
-    // Operations
+  // Operations
 
-  public:
-    /**
-     * This function peeks at the first message created in COPASI.
-     * If no more messages are in the deque the message
-     * (MCCopasiMessage + 1, "Message (1): No more messages." is returned.
-     * @return const CCopasiMessage & message
-     */
-    static const CCopasiMessage & peekFirstMessage();
+public:
+  /**
+   * This function peeks at the first message created in COPASI.
+   * If no more messages are in the dequeue the message
+   * (MCCopasiMessage + 1, "Message (1): No more messages." is returned.
+   * @return const CCopasiMessage & message
+   */
+  static const CCopasiMessage & peekFirstMessage();
 
-    /**
-     * This function peeks at the last message created in COPASI.
-     * If no more messages are in the deque the message
-     * (MCCopasiMessage + 1, "Message (1): No more messages." is returned.
-     * @return const CCopasiMessage & message
-     */
-    static const CCopasiMessage & peekLastMessage();
+  /**
+   * This function peeks at the last message created in COPASI.
+   * If no more messages are in the dequeue the message
+   * (MCCopasiMessage + 1, "Message (1): No more messages." is returned.
+   * @return const CCopasiMessage & message
+   */
+  static const CCopasiMessage & peekLastMessage();
 
-    /**
-     * This function retrieves the first message created in COPASI.
-     * Consecutive calls allow for the retrieval of all generated
-     * messages in chronological order. If no more messages are in
-     * the deque the message (MCCopasiMessage + 1, "Message (1):
-     * No more messages." is returned.
-     * @return CCopasiMessage  message
-     */
-    static CCopasiMessage getFirstMessage();
+  /**
+   * This function retrieves the first message created in COPASI.
+   * Consecutive calls allow for the retrieval of all generated
+   * messages in chronological order. If no more messages are in
+   * the dequeue the message (MCCopasiMessage + 1, "Message (1):
+   * No more messages." is returned.
+   * @return CCopasiMessage  message
+   */
+  static CCopasiMessage getFirstMessage();
 
-    /**
-     * This function retrieves the last message created in COPASI.
-     * Consecutive calls allow for the retrieval of all generated
-     * messages in reverse chronological order. If no more messages
-     * are in the deque the message (MCCopasiMessage + 1, "Message
-     * (1): No more messages." is returned.
-     * @return CCopasiMessage message
-     */
-    static CCopasiMessage getLastMessage();
+  /**
+   * This function retrieves the last message created in COPASI.
+   * Consecutive calls allow for the retrieval of all generated
+   * messages in reverse chronological order. If no more messages
+   * are in the dequeue the message (MCCopasiMessage + 1, "Message
+   * (1): No more messages." is returned.
+   * @return CCopasiMessage message
+   */
+  static CCopasiMessage getLastMessage();
 
-    /**
-     * Retrieve the text of all messages in the deque in chronological
-     * or reverse chronological order. If more than on message is in
-     * the deque the messages are seperated by an empty line.
-     * @return std::string messageTexts
-     */
-    static std::string getAllMessageText(const bool & chronological = true);
+  /**
+   * Retrieve the text of all messages in the dequeue in chronological
+   * or reverse chronological order. If more than on message is in
+   * the dequeue the messages are separated by an empty line.
+   * @return std::string messageTexts
+   */
+  static std::string getAllMessageText(const bool & chronological = true);
 
-    /**
-     * This function clears the message stack.
-     */
-    static void clearDeque();
+  /**
+   * This function clears the message stack.
+   */
+  static void clearDeque();
 
-    /**
-     * Retreive the size of the deque
-     * @return unsigned C_INT32 size
-     */
-    static unsigned C_INT32 size();
+  /**
+   * Retrieve the size of the dequeue
+   * @return unsigned C_INT32 size
+   */
+  static unsigned C_INT32 size();
 
-    /**
-     * Retrieve highest severity of the messages in the deque.
-     * @return const CCopasiMessage::Type & highestSeverity
-     */
-    static CCopasiMessage::Type getHighestSeverity();
+  /**
+   * Retrieve highest severity of the messages in the dequeue.
+   * @return const CCopasiMessage::Type & highestSeverity
+   */
+  static CCopasiMessage::Type getHighestSeverity();
 
-    /**
-     * Check whether a message with the given number is in the deque
-     * @param const unsigned C_INT32 & number
-     * @return bool found
-     */
-    static bool checkForMessage(const unsigned C_INT32 & number);
+  /**
+   * Check whether a message with the given number is in the dequeue
+   * @param const unsigned C_INT32 & number
+   * @return bool found
+   */
+  static bool checkForMessage(const unsigned C_INT32 & number);
 
-    /**
-     *  Default consructor.
-     *  This creates a default error messages, which actually does nothing.
-     */
-    CCopasiMessage();
+  /**
+   *  Default constructor.
+   *  This creates a default error messages, which actually does nothing.
+   */
+  CCopasiMessage();
 
-    /**
-     *  Copy consructor.
-     *  @param const CCopasiMessage & src
-     */
-    CCopasiMessage(const CCopasiMessage & src);
+  /**
+   *  Copy constructor.
+   *  @param const CCopasiMessage & src
+   */
+  CCopasiMessage(const CCopasiMessage & src);
 
-    /**
-     *  Specified consructor.
-     *  This creates a formated message.
-     *  @param CCopasiMessage::Type type (RAW|TRACE|WARNING|ERROR)
-     *  @param const char * format (printf like format string)
-     *  @param ... arguments like in printf
-     */
-    CCopasiMessage(Type type, const char *format, ...);
+  /**
+   *  Specified constructor.
+   *  This creates a formated message.
+   *  @param CCopasiMessage::Type type (RAW|TRACE|WARNING|ERROR)
+   *  @param const char * format (printf like format string)
+   *  @param ... arguments like in printf
+   */
+  CCopasiMessage(Type type, const char *format, ...);
 
-    /**
-     *  Specified consructor.
-     *  This creates a formated message.
-     *  @param CCopasiMessage::Type type (RAW|TRACE|WARNING|ERROR)
-     *  @param unsigned C_INT32 number (message number see message.h)
-     *  @param ... arguments like in printf
-     */
-    CCopasiMessage(Type type, unsigned C_INT32 number, ...);
+  /**
+   *  Specified constructor.
+   *  This creates a formated message.
+   *  @param CCopasiMessage::Type type (RAW|TRACE|WARNING|ERROR)
+   *  @param unsigned C_INT32 number (message number see message.h)
+   *  @param ... arguments like in printf
+   */
+  CCopasiMessage(Type type, unsigned C_INT32 number, ...);
 
-    /**
-     *  Destructor.
-     */
-    ~CCopasiMessage();
+  /**
+   *  Destructor.
+   */
+  ~CCopasiMessage();
 
-    /**
-     *  Assignement operator.
-     */
-    CCopasiMessage &operator=(const CCopasiMessage & RHS);
+  /**
+   *  Assignment operator.
+   */
+  CCopasiMessage &operator=(const CCopasiMessage & RHS);
 
-    /**
-     *  Retrieves the text of the message.
-     *  @return "const string &" mMessage
-     */
-    const std::string & getText() const;
+  /**
+   *  Retrieves the text of the message.
+   *  @return "const string &" mMessage
+   */
+  const std::string & getText() const;
 
-    /**
-     *  Retrieves thetype of the message.
-     *  @return "const CCopasiMessage::Type &" mType
-     */
-    const CCopasiMessage::Type & getType() const;
+  /**
+   *  Retrieves the type of the message.
+   *  @return "const CCopasiMessage::Type &" mType
+   */
+  const CCopasiMessage::Type & getType() const;
 
-    /**
-     *  Retrieves the number of the message.
-     *  @return "const unsigned C_INT32 &" mNumber
-     */
-    const unsigned C_INT32 & getNumber() const;
+  /**
+   *  Retrieves the number of the message.
+   *  @return "const unsigned C_INT32 &" mNumber
+   */
+  const unsigned C_INT32 & getNumber() const;
 
-  private:
-    /**
-     *  The actual constructor of a message.
-     *  @param const bool & throw (default: false)
-     *  @param text message text
-     */
-    void handler(const bool & _throw = false);
+private:
+  /**
+   *  The actual constructor of a message.
+   *  @param const bool & throw (default: false)
+   *  @param text message text
+   */
+  void handler(const bool & _throw = false);
 
-    /**
-     *  Inserts line breaks in the message text.
-     */
-    void lineBreak();
-  };
+  /**
+   *  Inserts line breaks in the message text.
+   */
+  void lineBreak();
+};
 
 #endif // COPASI_CCopasiMessage
