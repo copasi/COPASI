37596fa4ee50122fb07a19d69392cb0a6124f0a2
Stefan Hoops <shoops@vbi.vt.edu>
2009-05-19 16:08:16 +0000

    Fixed copy constructor to include parent pointer.

--- copasi/report/CCopasiObjectReference.h
+++ copasi/report/CCopasiObjectReference.h
@@ -1,12 +1,17 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/report/CCopasiObjectReference.h,v $
-   $Revision: 1.36 $
+   $Revision: 1.37 $
    $Name:  $
-   $Author: gauges $
-   $Date: 2006/10/15 08:00:56 $
+   $Author: shoops $
+   $Date: 2009/05/19 16:08:16 $
    End CVS Header */
 
-// Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -25,86 +30,87 @@
 #include "report/CCopasiObjectName.h"
 
 template <class CType> class CCopasiObjectReference: public CCopasiObject
+{
+  typedef CType referenceType;
+
+  //Attributes
+private:
+  CType * mpReference;
+
+  //Operations
+private:
+  CCopasiObjectReference() {};
+
+  void updateMethod(const CType & value)
+  {*mpReference = value;}
+
+public:
+  CCopasiObjectReference(const std::string & name,
+                         const CCopasiContainer * pParent,
+                         referenceType & reference,
+                         const unsigned C_INT32 & flag = 0):
+      CCopasiObject(name, pParent, "Reference",
+                    CCopasiObject::Reference |
+                    CCopasiObject::NonUniqueName |
+                    flag),
+      mpReference(&reference)
+  {assert(pParent != NULL);}
+
+  CCopasiObjectReference(const CCopasiObjectReference< referenceType > & src,
+                         const CCopasiContainer * pParent):
+      CCopasiObject(src, pParent),
+      mpReference(src.mpReference)
+  {}
+
+  virtual ~CCopasiObjectReference() {}
+
+  virtual void * getValuePointer() const {return mpReference;}
+
+  virtual const CCopasiObject * getValueObject() const {return this;}
+
+  virtual void setReference(referenceType & reference)
+  {mpReference = &reference;}
+
+  virtual void print(std::ostream * ostream) const
+  {(*ostream) << *mpReference;};
+
+  virtual std::string getObjectDisplayName(bool regular = true, bool richtext = false) const
   {
-    typedef CType referenceType;
+    //supress "Value"
+    if (getObjectParent() && getObjectName() == "Value")
+      return getObjectParent()->getObjectDisplayName(regular, richtext);
 
-    //Attributes
-  private:
-    CType * mpReference;
-
-    //Operations
-  private:
-    CCopasiObjectReference() {};
-
-    void updateMethod(const CType & value)
-    {*mpReference = value;}
-
-  public:
-    CCopasiObjectReference(const std::string & name,
-                           const CCopasiContainer * pParent,
-                           referenceType & reference,
-                           const unsigned C_INT32 & flag = 0):
-        CCopasiObject(name, pParent, "Reference",
-                      CCopasiObject::Reference |
-                      CCopasiObject::NonUniqueName |
-                      flag),
-        mpReference(&reference)
-    {assert(pParent != NULL);}
-
-    CCopasiObjectReference(const CCopasiObjectReference< referenceType > & src):
-        CCopasiObject(src),
-        mpReference(src.mpReference)
-    {}
-
-    virtual ~CCopasiObjectReference() {}
-
-    virtual void * getValuePointer() const {return mpReference;}
-
-    virtual const CCopasiObject * getValueObject() const {return this;}
-
-    virtual void setReference(referenceType & reference)
-    {mpReference = &reference;}
-
-    virtual void print(std::ostream * ostream) const
-      {(*ostream) << *mpReference;};
-
-    virtual std::string getObjectDisplayName(bool regular = true, bool richtext = false) const
+    //special treatment of metab children
+    if (getObjectParent()->getObjectType() == "Metabolite")
       {
-        //supress "Value"
-        if (getObjectParent() && getObjectName() == "Value")
-          return getObjectParent()->getObjectDisplayName(regular, richtext);
-
-        //special treatment of metab children
-        if (getObjectParent()->getObjectType() == "Metabolite")
+        if (getObjectName() == "Concentration")
           {
-            if (getObjectName() == "Concentration")
+            if (richtext)
               {
-                if (richtext)
-                  {
-                    return "<font color=\"#2222cc\">[</font>"
-                    + getObjectParent()->getObjectDisplayName(regular, richtext)
-                    + "<font color=\"#2222cc\">]</font>";
-                  }
-                else
-                {return "[" + getObjectParent()->getObjectDisplayName(regular, richtext) + "]";}
+                return "<font color=\"#2222cc\">[</font>"
+                       + getObjectParent()->getObjectDisplayName(regular, richtext)
+                       + "<font color=\"#2222cc\">]</font>";
               }
-
-            if (getObjectName() == "InitialConcentration")
-              {
-                if (richtext)
-                  {
-                    return "<font color=\"#2222cc\">[</font>"
-                    + getObjectParent()->getObjectDisplayName(regular, richtext)
-                    + "<font color=\"#2222cc\">]<sub>0</sub></font>";
-                  }
-                else
-                {return "[" + getObjectParent()->getObjectDisplayName(regular, richtext) + "]_0";}
-              }
+            else
+              {return "[" + getObjectParent()->getObjectDisplayName(regular, richtext) + "]";}
           }
 
-        return CCopasiObject::getObjectDisplayName(regular, richtext);
+        if (getObjectName() == "InitialConcentration")
+          {
+            if (richtext)
+              {
+                return "<font color=\"#2222cc\">[</font>"
+                       + getObjectParent()->getObjectDisplayName(regular, richtext)
+                       + "<font color=\"#2222cc\">]<sub>0</sub></font>";
+              }
+            else
+              {return "[" + getObjectParent()->getObjectDisplayName(regular, richtext) + "]_0";}
+          }
       }
-  };
+
+    return CCopasiObject::getObjectDisplayName(regular, richtext);
+  }
+};
 
 template<> inline
 CCopasiObjectReference<C_FLOAT64>::CCopasiObjectReference(const std::string & name,
@@ -152,114 +158,116 @@ CCopasiObjectReference<bool>::CCopasiObjectReference(const std::string & name,
 }
 
 template <class CType> class CCopasiVectorReference: public CCopasiObject
+{
+  typedef CType referenceType;
+
+  //Attributes
+private:
+  CType & mReference;
+
+  //Operations
+private:
+  CCopasiVectorReference() {};
+
+public:
+  CCopasiVectorReference(const std::string & name,
+                         const CCopasiContainer * pParent,
+                         referenceType & reference,
+                         const unsigned C_INT32 & flag = 0):
+      CCopasiObject(name, pParent, "Reference",
+                    CCopasiObject::Reference |
+                    CCopasiObject::NonUniqueName |
+                    flag),
+      mReference(reference)
+  {assert(pParent != NULL);}
+
+  CCopasiVectorReference(const CCopasiVectorReference< referenceType > & src,
+                         const CCopasiContainer * pParent):
+      CCopasiObject(src, pParent),
+      mReference(src.mReference)
+  {}
+
+  virtual ~CCopasiVectorReference() {}
+
+  virtual CCopasiObject * getObject(const CCopasiObjectName & cn)
   {
-    typedef CType referenceType;
-
-    //Attributes
-  private:
-    CType & mReference;
-
-    //Operations
-  private:
-    CCopasiVectorReference() {};
-
-  public:
-    CCopasiVectorReference(const std::string & name,
-                           const CCopasiContainer * pParent,
-                           referenceType & reference,
-                           const unsigned C_INT32 & flag = 0):
-        CCopasiObject(name, pParent, "Reference",
-                      CCopasiObject::Reference |
-                      CCopasiObject::NonUniqueName |
-                      flag),
-        mReference(reference)
-    {assert(pParent != NULL);}
-
-    CCopasiVectorReference(const CCopasiVectorReference< referenceType > & src):
-        CCopasiObject(src),
-        mReference(src.mReference)
-    {}
-
-    virtual ~CCopasiVectorReference() {}
-
-    virtual CCopasiObject * getObject(const CCopasiObjectName & cn)
-    {
 #ifdef WIN32
-      return new CCopasiObjectReference< referenceType::elementType >
+    return new CCopasiObjectReference< referenceType::elementType >
 #else
-      return new CCopasiObjectReference< typename referenceType::elementType >
+    return new CCopasiObjectReference< typename referenceType::elementType >
 #endif
-      (getObjectName() + cn,
-       getObjectParent(),
-       mReference[cn.getElementIndex()],
-       isValueBool() ? CCopasiObject::ValueBool :
-       isValueInt() ? CCopasiObject::ValueInt :
-       isValueDbl() ? CCopasiObject::ValueDbl : (CCopasiObject::Flag) 0
-);
-    }
+           (getObjectName() + cn,
+            getObjectParent(),
+            mReference[cn.getElementIndex()],
+            isValueBool() ? CCopasiObject::ValueBool :
+            isValueInt() ? CCopasiObject::ValueInt :
+            isValueDbl() ? CCopasiObject::ValueDbl : (CCopasiObject::Flag) 0
+           );
+  }
 
-    virtual void * getValuePointer() const {return &mReference;}
-    virtual const CCopasiObject * getValueObject() const {return this;}
+  virtual void * getValuePointer() const {return &mReference;}
+  virtual const CCopasiObject * getValueObject() const {return this;}
 
-    /**
-     * This is the output method for any object. The default implementation
-     * provided with CCopasiObject uses the ostream operator<< of the object
-     * to print the object.To overide this default behaviour one needs to
-     * reimplement the virtual print function.
-     * @param std::ostream * ostream
-     */
-    virtual void print(std::ostream * ostream) const
-      {*ostream << mReference;}
-  };
+  /**
+   * This is the output method for any object. The default implementation
+   * provided with CCopasiObject uses the ostream operator<< of the object
+   * to print the object.To overide this default behaviour one needs to
+   * reimplement the virtual print function.
+   * @param std::ostream * ostream
+   */
+  virtual void print(std::ostream * ostream) const
+  {*ostream << mReference;}
+};
 
 template <class CType> class CCopasiMatrixReference: public CCopasiObject
+{
+  typedef CType referenceType;
+
+  //Attributes
+private:
+  CType & mReference;
+
+  //Operations
+private:
+  CCopasiMatrixReference() {};
+
+public:
+  CCopasiMatrixReference(const std::string & name,
+                         const CCopasiContainer * pParent,
+                         referenceType & reference,
+                         const unsigned C_INT32 & flag = 0):
+      CCopasiObject(name, pParent, "Reference",
+                    CCopasiObject::Reference |
+                    CCopasiObject::NonUniqueName |
+                    flag),
+      mReference(reference)
+  {assert(pParent != NULL);}
+
+  CCopasiMatrixReference(const CCopasiMatrixReference< referenceType > & src,
+                         const CCopasiContainer * pParent):
+      CCopasiObject(src, pParent),
+      mReference(src.mReference)
+  {}
+
+  virtual ~CCopasiMatrixReference() {}
+
+  virtual CCopasiObject * getObject(const CCopasiObjectName & cn)
   {
-    typedef CType referenceType;
-
-    //Attributes
-  private:
-    CType & mReference;
-
-    //Operations
-  private:
-    CCopasiMatrixReference() {};
-
-  public:
-    CCopasiMatrixReference(const std::string & name,
-                           const CCopasiContainer * pParent,
-                           referenceType & reference,
-                           const unsigned C_INT32 & flag = 0):
-        CCopasiObject(name, pParent, "Reference",
-                      CCopasiObject::Reference |
-                      CCopasiObject::NonUniqueName |
-                      flag),
-        mReference(reference)
-    {assert(pParent != NULL);}
-
-    CCopasiMatrixReference(const CCopasiMatrixReference< referenceType > & src):
-        CCopasiObject(src),
-        mReference(src.mReference)
-    {}
-
-    virtual ~CCopasiMatrixReference() {}
-
-    virtual CCopasiObject * getObject(const CCopasiObjectName & cn)
-    {
 #ifdef WIN32
-      return new CCopasiObjectReference< referenceType::elementType >
+    return new CCopasiObjectReference< referenceType::elementType >
 #else
-      return new CCopasiObjectReference< typename referenceType::elementType >
+    return new CCopasiObjectReference< typename referenceType::elementType >
 #endif
-      (getObjectName() + cn,
-       getObjectParent(),
-       mReference(cn.getElementIndex(),
-                  cn.getElementIndex(1)),
-       isValueBool() ? CCopasiObject::ValueBool :
-       isValueInt() ? CCopasiObject::ValueInt :
-       isValueDbl() ? CCopasiObject::ValueDbl : (CCopasiObject::Flag) 0);
-    }
+           (getObjectName() + cn,
+            getObjectParent(),
+            mReference(cn.getElementIndex(),
+                       cn.getElementIndex(1)),
+            isValueBool() ? CCopasiObject::ValueBool :
+            isValueInt() ? CCopasiObject::ValueInt :
+            isValueDbl() ? CCopasiObject::ValueDbl : (CCopasiObject::Flag) 0);
+  }
 
-    virtual void * getValuePointer() const {return &mReference;}
-    virtual const CCopasiObject * getValueObject() const {return this;}
-  };
+  virtual void * getValuePointer() const {return &mReference;}
+  virtual const CCopasiObject * getValueObject() const {return this;}
+};
 #endif // COPASI_CCopasiObjectReference
