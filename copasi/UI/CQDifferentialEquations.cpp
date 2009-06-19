2d534cdcc1834521c9b4539d3004781bd34df8ea
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2009-06-19 08:27:46 +0000

    Follow the native structure of Qt4.

--- copasi/UI/CQDifferentialEquations.cpp
+++ copasi/UI/CQDifferentialEquations.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQDifferentialEquations.cpp,v $
-//   $Revision: 1.4 $
+//   $Revision: 1.5 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2008/12/18 19:56:21 $
+//   $Author: pwilly $
+//   $Date: 2009/06/19 08:27:46 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -14,8 +14,35 @@
 #include "CQDifferentialEquations.h"
 
 #include <qvariant.h>
+/*
 #include <q3scrollview.h>
 #include "CQDifferentialEquations.ui.h"
+ */
+#include "CQDifferentialEquations.h"
+
+#include <sstream>
+#include <set>
+#include <vector>
+
+#include <qmessagebox.h>
+#include <qregexp.h>
+//#include "Qt3Support/Q3ScrollView"
+
+#ifdef HAVE_MML
+# include "mml/qtmmlwidget.h"
+#endif // HAVE_MML
+
+#include "model/CMMLOutput.h"
+#include "CopasiDataModel/CCopasiDataModel.h"
+#include "qtUtilities.h"
+#include "utilities/utility.h"
+#include "tex/CMathMLToTeX.h"
+#include "report/CCopasiRootContainer.h"
+
+#include "CopasiFileDialog.h"
+
+#include <QtDebug>
+
 /*
  *  Constructs a CQDifferentialEquations which is a child of 'parent', with the
  *  name 'name'.'
@@ -43,4 +70,141 @@ CQDifferentialEquations::~CQDifferentialEquations()
 void CQDifferentialEquations::languageChange()
 {
   retranslateUi(this);
+}
+
+void CQDifferentialEquations::init()
+{
+  mpScrollView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  mpScrollView->setResizePolicy(Q3ScrollView::AutoOneFit);
+
+  mpMMLWidget = new QtMmlWidget(mpScrollView->viewport());
+  mpMMLWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  mpMMLWidget->setBaseFontPointSize(this->fontInfo().pointSize());
+  mpMMLWidget->setFontName(QtMmlWidget::NormalFont, this->fontInfo().family());
+  mpMMLWidget->show();
+
+  mpScrollView->addChild(mpMMLWidget);
+
+  comboBoxFunctions->setCurrentItem(1);
+}
+
+void CQDifferentialEquations::saveMML(const QString outfilename)
+{
+  std::ofstream ofile;
+  ofile.open(utf8ToLocale(TO_UTF8(outfilename)).c_str(), std::ios::trunc);
+
+  ofile << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << std::endl;
+  ofile << "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN\" \"HTMLFiles/xhtml-math11-f.dtd\">" << std::endl;
+  ofile << "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">" << std::endl;
+
+  ofile << mml.str();
+
+  ofile << "</math>" << std::endl;
+
+  ofile.close();
+}
+
+void CQDifferentialEquations::slotUpdateWidget()
+{
+  bool hasContents = true;
+  //std::ostringstream mml;
+  mml.str("");
+
+  bool expand, expandAll;
+
+  switch (comboBoxFunctions->currentItem())
+    {
+      case 0:
+        expand = false;
+        expandAll = false;
+        break;
+
+      case 1:
+        expand = true;
+        expandAll = false;
+        break;
+
+      default:
+        expand = true;
+        expandAll = true;
+        break;
+    };
+
+  bool parameterAsNumbers = false;
+
+  if (comboBoxParameters->currentItem() == 0)
+    parameterAsNumbers = true;
+
+  assert(CCopasiRootContainer::getDatamodelList()->size() > 0);
+  CMMLOutput::writeDifferentialEquations(mml, (*CCopasiRootContainer::getDatamodelList())[0]->getModel(), parameterAsNumbers, expand, expandAll);
+
+  QWidget* tmp = dynamic_cast<QWidget*>(parent());
+
+  if (tmp) tmp->setCursor(Qt::WaitCursor);
+
+  mpMMLWidget->setContent(FROM_UTF8(mml.str()));
+  mpScrollView->resizeContents(mpMMLWidget->sizeHint().width(), mpMMLWidget->sizeHint().height());
+
+  if (tmp) tmp->unsetCursor();
+
+  mpSaveButton->setEnabled(hasContents);
+}
+
+bool CQDifferentialEquations::enter(const std::string &)
+{
+  slotUpdateWidget();
+  return true;
+}
+
+void CQDifferentialEquations::newFunction()
+{}
+
+void CQDifferentialEquations::saveTeX(const QString outfilename)
+{
+  QString latexStr(FROM_UTF8(mml.str()));
+
+//  std::cout << qPrintable
+  qDebug() << latexStr;
+
+  CMathMLToTeX::convert(latexStr);
+
+  std::ofstream ofile;
+  ofile.open(utf8ToLocale(TO_UTF8(outfilename)).c_str(), std::ios::trunc);
+  ofile << TO_UTF8(latexStr);
+  ofile.close();
+}
+
+void CQDifferentialEquations::slotSave()
+{
+  QString sSelectedFilter;
+  QString outfilename = "";
+
+  C_INT32 Answer = QMessageBox::No;
+
+  while (Answer == QMessageBox::No)
+    {
+      outfilename =
+        CopasiFileDialog::getSaveFileNameAndFilter(sSelectedFilter,
+            this,
+            "Save File Dialog",
+            QString::null,
+//            "MathML (*.mml);;XML (*.xml);;TeX (*.tex);;",
+            "MathML (*.mml);;TeX (*.tex);;",
+            "Save Formula to Disk");
+
+      if (outfilename.isNull()) return;
+
+      // Checks whether the file exists
+      Answer = checkSelection(outfilename);
+
+      if (Answer == QMessageBox::Cancel)
+        return;
+    }
+
+  std::cout << "C " << qPrintable(sSelectedFilter) << std::endl;
+
+  if (sSelectedFilter.contains("tex"))
+    saveTeX(outfilename);
+  else
+    saveMML(outfilename);
 }
