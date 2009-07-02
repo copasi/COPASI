edbab4dc939ee3358fde75d1068a4b8ff3ffcee4
Ralph Gauges <gauges@hs-albsig.de>
2009-07-02 08:18:29 +0000

    Forgot to commit the header file for the test code.

--- copasi/compareExpressions/stresstest/stress_test.h
+++ copasi/compareExpressions/stresstest/stress_test.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/compareExpressions/stresstest/stress_test.h,v $
-//   $Revision: 1.5 $
+//   $Revision: 1.6 $
 //   $Name:  $
 //   $Author: gauges $
-//   $Date: 2009/02/18 20:53:06 $
+//   $Date: 2009/07/02 08:18:29 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -17,6 +17,8 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <sys/time.h>
 
 class CNormalFraction;
 class Model;
@@ -28,84 +30,89 @@ class CCopasiDataModel;
 // and compare the expressions from the model to those in the function database
 // in order to find out if they are the same.
 class stress_test
-  {
-  public:
-    /**
-     * Constructor.
-     */
-    stress_test();
+{
+public:
+  /**
+   * Constructor.
+   */
+  stress_test();
 
-    /**
-     * Destructor.
-     */
-    virtual ~stress_test();
+  /**
+   * Destructor.
+   */
+  virtual ~stress_test();
 
-    /**
-     * Goes through the list of files, loads each file and normalizes the
-     * mathematical expressions in the contained model.
-     */
-    void run(const std::vector<std::string>& filenames);
+  /**
+   * Goes through the list of files, loads each file and normalizes the
+   * mathematical expressions in the contained model.
+   */
+  void run(const std::vector<std::string>& filenames);
 
-  protected:
-    /**
-     * Loads an sbml model from the given file and
-     * normalized all mathematical expressions in the SBML model.
-     */
-    void normalizeMath(const std::string& filename);
+protected:
+  /**
+   * Loads an sbml model from the given file and
+   * normalized all mathematical expressions in the SBML model.
+   */
+  void normalizeMath(const std::string& filename);
 
-    /**
-     * Normalizes all expressions but the function definitions in the given
-     * model.
-     */
-    void normalizeExpressions(const Model* pModel);
+  /**
+   * Normalizes all expressions but the function definitions in the given
+   * model.
+  void normalizeExpressions(const Model* pModel);
+   */
 
-    /**
-     * Normalizes the function definitions in the given model.
-     */
-    void normalizeFunctionDefinitions(const Model* pModel);
+  /**
+   * Normalizes the function definitions in the given model.
+  void normalizeFunctionDefinitions(const Model* pModel);
+   */
 
-    /**
-     * Normalizes all expressions but the function definitions in the given
-     * model.
-     */
-    void normalizeAndSimplifyExpressions(const Model* pModel);
+  /**
+   * Normalizes all expressions but the function definitions in the given
+   * model.
+   */
+  void normalizeAndSimplifyExpressions(const Model* pModel);
 
-    /**
-     * Normalizes the function definitions in the given model.
-     */
-    void normalizeAndSimplifyFunctionDefinitions(const Model* pModel);
+  /**
+   * Normalizes the function definitions in the given model.
+   */
+  void normalizeAndSimplifyFunctionDefinitions(const Model* pModel);
 
-    /**
-     * Normalizes COPASIs function database.
-     */
-    void normalizeFunctionDB();
+  /**
+   * Normalizes COPASIs function database.
+   */
+  void normalizeFunctionDB();
 
-  protected:
-    unsigned int mNumFunctionDefinitions;
-    unsigned int mNumExceededFunctions;
-    unsigned int mNumFailedFunctions;
-    unsigned int mNumExpressions;
-    unsigned int mNumExceeded;
-    unsigned int mNumFailed;
-    unsigned int mNumCOPASIFunctions;
-    unsigned int mNumExceededCOPASIFunctions;
-    unsigned int mNumFailedCOPASIFunctions;
-    unsigned int mNumFiles;
-    unsigned int mNumKineticFunctions;
-    unsigned int mNumMassActionsKinetics;
-    unsigned int mNumConstantFluxKinetics;
-    unsigned int mNumMappedKineticExpressions;
-    unsigned int mNumUnmappedKineticExpressions;
-    std::vector<CNormalFraction*> mNormalizedExpressions;
-    std::vector<std::pair<std::string, CNormalFraction*> > mNormalizedFunctionDefinitions;
-    std::multimap<std::string, CNormalFraction*> mNormalizedCOPASIFunctionDefinitions;
-    std::vector<std::string> mUnreadableFiles;
-    std::map<std::string, unsigned int> mExpressionMappings;
-    std::map<int, std::vector<CNormalFraction*> > mSBOMap;
-    std::map<CNormalFraction*, int> mUnknownCategories;
-    unsigned int mDifferentNormalform;
-    unsigned int mNumSBO;
-    CCopasiDataModel* mpDataModel;
-  };
+protected:
+  unsigned int mNumFunctionDefinitions;
+  unsigned int mNumExceededFunctions;
+  unsigned int mNumFailedFunctions;
+  unsigned int mNumExpressions;
+  unsigned int mNumExceeded;
+  unsigned int mNumFailed;
+  unsigned int mNumCOPASIFunctions;
+  unsigned int mNumExceededCOPASIFunctions;
+  unsigned int mNumFailedCOPASIFunctions;
+  unsigned int mNumFiles;
+  unsigned int mNumKineticFunctions;
+  unsigned int mNumMassActionsKinetics;
+  unsigned int mNumConstantFluxKinetics;
+  unsigned int mNumMappedKineticExpressions;
+  unsigned int mNumUnmappedKineticExpressions;
+  std::vector<CNormalFraction*> mNormalizedExpressions;
+  std::vector<std::pair<std::string, CNormalFraction*> > mNormalizedFunctionDefinitions;
+  std::multimap<std::string, CNormalFraction*> mNormalizedCOPASIFunctionDefinitions;
+  std::vector<std::string> mUnreadableFiles;
+  std::map<std::string, unsigned int> mExpressionMappings;
+  std::map<int, std::vector<CNormalFraction*> > mSBOMap;
+  std::map<CNormalFraction*, int> mUnknownCategories;
+  unsigned int mDifferentNormalform;
+  unsigned int mNumSBO;
+  CCopasiDataModel* mpDataModel;
+  struct timeval mTV1;
+  struct timezone mTZ1;
+  struct timeval mTV2;
+  struct timezone mTZ2;
+  std::set<double> mProcessTimes;
+};
 
 #endif // STRESS_TEST_H__
