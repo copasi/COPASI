b5587c1f77dd21b65a15288b2ffbee9d79620e47
Ralph Gauges <gauges@hs-albsig.de>
2009-07-28 07:33:35 +0000

    Converted the sliders themselves to Qt4.

--- copasi/UI/CopasiSlider.cpp
+++ copasi/UI/CopasiSlider.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CopasiSlider.cpp,v $
-//   $Revision: 1.34 $
+//   $Revision: 1.35 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2008/12/18 19:54:59 $
+//   $Author: gauges $
+//   $Date: 2009/07/28 07:33:35 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -17,15 +17,14 @@
 
 #include <math.h>
 
-#include "qlabel.h"
-#include "qstring.h"
-#include "qslider.h"
-#include "qpixmap.h"
-#include "qtoolbutton.h"
-#include "q3vbox.h"
-#include "qtooltip.h"
-//Added by qt3to4:
-#include <Q3Frame>
+#include <QLabel>
+#include <QString>
+#include <QSlider>
+#include <QPixmap>
+#include <QToolButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QToolTip>
 
 #include "copasi.h"
 
@@ -35,30 +34,44 @@
 #include "icons/closeSlider.xpm"
 #include "icons/editSlider.xpm"
 
-CopasiSlider::CopasiSlider(CSlider* pSlider, QWidget* parent): Q3HBox(parent), mpCSlider(pSlider) , mpQSlider(NULL), mpLabel(NULL), mpCloseButton(NULL), mpEditButton(NULL), mValueOutOfRange(false)
+CopasiSlider::CopasiSlider(CSlider* pSlider, QWidget* parent): QFrame(parent), mpCSlider(pSlider) , mpQSlider(NULL), mpLabel(NULL), mpCloseButton(NULL), mpEditButton(NULL), mValueOutOfRange(false)
 {
-  this->setFrameShape(Q3Frame::Box);
-  this->setSpacing(2);
-  this->setMargin(2);
-  Q3VBox* sliderLayout = new Q3VBox(this);
-  this->mpLabel = new QLabel(sliderLayout);
+  this->setLayout(new QHBoxLayout(NULL));
+  this->setFrameShape(QFrame::Box);
+  this->layout()->setContentsMargins(2, 2, 2, 2);
+  //this->layout()->setSpacing(0);
+  //this->layout()->setMargin(0);
+  QFrame* pFrame = new QFrame(NULL);
+  pFrame->setLayout(new QVBoxLayout(NULL));
+  pFrame->layout()->setContentsMargins(2, 2, 2, 2);
+  this->mpLabel = new QLabel(NULL);
   this->mpLabel->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
-  this->mpQSlider = new QSlider(Qt::Horizontal, sliderLayout);
+  pFrame->layout()->addWidget(this->mpLabel);
+  this->mpQSlider = new QSlider(Qt::Horizontal);
   this->mpQSlider->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
+  pFrame->layout()->addWidget(this->mpQSlider);
+  this->layout()->addWidget(pFrame);
 
-  Q3VBox* buttonLayout = new Q3VBox(this);
-  buttonLayout->setSpacing(0);
+  pFrame = new QFrame(NULL);
+  pFrame->setLayout(new QVBoxLayout(NULL));
+  pFrame->layout()->setContentsMargins(2, 2, 2, 2);
+  //pFrame->layout()->setSpacing(0);
   QPixmap icons[2] = {closeSlider, editSlider};
-  this->mpCloseButton = new QToolButton(buttonLayout);
+  this->mpCloseButton = new QToolButton(NULL);
   this->mpCloseButton->setPixmap(icons[0]);
   this->mpCloseButton->setFixedSize(13, 13);
-  this->mpEditButton = new QToolButton(buttonLayout);
+  pFrame->layout()->addWidget(this->mpCloseButton);
+  this->mpEditButton = new QToolButton(NULL);
   this->mpEditButton->setPixmap(icons[1]);
   this->mpEditButton->setFixedSize(13, 13);
+  pFrame->layout()->addWidget(this->mpEditButton);
+  this->layout()->addWidget(pFrame);
+
   if (!this->mpCSlider->compile())
     {
       this->mpQSlider->setEnabled(false);
     }
+
   this->updateSliderData();
   QToolTip::add(this->mpCloseButton, tr("remove slider"));
   QToolTip::add(this->mpEditButton, tr("edit slider"));
@@ -89,6 +102,7 @@ void CopasiSlider::updateSliderData()
       this->mpQSlider->setValue(this->calculatePositionFromValue(this->mpCSlider->getSliderValue()));
       connect(this->mpQSlider, SIGNAL(valueChanged(int)), this, SLOT(sliderValueChanged(int)));
       this->updateLabel();
+
       if (this->mpQSlider->isEnabled() == false)
         {
           if (this->mpCSlider->getSliderObject() != NULL)
@@ -100,9 +114,9 @@ void CopasiSlider::updateSliderData()
 }
 
 C_FLOAT64 CopasiSlider::value() const
-  {
-    return this->mpCSlider->getSliderValue();
-  }
+{
+  return this->mpCSlider->getSliderValue();
+}
 
 void CopasiSlider::setValue(C_FLOAT64 value)
 {
@@ -118,9 +132,9 @@ void CopasiSlider::setValue(C_FLOAT64 value)
 }
 
 unsigned C_INT32 CopasiSlider::minorMajorFactor() const
-  {
-    return this->mpCSlider->getTickFactor();
-  }
+{
+  return this->mpCSlider->getTickFactor();
+}
 
 void CopasiSlider::setMinorMajorFactor(unsigned C_INT32 factor)
 {
@@ -129,9 +143,9 @@ void CopasiSlider::setMinorMajorFactor(unsigned C_INT32 factor)
 }
 
 C_FLOAT64 CopasiSlider::minorTickInterval() const
-  {
-    return (C_FLOAT64)(this->mpCSlider->getMaxValue() - this->mpCSlider->getMinValue()) / ((C_FLOAT64)this->mpCSlider->getTickNumber());
-  }
+{
+  return (C_FLOAT64)(this->mpCSlider->getMaxValue() - this->mpCSlider->getMinValue()) / ((C_FLOAT64)this->mpCSlider->getTickNumber());
+}
 
 void CopasiSlider::setNumMinorTicks(unsigned C_INT32 numMinorTicks)
 {
@@ -142,24 +156,24 @@ void CopasiSlider::setNumMinorTicks(unsigned C_INT32 numMinorTicks)
 }
 
 unsigned C_INT32 CopasiSlider::numMinorTicks() const
-  {
-    return this->mpCSlider->getTickNumber();
-  }
+{
+  return this->mpCSlider->getTickNumber();
+}
 
 C_FLOAT64 CopasiSlider::minValue() const
-  {
-    return this->mpCSlider->getMinValue();
-  }
+{
+  return this->mpCSlider->getMinValue();
+}
 
 C_FLOAT64 CopasiSlider::maxValue() const
-  {
-    return this->mpCSlider->getMaxValue();
-  }
+{
+  return this->mpCSlider->getMaxValue();
+}
 
 CCopasiObject* CopasiSlider::object() const
-  {
-    return this->mpCSlider->getSliderObject();
-  }
+{
+  return this->mpCSlider->getSliderObject();
+}
 
 void CopasiSlider::setObject(CCopasiObject* object)
 {
@@ -193,6 +207,7 @@ void CopasiSlider::updateLabel()
   currValue = this->mpCSlider->getSliderValue();
   CCopasiObject* object = this->mpCSlider->getSliderObject();
   QString labelString = "";
+
   if (object)
     {
       labelString += FROM_UTF8(object->getObjectDisplayName(true, true));
@@ -203,6 +218,7 @@ void CopasiSlider::updateLabel()
       labelString += "] {";
       labelString += QString::number(currValue);
       labelString += "}";
+
       if (this->mValueOutOfRange)
         {
           labelString += " (Value out of range!)";
@@ -212,6 +228,7 @@ void CopasiSlider::updateLabel()
     {
       labelString += "Object not available!";
     }
+
   this->mpLabel->setText(labelString);
 }
 
@@ -236,9 +253,9 @@ void CopasiSlider::qSliderPressed()
 }
 
 CSlider::Type CopasiSlider::type() const
-  {
-    return this->mpCSlider->getSliderType();
-  }
+{
+  return this->mpCSlider->getSliderType();
+}
 
 void CopasiSlider::setType(CSlider::Type type)
 {
@@ -278,44 +295,48 @@ void CopasiSlider::editButtonClicked()
 }
 
 CSlider* CopasiSlider::getCSlider() const
-  {
-    return this->mpCSlider;
-  }
+{
+  return this->mpCSlider;
+}
 
 C_FLOAT64 CopasiSlider::calculateValueFromPosition(int position)
 {
   double value;
   double exponent;
+
   switch (this->mpCSlider->getScaling())
     {
-    case CSlider::linear:
-      value = this->mpCSlider->getMinValue() + position * this->minorTickInterval();
-      break;
-    case CSlider::logarithmic:
-      exponent = (((double)position) * log10(this->mpCSlider->getMaxValue() / this->mpCSlider->getMinValue())) / this->mpCSlider->getTickNumber();
-      value = this->mpCSlider->getMinValue() * pow(10.0, exponent);
-      break;
-    default:
-      value = 0.0;
-      break;
+      case CSlider::linear:
+        value = this->mpCSlider->getMinValue() + position * this->minorTickInterval();
+        break;
+      case CSlider::logarithmic:
+        exponent = (((double)position) * log10(this->mpCSlider->getMaxValue() / this->mpCSlider->getMinValue())) / this->mpCSlider->getTickNumber();
+        value = this->mpCSlider->getMinValue() * pow(10.0, exponent);
+        break;
+      default:
+        value = 0.0;
+        break;
     }
+
   return value;
 }
 
 int CopasiSlider::calculatePositionFromValue(C_FLOAT64 value)
 {
   int position;
+
   switch (this->mpCSlider->getScaling())
     {
-    case CSlider::linear:
-      position = (int)floor(((value - this->mpCSlider->getMinValue()) / this->minorTickInterval()) + 0.5);
-      break;
-    case CSlider::logarithmic:
-      position = (int)floor((this->mpCSlider->getTickNumber() * (log10(value / this->mpCSlider->getMinValue()) / log10(this->mpCSlider->getMaxValue() / this->mpCSlider->getMinValue()))) + 0.5);
-      break;
-    default:
-      position = 0;
+      case CSlider::linear:
+        position = (int)floor(((value - this->mpCSlider->getMinValue()) / this->minorTickInterval()) + 0.5);
+        break;
+      case CSlider::logarithmic:
+        position = (int)floor((this->mpCSlider->getTickNumber() * (log10(value / this->mpCSlider->getMinValue()) / log10(this->mpCSlider->getMaxValue() / this->mpCSlider->getMinValue()))) + 0.5);
+        break;
+      default:
+        position = 0;
     }
+
   return position;
 }
 
@@ -325,9 +346,9 @@ void CopasiSlider::resetValue()
 }
 
 C_FLOAT64 CopasiSlider::originalValue() const
-  {
-    return this->mpCSlider->getOriginalValue();
-  }
+{
+  return this->mpCSlider->getOriginalValue();
+}
 
 void CopasiSlider::setOriginalValue(C_FLOAT64 value)
 {
--- copasi/UI/CopasiSlider.h
+++ copasi/UI/CopasiSlider.h
@@ -1,9 +1,9 @@
 /* Begin CVS Header
  $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CopasiSlider.h,v $
- $Revision: 1.16 $
+ $Revision: 1.17 $
  $Name:  $
- $Author: shoops $
- $Date: 2008/12/18 19:54:59 $
+ $Author: gauges $
+ $Date: 2009/07/28 07:33:35 $
  End CVS Header */
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -18,8 +18,7 @@
 #ifndef CopasiSlider_H__
 #define CopasiSlider_H__
 
-#include <q3hbox.h>
-//Added by qt3to4:
+#include <QFrame>
 #include <QLabel>
 
 #include "copasi.h"
@@ -34,61 +33,61 @@ class QLabel;
 class CCopasiParameterGroup;
 class QToolButton;
 
-class CopasiSlider: public Q3HBox
-  {
-    Q_OBJECT
-  public:
-    CopasiSlider(CSlider* pSlider, QWidget* parent = 0);
-    virtual ~CopasiSlider();
+class CopasiSlider: public QFrame
+{
+  Q_OBJECT
+public:
+  CopasiSlider(CSlider* pSlider, QWidget* parent = 0);
+  virtual ~CopasiSlider();
 
-    CSlider::Type type() const;
-    void setType(CSlider::Type type);
+  CSlider::Type type() const;
+  void setType(CSlider::Type type);
 
-    C_FLOAT64 value() const;
-    void setValue(C_FLOAT64 value);
-    unsigned C_INT32 minorMajorFactor() const;
-    void setMinorMajorFactor(unsigned C_INT32 factor);
-    C_FLOAT64 minorTickInterval() const;
-    unsigned C_INT32 numMinorTicks() const;
-    void setNumMinorTicks(unsigned C_INT32 numMinorTicks);
-    void setMinValue(C_FLOAT64 value);
-    void setMaxValue(C_FLOAT64 value);
-    void setOriginalValue(C_FLOAT64 value);
-    C_FLOAT64 minValue() const;
-    C_FLOAT64 maxValue() const;
-    C_FLOAT64 originalValue() const;
-    void updateValue(bool modifyRange);
-    CCopasiObject* object() const;
-    void setObject(CCopasiObject* object);
-    CSlider* getCSlider() const;
-    void updateLabel();
-    void updateSliderData();
-    void resetValue();
+  C_FLOAT64 value() const;
+  void setValue(C_FLOAT64 value);
+  unsigned C_INT32 minorMajorFactor() const;
+  void setMinorMajorFactor(unsigned C_INT32 factor);
+  C_FLOAT64 minorTickInterval() const;
+  unsigned C_INT32 numMinorTicks() const;
+  void setNumMinorTicks(unsigned C_INT32 numMinorTicks);
+  void setMinValue(C_FLOAT64 value);
+  void setMaxValue(C_FLOAT64 value);
+  void setOriginalValue(C_FLOAT64 value);
+  C_FLOAT64 minValue() const;
+  C_FLOAT64 maxValue() const;
+  C_FLOAT64 originalValue() const;
+  void updateValue(bool modifyRange);
+  CCopasiObject* object() const;
+  void setObject(CCopasiObject* object);
+  CSlider* getCSlider() const;
+  void updateLabel();
+  void updateSliderData();
+  void resetValue();
 
-  public slots:
-    void sliderValueChanged(int value);
-    void qSliderReleased();
-    void qSliderPressed();
-    void closeButtonClicked();
-    void editButtonClicked();
+public slots:
+  void sliderValueChanged(int value);
+  void qSliderReleased();
+  void qSliderPressed();
+  void closeButtonClicked();
+  void editButtonClicked();
 
-  signals:
-    void valueChanged(double);
-    void sliderReleased();
-    void sliderPressed();
-    void closeClicked(CopasiSlider* slider);
-    void editClicked(CopasiSlider* slider);
+signals:
+  void valueChanged(double);
+  void sliderReleased();
+  void sliderPressed();
+  void closeClicked(CopasiSlider* slider);
+  void editClicked(CopasiSlider* slider);
 
-  protected:
-    CSlider* mpCSlider;
-    QSlider* mpQSlider;
-    QLabel* mpLabel;
-    QToolButton* mpCloseButton;
-    QToolButton* mpEditButton;
-    bool mValueOutOfRange;
+protected:
+  CSlider* mpCSlider;
+  QSlider* mpQSlider;
+  QLabel* mpLabel;
+  QToolButton* mpCloseButton;
+  QToolButton* mpEditButton;
+  bool mValueOutOfRange;
 
-    int calculatePositionFromValue(C_FLOAT64 value);
-    C_FLOAT64 calculateValueFromPosition(int position);
-  };
+  int calculatePositionFromValue(C_FLOAT64 value);
+  C_FLOAT64 calculateValueFromPosition(int position);
+};
 
 #endif // CopasiSlider_H__
