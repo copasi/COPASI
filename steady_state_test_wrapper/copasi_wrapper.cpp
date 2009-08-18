370b76543c8ade3f1162ea1bfcf5242989a09257
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2009-08-18 12:31:12 +0000

    first steps...

--- steady_state_test_wrapper/copasi_wrapper.cpp
+++ steady_state_test_wrapper/copasi_wrapper.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/steady_state_test_wrapper/copasi_wrapper.cpp,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
 //   $Author: ssahle $
-//   $Date: 2009/08/17 13:28:40 $
+//   $Date: 2009/08/18 12:31:12 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -34,22 +34,24 @@
 #include "copasi/trajectory/CTrajectoryTask.h"
 #include "copasi/trajectory/CTrajectoryMethod.h"
 #include "copasi/trajectory/CTrajectoryProblem.h"
+
+#include "copasi/steadystate/CSteadyStateTask.h"
+#include "copasi/steadystate/CSteadyStateMethod.h"
+#include "copasi/steadystate/CSteadyStateProblem.h"
+
 #include "copasi/report/CReportDefinitionVector.h"
 #include "copasi/report/CReportDefinition.h"
 
 int main(int argc, char *argv[])
 {
   // Parse the commandline options
-  // first argument is the SBML filename
-  // second argument is the endtime
-  // third argument is the step number
-  // fourth argument is the filename where the results are to be written
-  // fifth argument is the tmp directory (this is not needed)
-  // the rest of the arguments are species names for the result
+  // 1. argument is the SBML filename
+  // 2. argument is the filename where the results are to be written
+  // (the rest of the arguments are species names for the result)
   try
     {
       // Create the root container.
-      CCopasiRootContainer::init(false, 0, NULL);
+      CCopasiRootContainer::init(0, NULL, false);
     }
 
   catch (copasi::autoexcept &e)
@@ -58,41 +60,27 @@ int main(int argc, char *argv[])
   catch (copasi::option_error &e)
     {}
 
-  if (argc < 5)
+  if (argc < 3)
     {
-      std::cout << "Usage: batch_wrapper SBMLFILENAME STARTTIME ENDTIME STEPNUMBER OUTFILENAME" << std::endl;
+      std::cout << "Usage: batch_wrapper SBMLFILENAME OUTFILENAME" << std::endl;
       exit(1);
     }
 
   char* pSBMLFilename = argv[1];
-  const char* pStartTime = argv[2];
-  const char* pEndTime = argv[3];
-  const char* pStepNumber = argv[4];
-  const char* pOutputFilename = argv[5];
-  CTrajectoryTask* pTrajectoryTask = NULL;
+  const char* pOutputFilename = argv[2];
+  //CTrajectoryTask* pTrajectoryTask = NULL;
+  CSteadyStateTask* pSSTask = NULL;
 
   std::string CWD = COptions::getPWD();
-  double startTime = strToDouble(pStartTime, &pStartTime);
-  double endTime = strToDouble(pEndTime, &pEndTime);
-  double stepNumber = strToDouble(pStepNumber, &pStepNumber);
+//  double startTime = strToDouble(pStartTime, &pStartTime);
+//  double endTime = strToDouble(pEndTime, &pEndTime);
+//  double stepNumber = strToDouble(pStepNumber, &pStepNumber);
 
-  if (startTime < 0.0)
-    {
-      std::cerr << "Invalid endtime " << pEndTime << std::endl;
-      exit(1);
-    }
-
-  if (endTime <= 0.0)
-    {
-      std::cerr << "Invalid endtime " << pEndTime << std::endl;
-      exit(1);
-    }
-
-  if (stepNumber <= 0.0)
-    {
-      std::cerr << "Invalid step number " << pStepNumber << std::endl;
-      exit(1);
-    }
+  /*  if (startTime < 0.0)
+      {
+        std::cerr << "Invalid endtime " << pEndTime << std::endl;
+        exit(1);
+      }*/
 
   try
     {
@@ -106,16 +94,20 @@ int main(int argc, char *argv[])
       // time.
       CReportDefinitionVector* pReports = pDataModel->getReportDefinitionList();
       CReportDefinition* pReport = pReports->createReportDefinition("Report", "Output for batch run");
-      pReport->setTaskType(CCopasiTask::timeCourse);
+      pReport->setTaskType(CCopasiTask::steadyState);
       pReport->setIsTable(false);
       pReport->setSeparator(CCopasiReportSeparator(", "));
 
       std::vector<CRegisteredObjectName>* pHeader = pReport->getHeaderAddr();
       std::vector<CRegisteredObjectName>* pBody = pReport->getBodyAddr();
-      pBody->push_back(CCopasiObjectName(pDataModel->getModel()->getCN() + ",Reference=Time"));
-      pBody->push_back(CRegisteredObjectName(pReport->getSeparator().getCN()));
-      pHeader->push_back(CCopasiStaticString("time").getCN());
-      pHeader->push_back(pReport->getSeparator().getCN());
+      std::vector<CRegisteredObjectName>* pFoot = pReport->getFooterAddr();
+
+      //Time is not needed for steady state
+      //pBody->push_back(CCopasiObjectName(pDataModel->getModel()->getCN() + ",Reference=Time"));
+      //pBody->push_back(CRegisteredObjectName(pReport->getSeparator().getCN()));
+      //pHeader->push_back(CCopasiStaticString("time").getCN());
+      //pHeader->push_back(pReport->getSeparator().getCN());
+
       const CCopasiVectorNS<CCompartment>& compartments = pDataModel->getModel()->getCompartments();
       unsigned int j, jMax = compartments.size();
       /*
@@ -130,6 +122,7 @@ int main(int argc, char *argv[])
         }
       }
       */
+
       const CCopasiVector<CMetab>& metabolites = pDataModel->getModel()->getMetabolites();
       jMax = metabolites.size();
 
@@ -137,8 +130,8 @@ int main(int argc, char *argv[])
         {
           if (metabolites[j]->getStatus() != CModelEntity::FIXED)
             {
-              pBody->push_back(metabolites[j]->getObject(CCopasiObjectName("Reference=Concentration"))->getCN());
-              pBody->push_back(pReport->getSeparator().getCN());
+              pFoot->push_back(metabolites[j]->getObject(CCopasiObjectName("Reference=Concentration"))->getCN());
+              pFoot->push_back(pReport->getSeparator().getCN());
               pHeader->push_back(CCopasiStaticString(metabolites[j]->getSBMLId()).getCN());
               pHeader->push_back(pReport->getSeparator().getCN());
             }
@@ -169,9 +162,10 @@ int main(int argc, char *argv[])
       */
 
       // delete the last separator
-      if ((*pBody->rbegin()) == pReport->getSeparator().getCN())
+
+      if ((*pFoot->rbegin()) == pReport->getSeparator().getCN())
         {
-          pBody->erase(--pBody->end());
+          pFoot->erase(--pFoot->end());
         }
 
       if ((*pHeader->rbegin()) == pReport->getSeparator().getCN())
@@ -179,40 +173,46 @@ int main(int argc, char *argv[])
           pHeader->erase(--pHeader->end());
         }
 
-      // create a trajectory task
-      pTrajectoryTask = new CTrajectoryTask();
+      //**** create a task ****
+
+      pSSTask = new CSteadyStateTask();
       // use LSODAR from now on since we will have events pretty soon
-      pTrajectoryTask->setMethodType(CCopasiMethod::LSODAR);
-      pTrajectoryTask->getProblem()->setModel(pDataModel->getModel());
+      pSSTask->setMethodType(CCopasiMethod::Newton);
+      pSSTask->getProblem()->setModel(pDataModel->getModel());
 
-      pTrajectoryTask->setScheduled(true);
+      pSSTask->setScheduled(true);
 
-      pTrajectoryTask->getReport().setReportDefinition(pReport);
-      pTrajectoryTask->getReport().setTarget(CWD + "/" + pOutputFilename);
-      pTrajectoryTask->getReport().setAppend(false);
+      pSSTask->getReport().setReportDefinition(pReport);
+      pSSTask->getReport().setTarget(CWD + "/" + pOutputFilename);
+      pSSTask->getReport().setAppend(false);
 
-      CTrajectoryProblem* pProblem = dynamic_cast<CTrajectoryProblem*>(pTrajectoryTask->getProblem());
+      //**** specify problem ****
 
-      pProblem->setStepNumber((const unsigned C_INT32)stepNumber);
-      pDataModel->getModel()->setInitialTime((const C_FLOAT64)startTime);
-      pProblem->setDuration((const C_FLOAT64)endTime - startTime);
-      pProblem->setTimeSeriesRequested(true);
+      CSteadyStateProblem* pProblem = dynamic_cast<CSteadyStateProblem*>(pSSTask->getProblem());
 
-      CTrajectoryMethod* pMethod = dynamic_cast<CTrajectoryMethod*>(pTrajectoryTask->getMethod());
+      //pProblem->setStepNumber((const unsigned C_INT32)stepNumber);
+      //pDataModel->getModel()->setInitialTime((const C_FLOAT64)startTime);
+      //pProblem->setDuration((const C_FLOAT64)endTime - startTime);
+      //pProblem->setTimeSeriesRequested(true);
 
-      pMethod->getParameter("Absolute Tolerance")->setValue(1.0e-12);
+      //**** method settings ****
 
+      CSteadyStateMethod* pMethod = dynamic_cast<CSteadyStateMethod*>(pSSTask->getMethod());
+
+      //pMethod->getParameter("Absolute Tolerance")->setValue(1.0e-12);
+
+      //***** run the task *********
       CCopasiVectorN< CCopasiTask > & TaskList = * pDataModel->getTaskList();
 
-      TaskList.remove("Time-Course");
-      TaskList.add(pTrajectoryTask, true);
+      TaskList.remove("Steady-State");
+      TaskList.add(pSSTask, true);
 
       // save the file for control purposes
-      //std::string saveFilename = pSBMLFilename;
-      //saveFilename = saveFilename.substr(0, saveFilename.length() - 4) + ".cps";
-      //pDataModel->saveModel(saveFilename, NULL, true);
+      std::string saveFilename = pSBMLFilename;
+      saveFilename = saveFilename.substr(0, saveFilename.length() - 4) + ".cps";
+      pDataModel->saveModel(saveFilename, NULL, true);
 
-      // Run the trajectory task
+      // Run the task
 
       //pTrajectoryTask->initialize(CCopasiTask::OUTPUT_COMPLETE, NULL,NULL);
       //pTrajectoryTask->process(true);
@@ -221,11 +221,11 @@ int main(int argc, char *argv[])
       // create another report that will write to the directory where the input file came from
       // this can be used for debugging
       // create a trajectory task
-      pTrajectoryTask->getReport().setTarget(pOutputFilename);
+      pSSTask->getReport().setTarget(pOutputFilename);
 
-      pTrajectoryTask->initialize(CCopasiTask::OUTPUT_COMPLETE, pDataModel, NULL);
-      pTrajectoryTask->process(true);
-      pTrajectoryTask->restore();
+      pSSTask->initialize(CCopasiTask::OUTPUT_COMPLETE, pDataModel, NULL);
+      pSSTask->process(true);
+      pSSTask->restore();
     }
   catch (CCopasiException Exception)
     {
