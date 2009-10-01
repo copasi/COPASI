ff87eaa47693ee7f6e76032058fe86c1f4357159
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2009-10-01 22:26:05 +0000

    add include <stdlib.h> needed to compile under linux

--- copasi/elementaryFluxModes/CStepMatrixColumn.cpp
+++ copasi/elementaryFluxModes/CStepMatrixColumn.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/elementaryFluxModes/CStepMatrixColumn.cpp,v $
-//   $Revision: 1.4 $
+//   $Revision: 1.5 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2009/09/24 18:13:13 $
+//   $Author: ssahle $
+//   $Date: 2009/10/01 22:26:05 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -11,6 +11,7 @@
 // and The University of Manchester.
 // All rights reserved.
 
+#include <stdlib.h>
 #include <math.h>
 #include <limits>
 
