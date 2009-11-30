5498b02cacc5852c55391f65d42cb996853c7a43
Natalia Simus <natalia.simus@bioquant.uni-heidelberg.de>
2009-11-30 13:15:48 +0000

    development of model expansion tool

--- copasi/model/CModelExpansion.cpp
+++ copasi/model/CModelExpansion.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/model/CModelExpansion.cpp,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
-//   $Author: ssahle $
-//   $Date: 2009/05/07 19:04:18 $
+//   $Author: nsimus $
+//   $Date: 2009/11/30 13:15:23 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -14,6 +14,10 @@
 #include "CModelExpansion.h"
 
 #include "CModel.h"
+#include "function/CExpression.h"
+#include "report/CCopasiObject.h"
+#include "CopasiDataModel/CCopasiDataModel.h"
+#include "report/CCopasiRootContainer.h"
 
 CModelExpansion::CModelExpansion(CModel* pModel)
     : mpModel(pModel)
@@ -31,7 +35,60 @@ void CModelExpansion::simpleCall()
 
   if (!mpModel->getCompartments().size()) return;
 
-  CompartmentInfo ci = copyCompartment(mpModel->getCompartments()[0], "copy");
+  ci.keyMap[mpModel->getKey()] = "";
+
+  unsigned C_INT32 i, imax;
+
+  imax = mpModel->getCompartments().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      const CCompartment* sourceComp = mpModel->getCompartments()[i];
+
+      if (!sourceComp)
+        continue; //TODO error handling, should not happen
+
+      ci.keyMap[sourceComp->getKey()] = "";
+    }
+
+  imax = mpModel->getMetabolites().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      const CMetab* sourceMetab = mpModel->getMetabolites()[i];
+
+      if (!sourceMetab)
+        continue; //TODO error handling, should not happen
+
+      ci.keyMap[sourceMetab->getKey()] = "";
+    }
+
+  imax = mpModel->getModelValues().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      const CModelValue* sourceModVal = mpModel->getModelValues()[i];
+
+      if (!sourceModVal)
+        continue; //TODO error handling, should not happen
+
+      ci.keyMap[sourceModVal->getKey()] = "";
+    }
+
+  CCompartment * source = mpModel->getCompartments()[0];
+
+  ci = copyCompartment(source, "copy");
+
+  bool  progress  =     copyCompartmentsExpressions(source)
+                        &&  copyMetabolitesExpressions(source)
+                        &&  copyModelValuesExpressions("copy")
+                        &&  copyEvents("copy");
+
+  if (!progress)
+    {
+      CCopasiMessage(CCopasiMessage::ERROR, MCModelExpansion + 1);
+      return;
+    }
 
   mpModel->compileIfNecessary(NULL);
 }
@@ -52,7 +109,8 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
   ret.key = newComp->getKey();
 
   newComp->setDimensionality(source->getDimensionality());
-  //TODO handle non const compartments
+
+  ret.keyMap[source->getKey()] = newComp->getKey();
 
   //create copies of the metabs
   unsigned C_INT32 i, imax = source->getMetabolites().size();
@@ -65,12 +123,12 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
       if (!pNewMetab)
         continue; //TODO error handling, should not happen
 
-      if (pSourceMetab->getStatus() == CModelEntity::FIXED) pNewMetab->setStatus(CModelEntity::FIXED);
+      pNewMetab->setStatus(pSourceMetab->getStatus());
 
-      //TODO handle metabs with rules
-
-      ret.metabMap[pSourceMetab->getKey()] = pNewMetab->getKey();
+      ret.keyMap[pSourceMetab->getKey()] = pNewMetab->getKey();
     }
+
+  newComp->setStatus(source->getStatus());
 
   //create copies of the relevant reactions
   imax = mpModel->getReactions().size();
@@ -97,9 +155,9 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
             {
               const CChemEqElement * sourceElement = pReac->getChemEq().getSubstrates()[j];
               //check if the metab is in the map. If yes, translate it, otherwise not.
-              mapIt = ret.metabMap.find(sourceElement->getMetaboliteKey());
+              mapIt = ret.keyMap.find(sourceElement->getMetaboliteKey());
 
-              if (mapIt == ret.metabMap.end())
+              if (mapIt == ret.keyMap.end())
                 targetKey = sourceElement->getMetaboliteKey();
               else
                 targetKey = mapIt->second;
@@ -113,9 +171,9 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
             {
               const CChemEqElement * sourceElement = pReac->getChemEq().getProducts()[j];
               //check if the metab is in the map. If yes, translate it, otherwise not.
-              mapIt = ret.metabMap.find(sourceElement->getMetaboliteKey());
+              mapIt = ret.keyMap.find(sourceElement->getMetaboliteKey());
 
-              if (mapIt == ret.metabMap.end())
+              if (mapIt == ret.keyMap.end())
                 targetKey = sourceElement->getMetaboliteKey();
               else
                 targetKey = mapIt->second;
@@ -129,9 +187,9 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
             {
               const CChemEqElement * sourceElement = pReac->getChemEq().getModifiers()[j];
               //check if the metab is in the map. If yes, translate it, otherwise not.
-              mapIt = ret.metabMap.find(sourceElement->getMetaboliteKey());
+              mapIt = ret.keyMap.find(sourceElement->getMetaboliteKey());
 
-              if (mapIt == ret.metabMap.end())
+              if (mapIt == ret.keyMap.end())
                 targetKey = sourceElement->getMetaboliteKey();
               else
                 targetKey = mapIt->second;
@@ -162,9 +220,9 @@ CModelExpansion::CompartmentInfo CModelExpansion::copyCompartment(const CCompart
 
                     for (k = 0; k < pReac->getParameterMappings()[j].size(); ++k)
                       {
-                        mapIt = ret.metabMap.find(pReac->getParameterMappings()[j][k]);
+                        mapIt = ret.keyMap.find(pReac->getParameterMappings()[j][k]);
 
-                        if (mapIt == ret.metabMap.end())
+                        if (mapIt == ret.keyMap.end())
                           targetKey = pReac->getParameterMappings()[j][k];
                         else
                           targetKey = mapIt->second;
@@ -240,4 +298,334 @@ bool CModelExpansion::reactionInvolvesCompartment(const CReaction * reac, const 
       return true;
 
   return false;
+}
+
+/* DEVELOPMENT : handle non constant model values, metabolites, compartments */
+
+bool CModelExpansion::copyModelValuesExpressions(std::string name)
+{
+  bool info = false;
+
+  unsigned C_INT32 i, imax = mpModel->getModelValues().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      CModelValue* source = mpModel->getModelValues()[i];
+
+      if (!source) return info;
+
+      std::string infix = "";
+      std::string newInfix = "";
+
+      const CExpression* pExpression = source->getExpressionPtr();
+      CModelValue* newModVal;
+
+      if (pExpression != NULL)
+        {
+          infix = pExpression->getInfix().c_str();
+          newInfix = copyExpression(pExpression);
+
+          if (infix != newInfix)
+            {
+              newModVal = mpModel->createModelValue(source->getObjectName() + "_" + name, source->getInitialValue());
+              newModVal->setStatus(source->getStatus());
+              newModVal->setExpression(newInfix);
+
+              ci.keyMap[source->getKey()] = newModVal->getKey();
+            }
+        }
+
+      if (source->getInitialExpression() != "")
+        {
+          pExpression = source->getInitialExpressionPtr();
+
+          infix = pExpression->getInfix().c_str();
+          newInfix = copyExpression(pExpression);
+
+          if (infix != newInfix)
+            if (ci.keyMap[source->getKey()] == "")
+              {
+                newModVal = mpModel->createModelValue(source->getObjectName() + "_" + name, source->getInitialValue());
+                newModVal->setStatus(source->getStatus());
+
+                const CExpression* tmpExpression = source->getExpressionPtr();
+
+                if (tmpExpression != NULL)
+                  newModVal->setExpression(tmpExpression->getInfix().c_str());
+
+                newModVal->setInitialExpression(newInfix);
+
+                ci.keyMap[source->getKey()] = newModVal->getKey();
+              }
+            else
+              newModVal->setInitialExpression(newInfix);
+        }
+    }
+
+  return true;
+}
+
+bool CModelExpansion::copyMetabolitesExpressions(const CCompartment* source)
+{
+  bool info = false;
+
+  unsigned C_INT32 i, imax = source->getMetabolites().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      CMetab* pSourceMetab = source->getMetabolites()[i];
+
+      if (!pSourceMetab) return info;
+
+      std::string newKey = ci.keyMap[pSourceMetab->getKey()];
+
+      CCopasiObject*  pObject = (CCopasiRootContainer::getKeyFactory()->get(newKey));
+
+      CMetab*  newMetab = dynamic_cast< CMetab * >(pObject);
+
+      if (!newMetab) return info;
+
+      std::string infix = "";
+
+      const CExpression* pExpression = pSourceMetab->getExpressionPtr();
+
+      if (pExpression != NULL)
+        {
+          infix = copyExpression(pExpression);
+          newMetab->setExpression(infix);
+        }
+
+      if (pSourceMetab->getInitialExpression() != "")
+        {
+          infix = copyExpression(pSourceMetab->getInitialExpressionPtr());
+          newMetab->setInitialExpression(infix);
+        }
+    }
+
+  return true;
+}
+
+bool CModelExpansion::copyCompartmentsExpressions(const CCompartment* source)
+{
+
+  bool info = false;
+
+  std::string newKey = ci.keyMap[source->getKey()];
+
+  CCopasiObject*  pObject = (CCopasiRootContainer::getKeyFactory()->get(newKey));
+
+  CCompartment*  newComp = dynamic_cast< CCompartment * >(pObject);
+
+  if (!newComp) return info;
+
+  std::string infix = "";
+
+  const CExpression* pExpression = source->getExpressionPtr();
+
+  if (pExpression != NULL)
+    {
+      infix = copyExpression(pExpression);
+      newComp->setExpression(infix);
+    }
+
+  if (source->getInitialExpression() != "")
+    {
+      infix = copyExpression(source->getInitialExpressionPtr());
+      newComp->setInitialExpression(infix);
+    }
+
+  return true;
+}
+
+std::string CModelExpansion::copyExpression(const CExpression * pExpression)
+{
+
+  CExpression* tmp;
+  tmp = new CExpression(*pExpression, mpModel);
+
+  const std::vector<CEvaluationNode*>& objectNodes = tmp->getNodeList();
+  unsigned j, jmax = objectNodes.size();
+
+  for (j = 0; j < jmax; ++j)
+    {
+      if (CEvaluationNode::type(objectNodes[j]->getType()) == CEvaluationNode::OBJECT)
+        {
+          CEvaluationNodeObject* pObjectNode = dynamic_cast<CEvaluationNodeObject*>(objectNodes[j]);
+
+          assert(pObjectNode != NULL);
+
+          CCopasiObjectName cn = pObjectNode->getObjectCN();
+
+          const CCopasiObject* mObject = mpModel->getObjectDataModel()->getObject(cn);
+
+          assert(mObject != NULL);
+
+          std::string host = "";
+
+          if (mObject->isReference())
+            {
+              host = ",Reference=" + mObject->getObjectName();
+              mObject = mObject->getObjectParent();
+            }
+
+          assert(mObject != NULL);
+
+          std::string key;
+
+          key = ci.keyMap[(dynamic_cast<const CModelEntity * >(mObject))->getKey()];
+
+          if (key == "") key = (dynamic_cast<const CModelEntity * >(mObject))->getKey();
+
+          CCopasiObject*  pObject = (CCopasiRootContainer::getKeyFactory()->get(key));
+
+          cn = pObject->getCN() + host;
+
+          pObjectNode->setData("<" + cn + ">");
+        }
+    }
+
+  tmp->updateTree();
+
+  return tmp->getInfix().c_str();
+}
+
+bool CModelExpansion::copyEvents(std::string name)
+{
+
+  bool info = false;
+
+  unsigned C_INT32 i, imax = mpModel->getEvents().size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      const CEvent* sourceEvent = mpModel->getEvents()[i];
+      CEvent* newEvent;
+
+      std::string infix = "";
+      std::string trigger = "";
+      std::string  delay = "";
+      std::string assignment = "";
+
+      if (!sourceEvent) return info;
+
+      /* handle trigger expression */
+
+      const CExpression* pExpression;
+
+      pExpression = sourceEvent->getTriggerExpressionPtr();
+
+      if (pExpression != NULL)
+        {
+          trigger = pExpression->getInfix().c_str();
+          infix = copyExpression(pExpression);
+
+          if (trigger != infix)
+            {
+              trigger = infix;
+              newEvent = mpModel->createEvent(sourceEvent->getObjectName() + "_" + name);
+              newEvent->setTriggerExpression(trigger);
+            }
+        }
+
+      /* handle  delay expression */
+
+      pExpression = sourceEvent->getDelayExpressionPtr();
+
+      if (sourceEvent->getDelayAssignment() &&  pExpression != NULL)
+        {
+          delay = pExpression->getInfix().c_str();
+          infix = copyExpression(pExpression);
+
+          if (delay != infix)
+            {
+              delay = infix;
+
+              if (newEvent == NULL)
+                {
+                  newEvent = mpModel->createEvent(sourceEvent->getObjectName() + "_" + name);
+                  newEvent->setTriggerExpression(trigger);
+                }
+            }
+
+          if (newEvent != NULL)
+            {
+              newEvent->setDelayAssignment(sourceEvent->getDelayAssignment());
+              newEvent->setDelayExpression(delay);
+            }
+        }
+
+      /* handle event  assignments */
+
+      CEvent * tmpEvent;
+
+      tmpEvent = mpModel->createEvent(sourceEvent->getObjectName() + "tmp");
+
+      bool identic = true;
+
+      unsigned C_INT32 j, jmax = sourceEvent->getAssignments().size();
+
+      for (j = 0; j < jmax; ++j)
+        {
+          const CEventAssignment* sourceAssignment = sourceEvent->getAssignments()[j];
+
+          std::string key = sourceAssignment->getTargetKey();
+
+          CEventAssignment* tmpAssignment = new CEventAssignment;
+
+          tmpEvent->getAssignments().add(tmpAssignment);
+
+          if (ci.keyMap[key] == "")  tmpAssignment->setTargetKey(key);
+          else
+            {
+              tmpAssignment->setTargetKey(ci.keyMap[key]);
+              identic = false;
+            }
+
+          pExpression = sourceAssignment->getExpressionPtr();
+
+          if (pExpression != NULL)
+            {
+              assignment = pExpression->getInfix().c_str();
+              infix = copyExpression(pExpression);
+
+              if (assignment != infix)
+                {
+                  tmpAssignment->setExpression(infix);
+                  identic = false;
+                }
+              else
+                tmpAssignment->setExpression(assignment);
+            }
+        }
+
+      if (newEvent == NULL && !identic)
+        {
+          newEvent = mpModel->createEvent(sourceEvent->getObjectName() + "_" + name);
+          newEvent->setTriggerExpression(trigger);
+          newEvent->setDelayExpression(delay);
+          newEvent->setDelayAssignment(sourceEvent->getDelayAssignment());
+        }
+
+      if (newEvent != NULL)
+
+        for (j = 0; j < jmax; ++j)
+          {
+            const CEventAssignment* tmpAssignment = tmpEvent->getAssignments()[j];
+
+            CEventAssignment* newAssignment = new CEventAssignment;
+
+            newEvent->getAssignments().add(newAssignment, true);
+
+            newAssignment->setTargetKey(tmpAssignment->getTargetKey());
+
+            pExpression = tmpAssignment->getExpressionPtr();
+
+            if (pExpression != NULL)
+              newAssignment->setExpression(pExpression->getInfix().c_str());
+          }
+
+      mpModel->removeEvent(tmpEvent, true);
+    }
+
+  return true;
 }
--- copasi/model/CModelExpansion.h
+++ copasi/model/CModelExpansion.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/model/CModelExpansion.h,v $
-//   $Revision: 1.1 $
+//   $Revision: 1.2 $
 //   $Name:  $
-//   $Author: ssahle $
-//   $Date: 2009/05/07 19:04:18 $
+//   $Author: nsimus $
+//   $Date: 2009/11/30 13:15:48 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -20,6 +20,8 @@
 class CModel;
 class CCompartment;
 class CReaction;
+class CModelEntity;
+class CExpression;
 
 class CModelExpansion
 {
@@ -38,17 +40,31 @@ protected:
   public:
     CompartmentInfo() {};
 
-    std::map<std::string, std::string> metabMap;
+    std::map<std::string, std::string> keyMap;
     std::string key;
   };
 
   CompartmentInfo copyCompartment(const CCompartment* source, std::string name) const;
+
+  CompartmentInfo ci;
 
   /**
    * determine whether the one of the substrate, products, or modifiers of the reaction
    * is located in the given compartmen
    */
   static bool reactionInvolvesCompartment(const CReaction * reac, const CCompartment* comp);
+
+  /**
+   *   handle non constant model entities
+   **/
+
+  bool  copyCompartmentsExpressions(const CCompartment *source);
+  bool  copyMetabolitesExpressions(const CCompartment *source);
+
+  bool  copyModelValuesExpressions(std::string name);
+  bool  copyEvents(std::string name);
+
+  std::string copyExpression(const CExpression *pExpression);
 
   CModel * mpModel;
 };
