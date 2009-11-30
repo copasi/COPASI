64fe91cd7c76bebb7078caf978af6ebd47d57fc3
Stefan Hoops <shoops@vbi.vt.edu>
2009-11-30 18:44:04 +0000

    Fixed Bug 1305. The information needed for switching the framework is now stored within the widgets table. Thus a model change will no longer lead to a crash.

--- copasi/UI/StateSubwidget.cpp
+++ copasi/UI/StateSubwidget.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/StateSubwidget.cpp,v $
-//   $Revision: 1.28 $
+//   $Revision: 1.29 $
 //   $Name:  $
-//   $Author: pwilly $
-//   $Date: 2009/10/05 11:16:36 $
+//   $Author: shoops $
+//   $Date: 2009/11/30 18:44:04 $
 // End CVS Header
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -107,21 +107,12 @@ void StateSubwidget::loadMetabolites()
       {
         mpTblMetabolites->setText(i, 0, FROM_UTF8(CMetabNameInterface::getDisplayName(mpModel, **it)));
         mpTblMetabolites->setText(i, 1, FROM_UTF8(CModelEntity::StatusName[(*it)->getStatus()]));
+        mpTblMetabolites->setText(i, 2, QString::number((*it)->getConcentration()));
+        mpTblMetabolites->setText(i, 3, QString::number((*it)->getValue()));
+        mpTblMetabolites->setText(i, 4, QString::number((*it)->getConcentrationRate()));
+        mpTblMetabolites->setText(i, 5, QString::number((*it)->getRate()));
+        mpTblMetabolites->setText(i, 6, QString::number((*it)->getTransitionTime()));
 
-        switch (mFramework)
-          {
-            case 0:
-              mpTblMetabolites->setText(i, 2, QString::number((*it)->getConcentration()));
-              mpTblMetabolites->setText(i, 3, QString::number((*it)->getConcentrationRate()));
-              break;
-
-            case 1:
-              mpTblMetabolites->setText(i, 2, QString::number((*it)->getValue()));
-              mpTblMetabolites->setText(i, 3, QString::number((*it)->getRate()));
-              break;
-          }
-
-        mpTblMetabolites->setText(i, 4, QString::number((*it)->getTransitionTime()));
         i++;
       }
 
@@ -132,6 +123,8 @@ void StateSubwidget::loadMetabolites()
   mpTblMetabolites->adjustColumn(2);
   mpTblMetabolites->adjustColumn(3);
   mpTblMetabolites->adjustColumn(4);
+  mpTblMetabolites->adjustColumn(5);
+  mpTblMetabolites->adjustColumn(6);
 }
 
 void StateSubwidget::loadCompartments()
@@ -172,25 +165,17 @@ void StateSubwidget::loadReactions()
   for (; it != end; ++it)
     {
       mpTblReactions->setText(i, 0, FROM_UTF8((*it)->getObjectName()));
+      mpTblReactions->setText(i, 1, QString::number((*it)->getFlux()));
+      mpTblReactions->setText(i, 2, QString::number((*it)->getParticleFlux()));
+      mpTblReactions->setText(i, 3, FROM_UTF8(CChemEqInterface::getChemEqString(mpModel, **it, false)));
 
-      switch (mFramework)
-        {
-          case 0:
-            mpTblReactions->setText(i, 1, QString::number((*it)->getFlux()));
-            break;
-
-          case 1:
-            mpTblReactions->setText(i, 1, QString::number((*it)->getParticleFlux()));
-            break;
-        }
-
-      mpTblReactions->setText(i, 2, FROM_UTF8(CChemEqInterface::getChemEqString(mpModel, **it, false)));
       i++;
     }
 
   mpTblReactions->adjustColumn(0);
   mpTblReactions->adjustColumn(1);
   mpTblReactions->adjustColumn(2);
+  mpTblReactions->adjustColumn(3);
 }
 
 void StateSubwidget::loadModelValues()
@@ -325,31 +310,23 @@ void StateSubwidget::showUnits()
   if (!QuantityRateUnits.isEmpty())
     QuantityRateUnits = "\n(" + QuantityRateUnits + ")";
 
-  mpTblMetabolites->horizontalHeader()
-  ->setLabel(4, "Transition Time" + TimeUnits);
-
   mpTblCompartments->horizontalHeader()->setLabel(2, "Volume" + VolumeUnits);
 
   mpTblCompartments->horizontalHeader()->setLabel(3, "Rate" + VolumeRateUnits);
 
-  switch (mFramework)
-    {
-      case 0:
-        mpTblMetabolites->horizontalHeader()->setLabel(2, "Concentration" + ConcentrationUnits);
+  mpTblMetabolites->horizontalHeader()->setLabel(2, "Concentration" + ConcentrationUnits);
 
-        mpTblMetabolites->horizontalHeader()->setLabel(3, "Rate" + ConcentrationRateUnits);
+  mpTblMetabolites->horizontalHeader()->setLabel(3, "Particle Numbers");
 
-        mpTblReactions->horizontalHeader()->setLabel(1, "Flux" + QuantityRateUnits);
-        break;
+  mpTblMetabolites->horizontalHeader()->setLabel(4, "Rate" + ConcentrationRateUnits);
 
-      case 1:
-        mpTblMetabolites->horizontalHeader()->setLabel(2, "Particle Numbers");
+  mpTblMetabolites->horizontalHeader()->setLabel(5, "Rate" + FrequencyUnits);
 
-        mpTblMetabolites->horizontalHeader()->setLabel(3, "Rate" + FrequencyUnits);
+  mpTblMetabolites->horizontalHeader()->setLabel(6, "Transition Time" + TimeUnits);
 
-        mpTblReactions->horizontalHeader()->setLabel(1, "Particle Flux" + FrequencyUnits);
-        break;
-    }
+  mpTblReactions->horizontalHeader()->setLabel(1, "Flux" + QuantityRateUnits);
+
+  mpTblReactions->horizontalHeader()->setLabel(2, "Particle Flux" + FrequencyUnits);
 }
 
 bool StateSubwidget::loadAll(const CSteadyStateTask * pTask)
@@ -382,8 +359,10 @@ bool StateSubwidget::loadAll(const CSteadyStateTask * pTask)
 
   setFramework(mFramework);
 
+  loadMetabolites();
   loadCompartments();
   loadModelValues();
+  loadReactions();
 
   const CSteadyStateProblem* pProblem =
     dynamic_cast<const CSteadyStateProblem *>(mpTask->getProblem());
@@ -452,13 +431,29 @@ void StateSubwidget::setFramework(int framework)
 {
   CopasiWidget::setFramework(framework);
 
+  // TODO hide/show columns
+  switch (mFramework)
+    {
+      case 0:
+        mpTblMetabolites->showColumn(2);
+        mpTblMetabolites->hideColumn(3);
+        mpTblMetabolites->showColumn(4);
+        mpTblMetabolites->hideColumn(5);
+        mpTblReactions->showColumn(1);
+        mpTblReactions->hideColumn(2);
+        break;
+
+      case 1:
+        mpTblMetabolites->hideColumn(2);
+        mpTblMetabolites->showColumn(3);
+        mpTblMetabolites->hideColumn(4);
+        mpTblMetabolites->showColumn(5);
+        mpTblReactions->hideColumn(1);
+        mpTblReactions->showColumn(2);
+        break;
+    }
+
   showUnits();
-
-  if (mpModel == NULL)
-    return;
-
-  loadMetabolites();
-  loadReactions();
 }
 
 bool StateSubwidget::update(ListViews::ObjectType objectType,
