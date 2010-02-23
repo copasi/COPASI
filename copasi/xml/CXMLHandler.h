632be2e07136b1c9e67159c8c424f673aaba6868
Stefan Hoops <shoops@vbi.vt.edu>
2010-02-23 17:18:51 +0000

    Added method whch assures that we do not accidentally delete common element handlers.

--- copasi/xml/CXMLHandler.h
+++ copasi/xml/CXMLHandler.h
@@ -1,12 +1,17 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/xml/CXMLHandler.h,v $
-   $Revision: 1.9 $
+   $Revision: 1.9.36.1 $
    $Name:  $
    $Author: shoops $
-   $Date: 2006/04/27 01:33:05 $
+   $Date: 2010/02/23 17:18:51 $
    End CVS Header */
 
-// Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -27,70 +32,88 @@
 #include "expat.h"
 
 template<class CType, class CCommon>
-      class CXMLElementHandler
-    {
-      // Attributes
-    protected:
-      /**
-       * The underlying parser.
-       */
-      CType & mParser;
+class CXMLElementHandler
+{
+  // Attributes
+protected:
+  /**
+   * The underlying parser.
+   */
+  CType & mParser;
 
-      /**
-       * The shared elements between all element handlers of a parser
-       */
-      CCommon & mCommon;
+  /**
+   * The shared elements between all element handlers of a parser
+   */
+  CCommon & mCommon;
 
-      /**
-       * The currently processed element.
-       */
-      C_INT32 mCurrentElement;
+  /**
+   * The currently processed element.
+   */
+  C_INT32 mCurrentElement;
 
-      /**
-       *
-       */
-      CXMLElementHandler * mpCurrentHandler;
+  /**
+   *
+   */
+  CXMLElementHandler * mpCurrentHandler;
 
-      /**
-       *
-       */
-      C_INT32 mLastKnownElement;
+  /**
+   *
+   */
+  C_INT32 mLastKnownElement;
 
-      // Operations
-    public:
-      /**
-       * Constructor
-       */
-      CXMLElementHandler(CType & parser, CCommon & common):
-          mParser(parser),
-          mCommon(common),
-          mCurrentElement(-1),
-          mpCurrentHandler(NULL),
-          mLastKnownElement(-1)
-      {}
+  // Operations
+public:
+  /**
+   * Constructor
+   */
+  CXMLElementHandler(CType & parser, CCommon & common):
+      mParser(parser),
+      mCommon(common),
+      mCurrentElement(-1),
+      mpCurrentHandler(NULL),
+      mLastKnownElement(-1)
+  {}
 
-      /**
-       * Destructor
-       */
-      virtual ~CXMLElementHandler() {}
+  /**
+   * Destructor
+   */
+  virtual ~CXMLElementHandler() {}
 
-      /**
-       * Start element handler
-       * @param const XML_Char *pszName
-       * @param const XML_Char **papszAttrs
-       */
-      virtual void start(const XML_Char * C_UNUSED(pszName),
-                         const XML_Char ** C_UNUSED(papszAttrs)) {}
+  /**
+   * Start element handler
+   * @param const XML_Char *pszName
+   * @param const XML_Char **papszAttrs
+   */
+  virtual void start(const XML_Char * C_UNUSED(pszName),
+                     const XML_Char ** C_UNUSED(papszAttrs)) {}
 
-      /**
-       * End element handler
-       * @param const XML_Char *pszName
-       */
-      virtual void end(const XML_Char * C_UNUSED(pszName)) {}
+  /**
+   * End element handler
+   * @param const XML_Char *pszName
+   */
+  virtual void end(const XML_Char * C_UNUSED(pszName)) {}
 
-      /**
-       * Reset the element handler to start values.
-       */
-      virtual void reset() {}};
+  /**
+   * Reset the element handler to start values.
+   */
+  virtual void reset() {}
+
+protected:
+  /**
+   * This assures that we do not accidentally delete common parser handlers.
+   */
+  void deleteCurrentHandler()
+  {
+    if (mpCurrentHandler != NULL &&
+        mpCurrentHandler != &mParser.mUnknownElement &&
+        mpCurrentHandler != &mParser.mCharacterDataElement &&
+        mpCurrentHandler != &mParser.mCommentElement &&
+        mpCurrentHandler != &mParser.mMiriamAnnotationElement)
+      {
+        delete mpCurrentHandler;
+        mpCurrentHandler = NULL;
+      }
+  }
+};
 
 #endif // COPASI_CXMLHandler
