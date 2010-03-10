adb3e5b98c93de2b37536a81deff30936e03bb26
Ralph Gauges <gauges@hs-albsig.de>
2010-03-10 12:35:58 +0000

    Changes to use the new render extension UI classes.

--- copasi/UI/CQLayoutsWidget.h
+++ copasi/UI/CQLayoutsWidget.h
@@ -1,10 +1,15 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQLayoutsWidget.h,v $
-//   $Revision: 1.4 $
+//   $Revision: 1.5 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2008/12/18 19:56:21 $
+//   $Author: gauges $
+//   $Date: 2010/03/10 12:35:58 $
 // End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
@@ -20,101 +25,109 @@
 
 #include "copasi/UI/CopasiTableWidget.h"
 
+#ifdef USE_CRENDER_EXTENSION
+class CQNewMainWindow;
+#else
 class CQLayoutMainWindow;
+#endif // USE_CRENDER_EXTENSION
 
 class CQLayoutsWidget : public CopasiTableWidget
-  {
-    Q_OBJECT
+{
+  Q_OBJECT
 
-  public:
-    CQLayoutsWidget(QWidget *parent, const char * name = 0, Qt::WFlags f = 0)
-        : CopasiTableWidget(parent, false, name, f)
-    {init();}
+public:
+  CQLayoutsWidget(QWidget *parent, const char * name = 0, Qt::WFlags f = 0)
+      : CopasiTableWidget(parent, false, name, f)
+  {init();}
 
-    void deleteLayoutWindows();
+  void deleteLayoutWindows();
 
-  protected:
-    /**
-     * shows units for the quantities
-     */
-    virtual void updateHeaderUnits();
+protected:
+  /**
+   * shows units for the quantities
+   */
+  virtual void updateHeaderUnits();
 
-    /**
-     * This initializes the widget
-     */
-    virtual void init();
+  /**
+   * This initializes the widget
+   */
+  virtual void init();
 
-    /**
-     * returns a list of objects that should be displayed
-     */
-    virtual std::vector<const CCopasiObject*> getObjects() const;
+  /**
+   * returns a list of objects that should be displayed
+   */
+  virtual std::vector<const CCopasiObject*> getObjects() const;
 
-    /**
-     * fills one table row with the data from one object
-     */
-    virtual void tableLineFromObject(const CCopasiObject* obj, unsigned C_INT32 row);
+  /**
+   * fills one table row with the data from one object
+   */
+  virtual void tableLineFromObject(const CCopasiObject* obj, unsigned C_INT32 row);
 
-    /**
-     * reads the contents of one row of the table and writes it to the object
-     */
-    virtual void tableLineToObject(unsigned C_INT32 row, CCopasiObject* obj);
+  /**
+   * reads the contents of one row of the table and writes it to the object
+   */
+  virtual void tableLineToObject(unsigned C_INT32 row, CCopasiObject* obj);
 
-    /**
-     * creates a new object
-     */
-    virtual CCopasiObject* createNewObject(const std::string & name);
+  /**
+   * creates a new object
+   */
+  virtual CCopasiObject* createNewObject(const std::string & name);
 
-    /**
-     * deletes objects. Performs all additional tasks, like asking the user, ...
-     */
-    virtual void deleteObjects(const std::vector<std::string> & keys);
+  /**
+   * deletes objects. Performs all additional tasks, like asking the user, ...
+   */
+  virtual void deleteObjects(const std::vector<std::string> & keys);
 
-    /**
-     * this is used to fill a row of the table when a new object is added to the table.
-     * it fills only the data columns, not the name. It should not fill column exc.
-     */
-    virtual void defaultTableLineContent(unsigned C_INT32 row, unsigned C_INT32 exc);
+  /**
+   * this is used to fill a row of the table when a new object is added to the table.
+   * it fills only the data columns, not the name. It should not fill column exc.
+   */
+  virtual void defaultTableLineContent(unsigned C_INT32 row, unsigned C_INT32 exc);
 
-    /**
-     * the prefix that is used to construct new object names
-     */
-    virtual QString defaultObjectName() const;
+  /**
+   * the prefix that is used to construct new object names
+   */
+  virtual QString defaultObjectName() const;
 
-    /**
-     * This method provides a hook for derived classes to act on changes in
-     * the table.
-     * @param unsigned C_INT32 row
-     * @param unsigned C_INT32 col
-     */
-    virtual void valueChanged(unsigned C_INT32 row, unsigned C_INT32 col);
+  /**
+   * This method provides a hook for derived classes to act on changes in
+   * the table.
+   * @param unsigned C_INT32 row
+   * @param unsigned C_INT32 col
+   */
+  virtual void valueChanged(unsigned C_INT32 row, unsigned C_INT32 col);
 
-  protected:
+protected:
 
-    /**
-     * map that stores the layout windows for each layout.
-     */
-    std::map<std::string, CQLayoutMainWindow*> mLayoutWindowMap;
+  /**
+   * map that stores the layout windows for each layout.
+   */
+#ifdef USE_CRENDER_EXTENSION
+  std::map<std::string, CQNewMainWindow*> mLayoutWindowMap;
+#else
+  std::map<std::string, CQLayoutMainWindow*> mLayoutWindowMap;
+#endif // USE_CRENDER_EXTENSION
 
-  protected slots:
-    void slotDoubleClicked(int row, int col, int m, const QPoint & n);
-    void slot_show(int row);
-  };
+protected slots:
+  void slotDoubleClicked(int row, int col, int m, const QPoint & n);
+  void slot_show(int row);
+};
 
 class CQShowLayoutButton : public QToolButton
-  {
-    Q_OBJECT
+{
+  Q_OBJECT
 
-  public:
-    CQShowLayoutButton(unsigned int row, QWidget* pParent = NULL, const char* name = "");
+public:
+  CQShowLayoutButton(unsigned int row, QWidget* pParent = NULL, const char* name = "");
 
-  signals:
-    void signal_show(int row);
+signals:
+  void signal_show(int row);
 
-  protected:
-    unsigned int mRow;
+protected:
+  unsigned int mRow;
 
-  protected slots:
-    void slot_clicked();
-  };
+protected slots:
+  void slot_clicked();
+};
 
 #endif // CQLAYOUT_WIDGETS_H
