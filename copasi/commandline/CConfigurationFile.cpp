d4d25b6507e116c99de74ce751e3edb0f4570b21
Stefan Hoops <shoops@vbi.vt.edu>
2010-04-30 18:01:59 +0000

    Fixed the name of the top group, which was arbitrary to be named Configuration.

--- copasi/commandline/CConfigurationFile.cpp
+++ copasi/commandline/CConfigurationFile.cpp
@@ -1,9 +1,9 @@
 /* Begin CVS Header
 $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/commandline/CConfigurationFile.cpp,v $
-$Revision: 1.14 $
+$Revision: 1.14.2.1 $
 $Name:  $
 $Author: shoops $
-$Date: 2010/02/17 19:35:02 $
+$Date: 2010/04/30 18:01:59 $
 End CVS Header */
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
@@ -364,6 +364,8 @@ bool CConfigurationFile::CXML::load(std::istream & is,
   if (success && Parser.getCurrentGroup() != NULL)
     {
       mConfiguration = * Parser.getCurrentGroup();
+      mConfiguration.setObjectName("Configuration");
+
       delete Parser.getCurrentGroup();
     }
   else
