41c1682a0db7b8f01642dbeb18eb9cbeeeadaf43
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2010-05-10 14:10:56 +0000

    remove one of the two addLineSegment() methods. One should be enough.

--- copasi/layout/CLCurve.cpp
+++ copasi/layout/CLCurve.cpp
@@ -1,10 +1,15 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layout/CLCurve.cpp,v $
-//   $Revision: 1.12 $
+//   $Revision: 1.12.2.1 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2009/01/07 18:56:03 $
+//   $Author: ssahle $
+//   $Date: 2010/05/10 14:10:56 $
 // End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
@@ -32,6 +37,7 @@ CLLineSegment::CLLineSegment(const LineSegment & ls)
 {
   //handle bezier
   const CubicBezier * cb = dynamic_cast<const CubicBezier *>(&ls);
+
   if (cb)
     {
       mIsBezier = true;
@@ -41,31 +47,33 @@ CLLineSegment::CLLineSegment(const LineSegment & ls)
 }
 
 void CLLineSegment::exportToSBMLBezier(CubicBezier * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const
-  {
-    if (!c) return;
+{
+  if (!c) return;
 
-    //first the part that is inherited from line segment
-    exportToSBMLLineSegment(c, copasimodelmap);
+  //first the part that is inherited from line segment
+  exportToSBMLLineSegment(c, copasimodelmap);
 
-    //now the rest
-    c->setBasePoint1(this->getBase1().getX(), this->getBase1().getY());
-    c->setBasePoint2(this->getBase2().getX(), this->getBase2().getY());
-  }
+  //now the rest
+  c->setBasePoint1(this->getBase1().getX(), this->getBase1().getY());
+  c->setBasePoint2(this->getBase2().getX(), this->getBase2().getY());
+}
 
 void CLLineSegment::exportToSBMLLineSegment(LineSegment * l,
     const std::map<CCopasiObject*, SBase*> & /* copasimodelmap */) const
-  {
-    if (!l) return;
+{
+  if (!l) return;
 
-    l->setStart(this->getStart().getX(), this->getStart().getY());
-    l->setEnd(this->getEnd().getX(), this->getEnd().getY());
-  }
+  l->setStart(this->getStart().getX(), this->getStart().getY());
+  l->setEnd(this->getEnd().getX(), this->getEnd().getY());
+}
 
 std::ostream & operator<<(std::ostream &os, const CLLineSegment & ls)
 {
   os << "[" << ls.mStart << "->" << ls.mEnd << "]";
+
   if (ls.mIsBezier)
     os << "  " << ls.mBase1 << ", " << ls.mBase2;
+
   return os;
 }
 
@@ -82,23 +90,21 @@ CLCurve::CLCurve(const Curve & sbmlcurve)
 {
   //TODO
   C_INT32 i, imax = sbmlcurve.getListOfCurveSegments()->size();
+
   for (i = 0; i < imax; ++i)
     {
       const LineSegment* tmp
       = dynamic_cast<const LineSegment*>(sbmlcurve.getListOfCurveSegments()->get(i));
+
       if (tmp)
         mvCurveSegments.push_back(CLLineSegment(*tmp));
+
       //addCurveSegment(CLLineSegment(*tmp));
     }
 }
 
 CLCurve::~CLCurve()
 {}
-
-void CLCurve::addCurveSegment(CLLineSegment & ls)
-{
-  mvCurveSegments.push_back(ls);
-}
 
 void CLCurve::addCurveSegment(CLLineSegment * pLs)
 {
@@ -112,52 +118,60 @@ void CLCurve::clear()
 }
 
 bool CLCurve::isContinuous() const
-  {
-    if (mvCurveSegments.size() <= 1) return true;
+{
+  if (mvCurveSegments.size() <= 1) return true;
 
-    C_INT32 i, imax = mvCurveSegments.size() - 1;
-    for (i = 0; i < imax; ++i)
-      if (!(mvCurveSegments[i].getEnd() == mvCurveSegments[i + 1].getStart()))
-        return false;
-    return true;
-  }
+  C_INT32 i, imax = mvCurveSegments.size() - 1;
+
+  for (i = 0; i < imax; ++i)
+    if (!(mvCurveSegments[i].getEnd() == mvCurveSegments[i + 1].getStart()))
+      return false;
+
+  return true;
+}
 
 std::vector <CLPoint> CLCurve::getListOfPoints() const
-  {
-    std::vector <CLPoint> ret;
-    if (mvCurveSegments.size() == 0) return ret;
-    if (!isContinuous()) return ret;
+{
+  std::vector <CLPoint> ret;
 
-    C_INT32 i, imax = mvCurveSegments.size();
-    for (i = 0; i < imax; ++i)
-      ret.push_back(mvCurveSegments[i].getStart());
-    ret.push_back(mvCurveSegments[i - 1].getEnd());
+  if (mvCurveSegments.size() == 0) return ret;
 
-    return ret;
-  }
+  if (!isContinuous()) return ret;
+
+  C_INT32 i, imax = mvCurveSegments.size();
+
+  for (i = 0; i < imax; ++i)
+    ret.push_back(mvCurveSegments[i].getStart());
+
+  ret.push_back(mvCurveSegments[i - 1].getEnd());
+
+  return ret;
+}
 
 void CLCurve::exportToSBML(Curve * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const
-  {
-    if (!c) return;
+{
+  if (!c) return;
 
-    //we will reconstruct the curve completely without checking the existing curve
-    unsigned C_INT32 i, imax = c->getListOfCurveSegments()->size();
-    for (i = 0; i < imax; ++i) delete c->getListOfCurveSegments()->remove(0);
+  //we will reconstruct the curve completely without checking the existing curve
+  unsigned C_INT32 i, imax = c->getListOfCurveSegments()->size();
 
-    //curve segments
-    imax = mvCurveSegments.size();
-    for (i = 0; i < imax; ++i)
-      {
-        if (mvCurveSegments[i].isBezier())
-          {
-            mvCurveSegments[i].exportToSBMLBezier(c->createCubicBezier(), copasimodelmap);
-          }
-        else
-          {
-            mvCurveSegments[i].exportToSBMLLineSegment(c->createLineSegment(), copasimodelmap);
-          }
-      }
-  }
+  for (i = 0; i < imax; ++i) delete c->getListOfCurveSegments()->remove(0);
+
+  //curve segments
+  imax = mvCurveSegments.size();
+
+  for (i = 0; i < imax; ++i)
+    {
+      if (mvCurveSegments[i].isBezier())
+        {
+          mvCurveSegments[i].exportToSBMLBezier(c->createCubicBezier(), copasimodelmap);
+        }
+      else
+        {
+          mvCurveSegments[i].exportToSBMLLineSegment(c->createLineSegment(), copasimodelmap);
+        }
+    }
+}
 
 std::ostream & operator<<(std::ostream &os, const CLCurve & c)
 {
@@ -165,8 +179,10 @@ std::ostream & operator<<(std::ostream &os, const CLCurve & c)
     {
       os << "      Curve:\n";
       C_INT32 i, imax = c.getNumCurveSegments();
+
       for (i = 0; i < imax; ++i)
         os << "        " << c.getCurveSegments()[i] << "\n";
     }
+
   return os;
 }
--- copasi/layout/CLCurve.h
+++ copasi/layout/CLCurve.h
@@ -1,10 +1,15 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/layout/CLCurve.h,v $
-//   $Revision: 1.14 $
+//   $Revision: 1.14.8.1 $
 //   $Name:  $
 //   $Author: ssahle $
-//   $Date: 2008/09/17 14:23:00 $
+//   $Date: 2010/05/10 14:10:56 $
 // End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
@@ -34,185 +39,191 @@ class LineSegment;
  * This class describes a line segment.
  */
 class CLLineSegment : public CLBase
+{
+protected:
+
+  CLPoint mStart;
+  CLPoint mEnd;
+
+  CLPoint mBase1;
+  CLPoint mBase2;
+
+  bool mIsBezier;
+
+public:
+
+  CLLineSegment()
+      : mStart(), mEnd(),
+      mBase1(), mBase2(),
+      mIsBezier(false) {};
+
+  /**
+   * Constructor for a straight line segment
+   */
+  CLLineSegment(const CLPoint & s, const CLPoint & e)
+      : mStart(s), mEnd(e),
+      mBase1(), mBase2(),
+      mIsBezier(false) {};
+
+  /**
+   * Constructor for a bezier segment
+   */
+  CLLineSegment(const CLPoint & s, const CLPoint & e,
+                const CLPoint & b1, const CLPoint & b2)
+      : mStart(s), mEnd(e),
+      mBase1(b1), mBase2(b2),
+      mIsBezier(true) {};
+
+  /**
+   * constructor from libsbml object
+   */
+  CLLineSegment(const LineSegment & ls);
+
+  const CLPoint & getStart() const {return mStart;};
+  const CLPoint & getEnd() const {return mEnd;};
+
+  void setStart(const CLPoint & p) {mStart = p;};
+  void setEnd(const CLPoint & p) {mEnd = p;};
+
+  const CLPoint & getBase1() const {return mBase1;};
+  const CLPoint & getBase2() const {return mBase2;};
+
+  void setBase1(const CLPoint & p) {mBase1 = p;};
+  void setBase2(const CLPoint & p) {mBase2 = p;};
+
+  bool isBezier() const {return mIsBezier;};
+  void setIsBezier(bool b) {mIsBezier = b;};
+
+  // if two lines are congruent, they are supposed to be ==
+  bool operator==(const CLLineSegment & rhs) const
   {
-  protected:
-
-    CLPoint mStart;
-    CLPoint mEnd;
-
-    CLPoint mBase1;
-    CLPoint mBase2;
-
-    bool mIsBezier;
-
-  public:
-
-    CLLineSegment()
-        : mStart(), mEnd(),
-        mBase1(), mBase2(),
-    mIsBezier(false) {};
-
-    /**
-     * Constructor for a straight line segment
-     */
-    CLLineSegment(const CLPoint & s, const CLPoint & e)
-        : mStart(s), mEnd(e),
-        mBase1(), mBase2(),
-    mIsBezier(false) {};
-
-    /**
-     * Constructor for a bezier segment
-     */
-    CLLineSegment(const CLPoint & s, const CLPoint & e,
-                  const CLPoint & b1, const CLPoint & b2)
-        : mStart(s), mEnd(e),
-        mBase1(b1), mBase2(b2),
-    mIsBezier(true) {};
-
-    /**
-     * constructor from libsbml object
-     */
-    CLLineSegment(const LineSegment & ls);
-
-    const CLPoint & getStart() const {return mStart;};
-    const CLPoint & getEnd() const {return mEnd;};
-
-    void setStart(const CLPoint & p) {mStart = p;};
-    void setEnd(const CLPoint & p) {mEnd = p;};
-
-    const CLPoint & getBase1() const {return mBase1;};
-    const CLPoint & getBase2() const {return mBase2;};
-
-    void setBase1(const CLPoint & p) {mBase1 = p;};
-    void setBase2(const CLPoint & p) {mBase2 = p;};
-
-    bool isBezier() const {return mIsBezier;};
-    void setIsBezier(bool b) {mIsBezier = b;};
-
-    // if two lines are congruent, they are supposed to be ==
-    bool operator==(const CLLineSegment & rhs) const
-      {
-        if (mIsBezier) return((mStart == rhs.mStart) && (mEnd == rhs.mEnd) && (mBase1 == rhs.mBase1) && (mBase2 == rhs.mBase2));
-        else return ((mStart == rhs.mStart) && (mEnd == rhs.mEnd));
-      };
-
-    // compare two line segments according to their start point (needed for use of line segment as a key in a map)
-    bool operator<(const CLLineSegment & rhs) const
-    {return (mStart < rhs.mStart);};
-
-    void scale (const double & scaleFactor)
-    {
-      mStart.scale(scaleFactor);mEnd.scale(scaleFactor);
-      if (mIsBezier) {mBase1.scale(scaleFactor);mBase2.scale(scaleFactor);}
-    }
-
-    /**
-     * This method writes the information of the copasi layout object into the
-     * corresponding sbml object. This is only guaranteed to work if
-     * isBezier() is true.
-     */
-    void exportToSBMLBezier(CubicBezier * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
-
-    /**
-     * This method writes the information of the copasi layout object into the
-     * corresponding sbml object. This is only guaranteed to work if
-     * isBezier() is false.
-     */
-    void exportToSBMLLineSegment(LineSegment * l, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
-
-    /**
-      * insert operator
-      */
-    friend std::ostream & operator<<(std::ostream &os, const CLLineSegment & ls);
+    if (mIsBezier) return((mStart == rhs.mStart) && (mEnd == rhs.mEnd) && (mBase1 == rhs.mBase1) && (mBase2 == rhs.mBase2));
+    else return ((mStart == rhs.mStart) && (mEnd == rhs.mEnd));
   };
 
-class CLCurve : public CLBase
+  // compare two line segments according to their start point (needed for use of line segment as a key in a map)
+  bool operator<(const CLLineSegment & rhs) const
+  {return (mStart < rhs.mStart);};
+
+  void scale(const double & scaleFactor)
   {
-  protected:
+    mStart.scale(scaleFactor); mEnd.scale(scaleFactor);
 
-    std::vector<CLLineSegment> mvCurveSegments;
+    if (mIsBezier) {mBase1.scale(scaleFactor); mBase2.scale(scaleFactor);}
+  }
 
-  public:
+  /**
+   * This method writes the information of the copasi layout object into the
+   * corresponding sbml object. This is only guaranteed to work if
+   * isBezier() is true.
+   */
+  void exportToSBMLBezier(CubicBezier * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
 
-    CLCurve()
-        : CLBase(), mvCurveSegments() {};
+  /**
+   * This method writes the information of the copasi layout object into the
+   * corresponding sbml object. This is only guaranteed to work if
+   * isBezier() is false.
+   */
+  void exportToSBMLLineSegment(LineSegment * l, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
 
-    /**
-     * copy constructor (should make deep copy)
-     */
-    CLCurve(const CLCurve & c);
+  /**
+    * insert operator
+    */
+  friend std::ostream & operator<<(std::ostream &os, const CLLineSegment & ls);
+};
 
-    /**
-     * constructor from libsbml object
-     */
-    CLCurve(const Curve & sbmlcurve);
+class CLCurve : public CLBase
+{
+protected:
 
-    ~CLCurve();
+  std::vector<CLLineSegment> mvCurveSegments;
 
-    const std::vector<CLLineSegment> & getCurveSegments() const {return mvCurveSegments;};
+public:
 
-    CLLineSegment* getSegmentAt(C_INT32 i)
-    {
-      if ((i >= 0) && ((unsigned C_INT32)i < mvCurveSegments.size()))return &(mvCurveSegments[i]);
-      else return NULL;
-    }
+  CLCurve()
+      : CLBase(), mvCurveSegments() {};
+
+  /**
+   * copy constructor (should make deep copy)
+   */
+  CLCurve(const CLCurve & c);
+
+  /**
+   * constructor from libsbml object
+   */
+  CLCurve(const Curve & sbmlcurve);
+
+  ~CLCurve();
+
+  const std::vector<CLLineSegment> & getCurveSegments() const {return mvCurveSegments;};
+
+  CLLineSegment* getSegmentAt(C_INT32 i)
+  {
+    if ((i >= 0) && ((unsigned C_INT32)i < mvCurveSegments.size()))return &(mvCurveSegments[i]);
+    else return NULL;
+  }
 
   C_INT32 getNumCurveSegments() const {return mvCurveSegments.size();};
 
-    void clear();
+  void clear();
 
-    void addCurveSegment(CLLineSegment & ls);
-    void addCurveSegment(CLLineSegment * pLs);
+  /**
+   * add a copy of the line segment to the curve
+   */
+  void addCurveSegment(CLLineSegment * pLs);
 
-    /**
-     * this method tells whether the curve is a continuous
-     * set of line segments, i.e. if the end point of one
-     * line segment is identical to the starting point of the
-     * following one.
-     */
-    bool isContinuous() const;
+  /**
+   * this method tells whether the curve is a continuous
+   * set of line segments, i.e. if the end point of one
+   * line segment is identical to the starting point of the
+   * following one.
+   */
+  bool isContinuous() const;
 
-    /**
-     * This method should only be called if isContinuous() is true.
-     * It returns the curve as a list of points rather than a list
-     * of line segments.
-     */
-    std::vector <CLPoint> getListOfPoints() const;
+  /**
+   * This method should only be called if isContinuous() is true.
+   * It returns the curve as a list of points rather than a list
+   * of line segments.
+   */
+  std::vector <CLPoint> getListOfPoints() const;
 
-    /**
-     * Two curves are supposed to be equal iff all there line segments
-     * including their order are equal
-     */
-    bool operator==(const CLCurve & rhs) const
+  /**
+   * Two curves are supposed to be equal iff all there line segments
+   * including their order are equal
+   */
+  bool operator==(const CLCurve & rhs) const
+  {
+    bool result = true;
+    unsigned int i;
+
+    for (i = 0; i < mvCurveSegments.size(); i++)
       {
-        bool result = true;
-        unsigned int i;
-        for (i = 0;i < mvCurveSegments.size();i++)
-          {
-            result = result && (mvCurveSegments[i] == rhs.mvCurveSegments[i]);
-          }
-        return result;
+        result = result && (mvCurveSegments[i] == rhs.mvCurveSegments[i]);
       }
 
-    void scale (const double & scaleFactor)
-    {
-      unsigned int i; // scale all segments
-      for (i = 0;i < mvCurveSegments.size();i++)
-        {
-          mvCurveSegments[i].scale(scaleFactor);
-        }
-    }
+    return result;
+  }
 
-    /**
-     * This method writes the information of the copasi layout object into the
-     * corresponding sbml object
-     */
-    void exportToSBML(Curve * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
+  void scale(const double & scaleFactor)
+  {
+    unsigned int i; // scale all segments
 
-    /**
-      * insert operator
-      */
-    friend std::ostream & operator<<(std::ostream &os, const CLCurve & c);
-  };
+    for (i = 0; i < mvCurveSegments.size(); i++)
+      {
+        mvCurveSegments[i].scale(scaleFactor);
+      }
+  }
+
+  /**
+   * This method writes the information of the copasi layout object into the
+   * corresponding sbml object
+   */
+  void exportToSBML(Curve * c, const std::map<CCopasiObject*, SBase*> & copasimodelmap) const;
+
+  /**
+    * insert operator
+    */
+  friend std::ostream & operator<<(std::ostream &os, const CLCurve & c);
+};
 
 #endif
