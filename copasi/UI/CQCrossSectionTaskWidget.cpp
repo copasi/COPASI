9de67582240f14539b49099dafe1fa303f38c5e3
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2010-05-26 12:08:53 +0000

    Add Cross Section Task

---
+++ copasi/UI/CQCrossSectionTaskWidget.cpp
@@ -0,0 +1,122 @@
+// Begin CVS Header
+//   $Source: /fs/turing/cvs/copasi_dev/cvs_admin/addHeader,v $
+//   $Revision: 1.15 $
+//   $Name:  $
+//   $Author: shoops $
+//   $Date: 2010/04/27 16:00:44 $
+// End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
+
+/*
+ *  Created by Paul on 5/21/10.
+ */
+
+#include "copasi.h"
+
+#include "CQCrossSectionTaskWidget.h"
+#include "listviews.h"
+#include "CQTaskBtnWidget.h"
+#include "CQTaskHeaderWidget.h"
+
+#include "crosssection/CCrossSectionTask.h"
+#include "crosssection/CCrossSectionProblem.h"
+
+/*
+ *  Constructs a CQCrossSectionWidget which is a child of 'parent', with the
+ *  name 'name'.'
+ */
+CQCrossSectionTaskWidget::CQCrossSectionTaskWidget(QWidget* parent, const char* name)
+    : TaskWidget(parent, name)
+{
+  setupUi(this);
+
+  init();
+}
+
+/*
+ *  Destroys the object and frees any allocated resources
+ */
+CQCrossSectionTaskWidget::~CQCrossSectionTaskWidget()
+{
+  destroy();
+  // no need to delete child widgets, Qt does it all for us
+}
+
+/*
+ *  Sets the strings of the subwidgets using the current
+ *  language.
+ */
+void CQCrossSectionTaskWidget::languageChange()
+{
+  retranslateUi(this);
+}
+
+void CQCrossSectionTaskWidget::init()
+{
+  mpHeaderWidget->setTaskName("Cross Section");
+
+  crossSectionTWLayout->insertWidget(0, mpHeaderWidget); // header
+  crossSectionTWLayout->insertSpacing(1, 14);      // space between header and body
+  crossSectionTWLayout->addWidget(mpBtnWidget);      // 'footer'
+
+  addMethodParameterTable(0);
+}
+
+bool CQCrossSectionTaskWidget::runTask()
+{
+}
+
+bool CQCrossSectionTaskWidget::update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key)
+{
+}
+
+/*
+ * Function to save actual changes of the Task
+ */
+bool CQCrossSectionTaskWidget::saveTask()
+{
+  // check the existence of task
+  CCrossSectionTask * pTask =
+    dynamic_cast< CCrossSectionTask * >(mpTask);
+
+  if (!pTask) return false;
+
+  // save functions from the Parent, TaskWidget
+  saveCommon();
+  saveMethod();
+
+  // check the existence of problem
+  CCrossSectionProblem* pProblem =
+    dynamic_cast<CCrossSectionProblem *>(pTask->getProblem());
+  assert(pProblem);
+
+  // save the actual changes
+
+  return true;
+}
+
+/*
+ * Function to load saved values of the Task
+ */
+bool CQCrossSectionTaskWidget::loadTask()
+{
+  /*
+    CCrossSectionTask * pTask =
+      dynamic_cast< CCrossSectionTask * >(mpTask);
+
+    if (!pTask) return false;
+  */
+  // load functions from the Parent, TaskWidget
+  loadCommon();
+  loadMethod();
+
+  return true;
+}
+
+CCopasiMethod * CQCrossSectionTaskWidget::createMethod(const CCopasiMethod::SubType & type)
+{
+}
---
+++ copasi/UI/CQCrossSectionTaskWidget.h
@@ -0,0 +1,51 @@
+// Begin CVS Header
+//   $Source: /fs/turing/cvs/copasi_dev/cvs_admin/addHeader,v $
+//   $Revision: 1.15 $
+//   $Name:  $
+//   $Author: shoops $
+//   $Date: 2010/04/27 16:00:44 $
+// End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
+
+/*
+ *  Created by Paul on 5/21/10.
+ */
+
+#ifndef CQCROSSSECTIONTASKWIDGET_H
+#define CQCROSSSECTIONTASKWIDGET_H
+
+#include <qvariant.h>
+
+#include "ui_CQCrossSectionTaskWidget.h"
+
+#include "TaskWidget.h"
+
+class CQCrossSectionTaskWidget : public TaskWidget, public Ui::CQCrossSectionTaskWidget
+{
+  Q_OBJECT
+
+public:
+  CQCrossSectionTaskWidget(QWidget* parent = 0, const char* name = 0);
+  ~CQCrossSectionTaskWidget();
+
+  virtual bool runTask();
+  virtual bool update(ListViews::ObjectType objectType, ListViews::Action action, const std::string & key);
+
+protected:
+  virtual bool saveTask();
+  virtual bool loadTask();
+//  virtual bool taskFinishedEvent();
+  virtual CCopasiMethod * createMethod(const CCopasiMethod::SubType & type);
+
+protected slots:
+  virtual void languageChange();
+
+private:
+  void init();
+};
+
+#endif
