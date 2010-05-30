adc2e429b39762e152db676379f07c772cce47ae
Paul Willy <paul.willy@bioquant.uni-heidelberg.de>
2010-05-30 15:23:55 +0000

    Add some icons for expression/formula editor.

--- copasi/UI/CQIcons.h
+++ copasi/UI/CQIcons.h
@@ -1,10 +1,15 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CQIcons.h,v $
-//   $Revision: 1.3 $
+//   $Revision: 1.4 $
 //   $Name:  $
-//   $Author: ssahle $
-//   $Date: 2009/12/17 02:47:12 $
+//   $Author: pwilly $
+//   $Date: 2010/05/30 15:23:55 $
 // End CVS Header
+
+// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
 
 // Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
@@ -27,6 +32,9 @@ public:
     SelectObject,
     Add,
     Delete,
+    View,
+    Edit,
+    Save,
     unknown_ID
   };
 
