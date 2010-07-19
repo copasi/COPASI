b81b310d0a3a73bec8c15e737d125385f6aa152b
Stefan Hoops <shoops@vbi.vt.edu>
2010-07-19 18:59:46 +0000

    Completely rewritten to minimize memory usage so that large files can be processed.

--- stochastic-testsuite/calculate_statistics.py
+++ stochastic-testsuite/calculate_statistics.py
@@ -8,6 +8,8 @@ if(len(sys.argv)!=6):
     print "Wrong number of arguments."
     sys.exit(1)
 
+SEPARATOR=","
+
 INPUTFILE=sys.argv[1]
 
 MEAN_OUTFILE=sys.argv[2]
@@ -22,71 +24,68 @@ INPUT=file(INPUTFILE,"r").readlines()
 
 if(len(INPUT) != (NUM_REPEATS * (NUM_STEPS + 3) + 1)):
     print "Wrong number of data points."
+    print "Found:     %d"%len(INPUT)
+    print "Expecting: %d"%(NUM_REPEATS * (NUM_STEPS + 3) + 1)
     sys.exit(1)
 
 DATA=[]
 LINENUMBER=1
-NUMCOLUMNS=len(string.split(INPUT[1], ","))
+NUMCOLUMNS=len(string.split(INPUT[1], SEPARATOR))
 MEAN=[]
+SD=[]
 
 HEADER=INPUT[0]
 
-for X in range(0,NUM_STEPS+1):
-    MEAN.append([])
-    for Y in range(0,NUMCOLUMNS - 1):
-        MEAN[X].append(0)
-
-for X in range(0,NUM_REPEATS):
-   SET=[]
-   for Y in range(0, NUM_STEPS+1):
-     LINE=INPUT[LINENUMBER]
-     COLUMNS=string.split(LINE, ",")
-     if(len(COLUMNS)!=NUMCOLUMNS):
-        print "Wrong number of elements on  line %d"%(LINENUMBER)
-        sys.exit(1)
-     ROW=[]
-     for Z in range(0,len(COLUMNS)-1):
-        v=float(COLUMNS[Z+1])
-        ROW.append(v)
-        MEAN[Y][Z]=MEAN[Y][Z]+v
-     SET.append(ROW)   
-     LINENUMBER=LINENUMBER+1
-   DATA.append(SET)  
-   LINENUMBER=LINENUMBER+2  # skip the empty line
-
-MEANOUT=file(MEAN_OUTFILE,"w")   
+MEANOUT=file(MEAN_OUTFILE,"w")
 MEANOUT.write(HEADER)
-for X in range(0,NUM_STEPS+1):
-    line=string.join([str(X)],",")
-    for Y in range(0,NUMCOLUMNS-1):
-        MEAN[X][Y]=MEAN[X][Y]/NUM_REPEATS
-        line=string.join([line,str(MEAN[X][Y])],",")
-    line=line+"\n"
-    MEANOUT.write(line)
-MEANOUT.close()    
-
-SD=[]
-for X in range(0,NUM_STEPS+1):
-    SD.append([])
-    for Y in range(0,NUMCOLUMNS-1):
-        SD[X].append(0)
-
-
-for X in range(0,NUM_REPEATS):
-    for Y in range(0,NUM_STEPS+1):
-        for Z in range(0,NUMCOLUMNS-1):
-            SD[Y][Z]=SD[Y][Z]+math.pow((DATA[X][Y][Z]-MEAN[Y][Z]),2)
-
 
 SDOUT=file(SD_OUTFILE,"w")   
 SDOUT.write(HEADER)
+
+# Allocate the memory
+for Y in range(0,NUMCOLUMNS - 1):
+    MEAN.append(0)
+    SD.append(0)
+
+# To save memory we process the data by rows
 for X in range(0,NUM_STEPS+1):
-    line=string.join([str(X)],",")
+
+    # Set Mean and SD to 0;
+    for Y in range(0,NUMCOLUMNS - 1):
+        MEAN[Y]=0
+        SD[Y]=0
+
+    for Z in range(0,NUM_REPEATS - 1):
+        LINE=INPUT[X + 1 + Z * (NUM_STEPS + 3)]
+
+        COLUMNS=string.split(LINE, SEPARATOR)
+        if(len(COLUMNS)!=NUMCOLUMNS):
+            print "Wrong number of elements on  line %d"%(LINENUMBER)
+            sys.exit(1)
+
+        for Y in range(0,len(COLUMNS)-1):
+            v=float(COLUMNS[Y+1])
+            DELTA=v-MEAN[Y]
+            MEAN[Y]=MEAN[Y]+DELTA/(Z+1)
+
+            # This uses the new mean, i.e., not DELTA * DELTA
+            SD[Y]=SD[Y]+DELTA*(v-MEAN[Y]);
+    
+    line=COLUMNS[0]
     for Y in range(0,NUMCOLUMNS-1):
-        SD[X][Y]=math.sqrt(SD[X][Y]/NUM_REPEATS)
-        line=string.join([line,str(SD[X][Y])],",")
+        line=string.join([line,str(MEAN[Y])],",")
     line=line+"\n"
+
+    MEANOUT.write(line)
+
+    line=COLUMNS[0]
+    for Y in range(0,NUMCOLUMNS-1):
+        line=string.join([line,str(math.sqrt(SD[Y]/NUM_REPEATS))],",")
+    line=line+"\n"
+
     SDOUT.write(line)
+
+MEANOUT.close()    
 SDOUT.close()    
 
     
