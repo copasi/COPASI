505a0c9289c11c983305e8d184beb7d673eefcb2
Stefan Hoops <shoops@vbi.vt.edu>
2010-11-12 17:02:58 +0000

    Fixed confirmDelete to display quantity instead of species.

--- copasi/UI/CQModelValue.cpp
+++ copasi/UI/CQModelValue.cpp
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQModelValue.cpp,v $
-//   $Revision: 1.15.4.4 $
+//   $Revision: 1.15.4.5 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2010/09/30 17:02:31 $
+//   $Date: 2010/11/12 17:02:58 $
 // End CVS Header
 
 // Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
@@ -115,7 +115,7 @@ void CQModelValue::slotBtnDelete()
     return;
 
   QMessageBox::StandardButton choice =
-    CQMessageBox::confirmDelete(this, "species",
+    CQMessageBox::confirmDelete(this, "quantity",
                                 FROM_UTF8(mpModelValue->getObjectName()),
                                 mpModelValue->getDeletedObjects());
 
