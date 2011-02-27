af54d8f3eb2981c03388347d05c2512f4806c510
Ralph Gauges <gauges@hs-albsig.de>
2011-02-27 17:50:38 +0000

    Fix for Bug 1585

--- copasi/sbml/CSBMLExporter.cpp
+++ copasi/sbml/CSBMLExporter.cpp
@@ -1,12 +1,12 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/sbml/CSBMLExporter.cpp,v $
-//   $Revision: 1.84.2.4 $
+//   $Revision: 1.84.2.5 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2011/01/12 19:05:47 $
+//   $Author: gauges $
+//   $Date: 2011/02/27 17:50:38 $
 // End CVS Header
 
-// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -2685,7 +2685,7 @@ const std::string CSBMLExporter::exportModelToString(CCopasiDataModel& dataModel
 
   if (this->mpSBMLDocument && this->mpSBMLDocument->getModel())
     dataModel.getListOfLayouts()->exportToSBML(this->mpSBMLDocument->getModel()->getListOfLayouts(),
-        dataModel.getCopasi2SBMLMap(), mIdMap);
+        this->mCOPASI2SBMLMap, mIdMap);
 
 #ifdef COPASI_DEBUG
 
