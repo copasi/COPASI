25dffbbe7bdc473c8d256bc4211a908889b6808d
Stefan Hoops <shoops@vbi.vt.edu>
2011-03-07 22:16:01 +0000

    Nerged changes from Build-33 into HEAD

--- copasi/compareExpressions/unittests/test_biomodels217_expression.h
+++ copasi/compareExpressions/unittests/test_biomodels217_expression.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/compareExpressions/unittests/test_biomodels217_expression.h,v $
-//   $Revision: 1.2 $
+//   $Revision: 1.3 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2011/03/07 19:26:44 $
+//   $Date: 2011/03/07 22:16:01 $
 // End CVS Header
 
 // Copyright (C) 2011 by Pedro Mendes, Virginia Tech Intellectual
@@ -37,6 +37,21 @@ class test_biomodels217_expression : public CppUnit::TestFixture
   CPPUNIT_TEST(test_term_den5);
   CPPUNIT_TEST(test_term_den6);
   CPPUNIT_TEST(test_term_den7);
+
+  CPPUNIT_TEST(test_term_den12);
+  CPPUNIT_TEST(test_term_den123);
+  CPPUNIT_TEST(test_term_den1234);
+  CPPUNIT_TEST(test_term_den12345);
+  CPPUNIT_TEST(test_term_den123456);
+  CPPUNIT_TEST(test_term_den1234567);
+  CPPUNIT_TEST(test_term_den67);
+  CPPUNIT_TEST(test_term_den56);
+  CPPUNIT_TEST(test_term_den456);
+  CPPUNIT_TEST(test_term_den3456);
+  CPPUNIT_TEST(test_term_den356);
+  CPPUNIT_TEST(test_term_den346);
+  CPPUNIT_TEST(test_term_den36);
+
   CPPUNIT_TEST_SUITE_END();
 
 protected:
@@ -80,6 +95,22 @@ public:
   void test_term_den5();
   void test_term_den6();
   void test_term_den7();
+
+  void test_term_den12();
+  void test_term_den123();
+  void test_term_den1234();
+  void test_term_den12345();
+  void test_term_den123456();
+  void test_term_den1234567();
+  void test_term_den67();
+  void test_term_den56();
+  void test_term_den456();
+  void test_term_den3456();
+  void test_term_den356();
+  void test_term_den346();
+  void test_term_den36();
+
+
 };
 
 #endif // TEST_BIOMODELS217_EXPRESSION_H_
