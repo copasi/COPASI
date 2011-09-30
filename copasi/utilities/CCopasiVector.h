eddee8a41e50e2aebc6eb3cd29694db8a036b649
Stefan Hoops <shoops@vbi.vt.edu>
2011-09-30 16:50:50 +0000

    Fixed comparison problem in getIndex which created problems for objects with multiple inheritance.

--- copasi/utilities/CCopasiVector.h
+++ copasi/utilities/CCopasiVector.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/CCopasiVector.h,v $
-//   $Revision: 1.88 $
+//   $Revision: 1.89 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2011/03/21 15:48:17 $
+//   $Date: 2011/09/30 16:50:50 $
 // End CVS Header
 
 // Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
@@ -461,7 +461,14 @@ public:
     const_iterator Target = begin();
 
     for (i = 0; i < imax; i++, Target++)
-      if (*Target == (void *) pObject) return i;
+      {
+        const CCopasiObject * pTarget = static_cast< const CCopasiObject * >(*Target);
+
+        if (pTarget == pObject)
+          {
+            return i;
+          }
+      }
 
     return C_INVALID_INDEX;
   }
