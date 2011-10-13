2edb0da5f37732f7d2e84e15211498a3a34225e6
Stefan Hoops <shoops@vbi.vt.edu>
2011-10-13 17:23:43 +0000

    Added icon for tool button ...

--- copasi/UI/CQIcons.h
+++ copasi/UI/CQIcons.h
@@ -1,9 +1,9 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CQIcons.h,v $
-//   $Revision: 1.5 $
+//   $Revision: 1.6 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2011/03/07 19:37:49 $
+//   $Date: 2011/10/13 17:23:43 $
 // End CVS Header
 
 // Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
@@ -38,6 +38,7 @@ public:
     Save,
     Up,
     Down,
+    Tool,
     unknown_ID
   };
 
