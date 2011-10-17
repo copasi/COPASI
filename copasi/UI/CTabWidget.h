bcf1e1e1970c509dbd2da6d887ddeea9abba1990
Ralph Gauges <gauges@hs-albsig.de>
2011-10-17 12:35:45 +0000

    Added missing include

--- copasi/UI/CTabWidget.h
+++ copasi/UI/CTabWidget.h
@@ -1,12 +1,12 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/Attic/CTabWidget.h,v $
-//   $Revision: 1.15 $
+//   $Revision: 1.16 $
 //   $Name:  $
-//   $Author: shoops $
-//   $Date: 2010/07/16 19:05:17 $
+//   $Author: gauges $
+//   $Date: 2011/10/17 12:35:45 $
 // End CVS Header
 
-// Copyright (C) 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -22,6 +22,8 @@
 
 #ifndef CTABWIDGET_H
 #define CTABWIDGET_H
+
+#include <vector>
 
 #include "copasi/UI/listviews.h"
 
