1cf68f374684fe920254c3ffa177cc9f692c96c1
Stefan Hoops <shoops@vbi.vt.edu>
2011-11-09 16:45:26 +0000

    Fixed Bug 1677. The characters & and < are now encoded for all names.

--- copasi/utilities/copasimathml.h
+++ copasi/utilities/copasimathml.h
@@ -1,12 +1,17 @@
 /* Begin CVS Header
    $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/utilities/copasimathml.h,v $
-   $Revision: 1.4 $
+   $Revision: 1.5 $
    $Name:  $
    $Author: shoops $
-   $Date: 2006/10/06 16:03:50 $
+   $Date: 2011/11/09 16:45:26 $
    End CVS Header */
 
-// Copyright © 2005 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2011 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -17,59 +22,68 @@
 //#include "model/CMetab.h"
 #include "model/CCompartment.h"
 #include "report/CCopasiObjectReference.h"
+#include "xml/CCopasiXMLInterface.h"
 
 #define SPC(level) std::string(level, ' ')
 
 class CMathMl
+{
+public:
+
+  static std::string fixName(const std::string & name)
   {
-  public:
+    std:: string Fixed;
 
-    static std::string fixName(const std::string & name)
-    {
-      if (name.find_first_of(" ()*+-") == std::string::npos)
-        return name;
-      else
-        return "\"" + name + "\"";
-    }
+    if (name.find_first_of(" ()*+-") == std::string::npos)
+      Fixed = name;
+    else
+      Fixed = "\"" + name + "\"";
 
-    static std::string getMMLName(const CCopasiObject* obj)
-    {
-      if (!obj) return "no object";
+    return CCopasiXMLInterface::encode(Fixed, CCopasiXMLInterface::character);
+  }
 
-      //model value
-      const CModelValue* mv = dynamic_cast<const CModelValue*>(obj);
-      if (mv) return "<mi>" + fixName(mv->getObjectName()) + "</mi>";
+  static std::string getMMLName(const CCopasiObject* obj)
+  {
+    if (!obj) return "no object";
 
-      //metabolite
-      //const CMetab* met = dynamic_cast<const CMetab*>(obj);
-      //if (met) return "<mi>[" + met->getObjectName() + "]</mi>";
+    //model value
+    const CModelValue* mv = dynamic_cast<const CModelValue*>(obj);
 
-      //compartment
-      const CCompartment* comp = dynamic_cast<const CCompartment*>(obj);
-      if (comp) return "<msub><mi>V</mi><mi>" + comp->getObjectName()
-        + "</mi></msub>";
+    if (mv) return "<mi>" + fixName(mv->getObjectName()) + "</mi>";
 
-      //reference
-      if (obj->getObjectType() == "Reference")
-        {
-          //model value
-          if (obj->getObjectName() == "Value")
-            {
-              mv = dynamic_cast<const CModelValue*>(obj->getObjectParent());
-              if (mv) return "<mi>" + fixName(mv->getObjectName()) + "</mi>";
-            }
+    //metabolite
+    //const CMetab* met = dynamic_cast<const CMetab*>(obj);
+    //if (met) return "<mi>[" + met->getObjectName() + "]</mi>";
 
-          //compartment
-          if (obj->getObjectName() == "Volume")
-            {
-              comp = dynamic_cast<const CCompartment*>(obj->getObjectParent());
-              if (comp) return "<msub><mi>V</mi><mi>" + comp->getObjectName()
-                + "</mi></msub>";
-            }
-        }
+    //compartment
+    const CCompartment* comp = dynamic_cast<const CCompartment*>(obj);
 
-      return "<mi>" + fixName(obj->getObjectDisplayName()) + "</mi>";
-    }
-  };
+    if (comp) return "<msub><mi>V</mi><mi>" + fixName(comp->getObjectName())
+                       + "</mi></msub>";
+
+    //reference
+    if (obj->getObjectType() == "Reference")
+      {
+        //model value
+        if (obj->getObjectName() == "Value")
+          {
+            mv = dynamic_cast<const CModelValue*>(obj->getObjectParent());
+
+            if (mv) return "<mi>" + fixName(mv->getObjectName()) + "</mi>";
+          }
+
+        //compartment
+        if (obj->getObjectName() == "Volume")
+          {
+            comp = dynamic_cast<const CCompartment*>(obj->getObjectParent());
+
+            if (comp) return "<msub><mi>V</mi><mi>" + fixName(comp->getObjectName())
+                               + "</mi></msub>";
+          }
+      }
+
+    return "<mi>" + fixName(obj->getObjectDisplayName()) + "</mi>";
+  }
+};
 
 #endif
