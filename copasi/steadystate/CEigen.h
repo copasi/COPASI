7f4ba29bb5b028377eba93fdfdbcfacf53d45b47
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2011-11-11 16:44:25 +0000

    implement a number of oscillation/bifurcation test functions calculated from eigenvalues. They are made accessible as object references.

--- copasi/steadystate/CEigen.h
+++ copasi/steadystate/CEigen.h
@@ -1,12 +1,17 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/steadystate/CEigen.h,v $
-//   $Revision: 1.30 $
+//   $Revision: 1.30.24.1 $
 //   $Name:  $
 //   $Author: ssahle $
-//   $Date: 2007/07/02 16:11:37 $
+//   $Date: 2011/11/11 16:44:25 $
 // End CVS Header
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2011 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -32,305 +37,332 @@
 #include "report/CCopasiContainer.h"
 
 class CEigen: public CCopasiContainer
-  {
-  private:
-    // variables for stability analysis
+{
+private:
+  // variables for stability analysis
 
-    /**
-     * the real part of the maximum eigenvalue
-     */
-    C_FLOAT64 mMaxrealpart;
+  /**
+   * the real part of the maximum eigenvalue
+   */
+  C_FLOAT64 mMaxrealpart;
 
-    /**
-     * the imaginary part of the maximum eigenvalue
-     */
-    C_FLOAT64 mMaximagpart;
+  /**
+   * the imaginary part of the maximum eigenvalue
+   */
+  C_FLOAT64 mMaximagpart;
 
-    /**
-     * the number of eigenvalues with positive real part
-     */
-    C_INT32 mNposreal;
+  /**
+   * the number of eigenvalues with positive real part
+   */
+  C_INT32 mNposreal;
 
-    /**
-     * the number of eigenvalues with negative real part
-     */
-    C_INT32 mNnegreal;
+  /**
+   * the number of eigenvalues with negative real part
+   */
+  C_INT32 mNnegreal;
 
-    /**
-     * the number of real eigenvalues
-     */
-    C_INT32 mNreal;
+  /**
+   * the number of real eigenvalues
+   */
+  C_INT32 mNreal;
 
-    /**
-     * the number of imaginary eigenvalue numbers
-     */
-    C_INT32 mNimag;
+  /**
+   * the number of imaginary eigenvalue numbers
+   */
+  C_INT32 mNimag;
 
-    /**
-     *
-     */
-    C_INT32 mNcplxconj;
+  /**
+   *
+   */
+  C_INT32 mNcplxconj;
 
-    /**
-     * the number of eigenvalues with value of zero
-     */
-    C_INT32 mNzero;
+  /**
+   * the number of eigenvalues with value of zero
+   */
+  C_INT32 mNzero;
 
-    /**
-     * the stiffness of eigenvalues
-     */
-    C_FLOAT64 mStiffness;
+  /**
+   * the stiffness of eigenvalues
+   */
+  C_FLOAT64 mStiffness;
 
-    /**
-     * the hierary of the eigenvalues
-     */
-    C_FLOAT64 mHierarchy;
+  /**
+   * the hierary of the eigenvalues
+   */
+  C_FLOAT64 mHierarchy;
 
-    /**
-     * largest real part of a complex eigenvalue.
-     * If there is no complex eigenvalue it will be the smallest ev.
-     */
-    C_FLOAT64 mMaxRealOfComplex;
+  /**
+   * largest real part of a complex eigenvalue.
+   * If there is no complex eigenvalue it will be the smallest ev.
+   */
+  C_FLOAT64 mMaxRealOfComplex;
 
-    /**
-     * imaginary part of complex with largest real part
-     */
-    C_FLOAT64 mImagOfMaxComplex;
+  /**
+   * imaginary part of complex with largest real part
+   */
+  C_FLOAT64 mImagOfMaxComplex;
 
-    /**
-     * frequency of complex with largest real part
-     */
-    C_FLOAT64 mFreqOfMaxComplex;
+  /**
+   * frequency of complex with largest real part
+   */
+  C_FLOAT64 mFreqOfMaxComplex;
 
-    /**
-     * An index that is supposed to indicate the presence of oscillations
-     */
-    C_FLOAT64 mOscillationIndicator;
+  /**
+   * An index that is supposed to indicate the presence of oscillations
+   */
+  C_FLOAT64 mOscillationIndicator;
 
-    /**
-     * The resolution of needed for the stability analysis
-     */
-    C_FLOAT64 mResolution;
+  /**
+   * A heuristical index that is supposed to indicate the presence of oscillations, based on Eigenvalues
+   */
+  C_FLOAT64 mOscillationIndicator_EV;
 
-    //there are 15 parameters in the dgees() subroutine
+  /**
+   * A bifurcation test function, it is 0 for a Hopf-bifurcation. Definition according to Kuznetsov.
+   * This is only a necessary, not a sufficient condition for a H.-bifurcation
+   */
+  C_FLOAT64 mBifurcationIndicator_Hopf;
 
-    /**
-     * #1: (input) characer*1
-     * = 'N': Schur vectors are not computed
-     * = 'V': Schur vectors are computed
-     */
-    char mJobvs;
+  /**
+   * A bifurcation test function, it is 0 for a Fold-type bifurcation.
+   */
+  C_FLOAT64 mBifurcationIndicator_Fold;
 
-    /**
-     * #2: (input) characer*1
-     * = 'N': Eigenvalues are not ordered
-     * = 'S': Eigenvalues are ordered
-     */
-    char mSort;
+  /**
+   * A bifurcation test function, defined according to the "Bifurcation Discovery Tool", Chickarmane et al 2005
+   */
+  C_FLOAT64 mBifurcationIndicator_Hopf_BDT;
 
-    /**
-     * #3: (input) Logical function of two double precision arguments
-     * It must be declared external
-     * = 'S': Select is used to select eigenvalues to sort to the top left
-     * of the Schur form
-     * = 'N': Eigenvalues are ordered Select is not refereced.
-     */
-    C_INT32 * mpSelect;
+  /**
+   * A bifurcation test function, defined according to the "Bifurcation Discovery Tool", Chickarmane et al 2005
+   */
+  C_FLOAT64 mBifurcationIndicator_Fold_BDT;
 
-    /**
-     * #4: (input) The order of the matrix A
-     */
-    C_INT mN;
 
-    /**
-     * #5: (input/output) The double precision array, dimension (LDA,N)
-     * On entry, the N-by-N matrix A
-     * On exit, A has been overwritten by its real Schur form T
-     * Use a pointer variable here instead of array since we don't know
-     * the dimension yet.
-     */
-    CMatrix< C_FLOAT64 > mA;
+  /**
+   * The resolution of needed for the stability analysis
+   */
+  C_FLOAT64 mResolution;
 
-    /**
-     * #6: (input) The leading dimension of the array A. LDA >= max(1,N)
-     */
-    C_INT mLDA;
+  //there are 15 parameters in the dgees() subroutine
 
-    /**
-     * #7: (output) an integer
-     * if Sort = 'N', its value is 0
-     * if Sort = 'S', its value = number of eigenvalues (after sorting)
-     *                for which mSelect is true.
-     */
-    C_INT mSdim;
+  /**
+   * #1: (input) characer*1
+   * = 'N': Schur vectors are not computed
+   * = 'V': Schur vectors are computed
+   */
+  char mJobvs;
 
-    /**
-     * #8: array with dimension (mN)
-     */
-    CVector< C_FLOAT64 > mR;
+  /**
+   * #2: (input) characer*1
+   * = 'N': Eigenvalues are not ordered
+   * = 'S': Eigenvalues are ordered
+   */
+  char mSort;
 
-    /**
-     * #9: array with dimension (mN)
-     */
-    CVector< C_FLOAT64 > mI;
+  /**
+   * #3: (input) Logical function of two double precision arguments
+   * It must be declared external
+   * = 'S': Select is used to select eigenvalues to sort to the top left
+   * of the Schur form
+   * = 'N': Eigenvalues are ordered Select is not refereced.
+   */
+  C_INT32 * mpSelect;
 
-    /**
-     * #10: (output) array with dimension (mLdvs, mN)
-     * If mJobvs='V', mVS contains the orthogoanl matrix Z of Schur vectors
-     * If mJobvs='N', mVS is not referenced
-     */
-    C_FLOAT64 * mpVS;
+  /**
+   * #4: (input) The order of the matrix A
+   */
+  C_INT mN;
 
-    /**
-     * #11: an integer, the leading dimension of the array VS. mLdvs >= 1;
-     * if mJobvs='V', mLdvs >= N.
-     */
-    C_INT mLdvs;
+  /**
+   * #5: (input/output) The double precision array, dimension (LDA,N)
+   * On entry, the N-by-N matrix A
+   * On exit, A has been overwritten by its real Schur form T
+   * Use a pointer variable here instead of array since we don't know
+   * the dimension yet.
+   */
+  CMatrix< C_FLOAT64 > mA;
 
-    /**
-     * #12: (workspace/output) double precision array, dimension (mLWork)
-     * On exit, if mInfo=0; mWork(1) contains the optimal mLWork
-     */
-    CVector< C_FLOAT64 > mWork;
+  /**
+   * #6: (input) The leading dimension of the array A. LDA >= max(1,N)
+   */
+  C_INT mLDA;
 
-    /**
-     * #13: (input) Dimension of array Work, its value >= max(1,3*mN).
-     * For good performance, it must generally be larger
-     */
-    C_INT mLWork;
+  /**
+   * #7: (output) an integer
+   * if Sort = 'N', its value is 0
+   * if Sort = 'S', its value = number of eigenvalues (after sorting)
+   *                for which mSelect is true.
+   */
+  C_INT mSdim;
 
-    /**
-     * #14: (workspace) Logical array, dimension (N)
-     * Not referenced if mSort = 'N'
-     */
-    C_INT * mpBWork;
+  /**
+   * #8: array with dimension (mN)
+   */
+  CVector< C_FLOAT64 > mR;
 
-    /**
-     * #15: (output) an integer
-     * =0: successful exit
-     * <0: if mInfo=-i, the ith argument had an illegal value
-     * >0: if mInfo=i, and i is
-     *     <=N: the QR algorithm failed to compute all the eigenvalues;
-     *     =N+1: the eigenvalues could not be reordered because some
-     *           eigenvalues were too close to separate (ill-conditioned)
-     *     =N+2: after reordering, roundoff changed values of some
-     *           complex eigenvalues so that leading eigenvalues in the
-     *           Schur form no longer satisfy mSelect=.True. This could
-     *           caused by underflow due to scaling
-     */
-    C_INT mInfo;
+  /**
+   * #9: array with dimension (mN)
+   */
+  CVector< C_FLOAT64 > mI;
 
-  public:
-    /**
-     * Default constructor
-     * @param const std::string & name (default: "NoName")
-     * @param const CCopasiContainer * pParent (default: NULL)
-     */
-    CEigen(const std::string & name = "NoName",
-           const CCopasiContainer * pParent = NULL);
+  /**
+   * #10: (output) array with dimension (mLdvs, mN)
+   * If mJobvs='V', mVS contains the orthogoanl matrix Z of Schur vectors
+   * If mJobvs='N', mVS is not referenced
+   */
+  C_FLOAT64 * mpVS;
 
-    /**
-     * Copy constructor
-     * @param const CMetab & src
-     * @param const CCopasiContainer * pParent (default: NULL)
-     */
-    CEigen(const CEigen & src,
-           const CCopasiContainer * pParent = NULL);
-    /**
-     * Destructor
-     */
-    virtual ~CEigen();
+  /**
+   * #11: an integer, the leading dimension of the array VS. mLdvs >= 1;
+   * if mJobvs='V', mLdvs >= N.
+   */
+  C_INT mLdvs;
 
-    /**
-     * This is the output method for any object. The default implementation
-     * provided with CCopasiObject uses the ostream operator<< of the object
-     * to print the object.To overide this default behaviour one needs to
-     * reimplement the virtual print function.
-     * @param std::ostream * ostream
-     */
-    virtual void print(std::ostream * ostream) const;
+  /**
+   * #12: (workspace/output) double precision array, dimension (mLWork)
+   * On exit, if mInfo=0; mWork(1) contains the optimal mLWork
+   */
+  CVector< C_FLOAT64 > mWork;
 
-    /**
-     * initialize variables for eigenvalue calculations
-     */
-    void initialize();
+  /**
+   * #13: (input) Dimension of array Work, its value >= max(1,3*mN).
+   * For good performance, it must generally be larger
+   */
+  C_INT mLWork;
 
-    /**
-     * cleanup()
-     */
-    void cleanup();
+  /**
+   * #14: (workspace) Logical array, dimension (N)
+   * Not referenced if mSort = 'N'
+   */
+  C_INT * mpBWork;
 
-    /**
-     * Eigenvalue calculations
-     * @param const C_FLOAT64 * matrix
-     * @param const unsigned C_INT32 & dim
-     */
-    void calcEigenValues(const CMatrix< C_FLOAT64 > & matrix);
+  /**
+   * #15: (output) an integer
+   * =0: successful exit
+   * <0: if mInfo=-i, the ith argument had an illegal value
+   * >0: if mInfo=i, and i is
+   *     <=N: the QR algorithm failed to compute all the eigenvalues;
+   *     =N+1: the eigenvalues could not be reordered because some
+   *           eigenvalues were too close to separate (ill-conditioned)
+   *     =N+2: after reordering, roundoff changed values of some
+   *           complex eigenvalues so that leading eigenvalues in the
+   *           Schur form no longer satisfy mSelect=.True. This could
+   *           caused by underflow due to scaling
+   */
+  C_INT mInfo;
 
-    /**
-     *  Calculate various eigenvalue statistics
-     */
-    void stabilityAnalysis(const C_FLOAT64 & resolution);
+public:
+  /**
+   * Default constructor
+   * @param const std::string & name (default: "NoName")
+   * @param const CCopasiContainer * pParent (default: NULL)
+   */
+  CEigen(const std::string & name = "NoName",
+         const CCopasiContainer * pParent = NULL);
 
-    /**
-     * Get the max eigenvalue real part
-     */
-    const C_FLOAT64 & getMaxrealpart() const;
+  /**
+   * Copy constructor
+   * @param const CMetab & src
+   * @param const CCopasiContainer * pParent (default: NULL)
+   */
+  CEigen(const CEigen & src,
+         const CCopasiContainer * pParent = NULL);
+  /**
+   * Destructor
+   */
+  virtual ~CEigen();
 
-    /**
-     * Get the max eigenvalue imaginary  part
-     */
-    const C_FLOAT64 & getMaximagpart() const;
+  /**
+   * This is the output method for any object. The default implementation
+   * provided with CCopasiObject uses the ostream operator<< of the object
+   * to print the object.To overide this default behaviour one needs to
+   * reimplement the virtual print function.
+   * @param std::ostream * ostream
+   */
+  virtual void print(std::ostream * ostream) const;
 
-    /**
-     * Get the number of zero eigenvalues
-     */
-    const C_INT32 & getNzero() const;
+  /**
+   * initialize variables for eigenvalue calculations
+   */
+  void initialize();
 
-    /**
-     * Get the eigenvalue stiffness
-     */
-    const C_FLOAT64 & getStiffness() const;
+  /**
+   * cleanup()
+   */
+  void cleanup();
 
-    /**
-     * Get the eigenvalue hierarchy
-     */
-    const C_FLOAT64 & getHierarchy() const;
+  /**
+   * Eigenvalue calculations
+   * @param const C_FLOAT64 * matrix
+   * @param const unsigned C_INT32 & dim
+   */
+  void calcEigenValues(const CMatrix< C_FLOAT64 > & matrix);
 
-    /**
-     * Return number of real eigenvalues WeiSun 3/28/02
-     */
-    const C_INT32 & getNreal() const;
+  /**
+   *  Calculate various eigenvalue statistics
+   */
+  void stabilityAnalysis(const C_FLOAT64 & resolution);
 
-    /**
-     * Return the number of imaginary eigenvalue numbers
-     */
-    const C_INT32 & getNimag() const;
+  /**
+   * Get the max eigenvalue real part
+   */
+  const C_FLOAT64 & getMaxrealpart() const;
 
-    const C_INT32 & getNcplxconj() const;
+  /**
+   * Get the max eigenvalue imaginary  part
+   */
+  const C_FLOAT64 & getMaximagpart() const;
 
-    /**
-     * Return the number of eigenvalues with positive real part
-     */
-    const C_INT32 & getNposreal() const;
+  /**
+   * Get the number of zero eigenvalues
+   */
+  const C_INT32 & getNzero() const;
 
-    /**
-     * Return the number of eigenvalues with negative real part
-     */
-    const C_INT32 & getNnegreal() const;
+  /**
+   * Get the eigenvalue stiffness
+   */
+  const C_FLOAT64 & getStiffness() const;
 
-    const CVector< C_FLOAT64 > & getI() const;
+  /**
+   * Get the eigenvalue hierarchy
+   */
+  const C_FLOAT64 & getHierarchy() const;
 
-    const CVector< C_FLOAT64 > & getR() const;
+  /**
+   * Return number of real eigenvalues WeiSun 3/28/02
+   */
+  const C_INT32 & getNreal() const;
 
-    // Friend functions
-    friend std::ostream &operator<<(std::ostream &os, const CEigen &A);
+  /**
+   * Return the number of imaginary eigenvalue numbers
+   */
+  const C_INT32 & getNimag() const;
 
-  private:
-    /**
-     * Initialize the contained CCopasiObjects
-     */
-    void initObjects();
-  };
+  const C_INT32 & getNcplxconj() const;
+
+  /**
+   * Return the number of eigenvalues with positive real part
+   */
+  const C_INT32 & getNposreal() const;
+
+  /**
+   * Return the number of eigenvalues with negative real part
+   */
+  const C_INT32 & getNnegreal() const;
+
+  const CVector< C_FLOAT64 > & getI() const;
+
+  const CVector< C_FLOAT64 > & getR() const;
+
+  // Friend functions
+  friend std::ostream &operator<<(std::ostream &os, const CEigen &A);
+
+private:
+  /**
+   * Initialize the contained CCopasiObjects
+   */
+  void initObjects();
+};
 
 #endif
