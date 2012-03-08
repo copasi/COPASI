57d2ed27317b9a8ef1b64e222a0f3aff76d0d486
Stefan Hoops <shoops@vbi.vt.edu>
2012-03-08 19:09:45 +0000

    Fixed missing break statement in switch.

--- copasi/xml/CCopasiXMLParser.cpp
+++ copasi/xml/CCopasiXMLParser.cpp
@@ -1,12 +1,12 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/xml/CCopasiXMLParser.cpp,v $
-//   $Revision: 1.237 $
+//   $Revision: 1.238 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2012/03/07 17:14:43 $
+//   $Date: 2012/03/08 19:09:45 $
 // End CVS Header
 
-// Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2012 - 2010 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -5891,6 +5891,8 @@ void CCopasiXMLParser::PlotSpecificationElement::start(const XML_Char *pszName, 
                 mpCurrentHandler = new ParameterGroupElement(mParser, mCommon);
               }
           }
+
+        break;
 
       case ListOfChannels:
 
