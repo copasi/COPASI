b031cdeb9790b2c16e7c6e8e14a9234df4783e4c
Stefan Hoops <shoops@vbi.vt.edu>
2012-03-09 04:15:36 +0000

    Added utility functions to determine include path and library names.

---
+++ admin/include.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function includepath ()
+{
+  _c1plus=`gcc -print-prog-name=cc1plus`
+  $_c1plus -Wp, -v /dev/null 2>&1 | grep '^ /'
+}
+
+function findSubdir ()
+{
+  _i="$1"
+  shift
+
+  _found=false
+
+  for _c in $@; do
+    for _d in $_i; do
+      if [ -e $_d/$_c ]; then
+        _found=true 
+        (cd $_d/$_c; pwd)
+        break
+      fi
+    done
+
+    [ $_found = true ] && break;
+  done
+}
+
+if [ x"$1" = x-i ]; then
+  IncludePath=$2
+  shift
+  shift
+else
+  IncludePath=`includepath`
+fi
+
+findSubdir "$IncludePath" $@
+
+
---
+++ admin/libs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function libpath ()
+{
+  _l=`g++ -print-search-dirs | grep libraries | sed -e 's?^[^/]*??'`
+  _l=`echo ${_l//:/ }`
+
+  for _d in $_l; do
+    [ -e $_d ] && (cd $_d; pwd)
+  done
+}
+
+function findLib ()
+{
+  _i="$1"
+  shift
+
+  _found=false
+
+  for _c in $@; do
+    for _d in $_i; do
+      if `ls ${_d} | grep -q lib${_c}'\.\(a\|so\|dylib\)'`; then
+        _found=true 
+        echo -l${_c}
+        break
+      fi
+    done
+
+    [ $_found = true ] && break;
+  done
+}
+
+if [ x"$1" = x-l ]; then
+  LibPath=$2
+  shift
+  shift
+else
+  LibPath=`libpath`
+fi
+
+findLib "$LibPath" $@
+
+
+
