2a463f9ed38609f8ee148212b37f867f954af5c5
Stefan Hoops <shoops@vbi.vt.edu>
2012-05-08 18:29:39 +0000

    Reaction fluxes are in amount and not in concentration.

--- copasi/UI/CQSimpleSelectionTree.cpp
+++ copasi/UI/CQSimpleSelectionTree.cpp
@@ -1,12 +1,12 @@
 // Begin CVS Header
 //   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQSimpleSelectionTree.cpp,v $
-//   $Revision: 1.5 $
+//   $Revision: 1.6 $
 //   $Name:  $
 //   $Author: shoops $
-//   $Date: 2011/09/16 18:13:46 $
+//   $Date: 2012/05/08 18:29:39 $
 // End CVS Header
 
-// Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2012 - 2010 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -72,7 +72,7 @@ CQSimpleSelectionTree::CQSimpleSelectionTree(QWidget* parent):
   mpReactionFluxNumberSubtree =
     new QTreeWidgetItem(mpReactionSubtree, QStringList("Fluxes (Particle Numbers)"));
   mpReactionFluxConcentrationSubtree =
-    new QTreeWidgetItem(mpReactionSubtree, QStringList("Fluxes (Concentration)"));
+    new QTreeWidgetItem(mpReactionSubtree, QStringList("Fluxes (Amount)"));
   mpReactionParameterSubtree =
     new QTreeWidgetItem(mpReactionSubtree, QStringList("Reaction Parameters"));
 
