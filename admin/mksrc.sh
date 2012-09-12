66f113b36bca5182c94b83e4218f72385caacf7d
Stefan Hoops <shoops@vbi.vt.edu>
2012-09-12 11:12:07 -0400

    Updated CopasiVersion.h in the soruce distribution.

--- admin/mksrc.sh
+++ admin/mksrc.sh
@@ -18,17 +18,23 @@ else
   }
 fi
 
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 major=`${AWK} -- '$2 ~ "COPASI_VERSION_MAJOR" {print $3}' copasi/CopasiVersion.h`
 minor=`${AWK} -- '$2 ~ "COPASI_VERSION_MINOR" {print $3}' copasi/CopasiVersion.h`
 build=`${AWK} -- '$2 ~ "COPASI_VERSION_BUILD" {print $3}' copasi/CopasiVersion.h`
 modified=`${AWK} -- '$2 ~ "COPASI_VERSION_MODIFIED" {print $3}' copasi/CopasiVersion.h`
-comment=`${AWK} -- '$2 ~ "COPASI_VERSION_COMMENT" {print $3}' copasi/CopasiVersion.h`
+compatibility=`${AWK} -- '$2 ~ "COPASI_VERSION_COMPATIBILITY" {print $3}' copasi/CopasiVersion.h`
+comment="Source"
+creator=""
+
 buildname=${build}
 
 if [ $modified == true ]; then
   buildname=${buildname}+
 fi
-  
+
 license="US"
 
 test -d copasi-${buildname}-src && rm -rf copasi-${buildname}-src
@@ -38,6 +44,17 @@ make src_distribution
 cd ..
 
 mv copasi_src copasi-${buildname}-src
+
+sed -e 's/COPASI_VERSION_MAJOR.*/COPASI_VERSION_MAJOR '${major}'/' \
+    -e 's/COPASI_VERSION_MINOR.*/COPASI_VERSION_MINOR '${minor}'/' \
+    -e 's/COPASI_VERSION_BUILD.*/COPASI_VERSION_BUILD '${build}'/' \
+    -e 's/COPASI_VERSION_MODIFIED.*/COPASI_VERSION_MODIFIED '${modified}'/' \
+    -e 's/COPASI_VERSION_COMPATIBILITY.*/COPASI_VERSION_COMPATIBILITY '${compatibility}'/' \
+    -e 's/COPASI_VERSION_COMMENT.*/COPASI_VERSION_COMMENT "'${comment}'"/' \
+    -e 's/COPASI_VERSION_CREATOR.*/COPASI_VERSION_CREATOR "'${creator}'"/' \
+    copasi/CopasiVersion.h.in > copasi-${buildname}-src/copasi/CopasiVersion.h
+    
+IFS=$SAVEIFS
 
 cp acinclude.m4 copasi-${buildname}-src
 
@@ -71,12 +88,12 @@ autoconf
 automake
 rm -rf autom4te.cache
 
-#remove any reference to CROSSVALIDATION
-CleanFiles=`find . -type f -exec grep -Hq '#ifdef COPASI_CROSSVALIDATION' {} \; -exec echo {} \; | sort -u`
-
-for file in $CleanFiles; do
-  gawk -- ' BEGIN {keep = 1} $0 ~ "#ifdef COPASI_CROSSVALIDATION" {keep = 0} {if (keep == 1) {print $0}} $0 ~ "#endif // COPASI_CROSSVALIDATION" {keep = 1}' $file > $$.tmp && mv $$.tmp $file;
-done;
+# remove any reference to CROSSVALIDATION
+# CleanFiles=`find . -type f -exec grep -Hq '#ifdef COPASI_CROSSVALIDATION' {} \; -exec echo {} \; | sort -u`
+# 
+# for file in $CleanFiles; do
+#   gawk -- ' BEGIN {keep = 1} $0 ~ "#ifdef COPASI_CROSSVALIDATION" {keep = 0} {if (keep == 1) {print $0}} $0 ~ "#endif // COPASI_CROSSVALIDATION" {keep = 1}' $file > $$.tmp && mv $$.tmp $file;
+# done;
 
 cd ..
 
