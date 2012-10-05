0413564019a620319dc0bdea5012d1f2431fa9fa
Stefan Hoops <shoops@vbi.vt.edu>
2012-10-05 10:40:39 -0400

    The integration approach always used the initial values. Now the integration starts with the current state, i.e., the state with which the steady state task starts.

--- copasi/steadystate/CNewtonMethod.h
+++ copasi/steadystate/CNewtonMethod.h
@@ -1,22 +1,14 @@
-// Begin CVS Header
-//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/steadystate/CNewtonMethod.h,v $
-//   $Revision: 1.33 $
-//   $Name:  $
-//   $Author: shoops $
-//   $Date: 2011/03/07 19:33:41 $
-// End CVS Header
-
-// Copyright (C) 2011 - 2010 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2010 - 2012 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
 
-// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
 // and The University of Manchester.
 // All rights reserved.
 
-// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2002 - 2007 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc. and EML Research, gGmbH.
 // All rights reserved.
 
@@ -76,6 +68,8 @@ private:
   C_INT * mIpiv;
 
   CTrajectoryTask * mpTrajectory;
+
+  CState mStartState;
 
   // Operations
 private:
