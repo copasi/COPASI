3bf6489ed7512666fd8dfb1ee1a07a880f347e7a
Frank Bergmann <fbergman@caltech.edu>
2013-02-14 17:50:48 +0100

    - remove unnecessary assert, since the case is handled

--- copasi/function/CEvaluationNodeObject.cpp
+++ copasi/function/CEvaluationNodeObject.cpp
@@ -266,7 +266,6 @@ ASTNode* CEvaluationNodeObject::toAST(const CCopasiDataModel* pDataModel) const
   // since I can not get the model in which this node is located, I just
   // assume that it will always be the current global model.
   const CCopasiObject* pOrigObject = pDataModel->getDataObject(mRegisteredObjectCN);
-  assert(pOrigObject);
 
   if (pOrigObject == NULL)
     {
