471f0ee23e722a86afb653204b5521726a35e58e
Stefan Hoops <shoops@vbi.vt.edu>
2013-03-21 16:46:04 -0400

    Merged CMake related updates from master.

--- CMakeModules/FindSBW.cmake
+++ CMakeModules/FindSBW.cmake
@@ -5,9 +5,41 @@
 #  SBW_INCLUDE_DIR - the BZip2 include directory
 #  SBW_LIBRARIES - Link these to use BZip2
 
-find_path(SBW_INCLUDE_DIR SBW/SBW.h )
+find_path(SBW_INCLUDE_DIR SBW/SBW.h
+    PATHS $ENV{SBW_DIR}/include
+          $ENV{SBW_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /usr/local/include
+          /usr/include/
+          /sw/include        # Fink
+          /opt/local/include # MacPorts
+          /opt/csw/include   # Blastwave
+          /opt/include
+          /usr/freeware/include
+)
 
-find_library(SBW_LIBRARIES NAMES SBW SBW-static sbw libSBW libsbw)
+find_library(SBW_LIBRARIES
+    NAMES SBW-static
+          SBW
+          sbw 
+          libSBW 
+          libsbw
+    PATHS $ENV{SBW_DIR}/lib
+          $ENV{SBW_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+          /sw/lib        # Fink
+          /opt/local/lib # MacPorts
+          /opt/csw/lib   # Blastwave
+          /opt/lib
+          /usr/freeware/lib64
+    NO_DEFAULT_PATH
+)
 
 # handle the QUIETLY and REQUIRED arguments and set SBW_FOUND to TRUE if
 # all listed variables are TRUE
@@ -17,6 +49,7 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(SBW DEFAULT_MSG SBW_LIBRARIES SBW_INCLUDE_DIR)
 mark_as_advanced(SBW_INCLUDE_DIR SBW_LIBRARIES)
 
 set(SBW_FOUND "NO")
+
 if(SBW_LIBRARIES AND SBW_INCLUDE_DIR)
         SET(SBW_FOUND "YES")
 endif(SBW_LIBRARIES AND SBW_INCLUDE_DIR)
