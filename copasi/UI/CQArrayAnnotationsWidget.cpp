f4e6f9eae0e5becefb04a5ae1e130eb747eff5a5
Brian Klahn <briandk@vbi.vt.edu>
2013-08-23 11:16:02 -0400

    Removed unused "#include "parametertable.h" //for color table item"

--- copasi/UI/CQArrayAnnotationsWidget.cpp
+++ copasi/UI/CQArrayAnnotationsWidget.cpp
@@ -1,16 +1,16 @@
-// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
-// and The University of Manchester.
-// All rights reserved.
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc. and EML Research, gGmbH.
-// All rights reserved.
+// Copyright (C) 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
+// All rights reserved. 
 
 #ifdef SunOS
 #include <ieeefp.h>
@@ -24,7 +24,6 @@
 #include "qtUtilities.h"
 #include "CQComboDelegate.h"
 
-#include "parametertable.h" //for color table item
 #include <iostream>
 
 #include "barChart/qwt3dPlot.h"
