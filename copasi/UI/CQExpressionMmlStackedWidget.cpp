32d9f7b4d5e525a6739acd56e094d28ca1e6f892
Brian Klahn <briandk@vbi.vt.edu>
2013-09-11 16:10:50 -0400

    Fixed syntax of undef statment.

--- copasi/UI/CQExpressionMmlStackedWidget.cpp
+++ copasi/UI/CQExpressionMmlStackedWidget.cpp
@@ -1,12 +1,12 @@
-// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2009 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
-// and The University of Manchester.
-// All rights reserved.
+// Copyright (C) 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
 
 #include "CQExpressionMmlStackedWidget.h"
 
@@ -25,7 +25,7 @@
 #include <utilities/CCopasiMessage.h>
 // turns out that the fatalError definition in copasi message is incompatible
 // with the mml widget
-#undef fatalError()
+#undef fatalError
 
 #ifdef HAVE_MML
 # include <qtmmlwidget.h>
