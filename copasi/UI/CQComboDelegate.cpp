61950aa472d0ed368c8658ea01322e6aa9265cb4
Stefan Hoops <shoops@vbi.vt.edu>
2013-09-12 13:22:44 -0400

    Fixed Bug 1961. Implemented workaround to avoid deletion of the combobox by delaying the modal object selection dialog.

--- copasi/UI/CQComboDelegate.cpp
+++ copasi/UI/CQComboDelegate.cpp
@@ -1,17 +1,9 @@
-// Begin CVS Header
-//   $Source: /Volumes/Home/Users/shoops/cvs/copasi_dev/copasi/UI/CQComboDelegate.cpp,v $
-//   $Revision: 1.5 $
-//   $Name:  $
-//   $Author: shoops $
-//   $Date: 2011/10/13 17:23:14 $
-// End CVS Header
-
-// Copyright (C) 2011 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
 
-// Copyright (C) 2008 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2009 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., EML Research, gGmbH, University of Heidelberg,
 // and The University of Manchester.
 // All rights reserved.
@@ -24,10 +16,10 @@
 #include "copasi.h"
 
 CQComboDelegate::CQComboDelegate(const QStringList *pComboItems, QObject *parent):
-    QItemDelegate(parent),
-    mpComboItems(pComboItems),
-    mEditorToRow(),
-    mRowToItems()
+  QItemDelegate(parent),
+  mpComboItems(pComboItems),
+  mEditorToRow(),
+  mRowToItems()
 {}
 
 CQComboDelegate::~CQComboDelegate()
@@ -75,7 +67,10 @@ void CQComboDelegate::setEditorData(QWidget *editor,
 {
   QString value = index.model()->data(index, Qt::DisplayRole).toString();
   QComboBox *comboBox = static_cast<QComboBox*>(editor);
+
+  comboBox->blockSignals(true);
   comboBox->setCurrentIndex(comboBox->findText(value));
+  comboBox->blockSignals(false);
 }
 
 void CQComboDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
@@ -118,7 +113,7 @@ void CQComboDelegate::slotEditorDeleted(QObject * pObject)
 }
 
 CQIndexComboDelegate::CQIndexComboDelegate(const QStringList *pComboItems, QObject *parent)
-    : CQComboDelegate(pComboItems, parent)
+  : CQComboDelegate(pComboItems, parent)
 {}
 
 CQIndexComboDelegate::~CQIndexComboDelegate()
