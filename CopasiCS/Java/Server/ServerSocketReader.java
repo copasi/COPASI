b25b6c194e30f877bb76a671126133a0db93a834
Pedro Mendes <mendes@copasi.org>
2013-10-02 15:35:33 +0100

    problems with line ending? (git)

--- CopasiCS/Java/Server/ServerSocketReader.java
+++ CopasiCS/Java/Server/ServerSocketReader.java
@@ -1,3 +1,17 @@
+// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
+
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
+
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
+// All rights reserved. 
+
 package Server;
 
 /*
