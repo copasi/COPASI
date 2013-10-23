63fe5760b63c50817776080d7d64125c8c7d42ee
Brian Klahn <briandk@vbi.vt.edu>
2013-10-23 16:30:24 -0400

    -full COPASI path for includes

--- copasi/UI/SliderDialog.h
+++ copasi/UI/SliderDialog.h
@@ -1,16 +1,16 @@
-// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
-// and The University of Manchester.
-// All rights reserved.
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2004 - 2007 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc. and EML Research, gGmbH.
-// All rights reserved.
+// Copyright (C) 2004 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
+// All rights reserved. 
 
 #ifndef SLIDER_DIALOG_H__
 #define SLIDER_DIALOG_H__
@@ -20,10 +20,10 @@
 #include <QtCore/QEvent>
 #include <QtGui/QContextMenuEvent>
 #include <QtGui/QCloseEvent>
-#include "copasi.h"
+#include "copasi/copasi.h"
 #include <vector>
 #include <map>
-#include "report/CCopasiObjectName.h"
+#include "copasi/report/CCopasiObjectName.h"
 
 class QScrollArea;
 class QCheckBox;
