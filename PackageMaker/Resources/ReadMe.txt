a0708aa47bab86fe1482db8159b127ab4e549aa0
Stefan Hoops <shoops@vbi.vt.edu>
2013-12-18 11:34:50 -0500

    Completed implementation of MacOS X installer.

---
+++ PackageMaker/Resources/ReadMe.txt
@@ -0,0 +1,5 @@
+DESCRIPTION
+===========
+
+This is an installer created from the COPASI sources (https://github.com/copasi/COPASI). No additional installation instructions provided.
+
--- PackageMaker/create.sh
+++ PackageMaker/create.sh
@@ -12,7 +12,7 @@ AWK=${COPASI_AWK:-gawk}
 SORT=${COPASI_SORT:-sort}
 PACKAGE=${COPASI_PACKAGE:-Windows}
 
-COMMON_ENVIRONMENT=${COPASI_COMMON_ENVIRONMENT:-"/Users/shoops/environment/10.6/"}
+COMMON_ENVIRONMENT=${COPASI_COMMON_ENVIRONMENT:-"/Users/shoops/environment/10.6"}
 BUILD=${COPASI_BUILD:-"${COMMON_ENVIRONMENT}/package"}
 SOURCE=${COPASI_SOURCE:-"${COMMON_ENVIRONMENT}/COPASI"}
 SETUP_DIR=${COPASI_SETUP_DIR:-"${COMMON_ENVIRONMENT}/setup"}
@@ -40,79 +40,99 @@ if [ x"${comment}" = x\"Snapshot\" ]; then
   MyAppVersion=${MyAppVersion}${buildname}
 fi
 
-[ -e ${SETUP_DIR} ] && rm -rf ${SETUP_DIR}/*
-mkdir -p ${SETUP_DIR}/package
-pushd ${SETUP_DIR}/package
+PACKAGE_NAME="COPASI-${MyAppVersion}-Darwin"
+
+[ -e "${SETUP_DIR}" ] && rm -rf "${SETUP_DIR}"/*
+mkdir -p "${SETUP_DIR}/${PACKAGE_NAME}"
+pushd "${SETUP_DIR}/${PACKAGE_NAME}"
 
 mkdir -p Applications/COPASI/
 
 # Copy CopasiUI
+echo cp -r "${BUILD}/copasi/CopasiUI/CopasiUI.app" Applications/COPASI/
 cp -r "${BUILD}/copasi/CopasiUI/CopasiUI.app" Applications/COPASI/
 
-# copy the icon into the Resources directory
-echo "Creating Resources directory."
+# Create the Info.plist file for CopasiUI
+echo sed -e 's/%COPASI_VERSION%/'$major.$minor.$build'/g' \
+  ${SOURCE}/copasi/CopasiUI/CopasiUI.plist > Applications/COPASI/CopasiUI.app/Contents/Info.plist
+sed -e 's/%COPASI_VERSION%/'$major.$minor.$build'/g' \
+  ${SOURCE}/copasi/CopasiUI/CopasiUI.plist > Applications/COPASI/CopasiUI.app/Contents/Info.plist
+
+echo mkdir -p Applications/COPASI/CopasiUI.app/Contents/Resources
 mkdir -p Applications/COPASI/CopasiUI.app/Contents/Resources
 
-echo "Copy the icon file to the bundle."
+# copy the icon into the Resources directory
+echo cp ${SOURCE}/copasi.icns Applications/COPASI/CopasiUI.app/Contents/Resources/
 cp ${SOURCE}/copasi.icns Applications/COPASI/CopasiUI.app/Contents/Resources/
 
 # copy the files for the wizard into the Resources directory
-echo "Making directory for wizard."
+echo mkdir -p Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html
 mkdir -p Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html
-echo "Copying files for wizard."
+
+echo cp ${SOURCE}/copasi/wizard/help_html/*.html \
+  Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html/
 cp ${SOURCE}/copasi/wizard/help_html/*.html \
+  Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html/
+
+echo cp -r ${SOURCE}/copasi/wizard/help_html/figures \
   Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html/
 cp -r ${SOURCE}/copasi/wizard/help_html/figures \
   Applications/COPASI/CopasiUI.app/Contents/Resources/doc/html/
 
-# add the readme to the image
-echo "Copying readme file."
-cp ${SOURCE}/README_MAC.rtf Applications/COPASI/COPASI-README.rtf
-
 # add the license to the image
-echo "Copying license file."
-cp ${SOURCE}/copasi/ArtisticLicense.txt Applications/COPASI/LICENSE.txt
+echo cp ${SOURCE}/copasi/ArtisticLicense.txt Applications/COPASI/License.txt
+cp ${SOURCE}/copasi/ArtisticLicense.txt Applications/COPASI/License.txt
 
 # copy the examples into the Resources directory
-echo "Make example directory."
+echo mkdir -p Applications/COPASI/examples
 mkdir -p Applications/COPASI/examples
-echo "Copy examples to example directory."
+
+echo cp ${SOURCE}/TestSuite/distribution/* Applications/COPASI/examples/
 cp ${SOURCE}/TestSuite/distribution/* Applications/COPASI/examples/
+
 chmod 444 Applications/COPASI/examples/*
 chmod 777 Applications/COPASI/examples
 
 # copy default configuration
-echo "Make directory for default configuration"
+echo mkdir -p Applications/COPASI/config
 mkdir -p Applications/COPASI/config
-echo "Copy default configuration files"
+echo cp ${SOURCE}/copasi/MIRIAM/MIRIAMResources.xml Applications/COPASI/config
 cp ${SOURCE}/copasi/MIRIAM/MIRIAMResources.xml Applications/COPASI/config
 chmod 444 Applications/COPASI/config/*
 chmod 777 Applications/COPASI/config
 
 # copy the commandline version if it exists
+echo cp ${BUILD}/copasi/CopasiSE/CopasiSE Applications/COPASI/
 cp ${BUILD}/copasi/CopasiSE/CopasiSE Applications/COPASI/
 strip Applications/COPASI/CopasiSE
 chmod 755 Applications/COPASI/CopasiSE
 
 popd
 
-mkdir -p ${SETUP_DIR}/Resources
-cp ${SOURCE}/copasi/ArtisticLicense.txt ${SETUP_DIR}/Resources/License.txt
-
+# Create the Info.plist file for CopasiUI
+echo sed -e 's/%COPASI_VERSION%/'$major.$minor.$build'/g' \
+  ${SOURCE}/PackageMaker/Description.plist > Description.plist
+sed -e 's/%COPASI_VERSION%/'$major.$minor.$build'/g' \
+  ${SOURCE}/PackageMaker/Info.plist > Info.plist
 
 # Run PackageMaker to create package
 pushd ${SETUP_DIR}
-echo Running PackageMaker ...
+
+echo "${PACKAGE_MAKER}" \
+  --root "${PACKAGE_NAME}" \
+  --info "Info.plist" \
+  --out "${PACKAGE_NAME}.pkg" 
 "${PACKAGE_MAKER}" \
-  --root "${SETUP_DIR}/package" \
-  --info "${SETUP_DIR}/package/Applications/COPASI/CopasiUI.app/Contents/Info.plist"
+  --root "${PACKAGE_NAME}" \
+  --info "${SOURCE}/PackageMaker/Info.plist" \
+  --out "${PACKAGE_NAME}.pkg"
 
-mv package.pkg "COPASI-${MyAppVersion}.pkg"
+echo cp -r ${SOURCE}/PackageMaker/Resources "${PACKAGE_NAME}.pkg/Contents"
+cp -r ${SOURCE}/PackageMaker/Resources "${PACKAGE_NAME}.pkg/Contents"
 
-echo Creating disk image
-hdiutil create -volname COPASI-${MyAppVersion}-Darwin -srcfolder "COPASI-${MyAppVersion}.pkg" -ov -format UDZO COPASI-${MyAppVersion}-Darwin
+SetFile -a E "${PACKAGE_NAME}.pkg"
+
+echo hdiutil create -volname "${PACKAGE_NAME}" -srcfolder "${PACKAGE_NAME}.pkg" -ov -format UDZO "${PACKAGE_NAME}"
+hdiutil create -volname "${PACKAGE_NAME}" -srcfolder "${PACKAGE_NAME}.pkg" -ov -format UDZO "${PACKAGE_NAME}"
 popd
 
-# Move the package to its final location
-# cp COPASI-${buildname}-Darwin.exe "${SETUP_DIR}/package"
-# chmod 755 "${SETUP_DIR}"/package/COPASI-${buildname}-${PACKAGE}.exe
