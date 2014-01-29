858e77caf266f6277a8f784378b96346332cef3f
Frank T. Bergmann <fbergman@caltech.edu>
2014-01-29 14:21:48 +0100

    - capture potential crash when switching elements in the parametertable.cpp

--- copasi/model/CReaction.cpp
+++ copasi/model/CReaction.cpp
@@ -1,16 +1,16 @@
-// Copyright (C) 2010 - 2013 by Pedro Mendes, Virginia Tech Intellectual 
-// Properties, Inc., University of Heidelberg, and The University 
-// of Manchester. 
-// All rights reserved. 
+// Copyright (C) 2010 - 2014 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and The University
+// of Manchester.
+// All rights reserved.
 
-// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
-// and The University of Manchester. 
-// All rights reserved. 
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
+// and The University of Manchester.
+// All rights reserved.
 
-// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
-// Properties, Inc. and EML Research, gGmbH. 
-// All rights reserved. 
+// Copyright (C) 2001 - 2007 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc. and EML Research, gGmbH.
+// All rights reserved.
 
 // CReaction
 //
@@ -433,7 +433,7 @@ const std::vector<std::string> & CReaction::getParameterMapping(const size_t & i
 {
   if (!mpFunction) fatalError();
 
-  if (C_INVALID_INDEX == index)
+  if (C_INVALID_INDEX == index || index == mMetabKeyMap.size())
     return mMetabKeyMap[0]; //TODO this is kind of ugly!
 
   return mMetabKeyMap[index];
