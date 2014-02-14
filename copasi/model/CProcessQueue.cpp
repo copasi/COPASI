d573d3ee017346752b8e1b7a7ecceeebd262786c
Frank T. Bergmann <fbergman@caltech.edu>
2014-02-14 13:58:01 +0100

    - add warning, when simultaneous event assignments occur

--- copasi/model/CProcessQueue.cpp
+++ copasi/model/CProcessQueue.cpp
@@ -348,11 +348,11 @@ CProcessQueue::iterator CProcessQueue::getAction()
       if (!mContinueSimultaneousEvents)
         {
 
-          // The resolution of simultaneous events is algorithm dependent.
-          // The simulation routine should provide a call back function.
-          if (mpResolveSimultaneousAssignments == NULL)
+              return (*mpResolveSimultaneousAssignments)(mAssignments, mTime, mEquality, mCascadingLevel);
+            }
+          else
             {
-              CCopasiMessage(CCopasiMessage::EXCEPTION, MCMathModel + 1);
+              CCopasiMessage(CCopasiMessage::WARNING_FILTERED, "CMathModel (1): Simultaneous event assignments encountered.");
             }
 
           return (*mpResolveSimultaneousAssignments)(mActions, mTime, mEquality, mCascadingLevel);
