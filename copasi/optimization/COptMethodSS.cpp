d4e2040d7709d6c3f757fd4371d1dfb0cc288225
Stefan Hoops <shoops@vbi.vt.edu>
2014-03-21 14:21:52 -0400

    The debug parameters "Random Number Generator" and "Seed" are automatically removed in a release version to avoid confusion.

--- copasi/optimization/COptMethodSS.cpp
+++ copasi/optimization/COptMethodSS.cpp
@@ -76,16 +76,20 @@ COptMethodSS::COptMethodSS(const COptMethodSS & src,
   mpLocalMinimizer(NULL)
 {
   // remove eventual existing parameters from the release version.
-#ifndef COPASI_DEBUG
-  removeParameter("Random Number Generator");
-  removeParameter("Seed");
-#endif
-
   initObjects();
 }
 
 COptMethodSS::~COptMethodSS()
 {cleanup();}
+
+// virtual
+bool COptMethodSS::elevateChildren()
+{
+#ifndef COPASI_DEBUG
+  removeParameter("Random Number Generator");
+  removeParameter("Seed");
+#endif
+}
 
 void COptMethodSS::initObjects()
 {
