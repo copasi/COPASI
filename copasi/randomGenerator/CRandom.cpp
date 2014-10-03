ae21ac112a6f98b44658757efd364cb5aa18f206
Stefan Hoops <shoops@vbi.vt.edu>
2014-10-03 09:07:31 -0400

    Fixed to compile under Windows.

--- copasi/randomGenerator/CRandom.cpp
+++ copasi/randomGenerator/CRandom.cpp
@@ -14,11 +14,13 @@
 
 #include <time.h>
 #ifdef WIN32
+# define _USE_MATH_DEFINES
 # include <Windows.h>
 #else
 # include <unistd.h>
 # include <sys/syscall.h>
 #endif // WIN32
+
 #include <cmath>
 #include <algorithm>
 #include <string.h>
