c79a0b7c85ae544f5ee3829b7d539e1758b97f78
Frank T. Bergmann <fbergman@caltech.edu>
2015-01-07 13:20:20 +0100

    - issue 2121: improve R build script

--- copasi/bindings/R/patch-swig-file.cmake
+++ copasi/bindings/R/patch-swig-file.cmake
@@ -1,25 +1,25 @@
+# Copyright (C) 2012 - 2015 by Pedro Mendes, Virginia Tech Intellectual 
+# Properties, Inc., University of Heidelberg, and The University 
+# of Manchester. 
+# All rights reserved. 
+
 if (NOT FILENAME OR NOT EXISTS ${FILENAME})
-	message(FATAL_ERROR "Please specify the filename to the file to patch")
+  message(FATAL_ERROR "Please specify the filename to the file to patch")
 endif()
 
 execute_process(COMMAND sed 
-					-f 
-				     ${SRC_DIR}/sedscript.txt -i
-					 "${FILENAME}"
-					 )
+          -f 
+             ${SRC_DIR}/sedscript.txt -i
+           "${FILENAME}"
+           )
 
-#file(STRINGS "${FILENAME}" SOURCECODE)
-#file(READ "${FILENAME}" SOURCECODE)
-#STRING( REGEX REPLACE "\r?\n.$" "\n" SOURCECODE "${SOURCECODE}" )
-# for whatever reason the enums don't quite work out as they should
-#STRING(REGEX REPLACE ";" "\\\\;" SOURCECODE "${SOURCECODE}")
-#STRING(REGEX REPLACE "\n" ";" SOURCECODE "${SOURCECODE}")
-# STRING(REGEX REPLACE "\r?\n" ";" SOURCECODE "${SOURCECODE}")
-# file(WRITE "${FILENAME}" "# modified code\n")
-# FOREACH(line ${SOURCECODE})
-# 	string(LENGTH "${line}" line_length)
-# 	Message( "${line_length}\n")
-# 	STRING(REGEX REPLACE "(.*)(enum)(.*)(_p)(.*)" "\\1\\2\\4" new_line "${line}" )
-# 	file(APPEND "${FILENAME}" "${new_line}\n")
-# endforeach()
-message ("DONE")
+file(READ "${FILENAME}" SOURCECODE)
+
+string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+string(REPLACE "if ( &&" "if (" SOURCECODE "${SOURCECODE}" )
+string(REPLACE "if ()" "if(TRUE)" SOURCECODE "${SOURCECODE}" )
+
+file(WRITE "${FILENAME}" "${SOURCECODE}")
+message (STATUS "Patched COPASI.R")
