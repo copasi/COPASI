dfc85d587781eade1254edb4148a90a5b8b96a91
Brian Klahn <briandk@vbi.vt.edu>
2015-04-08 16:54:02 -0400

    Compiles with a complete, but untested, CUnit parsing grammar.

--- copasi/utilities/CUnitParser.h
+++ copasi/utilities/CUnitParser.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2014 - 2015 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -53,11 +53,11 @@ protected:
 
   // Attributes
 public:
-//struct {CUnit * pUnit, CUnitComponent * pComponent, char * pText}
+//struct {CUnit unit, CUnitComponent component, char * pText}
   struct Data
   {
-    CUnit * pUnit;
-    CUnitComponent * pComponent;
+    CUnit unit;
+    CUnitComponent component;
     std::string text;
   };
 protected:
