748bb8c868b26a474c2cce872321b94606626fa7
Frank T. Bergmann <fbergman@caltech.edu>
2016-02-03 08:32:12 +0100

    - issue 2224: free all elements

--- copasi/utilities/CSparseMatrix.cpp
+++ copasi/utilities/CSparseMatrix.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2010 - 2015 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -495,7 +495,7 @@ CCompressedColumnFormat::~CCompressedColumnFormat()
 {
   pdelete(mpValue);
   pdelete(mpRowIndex);
-  pdelete(mpColumnStart);
+  pdeletev(mpColumnStart);
 }
 
 size_t CCompressedColumnFormat::numRows() const
