9fb45fe9a015da6845e24693d6d9142a38b6e025
Frank T. Bergmann <fbergman@caltech.edu>
2016-10-07 16:15:08 +0200

    - remove warnings about unsafe boolean operations

--- copasi/math/CMathEvent.cpp
+++ copasi/math/CMathEvent.cpp
@@ -1428,7 +1428,7 @@ void CMathEvent::createUpdateSequences()
   CObjectInterface::UpdateSequence StateEffects;
   mpContainer->getTransientDependencies().getUpdateSequence(StateEffects, CMath::Default, EventTargets, ExtendedStateValues);
 
-  if (!StateEffects.empty() > 0)
+  if (!StateEffects.empty())
     {
       mEffectsSimulation |= CMath::State;
     }
@@ -1436,7 +1436,7 @@ void CMathEvent::createUpdateSequences()
   CObjectInterface::UpdateSequence ContiousSimulationEffects;
   mpContainer->getTransientDependencies().getUpdateSequence(ContiousSimulationEffects, CMath::Default, EventTargets, ContinuousSimulationValues);
 
-  if (!ContiousSimulationEffects.empty() > 0)
+  if (!ContiousSimulationEffects.empty())
     {
       mEffectsSimulation |=  CMath::ContinuousSimulation;
     }
@@ -1444,7 +1444,7 @@ void CMathEvent::createUpdateSequences()
   CObjectInterface::UpdateSequence DiscreteSimulationEffects;
   mpContainer->getTransientDependencies().getUpdateSequence(DiscreteSimulationEffects, CMath::Default, EventTargets, DiscreteSimulationValues);
 
-  if (!DiscreteSimulationEffects.empty() > 0)
+  if (!DiscreteSimulationEffects.empty())
     {
       mEffectsSimulation |=  CMath::EventSimulation;
     }
