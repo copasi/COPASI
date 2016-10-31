88bb0c1aa2ac4e912225daccf39166bf3709215b
Sven Sahle <sven.sahle@bioquant.uni-heidelberg.de>
2016-10-31 15:02:41 +0100

    possible fix for #2322
    When accessing the CArrayAnnotation Headers, check for index range
    violation in the actual headers list, not the size of the data array.

--- copasi/UI/CQArrayAnnotationsWidgetDM.cpp
+++ copasi/UI/CQArrayAnnotationsWidgetDM.cpp
@@ -78,7 +78,7 @@ QVariant CQArrayAnnotationsWidgetDM::headerData(int section, Qt::Orientation ori
     {
       if (mRow == C_INVALID_INDEX ||
           section < 0 ||
-          (C_INT32)mpArray->size()[mRow] <= section)
+          (C_INT32)mHeaderData[orientation].size() <= section)
         {
           return QVariant();
         }
@@ -89,7 +89,7 @@ QVariant CQArrayAnnotationsWidgetDM::headerData(int section, Qt::Orientation ori
     {
       if (mColumn == C_INVALID_INDEX ||
           section < 0 ||
-          (C_INT32)mpArray->size()[mColumn] <= section)
+          (C_INT32)mHeaderData[orientation].size() <= section)
         {
           return QVariant();
         }
