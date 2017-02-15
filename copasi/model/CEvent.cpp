48a0b171050b47d1997add78b8647fe81a11cadf
Stefan Hoops <shoops@vbi.vt.edu>
2017-02-15 14:13:55 -0500

    Fixed Bug 2367. The priority expression was not re-compiled during event compile.

--- copasi/model/CEvent.cpp
+++ copasi/model/CEvent.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
 // Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
@@ -366,6 +371,13 @@ bool CEvent::compile(CObjectInterface::ContainerList listOfContainer)
     {
       success &= mpDelayExpression->compile(listOfContainer);
       addDirectDependency(mpDelayExpression);
+    }
+
+  // Compile the delay expression
+  if (mpPriorityExpression != NULL)
+    {
+      success &= mpPriorityExpression->compile(listOfContainer);
+      addDirectDependency(mpPriorityExpression);
     }
 
   // Compile the assignments
