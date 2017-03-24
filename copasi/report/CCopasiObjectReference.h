b940951f5ffa87ce9aaa09943b8b70eecce17edf
Frank T. Bergmann <fbergman@caltech.edu>
2017-03-24 09:29:32 +0100

    - remove warning

--- copasi/report/CCopasiObjectReference.h
+++ copasi/report/CCopasiObjectReference.h
@@ -1,4 +1,9 @@
-// Copyright (C) 2010 - 2015 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
+// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
 // All rights reserved.
@@ -25,6 +30,7 @@
 
 #include "report/CCopasiObject.h"
 #include "report/CCopasiObjectName.h"
+#include "report/CCopasiContainer.h"
 
 template <class CType> class CCopasiObjectReference: public CCopasiObject
 {
