0766eb58fde43546edfd0f9e920f86ab8d905e0c
Frank T. Bergmann <fbergman@caltech.edu>
2017-04-25 10:22:41 +0200

    - modify patching R scripts to improve enum handling (thanks to J. Foerster)

--- copasi/bindings/R/patch-swig-file.cmake
+++ copasi/bindings/R/patch-swig-file.cmake
@@ -21,6 +21,12 @@ endif()
 
 file(READ "${FILENAME}" SOURCECODE)
 
+#string (REGEX MATCHALL "enumToInteger.*coerceIfNotSubclass" matches ${SOURCECODE})
+#message("Matches:")
+#foreach(match ${matches})
+# message("${match}")
+#endforeach()
+
 string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
 string(REPLACE ", \"}" ", \"" SOURCECODE "${SOURCECODE}" )
 string(REPLACE "\${enumTo" "enumTo" SOURCECODE "${SOURCECODE}" )
@@ -28,5 +34,32 @@ string(REPLACE "\${enumFrom" "enumFrom" SOURCECODE "${SOURCECODE}" )
 string(REPLACE "if ( &&" "if (" SOURCECODE "${SOURCECODE}" )
 string(REPLACE "if ()" "if(TRUE)" SOURCECODE "${SOURCECODE}" )
 string(REPLACE "ans <- new(\"_p_size_t\", ref=ans) ;" "" SOURCECODE "${SOURCECODE}" )
+
+# remove enumFrom/ToInteger code
+SET(NEW_CODE "
+enumToInteger <- function(name,type)
+ {
+    if (is.character(name)) {
+    gettype <- paste(\".__E__\", type, sep = \"\")
+    if (!exists(gettype)) gettype <- paste(\".__E__\", substr(type, 3, nchar(type)), sep = \"\")
+    ans <- as.integer(get(gettype)[name])
+    if (is.na(ans)) {warning(\"enum not found \", name, \" \", type)}
+    ans
+    }
+ }
+
+ enumFromInteger =
+ function(i,type)
+ {
+   gettype <- paste(\".__E__\", type, sep = \"\")
+   if (!exists(gettype)) gettype <- paste(\".__E__\", substr(type, 3, nchar(type)), sep = \"\")
+   itemlist <- get(gettype)
+   names(itemlist)[match(i, itemlist)]
+ }
+
+")
+
+
+string(REGEX REPLACE "enumToInteger.*coerceIfNotSubclass" "${NEW_CODE}coerceIfNotSubclass" SOURCECODE "${SOURCECODE}")
 file(WRITE "${FILENAME}" "${SOURCECODE}")
 message (STATUS "Patched COPASI.R")
