bb53e2c745d1e8a1d89a106c42e6604b5929125f
Frank T. Bergmann <fbergman@caltech.edu>
2017-05-29 13:31:38 +0200

    - issue 2418: fix compilation of c++ wrapper code

---
+++ copasi/bindings/common/patch-wrapper.cmake
@@ -0,0 +1,27 @@
+# Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual 
+# Properties, Inc., University of Heidelberg, and University of 
+# of Connecticut School of Medicine. 
+# All rights reserved. 
+
+#
+# Description       : CMake script patching SWIG wrappers
+# Original author(s): Frank Bergmann <fbergman@caltech.edu>
+# Organization      : California Institute of Technology
+#
+
+
+# patch c++ wrapper file 
+set (WRAPPER_FILE "${BIN_DIRECTORY}/copasi_wrapper.cpp")
+
+if (NOT EXISTS ${WRAPPER_FILE})
+  message(FATAL_ERROR "Wrapper file does not exist")
+endif()
+
+message(STATUS "Patching C++ wrapper")
+
+file(READ "${WRAPPER_FILE}" SOURCECODE)
+
+string(REPLACE "enum CTaskEnum::Method " "CTaskEnum::Method " SOURCECODE "${SOURCECODE}" )
+string(REPLACE "enum CTaskEnum::Task " "CTaskEnum::Task " SOURCECODE "${SOURCECODE}" )
+
+file(WRITE "${WRAPPER_FILE}" "${SOURCECODE}")
