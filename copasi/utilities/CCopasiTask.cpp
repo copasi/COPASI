fd069e3f083f93dfb792cb5d83420720240ded05
Stefan Hoops <shoops@copasi.org>
2017-06-29 09:42:23 -0400

    Assure that the initial time is only updated for non autonomous models.

--- copasi/utilities/CCopasiTask.cpp
+++ copasi/utilities/CCopasiTask.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
 // Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
@@ -264,6 +269,12 @@ bool CCopasiTask::restore()
         {
           mpContainer->updateSimulatedValues(false);
           mpContainer->setInitialState(mpContainer->getState(false));
+
+          if (mpContainer->isAutonomous())
+            {
+              // Set the initial time to 0
+              mpContainer->getInitialState()[mpContainer->getCountFixed() + mpContainer->getCountFixedEventTargets()] = 0;
+            }
         }
       else
         {
