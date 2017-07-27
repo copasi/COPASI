eab0035d4e9320b99479fac9c7485758b8023940
Stefan Hoops <shoops@copasi.org>
2017-07-27 16:23:12 -0400

    Removed Debug output.

--- copasi/xml/parser/UNKNOWNHandler.cpp
+++ copasi/xml/parser/UNKNOWNHandler.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
 // Copyright (C) 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
@@ -29,7 +34,7 @@ void UNKNOWNHandler::start(const XML_Char * pszName,
 {
   if (mLevel == 0)
     {
-      std::cout << "UNKNONWN" << std::endl;
+      // std::cout << "UNKNONWN" << std::endl;
     }
 
   processStart(pszName, papszAttrs);
