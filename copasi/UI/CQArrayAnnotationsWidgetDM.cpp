94344469e79f81485c62c8036f3fba586f45a082
Frank T. Bergmann <fbergman@caltech.edu>
2017-10-19 10:21:03 +0200

    - avoid crash in annotation datamodel if headerdata is not correctly populated

--- copasi/UI/CQArrayAnnotationsWidgetDM.cpp
+++ copasi/UI/CQArrayAnnotationsWidgetDM.cpp
@@ -78,9 +78,11 @@ QVariant CQArrayAnnotationsWidgetDM::headerData(int section, Qt::Orientation ori
 
   if (orientation == Qt::Vertical)
     {
-      if (mRow == C_INVALID_INDEX ||
-          section < 0 ||
-          (C_INT32)mpArray->size()[mRow] <= section)
+      if (mRow == C_INVALID_INDEX
+          || section < 0
+          || (C_INT32)mpArray->size()[mRow] <= section
+          || (C_INT32)mHeaderData[orientation].size() <= section
+         )
         {
           return QVariant();
         }
@@ -89,9 +91,11 @@ QVariant CQArrayAnnotationsWidgetDM::headerData(int section, Qt::Orientation ori
     }
   else
     {
-      if (mColumn == C_INVALID_INDEX ||
-          section < 0 ||
-          (C_INT32)mpArray->size()[mColumn] <= section)
+      if (mColumn == C_INVALID_INDEX
+          || section < 0
+          || (C_INT32)mpArray->size()[mColumn] <= section
+          || (C_INT32)mHeaderData[orientation].size() <= section
+         )
         {
           return QVariant();
         }
