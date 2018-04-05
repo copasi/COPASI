e093619062927828cb8dedf833020ce1f69b4cb5
Frank T. Bergmann <fbergman@caltech.edu>
2018-04-05 12:06:49 +0200

    - issue 2596: allow default / copy constructors so language bindings can compile for now

--- copasi/core/CEnumAnnotation.h
+++ copasi/core/CEnumAnnotation.h
@@ -1,7 +1,8 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and University of
 // of Connecticut School of Medicine.
 // All rights reserved.
+
 
 #ifndef COPASI_CEnumAnnotation
 #define COPASI_CEnumAnnotation
@@ -18,7 +19,6 @@ public:
    */
   typedef std::array< Type, static_cast< size_t >(Enum::__SIZE) > base;
 
-private:
   /**
    * Default constructor
    */
