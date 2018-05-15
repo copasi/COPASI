09a249565e3a8ca014256f71ce3286f696ec6f02
Stefan Hoops <shoops@copasi.org>
2018-05-15 15:08:30 -0400

    - fix compilation on ubuntu

--- CMakeModules/FindLIBCOMBINE.cmake
+++ CMakeModules/FindLIBCOMBINE.cmake
@@ -22,6 +22,8 @@
 # Modified by Ralph Gauges
 
 # message (STATUS "$ENV{COMBINE_DIR}")
+
+
 set(COMBINE_LIBRARY_NAME)
 if (UNIX)
   set(COMBINE_LIBRARY_NAME Combine-static)
@@ -30,11 +32,11 @@ else()
 endif()
 
 
-find_package(combine CONFIG REQUIRED
-  CONFIGS Combine-config.cmake
-          Combine-static-config.cmake
-          libCombine-config.cmake
-          libCombine-static-config.cmake
+find_package(${COMBINE_LIBRARY_NAME} CONFIG QUIET)
+
+if (NOT ${COMBINE_LIBRARY_NAME}_FOUND)
+  find_package(${COMBINE_LIBRARY_NAME} CONFIG QUIET
+  CONFIGS ${COMBINE_LIBRARY_NAME}-config.cmake
   PATHS $ENV{COMBINE_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake
         ${COPASI_DEPENDENCY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake
         /usr/${CMAKE_INSTALL_LIBDIR}/cmake
@@ -42,16 +44,92 @@ find_package(combine CONFIG REQUIRED
         ${COPASI_DEPENDENCY_DIR}/lib/cmake
         /usr/lib/cmake
   )
+endif()
 
-set(COMBINE_FOUND ${combine_FOUND})
+if (NOT ${COMBINE_LIBRARY_NAME}_FOUND)
+  message(FATAL_ERROR "not found yet
+
+
+")
+
+endif()
+
+
+if (${COMBINE_LIBRARY_NAME}_FOUND)
 
 get_target_property(COMBINE_INCLUDE_DIR ${COMBINE_LIBRARY_NAME} INTERFACE_INCLUDE_DIRECTORIES)
-get_target_property(COMBINE_LIBRARY ${COMBINE_LIBRARY_NAME} IMPORTED_LOCATION_RELEASE)
+get_target_property(COMBINE_LIBRARY ${COMBINE_LIBRARY_NAME} IMPORTED_LOCATION)
+if (NOT COMBINE_LIBRARY)
+get_target_property(COMBINE_LIBRARY ${COMBINE_LIBRARY_NAME} IMPORTED_LOCATION_DEBUG)
+endif()
+
+if (NOT COMBINE_LIBRARY)
+set(COMBINE_LIBRARY)
+endif()
 
 get_target_property(COMBINE_INTERFACE_LINK_LIBRARIES ${COMBINE_LIBRARY_NAME} INTERFACE_LINK_LIBRARIES)
 if (COMBINE_INTERFACE_LINK_LIBRARIES)
   set(COMBINE_LIBRARY ${COMBINE_LIBRARY} ${COMBINE_INTERFACE_LINK_LIBRARIES})
 endif (COMBINE_INTERFACE_LINK_LIBRARIES)
+
+else()
+
+
+find_path(COMBINE_INCLUDE_DIR combine/combinearchive.h
+    PATHS $ENV{COMBINE_DIR}/include
+          $ENV{COMBINE_DIR}
+          ${COPASI_DEPENDENCY_DIR}/include
+          ${COPASI_DEPENDENCY_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /sw/include        # Fink
+          /opt/local/include # MacPorts
+          /opt/csw/include   # Blastwave
+          /opt/include
+          /usr/freeware/include
+    NO_DEFAULT_PATH)
+
+if (NOT COMBINE_INCLUDE_DIR)
+    message(FATAL_ERROR "COMBINE include dir not found not found!")
+endif (NOT COMBINE_INCLUDE_DIR)
+
+if (NOT COMBINE_INCLUDE_DIR)
+    find_path(COMBINE_INCLUDE_DIR combine/combinearchive.h)
+endif (NOT COMBINE_INCLUDE_DIR)
+
+find_library(COMBINE_LIBRARY 
+    NAMES ${COMBINE_LIBRARY_NAME}
+    PATHS $ENV{COMBINE_DIR}/lib
+          $ENV{COMBINE_DIR}
+          ${COPASI_DEPENDENCY_DIR}/lib
+          ${COPASI_DEPENDENCY_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /sw/lib         Fink
+          /opt/local/lib  MacPorts
+          /opt/csw/lib    Blastwave
+          /opt/lib
+          /usr/freeware/lib64
+    NO_DEFAULT_PATH)
+    
+if (NOT COMBINE_LIBRARY)
+    find_library(COMBINE_LIBRARY 
+        NAMES ${COMBINE_LIBRARY_NAME})
+endif (NOT COMBINE_LIBRARY)
+
+
+endif()
+
+if (NOT COMBINE_LIBRARY)
+    message(FATAL_ERROR "COMBINE library not found!")
+endif (NOT COMBINE_LIBRARY)
+
+set(COMBINE_FOUND "NO")
+if(COMBINE_LIBRARY)
+    if   (COMBINE_INCLUDE_DIR)
+        SET(COMBINE_FOUND "YES")
+    endif(COMBINE_INCLUDE_DIR)
+endif(COMBINE_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(COMBINE REQUIRED COMBINE_INCLUDE_DIR COMBINE_LIBRARY)
