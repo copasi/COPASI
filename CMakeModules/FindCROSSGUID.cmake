cd15e880a11aa67f308f64032569a5c6f5ca8902
Stefan Hoops <shoops@copasi.org>
2018-05-21 14:57:46 -0400

    Fixed to work with system crossguid library.

--- CMakeModules/FindCROSSGUID.cmake
+++ CMakeModules/FindCROSSGUID.cmake
@@ -22,19 +22,69 @@ find_package(crossguid CONFIG REQUIRED
         /usr/${CMAKE_INSTALL_LIBDIR}/cmake
   )
 
-set(CROSSGUID_FOUND ${crossguid_FOUND})
+if (NOT CROSSGUID_FOUND)
+  # Fallback if no CONFIG is found
 
-get_target_property(CROSSGUID_INCLUDE_DIR crossguid INTERFACE_INCLUDE_DIRECTORIES)
-get_target_property(CROSSGUID_LIBRARY crossguid IMPORTED_LOCATION_RELEASE)
-if (NOT CROSSGUID_LIBRARY)
-get_target_property(CROSSGUID_LIBRARY crossguid IMPORTED_LOCATION_DEBUG)
+  find_path(CROSSGUID_INCLUDE_DIR crossguid/guid.hpp
+    PATHS $ENV{CROSSGUID_DIR}/include
+          $ENV{CROSSGUID_DIR}
+          ${COPASI_DEPENDENCY_DIR}/include
+          ${COPASI_DEPENDENCY_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /sw/include        # Fink
+          /opt/local/include # MacPorts
+          /opt/csw/include   # Blastwave
+          /opt/include
+          /usr/freeware/include
+    NO_DEFAULT_PATH)
+
+  if (NOT CROSSGUID_INCLUDE_DIR)
+    find_path(CROSSGUID_INCLUDE_DIR crossguid/guid.hpp)
+  endif (NOT CROSSGUID_INCLUDE_DIR)
+
+  find_library(CROSSGUID_LIBRARY 
+    NAMES crossguid
+    PATHS $ENV{CROSSGUID_DIR}/lib
+          $ENV{CROSSGUID_DIR}
+          ${COPASI_DEPENDENCY_DIR}/lib
+          ${COPASI_DEPENDENCY_DIR}
+          ~/Library/Frameworks
+          /Library/Frameworks
+          /sw/lib         Fink
+          /opt/local/lib  MacPorts
+          /opt/csw/lib    Blastwave
+          /opt/lib
+          /usr/freeware/lib64
+    NO_DEFAULT_PATH)
+    
+  if (NOT CROSSGUID_LIBRARY)
+    find_library(CROSSGUID_LIBRARY 
+        NAMES ${CROSSGUID_LIBRARY_NAME})
+  endif (NOT CROSSGUID_LIBRARY)
+  
+  if (CROSSGUID_LIBRARY AND UNIX AND NOT APPLE)
+    set(CROSSGUID_LIBRARY ${CROSSGUID_LIBRARY} -luuid)
+  endif ()
+  
+  set(CROSSGUID_FOUND ${CROSSGUID_LIBRARY})
+else ()
+  set(CROSSGUID_FOUND ${crossguid_FOUND})
+
+  get_target_property(CROSSGUID_INCLUDE_DIR crossguid INTERFACE_INCLUDE_DIRECTORIES)
+  get_target_property(CROSSGUID_LIBRARY crossguid IMPORTED_LOCATION_RELEASE)
+  
+  if (NOT CROSSGUID_LIBRARY)
+    get_target_property(CROSSGUID_LIBRARY crossguid IMPORTED_LOCATION_DEBUG)
+  endif()
+
+  get_target_property(CROSSGUID_INTERFACE_LINK_LIBRARIES crossguid INTERFACE_LINK_LIBRARIES)
+
+  if (CROSSGUID_INTERFACE_LINK_LIBRARIES)
+    set(CROSSGUID_LIBRARY ${CROSSGUID_LIBRARY} ${CROSSGUID_INTERFACE_LINK_LIBRARIES})
+  endif (CROSSGUID_INTERFACE_LINK_LIBRARIES)
 endif()
 
-get_target_property(CROSSGUID_INTERFACE_LINK_LIBRARIES crossguid INTERFACE_LINK_LIBRARIES)
-
-if (CROSSGUID_INTERFACE_LINK_LIBRARIES)
-  set(CROSSGUID_LIBRARY ${CROSSGUID_LIBRARY} ${CROSSGUID_INTERFACE_LINK_LIBRARIES})
-endif (CROSSGUID_INTERFACE_LINK_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(CROSSGUID REQUIRED CROSSGUID_INCLUDE_DIR CROSSGUID_LIBRARY)
