8b0bae2a091f41f9acba76664e7952fab071f22c
Stefan Hoops <shoops@copasi.org>
2018-06-21 11:35:25 -0400

    Bug 2652. Missing modifies are ignored during load.

--- copasi/xml/parser/ModifierHandler.cpp
+++ copasi/xml/parser/ModifierHandler.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
 // Copyright (C) 2016 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and The University
 // of Manchester.
@@ -39,9 +44,11 @@ CXMLHandler * ModifierHandler::processStart(const XML_Char * pszName,
 
         pMetabolite = dynamic_cast< CMetab * >(mpData->mKeyMap.get(Metabolite));
 
-        if (!pMetabolite) fatalError();
+        if (pMetabolite != NULL)
+          {
+            mpData->pReaction->addModifier(pMetabolite->getKey());
+          }
 
-        mpData->pReaction->addModifier(pMetabolite->getKey());
         break;
 
       default:
