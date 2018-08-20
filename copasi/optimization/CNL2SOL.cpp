92e115fc903d7cfd113f2e537d2cded0c46f4743
Pedro Mendes <mendes@copasi.org>
2018-08-20 13:26:36 -0400

    added comments for d1mach_ (no code)

--- copasi/optimization/CNL2SOL.cpp
+++ copasi/optimization/CNL2SOL.cpp
@@ -360,6 +360,13 @@ L999:
 
 doublereal d1mach_(integer *i)
 {
+  /*  DOUBLE-PRECISION MACHINE CONSTANTS */
+  /*  D1MACH(1) = B**(EMIN-1), THE SMALLEST POSITIVE MAGNITUDE. */
+  /*  D1MACH(2) = B**EMAX*(1 - B**(-T)), THE LARGEST MAGNITUDE. */
+  /*  D1MACH(3) = B**(-T), THE SMALLEST RELATIVE SPACING. */
+  /*  D1MACH(4) = B**(1-T), THE LARGEST RELATIVE SPACING. */
+  /*  D1MACH(5) = LOG10(B) */
+
   switch (*i)
     {
       case 1: return DBL_MIN;
