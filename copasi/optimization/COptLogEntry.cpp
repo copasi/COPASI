22f26b7e3f96ee7afe0d600731530c50e07fdca5
Pedro Mendes <mendes@copasi.org>
2018-08-21 19:17:58 -0400

    first commit; simpler class to handle optimization log entries

---
+++ copasi/optimization/COptLogEntry.cpp
@@ -0,0 +1,43 @@
+// Copyright (C) 2018 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
+
+/**
+ *  COptLogEntry class
+ *  Container for a log message created by the copasi optimization methods.
+ *  Stores various variable for later compilation of a message string.
+ *
+ *  Written by Pedro Mendes, based on earlier code by Jonas Förster
+ */
+
+#include "COptLogEntry.h"
+#include "utilities/utility.h"
+
+// Constructor
+COptLogEntry::COptLogEntry(std::string &header, std::string &subtext = "", std::string &status = ""):
+  MsgSubtext(subtext),
+  StatusDump(status)
+{
+  MsgHeader = LocalTimeStamp();
+  MsgHeader.append(": ");
+  MsgHeader.append(header);
+}
+
+COptLogEntry::~COptLogEntry()
+{}
+
+std::string COptLogEntry::getHeader() const
+{
+  return MsgHeader;
+}
+
+std::string COptLogEntry::getSubtext() const
+{
+  return MsgSubtext;
+}
+
+std::string COptLogEntry::getStatusDetails() const
+{
+  return StatusDump;
+}
