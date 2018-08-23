cd8bedf36334a60c655daa4b7af7ea4f2fea2342
Pedro Mendes <mendes@copasi.org>
2018-08-23 09:37:00 -0400

    changed log messages to use COptLogEntry class

--- copasi/optimization/COptMethodNelderMead.cpp
+++ copasi/optimization/COptMethodNelderMead.cpp
@@ -185,7 +185,13 @@ bool COptMethodNelderMead::optimise()
       return false;
     }
 
-  mMethodLog.enterLogItem(COptLogItem(COptLogItem::STD_start).with("Nelder_-_Mead/"));
+  if (mLogVerbosity > 0)
+    mMethodLog.enterLogEntry(
+      COptLogEntry(
+        "Algorithm started.",
+        "For more information about this method see: http://copasi.org/Support/User_Manual/Methods/Optimization_Methods/Nelder_-_Mead/"
+      )
+    );
 
   // set tolerances for local minima test to zero
   C_FLOAT64 abstol, reltol;
@@ -251,7 +257,8 @@ bool COptMethodNelderMead::optimise()
       mStep[i] = (*OptItem.getUpperBoundValue() - *OptItem.getLowerBoundValue()) / mScale;
     }
 
-  if (!pointInParameterDomain) mMethodLog.enterLogItem(COptLogItem(COptLogItem::STD_initial_point_out_of_domain));
+  if (!pointInParameterDomain && (mLogVerbosity > 0))
+    mMethodLog.enterLogEntry(COptLogEntry("Initial point outside parameter domain."));
 
   evaluate();
 
@@ -562,7 +569,11 @@ First:
         }
     }   /* while not found and not quit ... */
 
-  if (mLogVerbosity >= 1 && found) mMethodLog.enterLogItem(COptLogItem(COptLogItem::NM_fval_change_lower_than_tol).iter(mIteration));
+  if (mLogVerbosity > 0 && found)
+    mMethodLog.enterLogEntry(
+      COptLogEntry(
+        "Iteration " + std::to_string(mIteration) + ": Variance of the objective function values at the vertices of the simplex are lower than tolerance. Checking whether local minimum was found."
+      ));
 
   /* **** bail out if necessary **** */
   if (quit || !mContinue) goto Finish;
@@ -601,16 +612,25 @@ First:
 
   /* ---- return on finding a local minimum to the desired accuracy. ---- */
 
-  if (ok) /* then */
+  if (ok)
     {
-      mMethodLog.enterLogItem(COptLogItem(COptLogItem::NM_local_min_termination).iter(mIteration));
+      if (mLogVerbosity > 0)
+        mMethodLog.enterLogEntry(
+          COptLogEntry(
+            "Iteration " + std::to_string(mIteration) + ": Found a local minimum. Terminating."
+          )
+        );
 
       goto Finish;
     }
 
   /* ---- Reduce the size of the simplex and restart the procedure. ---- */
 
-  if (mLogVerbosity >= 1) mMethodLog.enterLogItem(COptLogItem(COptLogItem::NM_no_local_min_reducing_simplex).iter(mIteration));
+  if (mLogVerbosity > 0)
+    mMethodLog.enterLogEntry(
+      COptLogEntry(
+        "Iteration " + std::to_string(mIteration) + ": No local minimum found. Reducing simplex size."
+      ));
 
   found = 0;   /* -- we did not find a 1 minimum -- */
   del = std::max(del * factor, 100.0 * std::numeric_limits< C_FLOAT64 >::epsilon());
@@ -618,7 +638,12 @@ First:
   goto First;
 
 Finish:  /* end of procedure */
-  mMethodLog.enterLogItem(COptLogItem(COptLogItem::STD_finish_x_of_max_iter).iter(mIteration).with(mIterationLimit));
+
+  if (mLogVerbosity > 0)
+    mMethodLog.enterLogEntry(
+      COptLogEntry("Algorithm finished.",
+                   "Terminated after " + std::to_string(mIteration) + " of " + std::to_string(mIterationLimit) + " iterations."
+                  ));
 
   if (mpCallBack)
     mpCallBack->finishItem(mhIteration);
