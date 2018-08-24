0d6bb42bbee30cddf780b295a3bc8e69fd96f352
Frank T. Bergmann <fbergman@caltech.edu>
2018-08-24 14:05:58 +0200

    - issue 2660: enable steady state analysis by default

--- copasi/lna/CLNAProblem.cpp
+++ copasi/lna/CLNAProblem.cpp
@@ -1,12 +1,12 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and University of
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and University of 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2011 - 2016 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2011 - 2016 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
 /**
  *  CLNAProblem class.
@@ -36,6 +36,7 @@ CLNAProblem::CLNAProblem(const CDataContainer * pParent):
   CCopasiProblem(CTaskEnum::Task::lna, pParent)
 {
   addParameter("Steady-State", CCopasiParameter::Type::KEY, std::string(""));
+  setSteadyStateRequested(true);
   CONSTRUCTOR_TRACE;
 }
 
