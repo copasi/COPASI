60e3b227c7d14dc8767a8df134d50cb510c35fc2
Frank T. Bergmann <fbergman@caltech.edu>
2018-08-27 10:36:09 +0200

    - issue 2676: load log if log tab was already selected

--- copasi/UI/CQFittingResult.cpp
+++ copasi/UI/CQFittingResult.cpp
@@ -1,21 +1,21 @@
-// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and University of
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and University of 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
-// and The University of Manchester.
-// All rights reserved.
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2006 - 2007 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc. and EML Research, gGmbH.
-// All rights reserved.
+// Copyright (C) 2006 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
+// All rights reserved. 
 
 /*
  *  CQFittingResult.cpp
@@ -441,6 +441,8 @@ bool CQFittingResult::enterProtected()
   mpCrossValidationValues->setSortingEnabled(true);
 
   mpLogTree->clear();
+  if (mpTabWidget->currentWidget() == mpLogPage)
+	  loadTab(mpTabWidget->currentIndex());
 
   //preliminary testing code
   /*tcs = new CColorScaleBiLog();
