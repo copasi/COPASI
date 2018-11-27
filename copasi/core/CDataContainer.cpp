a8b5d5859c237b9cf0502ad313e2624bc96fee0b
Frank T. Bergmann <fbergman@caltech.edu>
2018-11-27 10:39:52 +0100

    - issue 2725: be a bit more stringent as to when to defer to the root container

--- copasi/core/CDataContainer.cpp
+++ copasi/core/CDataContainer.cpp
@@ -253,7 +253,7 @@ const CObjectInterface * CDataContainer::getObject(const CCommonName & cn) const
         return new CDataString(Name, this);
       else if (Type == "Separator")
         return new CCopasiReportSeparator(Name, this);
-      else if (getObjectName() == "Root" && Type == "Vector")
+      else if (getObjectName() == "Root" && Type == "Vector" && CRootContainer::getRoot() != this)
         return CRootContainer::getRoot()->getObject(cn);
       else
         return NULL;
