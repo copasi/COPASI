5c613502959cd0e9b20936889d9642ad913b7b40
Frank T. Bergmann <fbergman@caltech.edu>
2019-05-28 11:40:09 +0200

    - issue 2770: implement setting of values by ini file in c++ example for testing

--- copasi/bindings/cpp_examples/runScan/runScan.cpp
+++ copasi/bindings/cpp_examples/runScan/runScan.cpp
@@ -4,14 +4,16 @@
 // All rights reserved.
 
 /**
- * This examples demonstrates how to read in a copasi file, change its parameters and run a parameter scan
- * while changing the report filename
+ * This examples demonstrates how to read in a copasi file, change its
+ * parameters and run a parameter scan while changing the report filename
  */
 
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
+
+#include <copasi/utilities/CParameterEstimationUtils.h>
 
 #define COPASI_MAIN
 
@@ -21,15 +23,123 @@ using namespace std;
 
 
 /**
+ * This function changes the double value of the given element to
+ * the specified value
+ *
+ * @param element the data object whose value to change
+ * @param value the new double value
+ *
+ * @return true, indicating that the model value was changed,
+ *         false if it is not a supported object
+ */
+bool
+changeModelParameter(CDataObject* element, double value)
+{
+  if (element == NULL)
+    return false;
+
+  CDataObjectReference<double>* pRef =
+    dynamic_cast<CDataObjectReference<double>*>(element);
+
+  if (pRef != NULL)
+    {
+      if (pRef->getValuePointer() != NULL)
+        {
+          *static_cast<double*>(pRef->getValuePointer()) = value;
+
+          if (pRef->getObjectName() == "InitialConcentration" &&
+              pRef->getObjectDataModel() != NULL &&
+              pRef->getObjectDataModel()->getModel() != NULL)
+            pRef->getObjectDataModel()->getModel()->updateInitialValues(pRef);
+
+          return true;
+        }
+    }
+
+  CCopasiParameter* pParam = dynamic_cast<CCopasiParameter*>(element);
+
+  if (pParam != NULL)
+    {
+      if (pParam->getValuePointer() != NULL)
+        {
+          *static_cast<double*>(pParam->getValuePointer()) = value;
+          return true;
+        }
+    }
+
+  return false;
+}
+
+/**
+ * updates the given data model with values from the provided ini file.
+ * This file should have the simple syntax, of one of more statements:
+ *
+ *    name = value
+ *
+ * where name is a valid DisplayName of a model object, and value a double
+ * value to be assigned. To indicate a comment, use the semicolon ';' after
+ * which the remaining line will be disregarded.
+ *
+ * @param pDataModel the data model to update
+ * @param fileName the fileName of the ini file
+ */
+void
+changeParametersFromIniFile(CDataModel* pDataModel, const std::string& fileName)
+{
+  ifstream ifs(fileName.c_str(), std::ios_base::in);
+
+  if (!ifs.good())
+    {
+      cerr << "Couldn't open file with parameters" << endl;
+      exit(1);
+    }
+
+  std::string line;
+
+  while (std::getline(ifs, line))
+    {
+      // remove comments
+      size_t pos = line.find(';');
+
+      if (pos != std::string::npos)
+        line = line.substr(0, pos);
+
+      pos = line.rfind('=');
+
+      if (pos == std::string::npos) // no equals sign found invalid
+        continue;
+
+      std::string key = ResultParser::trim(line.substr(0, pos));
+      std::string value = ResultParser::trim(line.substr(pos + 1));
+
+      if (key.empty() || value.empty())
+        continue;
+
+      double dValue = ResultParser::saveToDouble(value);
+
+      CDataObject* element =
+        const_cast<CDataObject*>(pDataModel->findObjectByDisplayName(key));
+
+      if (element == NULL) // no such element, or no supported element
+        continue;
+
+      if (changeModelParameter(element, dValue))
+        pDataModel->getModel()->setCompileFlag(); // mark as changed
+    }
+
+  pDataModel->getModel()->compileIfNecessary(NULL); // compile if needed
+}
+
+/**
  * This function will read in reaction parameters in the form
  *
  *   reaction_id parameterId value
  *
  * and will reparameterize the model with those values
  */
-void changeParameters(CDataModel* pDataModel, const std::string& parameterSet)
+void changeParametersFromTsvFile(CDataModel* pDataModel, const std::string& fileName)
 {
-  ifstream ifs(parameterSet.c_str(), std::ios_base::in);
+  ifstream ifs(fileName.c_str(), std::ios_base::in);
 
   if (!ifs.good())
     {
@@ -104,7 +214,7 @@ int main(int argc, char** argv)
     }
 
   // changeParameters
-  changeParameters(pDataModel, parameterSet);
+  changeParametersFromIniFile(pDataModel, parameterSet);
 
   // change the filename
   CScanTask* scanTask = dynamic_cast<CScanTask*>(&(*pDataModel->getTaskList())["Scan"]);
