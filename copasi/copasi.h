0ee56d8e73bd9763d7ae059de9fe3e82cb3edd78
Frank T. Bergmann <fbergman@caltech.edu>
2020-01-16 16:29:27 +0100

    - fix compilation on mac

--- copasi/copasi.h
+++ copasi/copasi.h
@@ -1,4 +1,9 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// University of Virginia, University of Heidelberg, and University
+// of Connecticut School of Medicine.
+// All rights reserved.
+
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and University of
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -72,11 +77,11 @@ LIBSEDML_CPP_NAMESPACE_USE
 using std::isnan;
 #endif
 
-#ifdef Darwin
-# ifndef isnan
-#  define isnan(__x) ((__x == __x) != true)
-# endif
-#endif
+// #ifdef Darwin
+// # ifndef isnan
+// #  define isnan(__x) ((__x == __x) != true)
+// # endif
+// #endif
 
 #if (defined __GNUC__ && __GNUC__ < 3)
 # define ios_base ios
