d98610fed43a6b4275f5ac3e23509d4f15a6c729
Stefan Hoops <shoops@copasi.org>
2020-02-22 15:11:45 -0500

    Simplified default noise expression.

--- copasi/model/CModelValue.cpp
+++ copasi/model/CModelValue.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -488,7 +488,7 @@ const CExpression* CModelEntity::getInitialExpressionPtr() const
 
 std::string CModelEntity::getDefaultNoiseExpression() const
 {
-  return "sign(<" + mpRateReference->getCN() + ">)*sqrt(abs(<" + mpRateReference->getCN() + ">))";
+  return "sqrt(abs(<" + mpRateReference->getCN() + ">))";
 }
 
 bool CModelEntity::setNoiseExpression(const std::string & expression)
--- copasi/model/CReaction.cpp
+++ copasi/model/CReaction.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -1255,7 +1255,7 @@ void CReaction::initObjects()
 
 std::string CReaction::getDefaultNoiseExpression() const
 {
-  return "sign(<" + mpParticleFluxReference->getCN() + ">)*sqrt(abs(<" + mpParticleFluxReference->getCN() + ">))";
+  return "sqrt(abs(<" + mpParticleFluxReference->getCN() + ">))";
 }
 
 bool CReaction::setNoiseExpression(const std::string & expression)
