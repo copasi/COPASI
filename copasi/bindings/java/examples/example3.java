b596505ea37f65912446a0f1686417c9a795029e
Frank T. Bergmann <fbergman@caltech.edu>
2020-07-09 13:20:44 +0200

    - remove warnings from java examples

--- copasi/bindings/java/examples/example3.java
+++ copasi/bindings/java/examples/example3.java
@@ -1,3 +1,8 @@
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
+
 // Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual 
 // Properties, Inc., University of Heidelberg, and University of 
 // of Connecticut School of Medicine. 
@@ -28,172 +33,186 @@ import org.COPASI.*;
 
 public class example3
 {
-  public static void main(String[] args)
-  {
-      assert CRootContainer.getRoot() != null;
-      // create a new datamodel
-      CDataModel dataModel = CRootContainer.addDatamodel();
-      assert CRootContainer.getDatamodelList().size() == 1;
-      // the only argument to the main routine should be the name of an SBML file
-      if (args.length == 1)
-      {
-          String filename = args[0];
-          try
-          {
-              // load the model 
-              dataModel.importSBML(filename);
-          }
-          catch (java.lang.Exception ex)
-          {
-              System.err.println( "Error while importing the model from file named \"" + filename + "\"." );
-              System.exit(1);
-          }
-          CModel model = dataModel.getModel();
-          assert model != null;
-          // create a report with the correct filename and all the species against
-          // time.
-          CReportDefinitionVector reports = dataModel.getReportDefinitionList();
-          // create a new report definition object
-          CReportDefinition report = reports.createReportDefinition("Report", "Output for timecourse");
-          // set the task type for the report definition to timecourse
-          report.setTaskType(CTaskEnum.Task_timeCourse);
-          // we don't want a table
-          report.setIsTable(false);
-          // the entries in the output should be seperated by a ", "
-          report.setSeparator(new CCopasiReportSeparator(", "));
+	public static void main(String[] args)
+	{
+		assert CRootContainer.getRoot() != null;
+		// create a new datamodel
+		CDataModel dataModel = CRootContainer.addDatamodel();
+		assert CRootContainer.getDatamodelList().size() == 1;
 
-          // we need a handle to the header and the body
-          // the header will display the ids of the metabolites and "time" for
-          // the first column
-          // the body will contain the actual timecourse data
-          ReportItemVector header = report.getHeaderAddr();
-          ReportItemVector body = report.getBodyAddr();
-          
-          body.add(new CRegisteredCommonName(model.getObject(new CCommonName("Reference=Time")).getCN().getString()));
-          body.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
-          header.add(new CRegisteredCommonName(new CDataString("time").getCN().getString()));
-          header.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
+		// the only argument to the main routine should be the name of an SBML file
+		if (args.length != 1) {
+			System.err.println("Usage: example3 SBMLFILE" );
+			System.exit(1);
+		}
 
-          int i, iMax =(int) model.getMetabolites().size();
-          for (i = 0;i < iMax;++i)
-          {
-              CMetab metab = model.getMetabolite(i);
-              assert metab != null;
-              // we don't want output for FIXED metabolites right now
-              if (metab.getStatus() != CModelEntity.Status_FIXED)
-              {
-                  // we want the concentration oin the output
-                  // alternatively, we could use "Reference=Amount" to get the
-                  // particle number
-                  body.add(new CRegisteredCommonName(metab.getObject(new CCommonName("Reference=Concentration")).getCN().getString()));
-                  // add the corresponding id to the header
-                  header.add(new CRegisteredCommonName(new CDataString(metab.getSBMLId()).getCN().getString()));
-                  // after each entry, we need a seperator
-                  if(i!=iMax-1)
-                  {
-                    body.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
-                    header.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
-                  }
+		String filename = args[0];
+		try
+		{
+			// load the model 
+			dataModel.importSBML(filename);
+		}
+		catch (java.lang.Exception ex)
+		{
+			System.err.println( "Error while importing the model from file named \"" + filename + "\"." );
+			System.exit(1);
+		}
+		
+		CModel model = dataModel.getModel();
+		assert model != null;
+		
+		// create a report with the correct filename and all the species against
+		// time.
+		CReportDefinitionVector reports = dataModel.getReportDefinitionList();
 
-              }
-          }
+		// create a new report definition object
+		CReportDefinition report = createReportDefinition(model, reports);
 
 
-          // get the trajectory task object
-          CTrajectoryTask trajectoryTask = (CTrajectoryTask)dataModel.getTask("Time-Course");
-          assert trajectoryTask != null;
+		// get the trajectory task object
+		CTrajectoryTask trajectoryTask = 
+				(CTrajectoryTask)dataModel.getTask("Time-Course");
+		assert trajectoryTask != null;
 
-          // run a deterministic time course
-          trajectoryTask.setMethodType(CTaskEnum.Method_deterministic);
+		// run a deterministic time course
+		trajectoryTask.setMethodType(CTaskEnum.Method_deterministic);
 
-          // pass a pointer of the model to the problem
-          trajectoryTask.getProblem().setModel(dataModel.getModel());
+		// pass a pointer of the model to the problem
+		trajectoryTask.getProblem().setModel(dataModel.getModel());
 
-          // actiavate the task so that it will be run when the model is saved
-          // and passed to CopasiSE
-          trajectoryTask.setScheduled(true);
+		// activate the task so that it will be run when the model is saved
+		// and passed to CopasiSE
+		trajectoryTask.setScheduled(true);
 
-          // set the report for the task
-          trajectoryTask.getReport().setReportDefinition(report);
-          // set the output filename
-          trajectoryTask.getReport().setTarget("example3.txt");
-          // don't append output if the file exists, but overwrite the file
-          trajectoryTask.getReport().setAppend(false);
+		// set the report for the task
+		trajectoryTask.getReport().setReportDefinition(report);
+		// set the output filename
+		trajectoryTask.getReport().setTarget("example3.txt");
+		// don't append output if the file exists, but overwrite the file
+		trajectoryTask.getReport().setAppend(false);
 
-          // get the problem for the task to set some parameters
-          CTrajectoryProblem problem = (CTrajectoryProblem)trajectoryTask.getProblem();
+		// get the problem for the task to set some parameters
+		CTrajectoryProblem problem = (CTrajectoryProblem)trajectoryTask.getProblem();
 
-          // simulate 100 steps
-          problem.setStepNumber(100);
-          // start at time 0
-          dataModel.getModel().setInitialTime(0.0);
-          // simulate a duration of 10 time units
-          problem.setDuration(10);
-          // tell the problem to actually generate time series data
-          problem.setTimeSeriesRequested(true);
+		// simulate 100 steps
+		problem.setStepNumber(100);
+		// start at time 0
+		dataModel.getModel().setInitialTime(0.0);
+		// simulate a duration of 10 time units
+		problem.setDuration(10);
+		// tell the problem to actually generate time series data
+		problem.setTimeSeriesRequested(true);
 
-          // set some parameters for the LSODA method through the method
-          CTrajectoryMethod method = (CTrajectoryMethod)trajectoryTask.getMethod();
+		// set some parameters for the LSODA method through the method
+		CTrajectoryMethod method = (CTrajectoryMethod)trajectoryTask.getMethod();
 
-          CCopasiParameter parameter = method.getParameter("Absolute Tolerance");
-          assert parameter != null;
-          assert parameter.getType() == CCopasiParameter.Type_DOUBLE;
-          parameter.setDblValue(1.0e-12);
+		CCopasiParameter parameter = method.getParameter("Absolute Tolerance");
+		assert parameter != null;
+		assert parameter.getType() == CCopasiParameter.Type_DOUBLE;
+		parameter.setDblValue(1.0e-12);
 
-          boolean result=true;
-          try
-          {
-              // now we run the actual trajectory
-              result=trajectoryTask.processWithOutputFlags(true, (int)CCopasiTask.ONLY_TIME_SERIES);
-          }
-          catch (java.lang.Exception ex)
-          {
-              System.err.println( "Error. Running the time course simulation failed." );
-              			String lastError = trajectoryTask.getProcessError();
-            // check if there are additional error messages
-            if (lastError.length() > 0)
-            {
-                // print the messages in chronological order
-                System.err.println(lastError);
-            }
-              System.exit(1);
-          }
-          if(result==false)
-          {
-              System.err.println( "An error occured while running the time course simulation." );
-              // check if there are additional error messages
-              if (CCopasiMessage.size() > 0)
-              {
-                  // print the messages in chronological order
-                  System.err.println(CCopasiMessage.getAllMessageText(true));
-              }
-              System.exit(1);
-          }
+		boolean result=true;
+		try
+		{
+			// now we run the actual trajectory , the output flag identifies, what 
+			// sort of output is collected when running the task, important options
+			// are: 
+			//
+			//	CCopasiTask.OUTPUT_UI: collect the same output, as the UI 
+			//	CCopasiTask.OUTPUT_UI
+			//
+			result=trajectoryTask.processWithOutputFlags(true, (int)CCopasiTask.OUTPUT_UI);
+		}
+		catch (java.lang.Exception ex)
+		{
+			System.err.println( "Error. Running the time course simulation failed." );
+			String lastError = trajectoryTask.getProcessError();
+			// check if there are additional error messages
+			if (lastError.length() > 0)
+			{
+				// print the messages in chronological order
+				System.err.println(lastError);
+			}
+			System.exit(1);
+		}
+		
+		if(result==false)
+		{
+			System.err.println( "An error occured while running the time course simulation." );
+			// check if there are additional error messages
+			if (CCopasiMessage.size() > 0)
+			{
+				// print the messages in chronological order
+				System.err.println(CCopasiMessage.getAllMessageText(true));
+			}
+			System.exit(1);
+		}
 
-          // look at the timeseries
-          CTimeSeries timeSeries = trajectoryTask.getTimeSeries();
-          // we simulated 100 steps, including the initial state, this should be
-          // 101 step in the timeseries
-          assert timeSeries.getRecordedSteps() == 101;
-          System.out.println( "The time series consists of " + (new Long(timeSeries.getRecordedSteps())).toString() + "." );
-          System.out.println( "Each step contains " + (new Long(timeSeries.getNumVariables())).toString() + " variables." );
-          System.out.println( "The final state is: " );
-          iMax = (int)timeSeries.getNumVariables();
-          int lastIndex = (int)timeSeries.getRecordedSteps() - 1;
-          for (i = 0;i < iMax;++i)
-          {
-              // here we get the particle number (at least for the species)
-              // the unit of the other variables may not be particle numbers
-              // the concentration data can be acquired with getConcentrationData
-              System.out.println(timeSeries.getTitle(i) + ": " + (new Double(timeSeries.getData(lastIndex, i))).toString() );
-          }
+		// look at the timeseries
+		CTimeSeries timeSeries = trajectoryTask.getTimeSeries();
+		// we simulated 100 steps, including the initial state, this should be
+		// 101 step in the timeseries
+		assert timeSeries.getRecordedSteps() == 101;
+		System.out.println( "The time series consists of " + (new Long(timeSeries.getRecordedSteps())).toString() + "." );
+		System.out.println( "Each step contains " + (new Long(timeSeries.getNumVariables())).toString() + " variables." );
+		System.out.println( "The final state is: " );
+		int iMax = (int)timeSeries.getNumVariables();
+		int lastIndex = (int)timeSeries.getRecordedSteps() - 1;
+		for (int i = 0;i < iMax;++i)
+		{
+			// here we get the particle number (at least for the species)
+			// the unit of the other variables may not be particle numbers
+			// the concentration data can be acquired with getConcentrationData
+			System.out.println(timeSeries.getTitle(i) + ": " + (new Double(timeSeries.getData(lastIndex, i))).toString() );
+		}
 
-      }
-      else
-      {
-          System.err.println("Usage: example3 SBMLFILE" );
-          System.exit(1);
-      }
+	}
 
-  }
+	private static CReportDefinition createReportDefinition(CModel model, CReportDefinitionVector reports) 
+	{
+		CReportDefinition report = reports.createReportDefinition("Report", "Output for timecourse");
+		// set the task type for the report definition to timecourse
+		report.setTaskType(CTaskEnum.Task_timeCourse);
+		// we don't want a table
+		report.setIsTable(false);
+		// the entries in the output should be separated by a ", "
+		report.setSeparator(new CCopasiReportSeparator(", "));
+
+		// we need a handle to the header and the body
+		// the header will display the ids of the metabolites and "time" for
+		// the first column
+		// the body will contain the actual timecourse data
+		ReportItemVector header = report.getHeaderAddr();
+		ReportItemVector body = report.getBodyAddr();
+
+		body.add(new CRegisteredCommonName(model.getObject(new CCommonName("Reference=Time")).getCN().getString()));
+		body.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
+		header.add(new CRegisteredCommonName(new CDataString("time").getCN().getString()));
+		header.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
+
+		int i, iMax =(int) model.getMetabolites().size();
+		for (i = 0;i < iMax;++i)
+		{
+			CMetab metab = model.getMetabolite(i);
+			assert metab != null;
+			// we don't want output for FIXED metabolites right now
+			if (metab.getStatus() != CModelEntity.Status_FIXED)
+			{
+				// we want the concentration oin the output
+				// alternatively, we could use "Reference=Amount" to get the
+				// particle number
+				body.add(new CRegisteredCommonName(metab.getObject(new CCommonName("Reference=Concentration")).getCN().getString()));
+				// add the corresponding id to the header
+				header.add(new CRegisteredCommonName(new CDataString(metab.getSBMLId()).getCN().getString()));
+				// after each entry, we need a separator
+				if(i!=iMax-1)
+				{
+					body.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
+					header.add(new CRegisteredCommonName(report.getSeparator().getCN().getString()));
+				}
+
+			}
+		}
+		return report;
+	}
 }
--- copasi/bindings/java/examples/processCallback.java
+++ copasi/bindings/java/examples/processCallback.java
@@ -1,8 +1,17 @@
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
+
+// Copyright (C) 2018 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and University of 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
+
 /**
  * This is an example on how to build models with the COPASI backend API.
  */
 import org.COPASI.*;
-import java.io.*;
 
 public class processCallback extends org.COPASI.CProcessReport
 {
