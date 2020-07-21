af59e53209d123d36129cda28a289b5b9c24846e
Frank T. Bergmann <fbergman@caltech.edu>
2020-07-21 12:24:33 +0200

    - emit information when layout/render information was changed

--- copasi/qlayout/CQLayoutScene.h
+++ copasi/qlayout/CQLayoutScene.h
@@ -1,4 +1,9 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// University of Virginia, University of Heidelberg, and University
+// of Connecticut School of Medicine.
+// All rights reserved.
+
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and University of
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -32,6 +37,7 @@ public:
   virtual ~CQLayoutScene();
   void setResolver(CLRenderResolver* resolver);
   const CLRenderResolver* getResolver() const;
+  CLRenderResolver * getResolver();
   void saveToFile(const std::string& fileName, const std::string& fileType = "pdf");
   void setLayout(CLayout *layout, CDataModel* model = NULL, CLRenderInformationBase* renderInformation = NULL);
   void setRenderInformation(CDataModel* model, CLRenderInformationBase* renderInformation);
