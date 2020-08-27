00451b1a67eeec8272c73791ca861da754a7c4c4
Stefan Hoops <shoops@copasi.org>
2020-08-27 19:45:25 -0400

    Bug 2917: mFirstWorstIssue was not updated when combining validities.

--- copasi/utilities/CValidity.cpp
+++ copasi/utilities/CValidity.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -161,6 +161,8 @@ CValidity & CValidity::operator |= (const CValidity & rhs)
       mErrors |= rhs.mErrors;
       mWarnings |= rhs.mWarnings;
       mInformation |= rhs.mInformation;
+
+      mFirstWorstIssue &= rhs.mFirstWorstIssue;
     }
 
   if (mpObjectInterface != NULL &&
