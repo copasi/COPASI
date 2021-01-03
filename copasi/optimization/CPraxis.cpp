9816b56f5bb5d62a49a9b48a842110a15a58621d
Pedro Mendes <mendes@copasi.org>
2021-01-03 18:10:39 -0500

    removed printf of QR FAILED

--- copasi/optimization/CPraxis.cpp
+++ copasi/optimization/CPraxis.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2021 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -1065,7 +1065,7 @@ L101:
         }
 
       e[k - 1] = 0.;
-      printf("QR FAILED\n");
+      /* printf("QR FAILED\n"); */
 L102:
       i__3 = k;
 
