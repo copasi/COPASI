4e65aa874d4ad98668e05b9b24c1c9d6e5679b14
Frank T. Bergmann <fbergman@caltech.edu>
2021-01-28 16:00:10 +0100

    - issue 2955: correct default enum selection

--- copasi/analytics/CAnalyticsMethod.h
+++ copasi/analytics/CAnalyticsMethod.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2021 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -37,7 +37,7 @@ public:
    */
   CAnalyticsMethod(const CDataContainer * pParent,
                    const CTaskEnum::Method & methodType,
-                   const CTaskEnum::Task & taskType = CTaskEnum::analytics);
+                   const CTaskEnum::Task & taskType = CTaskEnum::Task::analytics);
 
   /**Analytics
    * Copy constructor.
