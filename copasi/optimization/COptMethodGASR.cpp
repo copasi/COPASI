e8733c367dcb4f80d7df300c90c1b78ebed55857
Pedro Mendes <mendes@copasi.org>
2021-05-07 09:52:02 -0400

    Fixed bug 3004: pop w/ odd number was copying wrong individual

--- copasi/optimization/COptMethodGASR.cpp
+++ copasi/optimization/COptMethodGASR.cpp
@@ -211,7 +211,7 @@ bool COptMethodGASR::replicate()
 
   // check if there is one left over and just copy it
   if (mPopulationSize % 2 > 0)
-    *mIndividuals[2 * mPopulationSize - 1] = *mIndividuals[mPopulationSize - 1];
+    *mIndividuals[2 * mPopulationSize - 1] = *mIndividuals[mpPermutation->next()];
 
   // mutate the offspring
   for (i = mPopulationSize; i < 2 * mPopulationSize && Continue; i++)
