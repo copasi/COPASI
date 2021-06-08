af132eb9ef3b8d7020ee01ca25b056284cdcbe35
Stefan Hoops <shoops@copasi.org>
2021-06-08 19:34:46 -0400

    Fixed to include config.h

--- copasi/CopasiTypes.h
+++ copasi/CopasiTypes.h
@@ -1,8 +1,12 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and University of
+// Copyright (C) 2019 - 2021 by Pedro Mendes, Rector and Visitors of the
+// University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
 
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
+// Properties, Inc., University of Heidelberg, and University of
+// of Connecticut School of Medicine.
+// All rights reserved.
 
 /**
  * This file is a convenience header that includes the other COPASI types. Please ensure
@@ -11,6 +15,8 @@
 
 #ifndef COPASI_TYPES_H
 #define COPASI_TYPES_H
+
+#include <copasi/config.h>
 
 #include <copasi/copasi.h>
 
