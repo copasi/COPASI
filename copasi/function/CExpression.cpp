b031b97639877067965bb119ef1304c41a6be116
Frank T. Bergmann <fbergman@caltech.edu>
2021-07-22 17:53:43 +0200

    Bug 3041: Revert "- issue 3040: dont replace displaystring / infix ..."
    This reverts commit 5fda4713b178a1566a49377cdd23a3827a82e7f3.

--- copasi/function/CExpression.cpp
+++ copasi/function/CExpression.cpp
@@ -129,7 +129,7 @@ CIssue CExpression::compile(CObjectInterface::ContainerList listOfContainer)
   CIssue issue = compileNodes();
   mValidity.add(issue);
 
-  if (issue.isSuccess() && getRoot() != NULL)
+  if (getRoot() != NULL)
     {
       mDisplayString = getRoot()->buildDisplayString();
       mInfix = getRoot()->buildInfix();
