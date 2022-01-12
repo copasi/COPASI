962aee248c4bf2db3668e16a4a60a03739fe1263
Stefan Hoops <shoops@copasi.org>
2022-01-12 18:05:28 -0500

    CMethodFactory checks whether the created method is valid for the task.

--- copasi/utilities/CCopasiTask.h
+++ copasi/utilities/CCopasiTask.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -355,6 +355,7 @@ public:
    */
   virtual const CTaskEnum::Method * getValidMethods() const;
 #endif
+
   /**
    * Retrieve the problem
    */
