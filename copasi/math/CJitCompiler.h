0494000e4eab953febe1b76491e449aaa203cfbd
Frank Bergmann <fbergman@caltech.edu>
2022-01-14 15:18:05 +0100

    - fix compilation of language bindings with jit compiler

--- copasi/math/CJitCompiler.h
+++ copasi/math/CJitCompiler.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 - 2021 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2020 - 2022 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -32,12 +32,15 @@ public:
 
   virtual ~CJitCompiler();
 
+#ifdef USE_JIT
+
   virtual CJitCompiler * copy() const = 0;
 
   virtual bool compile() = 0;
 
   virtual Function compile(const CMathExpression & mathExpression) = 0;
 
+#endif
   void registerExpression(CJitExpression * pExpression);
 
   void deregisterExpression(CJitExpression * pExpression);
