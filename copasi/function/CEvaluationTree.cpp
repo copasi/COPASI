a3487e472502cb036c814a6ddd2af4fc2f45a274
Stefan Hoops <shoops@copasi.org>
2022-01-18 12:41:07 -0500

    Bug 3062: CEvalutionTree::clearNodes() did not reset the mpRootNode.

--- copasi/function/CEvaluationTree.cpp
+++ copasi/function/CEvaluationTree.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2021 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -532,7 +532,7 @@ void CEvaluationTree::clearNodes()
 
   pdelete(mpNodeList);
 
-  setRoot(NULL);
+  mpRootNode = NULL;
   mpRootValue = NULL;
   mValue = std::numeric_limits< C_FLOAT64 >::quiet_NaN();
 }
