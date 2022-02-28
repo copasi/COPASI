5e3b62a75e6e2de8f8daa04b790860358a66b05e
Stefan Hoops <shoops@copasi.org>
2022-02-28 16:01:36 -0500

    Improve debugging of windows package process.

--- InnoSetup/COPASI.sh
+++ InnoSetup/COPASI.sh
@@ -14,19 +14,22 @@
 # of Manchester. 
 # All rights reserved. 
 
+# Echo all bash commands to ease debugging
+set -x
 
 INNO_SETUP=${COPASI_INNO_SETUP:-"/cygdrive/c/Program Files (x86)/Inno Setup 5/ISCC.exe"}
 INNO_FILE=${COPASI_INNO_FILE:-"${SOURCE}/InnoSetup/copasi-universal.iss"}
 
 # Create the unique product code based on version and application name
-GUID=`md5sum << EOF
+GUID=$(md5sum << EOF
 #define MyAppName "COPASI"
 #define MyAppVersion "${MyAppVersion}"
 #define MyAppPublisher "copasi.org"
 #define MyAppURL "http://www.copasi.org/"
 #define MyAppExeName "bin\CopasiUI.exe"
-EOF` 
-GUID=`echo $GUID | sed 'y/abcdef/ABCDEF/'`
+EOF
+) 
+GUID=$(echo $GUID | sed 'y/abcdef/ABCDEF/')
 productcode=${GUID:0:8}-${GUID:8:4}-${GUID:12:4}-${GUID:16:4}-${GUID:20:12}
 
 [ -e ${SETUP_DIR}/package ] && rm -rf ${SETUP_DIR}/package
@@ -87,14 +90,13 @@ chmod 755 bin/64/CopasiSE.exe
 cp ${SOURCE}/InnoSetup/qt.conf bin/64
 chmod 644 bin/64/qt.conf
 
-
 # Execute InnoSetup to create Installation package
 cd ${SOURCE}/InnoSetup
 
-workdir=`cygpath -wa .`
+workdir=$(cygpath -wa .)
 workdir=${workdir//\\/\\\\}
 
-stagedir=`cygpath -wa "${SETUP_DIR}/package"`
+stagedir=$(cygpath -wa "${SETUP_DIR}/package")
 stagedir=${stagedir//\\/\\\\}
 
 #   modify product code, product version, and package name
