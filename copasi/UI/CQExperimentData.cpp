51914d7033b55a8d48627864ddff449bffd934d5
Frank T. Bergmann <fbergman@caltech.edu>
2022-06-09 16:16:03 +0200

    - issue 3099: ensure that the scale is editable.

--- copasi/UI/CQExperimentData.cpp
+++ copasi/UI/CQExperimentData.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -1357,6 +1357,10 @@ void CQExperimentData::loadTable(CExperiment * pExperiment, const bool & guess)
 
           pItem->setText(ScaleText);
           pItem->setFlags(pItem->flags() & ~FlagMask);
+
+          // ensure item is editable
+          pItem->setFlags(pItem->flags() | Qt::ItemIsEditable);
+
         }
     }
 
