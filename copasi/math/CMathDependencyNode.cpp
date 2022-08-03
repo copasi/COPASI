1d6f1791e254f7db3a77c9ccb4a627ae19c74179
Stefan Hoops <shoops@copasi.org>
2022-08-03 14:45:41 -0400

    Deleted default constructor.

--- copasi/math/CMathDependencyNode.cpp
+++ copasi/math/CMathDependencyNode.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -21,14 +21,6 @@
 #include "CMathContainer.h"
 
 #include "copasi/model/CMetab.h"
-
-CMathDependencyNode::CMathDependencyNode():
-  mpObject(NULL),
-  mPrerequisites(),
-  mDependents(),
-  mChanged(false),
-  mRequested(false)
-{}
 
 CMathDependencyNode::CMathDependencyNode(const CObjectInterface * pObject):
   mpObject(pObject),
