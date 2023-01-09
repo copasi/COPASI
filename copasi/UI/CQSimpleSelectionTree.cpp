e41231326bbac893a605412465778396e21ad70a
Frank T. Bergmann <fbergman@caltech.edu>
2023-01-09 13:47:21 +0100

    - issue 3134: ensure expression selection is disabled by default

--- copasi/UI/CQSimpleSelectionTree.cpp
+++ copasi/UI/CQSimpleSelectionTree.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -41,8 +41,11 @@
 #include "copasi/tssanalysis/CCSPMethod.h"
 #include "copasi/tssanalysis/CTSSATask.h"
 
-CQSimpleSelectionTree::CQSimpleSelectionTree(QWidget *parent):
-  QTreeWidget(parent), mpOutputVector(NULL)
+CQSimpleSelectionTree::CQSimpleSelectionTree(QWidget *parent)
+  : QTreeWidget(parent)
+  , mpOutputVector(NULL)
+  , mpExpressionGenerator(NULL)
+  , mAllowExpressions(false)
 {
   setSelectionMode(QAbstractItemView::ExtendedSelection);
   setSortingEnabled(true);
