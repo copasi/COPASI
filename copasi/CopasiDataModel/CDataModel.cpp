a3a863fa30c5492503b50c1a2d989bd2f58d599f
Frank T. Bergmann <fbergman@caltech.edu>
2023-01-18 13:29:01 +0100

    - issue 3137: processing tasks might require a recompilation of the model
    also signal that math container changed after removing the event

--- copasi/CopasiDataModel/CDataModel.cpp
+++ copasi/CopasiDataModel/CDataModel.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -82,12 +82,12 @@ LIBCOMBINE_CPP_NAMESPACE_USE
 
 // static
 const CEnumAnnotation< std::string, CDataModel::ContentType >
-CDataModel::ContentTypeNames({"COPASI",
-                              "GEPASI",
-                              "SBML",
-                              "SED-ML",
-                              "OMEX"
-                             });
+CDataModel::ContentTypeNames( {"COPASI",
+  "GEPASI",
+  "SBML",
+  "SED-ML",
+  "OMEX"
+});
 
 // static
 CDataModel::ContentType CDataModel::contentType(std::istream & content)
@@ -719,9 +719,9 @@ bool CDataModel::loadModelParameterSets(const std::string & fileName,
   CDataVectorN< CModelParameterSet > & loadedSet = parameterSetModel->getModelParameterSets();
   CCommonName loadedModelCn = parameterSetModel->getCN();
 
-  for (CModelParameterSet & set : loadedSet)
+for (CModelParameterSet & set : loadedSet)
     {
-      for (CModelParameter * current : dynamic_cast< CModelParameterGroup & >(set))
+for (CModelParameter * current : dynamic_cast< CModelParameterGroup & >(set))
         {
           replaceCnInGroup(current, loadedModelCn, thisModelsCn);
         }
@@ -764,7 +764,7 @@ void CDataModel::replaceCnInGroup(CModelParameter * pParam,
   if (!group)
     return;
 
-  for (CModelParameter * element : *group)
+for (CModelParameter * element : *group)
     {
       CModelParameterGroup * inside = dynamic_cast< CModelParameterGroup * >(element);
 
@@ -2706,7 +2706,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Optimization],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::parameterFitting:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -2735,7 +2735,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Parameter Estimation],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::lyap:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -2752,7 +2752,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Lyapunov Exponents],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::mca:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -2769,7 +2769,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Metabolic Control Analysis],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::lna:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -2786,7 +2786,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Linear Noise Approximation],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::sens:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -2803,7 +2803,7 @@ CReportDefinition * CDataModel::addReport(const CTaskEnum::Task & taskType)
         pReport->getFooterAddr()->push_back(CCommonName("CN=Root,Vector=TaskList[Sensitivities],Object=Result"));
         break;
 
-      //**************************************************************************
+        //**************************************************************************
       case CTaskEnum::Task::tssAnalysis:
         pReport = new CReportDefinition(CTaskEnum::TaskName[taskType]);
         pReport->setTaskType(taskType);
@@ -3280,7 +3280,7 @@ void CDataModel::commonAfterLoad(CProcessReport * pProcessReport,
   if (mOldData.pCurrentSEDMLDocument == mData.pCurrentSEDMLDocument)
     mOldData.pCurrentSEDMLDocument = NULL;
 
-  if (mData.pModel->isCompileNecessary() && mData.pModel->compileIfNecessary(pProcessReport))
+  if (mData.pModel && mData.pModel->compileIfNecessary(pProcessReport))
     {
       mData.pModel->getActiveModelParameterSet().updateModel();
     }
@@ -3300,7 +3300,10 @@ void CDataModel::commonAfterLoad(CProcessReport * pProcessReport,
           // need to update math container, since the one set automatically
           // by the task factory might have set an invalid old one
           if (mData.pModel)
-            it->setMathContainer(&mData.pModel->getMathContainer());
+            {
+              mData.pModel->compileIfNecessary(pProcessReport);
+              it->setMathContainer(&mData.pModel->getMathContainer());
+            }
 
           // need initialize, so that all objects are created for the
           // object browser
--- copasi/crosssection/CCrossSectionTask.cpp
+++ copasi/crosssection/CCrossSectionTask.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -176,7 +176,10 @@ void CCrossSectionTask::removeEvent()
       mpContainer->removeAnalysisEvent(mpMathEvent);
     }
 
-  pdelete(mpDataEvent)
+  pdelete(mpDataEvent);
+
+  signalMathContainerChanged();
+
 }
 
 bool CCrossSectionTask::process(const bool & useInitialValues)
