d4d54c51efba3bacd782e7ab22cdfe0f621ca35d
Frank T. Bergmann <fbergman@caltech.edu>
2023-01-18 13:30:31 +0100

    - issue 3138: update the delayExpression from the generated string

--- copasi/UI/CQEventWidget1.cpp
+++ copasi/UI/CQEventWidget1.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2020 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -345,7 +345,7 @@ void CQEventWidget1::saveToEvent()
 
   if (mpEvent->getDelayExpression() != DelayExpression)
     {
-      mpEvent->setDelayExpression(mpExpressionDelay->mpExpressionWidget->getExpression());
+      mpEvent->setDelayExpression(DelayExpression);
       mChanged = true;
     }
 
