37b61b90972a25ec4d7952b98ffce1b6d0be0782
Frank T. Bergmann <fbergman@caltech.edu>
2023-02-10 12:32:53 +0100

    - issue 3142: map sorted index to get correct row

--- copasi/UI/CQParameterEstimationResult.cpp
+++ copasi/UI/CQParameterEstimationResult.cpp
@@ -1,17 +1,17 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
-// University of Virginia, University of Heidelberg, and University
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and University of
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and University of 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2015 - 2016 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2015 - 2016 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
 #include "CQParameterEstimationResult.h"
 #include "ui_CQParameterEstimationResult.h"
@@ -266,8 +266,10 @@ CQParameterEstimationResult::setTaskStartValues()
                                QMessageBox::Ok, QMessageBox::Ok);
       return;
     }
+  
+  auto mappedIndex = mpProxy->mapToSource(select->selectedRows().at(0));
 
-  data->setTaskStartValues(select->selectedRows().at(0).row());
+  data->setTaskStartValues(mappedIndex.row());
 }
 
 void
@@ -335,7 +337,9 @@ CQParameterEstimationResult::applyToModelState()
       experiments = newOrder;
     }
 
-  data->applyToModelState(select->selectedRows().at(0).row(), experiments);
+  auto mappedIndex = mpProxy->mapToSource(select->selectedRows().at(0));
+
+  data->applyToModelState(mappedIndex.row(), experiments);
 
   if (mpDataModelGUI != NULL &&
       mpDataModel != NULL)
