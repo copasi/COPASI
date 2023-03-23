f7542240a8e7f466ad05f3a74e5937324d60148d
Stefan Hoops <shoops@copasi.org>
2023-03-23 10:57:49 -0400

    Bug 3153: Check whether the model contains events.

--- copasi/trajectory/CTauLeapMethod.cpp
+++ copasi/trajectory/CTauLeapMethod.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -418,7 +418,7 @@ bool CTauLeapMethod::isValidProblem(const CCopasiProblem * pProblem)
   //events are not supported at the moment
   if (mpContainer->getEvents().size() > 0)
     {
-      CCopasiMessage(CCopasiMessage::ERROR, MCTrajectoryMethod + 23);
+      CCopasiMessage(CCopasiMessage::ERROR, MCTrajectoryMethod + 23, CTaskEnum::MethodName[getSubType()].c_str());
       return false;
     }
 
