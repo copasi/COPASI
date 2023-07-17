a878ba175d62ae7018f9cade1349c93f8e87181d
Stefan Hoops <shoops@copasi.org>
2023-07-17 12:27:06 -0400

    Bug 3145: Added: CFunctionDB::purgeUnusedUserDefinedFunctions()

--- copasi/function/CFunctionDB.h
+++ copasi/function/CFunctionDB.h
@@ -1,4 +1,9 @@
-// Copyright (C) 2017 by Pedro Mendes, Virginia Tech Intellectual
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
+// University of Virginia, University of Heidelberg, and University
+// of Connecticut School of Medicine.
+// All rights reserved.
+
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
 // Properties, Inc., University of Heidelberg, and University of
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -200,6 +205,11 @@ public:
    * @return std::vector< const CFunction * > usedFunctions
    */
   std::vector< const CFunction * > getUsedFunctions(const CModel* pModel) const;
+
+  /**
+   * Purge all unused user defined functions from the data base
+   */
+  void purgeUnusedUserDefinedFunctions();
 };
 
 #endif // COPASI_CFunctionDB
--- tests/test_sbml_import.cpp
+++ tests/test_sbml_import.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 - 2022 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2021 - 2023 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -260,9 +260,13 @@ TEST_CASE("2: importing an sbml file and saving as COPASI file", "[copasi,sbml]"
 
 TEST_CASE("importing an SBML file multiple times", "[copasi,sbml]")
 {
-
   auto * dm = CRootContainer::addDatamodel();
   REQUIRE(dm != nullptr);
+
+  auto * pFunDB = CRootContainer::getFunctionList();
+  REQUIRE(pFunDB->loadedFunctions().size() == 43);
+  pFunDB->purgeUnusedUserDefinedFunctions();
+  REQUIRE(pFunDB->loadedFunctions().size() == 41);
 
   std::string test_file = getTestFile("test-data/BIOMD0000000027_url.xml");
 
