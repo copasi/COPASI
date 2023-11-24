55b70386bc34547aa36a30cfc4d57fd57433db9b
Frank T. Bergmann <fbergman@caltech.edu>
2023-11-24 13:30:19 +0100

    - allow to print messages on windows debug mode

--- copasi/CopasiUI/main.cpp
+++ copasi/CopasiUI/main.cpp
@@ -1,26 +1,26 @@
-// Copyright (C) 2019 by Pedro Mendes, Rector and Visitors of the
-// University of Virginia, University of Heidelberg, and University
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the 
+// University of Virginia, University of Heidelberg, and University 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and University of
-// of Connecticut School of Medicine.
-// All rights reserved.
+// Copyright (C) 2017 - 2018 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and University of 
+// of Connecticut School of Medicine. 
+// All rights reserved. 
 
-// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., University of Heidelberg, and The University
-// of Manchester.
-// All rights reserved.
+// Copyright (C) 2010 - 2016 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., University of Heidelberg, and The University 
+// of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc., EML Research, gGmbH, University of Heidelberg,
-// and The University of Manchester.
-// All rights reserved.
+// Copyright (C) 2008 - 2009 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc., EML Research, gGmbH, University of Heidelberg, 
+// and The University of Manchester. 
+// All rights reserved. 
 
-// Copyright (C) 2002 - 2007 by Pedro Mendes, Virginia Tech Intellectual
-// Properties, Inc. and EML Research, gGmbH.
-// All rights reserved.
+// Copyright (C) 2002 - 2007 by Pedro Mendes, Virginia Tech Intellectual 
+// Properties, Inc. and EML Research, gGmbH. 
+// All rights reserved. 
 
 #include <stdexcept>
 #include <locale.h>
@@ -72,6 +72,34 @@ int main(int argc, char *argv[])
   QFont::insertSubstitution(".Lucida Grande UI", "Lucida Grande");
 
 #endif
+
+
+
+#if defined(Q_OS_WIN) && defined(QT_DEBUG)
+
+  qInstallMessageHandler([](QtMsgType type, const QMessageLogContext & ctx, const QString & message) {
+    const QString msg = qFormatLogMessage(type, ctx, message);
+    // write logs to Output window of Visual Studio
+    {
+      QString prefix;
+      switch (type)
+        {
+        case QtWarningMsg:
+          prefix = "[WARNING] ";
+          break;
+        case QtCriticalMsg:
+          prefix = "[CRITICAL ERROR] ";
+          break;
+        case QtFatalMsg:
+          prefix = "[FATAL ERROR] ";
+          break;
+        }
+      auto msgW = QString(prefix + message + QString("\n")).toStdWString();
+      OutputDebugStringW(msgW.c_str());
+    }
+  });
+#endif
+
 
   CQCopasiApplication a(argc, argv);
 
