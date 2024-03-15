6286e962efa9e1980ba4561b0571f5f67930b9e0
Stefan Hoops <shoops@copasi.org>
2024-03-15 08:45:02 -0400

    Bug 3222: Retrieve the CModelEntity associated with the event target.

--- copasi/UI/CQEventDM.cpp
+++ copasi/UI/CQEventDM.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2024 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -105,29 +105,25 @@ QVariant CQEventDM::data(const QModelIndex &index, int role) const
               CDataVectorN< CEventAssignment >::const_iterator it = Event.getAssignments().begin();
               CDataVectorN< CEventAssignment >::const_iterator begin = Event.getAssignments().begin();
               CDataVectorN< CEventAssignment >::const_iterator end = Event.getAssignments().end();
+              const CModelEntity * pEntity = nullptr;
 
               for (; it != end; ++it)
-                {
-                  const CModelEntity * pEntity =
-                    dynamic_cast< const CModelEntity * >(CObjectInterface::DataObject(mpDataModel->getObject(it->getTargetCN())));
+                if (CDataContainer::findObjectAncestor(it->getTargetObject(), pEntity))
+                  {
+                    if (it != begin)
+                      {
+                        assignmentTarget += "\n";
+                        assignmentExpression += "\n";
+                      }
 
-                  if (pEntity != NULL)
-                    {
-                      if (it != begin)
-                        {
-                          assignmentTarget += "\n";
-                          assignmentExpression += "\n";
-                        }
+                    assignmentTarget += FROM_UTF8(it->getTargetObject()->getObjectDisplayName());
+                    pExpression = it->getExpressionPtr();
 
-                      assignmentTarget += FROM_UTF8(pEntity->getObjectDisplayName());
-                      pExpression = it->getExpressionPtr();
-
-                      if (pExpression != NULL)
-                        assignmentExpression += pExpression->getDisplayString().c_str();
-                      else
-                        assignmentExpression += FROM_UTF8(it->getExpression());
-                    }
-                }
+                    if (pExpression != NULL)
+                      assignmentExpression += pExpression->getDisplayString().c_str();
+                    else
+                      assignmentExpression += FROM_UTF8(it->getExpression());
+                  }
             }
 
           switch (index.column())
--- copasi/core/CDataContainer.h
+++ copasi/core/CDataContainer.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2024 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -41,6 +41,29 @@ class CDataContainer: public CDataObject
   //Attributes
 public:
   static const CObjectInterface::ContainerList EmptyList;
+
+  template < class CType > static bool findObjectAncestor(const CDataObject * pObject, CType *& pAncestor)
+  {
+    pAncestor = nullptr;
+
+    if (pObject == nullptr)
+      return false;
+
+    CDataContainer * p = pObject->getObjectParent();
+
+    while (p)
+      {
+        if (dynamic_cast< CType * >(p) != nullptr)
+          {
+            pAncestor = static_cast< CType * >(p);
+            return true;
+          }
+
+        p = p->getObjectParent();
+      }
+
+    return false;
+  }
 
   typedef CDataObjectMap objectMap;
 
