ef9172e4534596681d3d161ad994900fa4e71cb1
Stefan Hoops <shoops@copasi.org>
2024-05-23 17:02:59 -0400

    Bug 3228: LNA and MCA tasks print steady-state results if requested.

--- copasi/steadystate/CMCATask.cpp
+++ copasi/steadystate/CMCATask.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2023 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2024 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -275,6 +275,6 @@ void CMCATask::printResult(std::ostream * ostream) const
         }
 
       os << "Results of the steady state subtask (the state for which the MCA was performed):" << std::endl;
-      os << *pProblem->getSubTask();
+      os << *static_cast< CSteadyStateTask *>(pProblem->getSubTask());
     }
 }
