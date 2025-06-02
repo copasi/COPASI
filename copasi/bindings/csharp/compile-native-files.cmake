1594bae2fc1f8e2c8e9caf1f4c6f8307498b9859
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2025-06-02 11:24:24 +0200

    - fix compilation of language bindings
    added opt method and opt item to list of files to postprocess for C# and Java

--- copasi/bindings/csharp/compile-native-files.cmake
+++ copasi/bindings/csharp/compile-native-files.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 - 2022 by Pedro Mendes, Rector and Visitors of the 
+# Copyright (C) 2019 - 2025 by Pedro Mendes, Rector and Visitors of the 
 # University of Virginia, University of Heidelberg, and University 
 # of Connecticut School of Medicine. 
 # All rights reserved. 
@@ -77,6 +77,8 @@ set (SWIG_ENUM_FIX_FILES
         "${CMAKE_CURRENT_BINARY_DIR}/csharp-files/CCopasiTimer.cs"
         "${CMAKE_CURRENT_BINARY_DIR}/csharp-files/CDataModel.cs"
         "${CMAKE_CURRENT_BINARY_DIR}/csharp-files/CPlotItem.cs"
+        "${CMAKE_CURRENT_BINARY_DIR}/csharp-files/COptMethod.cs"
+        "${CMAKE_CURRENT_BINARY_DIR}/csharp-files/COptItem.cs"
     )
 
 
