29a0343960363bd83e36340ce20f3359664041f7
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2025-06-20 11:07:23 +0200

    - added default

--- copasi/UI/listviews.cpp
+++ copasi/UI/listviews.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2024 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2019 - 2025 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -811,6 +811,8 @@ CopasiWidget* ListViews::createWidgetFromId(const ListViews::WidgetType& id)
         CREATE_TAB_WIDGET(mpUnitDetail, mpStackedWidget, ListViews::ObjectType::UNIT, CQUnitDetail, this);
         return mpUnitDetail;
         break;
+
+      default:
     }
 
   //give up
