a9b589027d8c71f0f67556101eec16551705a29d
Stefan Hoops <shoops@copasi.org>
2026-01-12 11:06:42 -0500

    Undefines min macro before including headers.
    Undefines the `min` macro to avoid potential conflicts
    when including headers that might define it. This ensures
    compatibility across different compilers and environments.

--- copasi/math/CJitCompilerImplementation.h
+++ copasi/math/CJitCompilerImplementation.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 by Pedro Mendes, Rector and Visitors of the
+// Copyright (C) 2021 - 2026 by Pedro Mendes, Rector and Visitors of the
 // University of Virginia, University of Heidelberg, and University
 // of Connecticut School of Medicine.
 // All rights reserved.
@@ -17,6 +17,10 @@
 
 #ifdef ERROR
 #  undef ERROR
+#endif
+
+#ifdef min
+#  undef min
 #endif
 
 #ifdef max
