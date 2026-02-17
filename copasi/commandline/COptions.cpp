124a55a8e383471628fb46f3edefc5fed37d93c5
Frank T. Bergmann <fbergman@caltech.edu>
2026-02-17 16:56:49 +0100

    - issue 3322: add command line option to parser
    It will export / import the task settings of the (first) scheduled task, which can be overwritten using the existing --scheduled-task option

--- copasi/commandline/COptions.cpp
+++ copasi/commandline/COptions.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 - 2024 by Pedro Mendes, Rector and Visitors of the 
+// Copyright (C) 2019 - 2026 by Pedro Mendes, Rector and Visitors of the 
 // University of Virginia, University of Heidelberg, and University 
 // of Connecticut School of Medicine. 
 // All rights reserved. 
@@ -277,6 +277,8 @@ void COptions::init(C_INT argc, char *argv[])
   setValue("ExportIni", Options.ExportIni);
   setValue("SedmlTask", Options.SedmlTask);
   setValue("PrintSedMLTasks", Options.PrintSedMLTasks);
+  setValue("ExportTaskSpec", Options.ExportTaskSpec);
+  setValue("ImportTaskSpec", Options.ImportTaskSpec);
 
   delete pPreParser;
 
