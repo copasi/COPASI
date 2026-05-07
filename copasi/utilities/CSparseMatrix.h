a48c01f54de800cbddccb44935c558394f96654c
Stefan Hoops <shoops@copasi.org>
2026-05-07 13:12:37 -0400

    Use `std::iterator` as base for `CSparseMatrix::const_iterator`

--- copasi/utilities/CSparseMatrix.h
+++ copasi/utilities/CSparseMatrix.h
@@ -164,7 +164,7 @@ public:
     typedef std::forward_iterator_tag iterator_category;
 #else
   class const_row_iterator:
-    public std::const_iterator< std::forward_iterator_tag, C_FLOAT64, ptrdiff_t >
+    public std::iterator< std::forward_iterator_tag, C_FLOAT64, ptrdiff_t >
   {
 #endif // __cplusplus >= 201703L
 #endif // (defined __GNUC__ && __GNUC__ < 3 && !defined __APPLE_CC__)
