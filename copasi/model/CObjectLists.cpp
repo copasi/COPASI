aa768c35faf0c121e47bb17c2f9079043a78e146
Stefan Hoops <shoops@copasi.org>
2026-06-19 10:21:06 -0400

    Fixed common name resolution for eigenvalues array

--- copasi/model/CObjectLists.cpp
+++ copasi/model/CObjectLists.cpp
@@ -506,7 +506,8 @@ CObjectLists::getListOfConstObjects(ListType t, const CModel* pModel)
         if (!pParent)
           break;
 
-        const CDataArray * pEV = dynamic_cast<const CDataArray*>(pParent->getChildObject(CCommonName("Vector=TaskList[Steady-State],Array=Eigenvalues of reduced system Jacobian")));
+        CCommonName cn(pParent->getCN() + "Vector=TaskList[Steady-State],Array=Eigenvalues of reduced system Jacobian");
+        const CDataArray * pEV = dynamic_cast<const CDataArray * >(cn.resolve(pParent));
 
         if (pEV->dimensionality() != 2) //2d matrix
           break;
@@ -529,7 +530,8 @@ CObjectLists::getListOfConstObjects(ListType t, const CModel* pModel)
         if (!pParent)
           break;
 
-        const CDataArray * pEV = dynamic_cast<const CDataArray*>(pParent->getChildObject(CCommonName("Vector=TaskList[Steady-State],Array=Eigenvalues of reduced system Jacobian")));
+        CCommonName cn(pParent->getCN() + "Vector=TaskList[Steady-State],Array=Eigenvalues of reduced system Jacobian");
+        const CDataArray * pEV = dynamic_cast<const CDataArray * >(cn.resolve(pParent));
 
         if (pEV->dimensionality() != 2) //2d matrix
           break;
